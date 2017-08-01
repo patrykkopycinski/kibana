patrykkopycinski/kibana

commit 5703ffc9def917a5910c14189d886375a0280340

in percentage mode tooltip should also show percentages (#13217)

src/ui/public/vislib/lib/dispatch.js
@@ -40,7 +40,6 @@ export function VislibLibDispatchProvider(Private, config) {
       const slices = isSlices ? data.slices : undefined;
       const handler = this.handler;
       const color = _.get(handler, 'data.color');
-      const isPercentage = (handler && handler.visConfig.get('mode', 'normal') === 'percentage');
 
       const eventData = {
         value: d.y,
@@ -59,11 +58,13 @@ export function VislibLibDispatchProvider(Private, config) {
 
       if (isSeries) {
         // Find object with the actual d value and add it to the point object
+        const aggId = d3.event.target.parentElement.__data__.aggId;
+        const percentageMode = handler.charts[0].getSeries(aggId).getValueAxis().axisConfig.isPercentage();
         const object = _.find(series, { 'label': label });
         if (object) {
           eventData.value = +object.values[i].y;
 
-          if (isPercentage) {
+          if (percentageMode) {
             // Add the formatted percentage to the point object
             eventData.percent = (100 * d.y).toFixed(1) + '%';
           }

src/ui/public/vislib/visualizations/point_series.js
@@ -40,6 +40,10 @@ export function VislibVisualizationsPointSeriesProvider(Private) {
       return charts[chartIndex];
     }
 
+    getSeries(seriesId) {
+      return this.series.find(series => series.chartData.aggId === seriesId);
+    }
+
     addBackground(svg, width, height) {
       const startX = 0;
       const startY = 0;

commit 277cf04556dc26c7d1e309589da1f3c8fc05a961

ensure we are working with data-series (#13266)
This fixes a regression introduced by https://github.com/elastic/kibana/pull/13217.

src/ui/public/vislib/lib/dispatch.js
@@ -57,9 +57,16 @@ export function VislibLibDispatchProvider(Private, config) {
       };
 
       if (isSeries) {
-        // Find object with the actual d value and add it to the point object
-        const aggId = d3.event.target.parentElement.__data__.aggId;
-        const percentageMode = handler.charts[0].getSeries(aggId).getValueAxis().axisConfig.isPercentage();
+        let percentageMode = false;
+        //only series charts work in percentage mode.
+        if (handler.charts && handler.charts[0] && handler.charts[0].getSeries && d3.event.target.parentElement.__data__) {
+          const aggId = d3.event.target.parentElement.__data__.aggId;
+          const seriesFromAggId = handler.charts[0].getSeries(aggId);
+          if (seriesFromAggId && seriesFromAggId.getValueAxis) {
+            percentageMode = seriesFromAggId.getValueAxis().axisConfig.isPercentage();
+          }
+        }
+
         const object = _.find(series, { 'label': label });
         if (object) {
           eventData.value = +object.values[i].y;
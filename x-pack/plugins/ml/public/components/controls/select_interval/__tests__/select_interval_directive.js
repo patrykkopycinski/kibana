patrykkopycinski/kibana

commit cca73855d7885f03143d95aa9f7e302c25742900

[ML] Deprecate stateFactory(). (#31692)
This deprecates our own implementation of `stateFactory()`. This utility was able to manage shared state across angular and react code including persisting it to the URL using `AppState`. This PR replaces it with a simpler way using RxJs' Observables.

x-pack/plugins/ml/public/components/controls/select_interval/select_interval.js
@@ -9,8 +9,8 @@
 /*
  * React component for rendering a select element with various aggregation interval levels.
  */
-import { get } from 'lodash';
 import React, { Component } from 'react';
+import { BehaviorSubject } from 'rxjs';
 
 import {
   EuiSelect
@@ -18,6 +18,7 @@ import {
 
 import { i18n } from '@kbn/i18n';
 
+import { injectObservablesAsProps } from '../../../util/observable_utils';
 
 const OPTIONS = [
   {
@@ -52,35 +53,12 @@ function optionValueToInterval(value) {
   return interval;
 }
 
-// This service will be populated by the corresponding angularjs based one.
-export const mlSelectIntervalService = {
-  intialized: false,
-  state: null
-};
+export const interval$ = new BehaviorSubject(OPTIONS[0]);
 
-class SelectInterval extends Component {
-  constructor(props) {
-    super(props);
-
-    // Restore the interval from the state, or default to auto.
-    this.mlSelectIntervalService = mlSelectIntervalService;
-    const intervalState = this.mlSelectIntervalService.state.get('interval');
-    const intervalValue = get(intervalState, 'val', 'auto');
-    const interval = optionValueToInterval(intervalValue);
-    this.mlSelectIntervalService.state.set('interval', interval);
-
-    this.state = {
-      value: interval.val
-    };
-  }
-
+class SelectIntervalUnwrapped extends Component {
   onChange = (e) => {
-    this.setState({
-      value: e.target.value,
-    });
-
     const interval = optionValueToInterval(e.target.value);
-    this.mlSelectIntervalService.state.set('interval', interval).changed();
+    interval$.next(interval);
   };
 
   render() {
@@ -88,11 +66,16 @@ class SelectInterval extends Component {
       <EuiSelect
         options={OPTIONS}
         className="ml-select-interval"
-        value={this.state.value}
+        value={this.props.interval.value}
         onChange={this.onChange}
       />
     );
   }
 }
+
+const SelectInterval = injectObservablesAsProps(
+  { interval: interval$ },
+  SelectIntervalUnwrapped
+);
 
 export { SelectInterval };

x-pack/plugins/ml/public/components/controls/select_interval/select_interval_directive.js
@@ -7,22 +7,14 @@
 
 import 'ngreact';
 
-import { stateFactoryProvider } from 'plugins/ml/factories/state_factory';
-
 import { uiModules } from 'ui/modules';
 const module = uiModules.get('apps/ml', ['react']);
 
-import { SelectInterval, mlSelectIntervalService } from './select_interval';
+import { subscribeAppStateToObservable } from '../../../util/app_state_utils';
+import { SelectInterval, interval$ } from './select_interval';
 
-module.service('mlSelectIntervalService', function (Private, i18n) {
-  const stateFactory = Private(stateFactoryProvider);
-  this.state = mlSelectIntervalService.state = stateFactory('mlSelectInterval', {
-    interval: {
-      display: i18n('xpack.ml.controls.selectInterval.autoInitLabel', { defaultMessage: 'Auto' }),
-      val: 'auto'
-    }
-  });
-  mlSelectIntervalService.initialized = true;
+module.service('mlSelectIntervalService', function (AppState, $rootScope) {
+  subscribeAppStateToObservable(AppState, 'mlSelectInterval', interval$, () => $rootScope.$applyAsync());
 })
   .directive('mlSelectInterval', function ($injector) {
     const reactDirective = $injector.get('reactDirective');

x-pack/plugins/ml/public/app.js
@@ -15,7 +15,6 @@ import 'ui/persisted_log';
 import 'ui/autoload/all';
 
 import 'plugins/ml/access_denied';
-import 'plugins/ml/factories/state_factory';
 import 'plugins/ml/lib/angular_bootstrap_patch';
 import 'plugins/ml/jobs';
 import 'plugins/ml/services/calendar_service';

x-pack/plugins/ml/public/components/annotations/annotation_flyout/index.tsx
@@ -106,7 +106,7 @@ class AnnotationFlyoutIntl extends Component<CommonProps & Props & InjectedIntlP
 
     this.closeDeleteModal();
     annotation$.next(null);
-    annotationsRefresh$.next();
+    annotationsRefresh$.next(true);
   };
 
   public closeDeleteModal = () => {
@@ -125,7 +125,7 @@ class AnnotationFlyoutIntl extends Component<CommonProps & Props & InjectedIntlP
     ml.annotations
       .indexAnnotation(annotation)
       .then(() => {
-        annotationsRefresh$.next();
+        annotationsRefresh$.next(true);
         if (typeof annotation._id === 'undefined') {
           toastNotifications.addSuccess(
             intl.formatMessage(

x-pack/plugins/ml/public/components/annotations/annotations_table/annotations_table.js
@@ -126,7 +126,7 @@ const AnnotationsTable = injectI18n(class AnnotationsTable extends Component {
       Array.isArray(this.props.jobs) && this.props.jobs.length > 0
     ) {
       this.annotationsRefreshSubscription = annotationsRefresh$.subscribe(() => this.getAnnotations());
-      annotationsRefresh$.next();
+      annotationsRefresh$.next(true);
     }
   }
 
@@ -137,7 +137,7 @@ const AnnotationsTable = injectI18n(class AnnotationsTable extends Component {
       Array.isArray(this.props.jobs) && this.props.jobs.length > 0 &&
       this.state.jobId !== this.props.jobs[0].job_id
     ) {
-      annotationsRefresh$.next();
+      annotationsRefresh$.next(true);
     }
   }
 

x-pack/plugins/ml/public/components/controls/checkbox_showcharts/checkbox_showcharts.js
@@ -4,12 +4,11 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-
-
 /*
  * React component for a checkbox element to toggle charts display.
  */
 import React, { Component } from 'react';
+import { BehaviorSubject } from 'rxjs';
 
 import {
   EuiCheckbox
@@ -18,33 +17,14 @@ import {
 import makeId from '@elastic/eui/lib/components/form/form_row/make_id';
 import { FormattedMessage } from '@kbn/i18n/react';
 
-// This service will be populated by the corresponding angularjs based one.
-export const mlCheckboxShowChartsService = {
-  intialized: false,
-  state: null
-};
+import { injectObservablesAsProps } from '../../../util/observable_utils';
 
-class CheckboxShowCharts extends Component {
-  constructor(props) {
-    super(props);
+export const showCharts$ = new BehaviorSubject(true);
 
-    // Restore the checked setting from the state.
-    this.mlCheckboxShowChartsService = mlCheckboxShowChartsService;
-    const showCharts = this.mlCheckboxShowChartsService.state.get('showCharts');
-
-    this.state = {
-      checked: showCharts
-    };
-  }
-
+class CheckboxShowChartsUnwrapped extends Component {
   onChange = (e) => {
     const showCharts = e.target.checked;
-    this.mlCheckboxShowChartsService.state.set('showCharts', showCharts);
-    this.mlCheckboxShowChartsService.state.changed();
-
-    this.setState({
-      checked: showCharts,
-    });
+    showCharts$.next(showCharts);
   };
 
   render() {
@@ -55,11 +35,15 @@ class CheckboxShowCharts extends Component {
           id="xpack.ml.controls.checkboxShowCharts.showChartsCheckboxLabel"
           defaultMessage="Show charts"
         />}
-        checked={this.state.checked}
+        checked={this.props.showCharts}
         onChange={this.onChange}
       />
     );
   }
 }
+
+const CheckboxShowCharts = injectObservablesAsProps({
+  showCharts: showCharts$
+}, CheckboxShowChartsUnwrapped);
 
 export { CheckboxShowCharts };

x-pack/plugins/ml/public/components/controls/checkbox_showcharts/checkbox_showcharts_service.js
@@ -4,20 +4,12 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-
-import 'ngreact';
-
-import { stateFactoryProvider } from 'plugins/ml/factories/state_factory';
-
 import { uiModules } from 'ui/modules';
 const module = uiModules.get('apps/ml', ['react']);
 
-import { mlCheckboxShowChartsService } from './checkbox_showcharts';
+import { subscribeAppStateToObservable } from '../../../util/app_state_utils';
+import { showCharts$ } from './checkbox_showcharts';
 
-module.service('mlCheckboxShowChartsService', function (Private) {
-  const stateFactory = Private(stateFactoryProvider);
-  this.state = mlCheckboxShowChartsService.state = stateFactory('mlCheckboxShowCharts', {
-    showCharts: true
-  });
-  mlCheckboxShowChartsService.initialized = true;
+module.service('mlCheckboxShowChartsService', function (AppState, $rootScope) {
+  subscribeAppStateToObservable(AppState, 'mlShowCharts', showCharts$, () => $rootScope.$applyAsync());
 });

x-pack/plugins/ml/public/components/controls/select_severity/select_severity.js
@@ -10,10 +10,10 @@
  * React component for rendering a select element with threshold levels.
  */
 import PropTypes from 'prop-types';
-import { get } from 'lodash';
 import React, { Component, Fragment } from 'react';
 import { i18n } from '@kbn/i18n';
 import { FormattedMessage } from '@kbn/i18n/react';
+import { BehaviorSubject } from 'rxjs';
 
 import {
   EuiHealth,
@@ -23,6 +23,7 @@ import {
 } from '@elastic/eui';
 
 import { getSeverityColor } from '../../../../common/util/anomaly_utils';
+import { injectObservablesAsProps } from '../../../util/observable_utils';
 
 const warningLabel = i18n.translate('xpack.ml.controls.selectSeverity.warningLabel', { defaultMessage: 'warning' });
 const minorLabel = i18n.translate('xpack.ml.controls.selectSeverity.minorLabel', { defaultMessage: 'minor' });
@@ -71,50 +72,12 @@ function optionValueToThreshold(value) {
   return threshold;
 }
 
-// This service will be populated by the corresponding angularjs based one.
-export const mlSelectSeverityService = {
-  intialized: false,
-  state: null
-};
+export const severity$ = new BehaviorSubject(SEVERITY_OPTIONS[0]);
 
-class SelectSeverity extends Component {
-  constructor(props) {
-    super(props);
-
-    // Restore the threshold from the state, or default to warning.
-    if (mlSelectSeverityService.intialized) {
-      this.mlSelectSeverityService = mlSelectSeverityService;
-    }
-
-    this.state = {
-      valueDisplay: SEVERITY_OPTIONS[0].display,
-    };
-  }
-
-  componentDidMount() {
-    // set initial state from service if available
-    if (this.mlSelectSeverityService !== undefined) {
-      const thresholdState = this.mlSelectSeverityService.state.get('threshold');
-      const thresholdValue = get(thresholdState, 'val', 0);
-      const threshold = optionValueToThreshold(thresholdValue);
-      // set initial selected option equal to threshold value
-      const selectedOption = SEVERITY_OPTIONS.find(opt => (opt.val === threshold.val));
-      this.mlSelectSeverityService.state.set('threshold', threshold);
-      this.setState({ valueDisplay: selectedOption.display, });
-    }
-  }
-
+class SelectSeverityUnwrapped extends Component {
   onChange = (valueDisplay) => {
-    this.setState({
-      valueDisplay: valueDisplay,
-    });
     const threshold = optionValueToThreshold(optionsMap[valueDisplay]);
-
-    if (this.mlSelectSeverityService !== undefined) {
-      this.mlSelectSeverityService.state.set('threshold', threshold).changed();
-    } else {
-      this.props.onChangeHandler(threshold);
-    }
+    severity$.next(threshold);
   }
 
   getOptions = () =>
@@ -147,7 +110,7 @@ class SelectSeverity extends Component {
     }));
 
   render() {
-    const { valueDisplay } = this.state;
+    const { severity } = this.props;
     const options = this.getOptions();
 
     return (
@@ -155,23 +118,24 @@ class SelectSeverity extends Component {
         className={this.props.classNames}
         hasDividers
         options={options}
-        valueOfSelected={valueDisplay}
+        valueOfSelected={severity.display}
         onChange={this.onChange}
       />
     );
   }
 }
 
-SelectSeverity.propTypes = {
-  mlSelectSeverityService: PropTypes.object,
-  onChangeHandler: PropTypes.func,
+SelectSeverityUnwrapped.propTypes = {
   classNames: PropTypes.string
 };
 
-SelectSeverity.defaultProps = {
-  mlSelectSeverityService: undefined,
-  onChangeHandler: () => {},
+SelectSeverityUnwrapped.defaultProps = {
   classNames: ''
 };
+
+const SelectSeverity = injectObservablesAsProps(
+  { severity: severity$ },
+  SelectSeverityUnwrapped
+);
 
 export { SelectSeverity };

commit b1a89fd87a2e9ffc49a663452ae9837f7f968a22

[ML] Fixes restoring table interval selection from appState. (#32056)
Fixes a regression introduced in #31692. The table interval selection wasn't properly restored from `AppState` because of a mismatch in the expected object structure.

x-pack/plugins/ml/public/components/controls/select_interval/select_interval.js
@@ -9,6 +9,8 @@
 /*
  * React component for rendering a select element with various aggregation interval levels.
  */
+
+import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import { BehaviorSubject } from 'rxjs';
 
@@ -53,9 +55,13 @@ function optionValueToInterval(value) {
   return interval;
 }
 
-export const interval$ = new BehaviorSubject(OPTIONS[0]);
+export const interval$ = new BehaviorSubject(optionValueToInterval(OPTIONS[0].value));
 
 class SelectIntervalUnwrapped extends Component {
+  static propTypes = {
+    interval: PropTypes.object.isRequired,
+  };
+
   onChange = (e) => {
     const interval = optionValueToInterval(e.target.value);
     interval$.next(interval);
@@ -66,7 +72,7 @@ class SelectIntervalUnwrapped extends Component {
       <EuiSelect
         options={OPTIONS}
         className="ml-select-interval"
-        value={this.props.interval.value}
+        value={this.props.interval.val}
         onChange={this.onChange}
       />
     );

x-pack/plugins/ml/public/components/controls/select_interval/__tests__/select_interval_directive.js
@@ -1,0 +1,53 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+import expect from 'expect.js';
+import ngMock from 'ng_mock';
+
+import { interval$ } from '../select_interval';
+
+describe('ML - mlSelectIntervalService', () => {
+  let appState;
+
+  beforeEach(ngMock.module('kibana', (stateManagementConfigProvider) => {
+    stateManagementConfigProvider.enable();
+  }));
+  beforeEach(ngMock.module(($provide) => {
+    appState = {
+      fetch: () => {},
+      save: () => {}
+    };
+
+    $provide.factory('AppState', () => () => appState);
+  }));
+
+  it('initializes AppState with correct default value', (done) => {
+    ngMock.inject(($injector) => {
+      $injector.get('mlSelectIntervalService');
+      const defaultValue = { display: 'Auto', val: 'auto' };
+
+      expect(appState.mlSelectInterval).to.eql(defaultValue);
+      expect(interval$.getValue()).to.eql(defaultValue);
+
+      done();
+    });
+  });
+
+  it('restores AppState to interval$ observable', (done) => {
+    ngMock.inject(($injector) => {
+      const restoreValue = { display: '1 day', val: 'day' };
+      appState.mlSelectInterval = restoreValue;
+
+      $injector.get('mlSelectIntervalService');
+
+      expect(appState.mlSelectInterval).to.eql(restoreValue);
+      expect(interval$.getValue()).to.eql(restoreValue);
+
+      done();
+    });
+  });
+
+});

x-pack/plugins/ml/public/components/controls/select_interval/select_interval.test.js
@@ -1,0 +1,32 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+import React from 'react';
+import { shallowWithIntl } from 'test_utils/enzyme_helpers';
+import { SelectInterval } from './select_interval';
+
+describe('SelectInterval', () => {
+
+  test('creates correct initial selected value', () => {
+    const wrapper = shallowWithIntl(<SelectInterval/>);
+    const defaultSelectedValue = wrapper.props().interval.val;
+
+    expect(defaultSelectedValue).toBe('auto');
+  });
+
+  test('currently selected value is updated correctly on click', () => {
+    const wrapper = shallowWithIntl(<SelectInterval/>);
+    const select = wrapper.first().shallow();
+
+    const defaultSelectedValue = wrapper.props().interval.val;
+    expect(defaultSelectedValue).toBe('auto');
+
+    select.simulate('change', { target: { value: 'day' } });
+    const updatedSelectedValue = wrapper.props().interval.val;
+    expect(updatedSelectedValue).toBe('day');
+  });
+
+});
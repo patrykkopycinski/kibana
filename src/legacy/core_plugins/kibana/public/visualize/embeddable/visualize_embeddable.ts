patrykkopycinski/kibana

commit 15d2331ed8241e8fed75cbb43044e4b27e6c570e

remove visualize loader (#46910)

src/legacy/core_plugins/kibana/public/visualize/embeddable/visualize_embeddable.ts
@@ -17,39 +17,53 @@
  * under the License.
  */
 
-import _ from 'lodash';
-import { EmbeddedVisualizeHandler } from 'ui/visualize/loader/embedded_visualize_handler';
+import _, { forEach } from 'lodash';
+import { StaticIndexPattern } from 'ui/index_patterns';
+import { PersistedState } from 'ui/persisted_state';
 import { Subscription } from 'rxjs';
 import * as Rx from 'rxjs';
+import { buildPipeline } from 'ui/visualize/loader/pipeline_helpers';
+import { SavedObject } from 'ui/saved_objects/saved_object';
+import { Vis } from 'ui/vis';
+import { SearchSource } from 'ui/courier';
+import { queryGeohashBounds } from 'ui/visualize/loader/utils';
+import { getTableAggs } from 'ui/visualize/loader/pipeline_helpers/utilities';
+import { AppState } from 'ui/state_management/app_state';
+import { npStart } from 'ui/new_platform';
+import { IExpressionLoaderParams } from '../../../../expressions/public/np_ready/public/types';
+import { start as expressions } from '../../../../expressions/public/legacy';
+import { VISUALIZE_EMBEDDABLE_TYPE } from './constants';
+import { Query } from '../../../../data/public';
 import {
   TimeRange,
   onlyDisabledFiltersChanged,
   esFilters,
 } from '../../../../../../plugins/data/public';
-import { Query } from '../../../../data/public';
-import { VISUALIZE_EMBEDDABLE_TYPE } from './constants';
-
 import {
-  AppState,
-  Container,
-  Embeddable,
   EmbeddableInput,
   EmbeddableOutput,
-  PersistedState,
-  StaticIndexPattern,
-  VisSavedObject,
-  VisualizeLoader,
-  VisualizeLoaderParams,
-  VisualizeUpdateParams,
-} from '../kibana_services';
+  Embeddable,
+  Container,
+  APPLY_FILTER_TRIGGER,
+} from '../../../../../../plugins/embeddable/public';
+import { dispatchRenderComplete } from '../../../../../../plugins/kibana_utils/public';
+import { mapAndFlattenFilters } from '../../../../../../plugins/data/public';
 
 const getKeys = <T extends {}>(o: T): Array<keyof T> => Object.keys(o) as Array<keyof T>;
+
+export interface VisSavedObject extends SavedObject {
+  vis: Vis;
+  description?: string;
+  searchSource: SearchSource;
+  title: string;
+  uiStateJSON?: string;
+  destroy: () => void;
+}
 
 export interface VisualizeEmbeddableConfiguration {
   savedVisualization: VisSavedObject;
   indexPatterns?: StaticIndexPattern[];
   editUrl: string;
-  loader: VisualizeLoader;
   editable: boolean;
   appState?: AppState;
   uiState?: PersistedState;
@@ -73,24 +87,28 @@ export interface VisualizeOutput extends EmbeddableOutput {
   visTypeName: string;
 }
 
+type ExpressionLoader = InstanceType<typeof expressions.ExpressionLoader>;
+
 export class VisualizeEmbeddable extends Embeddable<VisualizeInput, VisualizeOutput> {
+  private handler?: ExpressionLoader;
   private savedVisualization: VisSavedObject;
-  private loader: VisualizeLoader;
   private appState: AppState | undefined;
   private uiState: PersistedState;
-  private handler?: EmbeddedVisualizeHandler;
   private timeRange?: TimeRange;
   private query?: Query;
   private title?: string;
   private filters?: esFilters.Filter[];
   private visCustomizations: VisualizeInput['vis'];
-  private subscription: Subscription;
+  private subscriptions: Subscription[] = [];
+  private expression: string = '';
+  private actions: any = {};
+  private vis: Vis;
+  private domNode: any;
   public readonly type = VISUALIZE_EMBEDDABLE_TYPE;
 
   constructor(
     {
       savedVisualization,
-      loader,
       editUrl,
       indexPatterns,
       editable,
@@ -112,8 +130,12 @@ export class VisualizeEmbeddable extends Embeddable<VisualizeInput, VisualizeOut
       },
       parent
     );
+    this.appState = appState;
     this.savedVisualization = savedVisualization;
-    this.loader = loader;
+    this.vis = this.savedVisualization.vis;
+
+    this.vis.on('update', this.handleVisUpdate);
+    this.vis.on('reload', this.reload);
 
     if (uiState) {
       this.uiState = uiState;
@@ -126,23 +148,33 @@ export class VisualizeEmbeddable extends Embeddable<VisualizeInput, VisualizeOut
       this.uiState.on('change', this.uiStateChangeHandler);
     }
 
+    this.vis._setUiState(this.uiState);
+
     this.appState = appState;
 
-    this.subscription = Rx.merge(this.getOutput$(), this.getInput$()).subscribe(() => {
-      this.handleChanges();
-    });
+    this.subscriptions.push(
+      Rx.merge(this.getOutput$(), this.getInput$()).subscribe(() => {
+        this.handleChanges();
+      })
+    );
   }
 
   public getVisualizationDescription() {
     return this.savedVisualization.description;
   }
 
-  public getInspectorAdapters() {
+  public getInspectorAdapters = () => {
     if (!this.handler) {
       return undefined;
     }
-    return this.handler.inspectorAdapters;
-  }
+    return this.handler.inspect();
+  };
+
+  public openInspector = () => {
+    if (this.handler) {
+      return this.handler.openInspector(this.getTitle() || '');
+    }
+  };
 
   /**
    * Transfers all changes in the containerState.customization into
@@ -170,87 +202,148 @@ export class VisualizeEmbeddable extends Embeddable<VisualizeInput, VisualizeOut
     }
   }
 
-  public handleChanges() {
+  public async handleChanges() {
     this.transferCustomizationsToUiState();
 
-    const updatedParams: VisualizeUpdateParams = {};
+    let dirty = false;
 
     // Check if timerange has changed
     if (!_.isEqual(this.input.timeRange, this.timeRange)) {
       this.timeRange = _.cloneDeep(this.input.timeRange);
-      updatedParams.timeRange = this.timeRange;
+      dirty = true;
     }
 
     // Check if filters has changed
     if (!onlyDisabledFiltersChanged(this.input.filters, this.filters)) {
-      updatedParams.filters = this.input.filters;
       this.filters = this.input.filters;
+      dirty = true;
     }
 
     // Check if query has changed
     if (!_.isEqual(this.input.query, this.query)) {
-      updatedParams.query = this.input.query;
       this.query = this.input.query;
+      dirty = true;
     }
 
     if (this.output.title !== this.title) {
       this.title = this.output.title;
-      updatedParams.dataAttrs = {
-        title: this.title || '',
-      };
+      if (this.domNode) {
+        this.domNode.setAttribute('data-title', this.title || '');
+      }
     }
 
-    if (this.handler && !_.isEmpty(updatedParams)) {
-      this.handler.update(updatedParams);
-      this.handler.reload();
+    if (this.savedVisualization.description && this.domNode) {
+      this.domNode.setAttribute('data-description', this.savedVisualization.description);
+    }
+
+    if (this.handler && dirty) {
+      this.updateHandler();
     }
   }
 
   /**
    *
    * @param {Element} domNode
-   * @param {ContainerState} containerState
    */
-  public render(domNode: HTMLElement) {
+  public async render(domNode: HTMLElement) {
     this.timeRange = _.cloneDeep(this.input.timeRange);
-    this.query = this.input.query;
-    this.filters = this.input.filters;
 
     this.transferCustomizationsToUiState();
 
-    const dataAttrs: { [key: string]: string } = {
-      'shared-item': '',
-      title: this.output.title || '',
+    this.savedVisualization.vis._setUiState(this.uiState);
+    this.uiState = this.savedVisualization.vis.getUiState();
+
+    // init default actions
+    forEach(this.vis.type.events, (event, eventName) => {
+      if (event.disabled || !eventName) {
+        return;
+      } else {
+        this.actions[eventName] = event.defaultAction;
+      }
+    });
+
+    // This is a hack to give maps visualizations access to data in the
+    // globalState, since they can no longer access it via searchSource.
+    // TODO: Remove this as a part of elastic/kibana#30593
+    this.vis.API.getGeohashBounds = () => {
+      return queryGeohashBounds(this.savedVisualization.vis, {
+        filters: this.filters,
+        query: this.query,
+        searchSource: this.savedVisualization.searchSource,
+      });
     };
+
+    // this is a hack to make editor still work, will be removed once we clean up editor
+    this.vis.hasInspector = () => {
+      const visTypesWithoutInspector = ['markdown', 'input_control_vis', 'metrics', 'vega'];
+      if (visTypesWithoutInspector.includes(this.vis.type.name)) {
+        return false;
+      }
+      return this.getInspectorAdapters();
+    };
+
+    this.vis.openInspector = this.openInspector;
+
+    const div = document.createElement('div');
+    div.className = `visualize panel-content panel-content--fullWidth`;
+    domNode.appendChild(div);
+    this.domNode = div;
+
+    this.handler = new expressions.ExpressionLoader(this.domNode);
+
+    this.subscriptions.push(
+      this.handler.events$.subscribe(async event => {
+        if (this.actions[event.name]) {
+          event.data.aggConfigs = getTableAggs(this.vis);
+          const filters: esFilters.Filter[] = this.actions[event.name](event.data) || [];
+          const mappedFilters = mapAndFlattenFilters(filters);
+          const timeFieldName = this.vis.indexPattern.timeFieldName;
+
+          npStart.plugins.uiActions.executeTriggerActions(APPLY_FILTER_TRIGGER, {
+            embeddable: this,
+            filters: mappedFilters,
+            timeFieldName,
+          });
+        }
+      })
+    );
+
+    div.setAttribute('data-title', this.output.title || '');
+
     if (this.savedVisualization.description) {
-      dataAttrs.description = this.savedVisualization.description;
+      div.setAttribute('data-description', this.savedVisualization.description);
     }
 
-    const handlerParams: VisualizeLoaderParams = {
-      appState: this.appState,
-      uiState: this.uiState,
-      // Append visualization to container instead of replacing its content
-      append: true,
-      timeRange: _.cloneDeep(this.input.timeRange),
-      query: this.query,
-      filters: this.filters,
-      cssClass: `panel-content panel-content--fullWidth`,
-      dataAttrs,
-    };
+    div.setAttribute('data-test-subj', 'visualizationLoader');
+    div.setAttribute('data-shared-item', '');
+    div.setAttribute('data-rendering-count', '0');
+    div.setAttribute('data-render-complete', 'false');
 
-    this.handler = this.loader.embedVisualizationWithSavedObject(
-      domNode,
-      this.savedVisualization,
-      handlerParams
+    this.subscriptions.push(
+      this.handler.loading$.subscribe(() => {
+        div.setAttribute('data-render-complete', 'false');
+        div.setAttribute('data-loading', '');
+      })
     );
+
+    this.subscriptions.push(
+      this.handler.render$.subscribe(count => {
+        div.removeAttribute('data-loading');
+        div.setAttribute('data-render-complete', 'true');
+        div.setAttribute('data-rendering-count', count.toString());
+        dispatchRenderComplete(div);
+      })
+    );
+
+    this.updateHandler();
   }
 
   public destroy() {
     super.destroy();
-    if (this.subscription) {
-      this.subscription.unsubscribe();
-    }
+    this.subscriptions.forEach(s => s.unsubscribe());
     this.uiState.off('change', this.uiStateChangeHandler);
+    this.savedVisualization.vis.removeListener('reload', this.reload);
+    this.savedVisualization.vis.removeListener('update', this.handleVisUpdate);
     this.savedVisualization.destroy();
     if (this.handler) {
       this.handler.destroy();
@@ -258,11 +351,43 @@ export class VisualizeEmbeddable extends Embeddable<VisualizeInput, VisualizeOut
     }
   }
 
-  public reload() {
+  public reload = () => {
+    this.handleVisUpdate();
+  };
+
+  private async updateHandler() {
+    const expressionParams: IExpressionLoaderParams = {
+      searchContext: {
+        type: 'kibana_context',
+        timeRange: this.timeRange,
+        query: this.input.query,
+        filters: this.input.filters,
+      },
+      extraHandlers: {
+        vis: this.vis,
+        uiState: this.uiState,
+      },
+    };
+    this.expression = await buildPipeline(this.vis, {
+      searchSource: this.savedVisualization.searchSource,
+      timeRange: this.timeRange,
+    });
+
+    this.vis.filters = { timeRange: this.timeRange };
+
     if (this.handler) {
-      this.handler.reload();
+      this.handler.update(this.expression, expressionParams);
     }
   }
+
+  private handleVisUpdate = async () => {
+    if (this.appState) {
+      this.appState.vis = this.savedVisualization.vis.getState();
+      this.appState.save();
+    }
+
+    this.updateHandler();
+  };
 
   private uiStateChangeHandler = () => {
     this.updateInput({

src/legacy/ui/public/vis/editors/default/default.html
@@ -11,11 +11,6 @@
 
   <div
     class="visEditor__canvas"
-    data-shared-item
-    data-shared-items-container
-    render-complete
-    data-title="{{vis.title}}"
-    data-description="{{vis.description}}"
   />
 
 </div>

src/legacy/core_plugins/data/public/shim/legacy_module.ts
@@ -24,7 +24,7 @@ import { wrapInI18nContext } from 'ui/i18n';
 // @ts-ignore
 import { uiModules } from 'ui/modules';
 import { npStart } from 'ui/new_platform';
-import { FilterBar, ApplyFiltersPopover } from '../filter';
+import { FilterBar } from '../filter';
 import { IndexPatterns } from '../index_patterns/index_patterns';
 
 /** @internal */
@@ -70,54 +70,7 @@ export const initLegacyModule = once((indexPatterns: IndexPatterns): void => {
         ['className', { watchDepth: 'reference' }],
         ['pluginDataStart', { watchDepth: 'reference' }],
       ]);
-    })
-    .directive('applyFiltersPopover', () => {
-      return {
-        restrict: 'E',
-        template: '',
-        compile: (elem: any) => {
-          const child = document.createElement('apply-filters-popover-helper');
-
-          // Copy attributes to the child directive
-          for (const attr of elem[0].attributes) {
-            child.setAttribute(attr.name, attr.value);
-          }
-
-          // Add a key attribute that will force a full rerender every time that
-          // a filter changes.
-          child.setAttribute('key', 'key');
-
-          // Append helper directive
-          elem.append(child);
-
-          const linkFn = ($scope: any, _: any, $attr: any) => {
-            // Watch only for filter changes to update key.
-            $scope.$watch(
-              () => {
-                return $scope.$eval($attr.filters) || [];
-              },
-              (newVal: any) => {
-                $scope.key = Date.now();
-              },
-              true
-            );
-          };
-
-          return linkFn;
-        },
-      };
-    })
-    .directive('applyFiltersPopoverHelper', (reactDirective: any) =>
-      reactDirective(wrapInI18nContext(ApplyFiltersPopover), [
-        ['filters', { watchDepth: 'collection' }],
-        ['onCancel', { watchDepth: 'reference' }],
-        ['onSubmit', { watchDepth: 'reference' }],
-        ['indexPatterns', { watchDepth: 'collection' }],
-
-        // Key is needed to trigger a full rerender of the component
-        'key',
-      ])
-    );
+    });
 
   uiModules.get('kibana/index_patterns').value('indexPatterns', indexPatterns);
 });

src/legacy/core_plugins/expressions/public/np_ready/public/execute.ts
@@ -17,7 +17,7 @@
  * under the License.
  */
 
-import { fromExpression } from '@kbn/interpreter/target/common';
+import { fromExpression, toExpression } from '@kbn/interpreter/target/common';
 import { DataAdapter, RequestAdapter, Adapters } from '../../../../../../plugins/inspector/public';
 import { getInterpreter } from './services';
 import { ExpressionAST, IExpressionLoaderParams, IInterpreterResult } from './types';
@@ -38,17 +38,18 @@ export class ExpressionDataHandler {
   private inspectorAdapters: Adapters;
   private promise: Promise<IInterpreterResult>;
 
+  public isPending: boolean = true;
   constructor(expression: string | ExpressionAST, params: IExpressionLoaderParams) {
     if (typeof expression === 'string') {
       this.expression = expression;
       this.ast = fromExpression(expression) as ExpressionAST;
     } else {
       this.ast = expression;
-      this.expression = '';
+      this.expression = toExpression(this.ast);
     }
 
     this.abortController = new AbortController();
-    this.inspectorAdapters = this.getActiveInspectorAdapters();
+    this.inspectorAdapters = params.inspectorAdapters || this.getActiveInspectorAdapters();
 
     const getInitialContext = () => ({
       type: 'kibana_context',
@@ -58,11 +59,21 @@ export class ExpressionDataHandler {
     const defaultContext = { type: 'null' };
 
     const interpreter = getInterpreter();
-    this.promise = interpreter.interpretAst(this.ast, params.context || defaultContext, {
-      getInitialContext,
-      inspectorAdapters: this.inspectorAdapters,
-      abortSignal: this.abortController.signal,
-    });
+    this.promise = interpreter
+      .interpretAst(this.ast, params.context || defaultContext, {
+        getInitialContext,
+        inspectorAdapters: this.inspectorAdapters,
+        abortSignal: this.abortController.signal,
+      })
+      .then(
+        (v: IInterpreterResult) => {
+          this.isPending = false;
+          return v;
+        },
+        () => {
+          this.isPending = false;
+        }
+      );
   }
 
   cancel = () => {

src/legacy/core_plugins/expressions/public/np_ready/public/loader.test.ts
@@ -67,7 +67,7 @@ describe('execute helper function', () => {
 });
 
 describe('ExpressionLoader', () => {
-  const expressionString = '';
+  const expressionString = 'demodata';
 
   describe('constructor', () => {
     it('accepts expression string', () => {
@@ -134,6 +134,8 @@ describe('ExpressionLoader', () => {
     (ExpressionDataHandler as jest.Mock).mockImplementationOnce(() => ({
       getData: () => true,
       cancel: cancelMock,
+      isPending: () => true,
+      inspect: () => {},
     }));
 
     const expressionLoader = new ExpressionLoader(element, expressionString, {});
@@ -160,10 +162,15 @@ describe('ExpressionLoader', () => {
     (ExpressionDataHandler as jest.Mock).mockImplementationOnce(() => ({
       getData,
       cancel: cancelMock,
+      isPending: () => true,
+      inspect: () => {},
     }));
+
     (ExpressionDataHandler as jest.Mock).mockImplementationOnce(() => ({
       getData,
       cancel: cancelMock,
+      isPending: () => true,
+      inspect: () => {},
     }));
 
     const expressionLoader = new ExpressionLoader(element, expressionString, {});
@@ -193,6 +200,8 @@ describe('ExpressionLoader', () => {
     (ExpressionDataHandler as jest.Mock).mockImplementationOnce(() => ({
       getData,
       cancel: cancelMock,
+      isPending: () => true,
+      inspect: () => {},
     }));
 
     const expressionLoader = new ExpressionLoader(element, expressionString, {});

src/legacy/core_plugins/expressions/public/np_ready/public/loader.ts
@@ -38,11 +38,12 @@ export class ExpressionLoader {
   private loadingSubject: Subject<void>;
   private data: Data;
   private params: IExpressionLoaderParams = {};
+  private ignoreNextResponse = false;
 
   constructor(
     element: HTMLElement,
-    expression: string | ExpressionAST,
-    params: IExpressionLoaderParams
+    expression?: string | ExpressionAST,
+    params?: IExpressionLoaderParams
   ) {
     this.dataSubject = new Subject();
     this.data$ = this.dataSubject.asObservable().pipe(share());
@@ -65,7 +66,9 @@ export class ExpressionLoader {
 
     this.setParams(params);
 
-    this.loadData(expression, this.params);
+    if (expression) {
+      this.loadData(expression, this.params);
+    }
   }
 
   destroy() {
@@ -117,9 +120,10 @@ export class ExpressionLoader {
   update(expression?: string | ExpressionAST, params?: IExpressionLoaderParams): void {
     this.setParams(params);
 
+    this.loadingSubject.next();
     if (expression) {
       this.loadData(expression, this.params);
-    } else {
+    } else if (this.data) {
       this.render(this.data);
     }
   }
@@ -128,18 +132,22 @@ export class ExpressionLoader {
     expression: string | ExpressionAST,
     params: IExpressionLoaderParams
   ): Promise<void> => {
-    this.loadingSubject.next();
-    if (this.dataHandler) {
+    if (this.dataHandler && this.dataHandler.isPending) {
+      this.ignoreNextResponse = true;
       this.dataHandler.cancel();
     }
     this.setParams(params);
     this.dataHandler = new ExpressionDataHandler(expression, params);
+    if (!params.inspectorAdapters) params.inspectorAdapters = this.dataHandler.inspect();
     const data = await this.dataHandler.getData();
+    if (this.ignoreNextResponse) {
+      this.ignoreNextResponse = false;
+      return;
+    }
     this.dataSubject.next(data);
   };
 
   private render(data: Data): void {
-    this.loadingSubject.next();
     this.renderHandler.render(data, this.params.extraHandlers);
   }
 
@@ -148,22 +156,15 @@ export class ExpressionLoader {
       return;
     }
 
-    if (params.searchContext && this.params.searchContext) {
+    if (params.searchContext) {
       this.params.searchContext = _.defaults(
         {},
         params.searchContext,
-        this.params.searchContext
+        this.params.searchContext || {}
       ) as any;
     }
     if (params.extraHandlers && this.params) {
       this.params.extraHandlers = params.extraHandlers;
-    }
-
-    if (!Object.keys(this.params).length) {
-      this.params = {
-        ...params,
-        searchContext: { type: 'kibana_context', ...(params.searchContext || {}) },
-      };
     }
   }
 }

src/legacy/core_plugins/interpreter/public/functions/esaggs.ts
@@ -22,9 +22,15 @@ import { i18n } from '@kbn/i18n';
 import { AggConfigs } from 'ui/agg_types/agg_configs';
 import { createFormat } from 'ui/visualize/loader/pipeline_helpers/utilities';
 import chrome from 'ui/chrome';
-import { TimeRange } from 'src/plugins/data/public';
+
+import { Query, TimeRange, esFilters } from 'src/plugins/data/public';
 import { SearchSource } from '../../../../ui/public/courier/search_source';
-import { FilterBarQueryFilterProvider } from '../../../../ui/public/filter_manager/query_filter';
+// @ts-ignore
+import {
+  FilterBarQueryFilterProvider,
+  QueryFilter,
+} from '../../../../ui/public/filter_manager/query_filter';
+
 import { buildTabularInspectorData } from '../../../../ui/public/inspector/build_tabular_inspector_data';
 import {
   getRequestInspectorStats,
@@ -32,15 +38,30 @@ import {
 } from '../../../../ui/public/courier/utils/courier_inspector_utils';
 import { calculateObjectHash } from '../../../../ui/public/vis/lib/calculate_object_hash';
 import { getTime } from '../../../../ui/public/timefilter';
-import { RequestHandlerParams } from '../../../../ui/public/visualize/loader/embedded_visualize_handler';
-import { KibanaContext, KibanaDatatable } from '../../common';
-import { ExpressionFunction, KibanaDatatableColumn } from '../../types';
-import { start as data } from '../../../data/public/legacy';
+
+export interface RequestHandlerParams {
+  searchSource: SearchSource;
+  aggs: AggConfigs;
+  timeRange?: TimeRange;
+  query?: Query;
+  filters?: esFilters.Filter[];
+  forceFetch: boolean;
+  queryFilter: QueryFilter;
+  uiState?: PersistedState;
+  partialRows?: boolean;
+  inspectorAdapters: Adapters;
+  metricsAtAllLevels?: boolean;
+  visParams?: any;
+  abortSignal?: AbortSignal;
+}
 
 // @ts-ignore
 import { tabifyAggResponse } from '../../../../ui/public/agg_response/tabify/tabify';
-// @ts-ignore
-import { SearchSourceProvider } from '../../../../ui/public/courier/search_source';
+import { KibanaContext, KibanaDatatable } from '../../common';
+import { ExpressionFunction, KibanaDatatableColumn } from '../../types';
+import { start as data } from '../../../data/public/legacy';
+import { PersistedState } from '../../../../ui/public/persisted_state';
+import { Adapters } from '../../../../../plugins/inspector/public';
 
 const name = 'esaggs';
 

src/legacy/core_plugins/interpreter/public/renderers/visualization.tsx
@@ -18,9 +18,11 @@
  */
 
 import chrome from 'ui/chrome';
-import { visualizationLoader } from 'ui/visualize/loader/visualization_loader';
+import React from 'react';
+import { render, unmountComponentAtNode } from 'react-dom';
 // @ts-ignore
-import { VisProvider } from 'ui/visualize/loader/vis';
+import { VisProvider } from '../../../../ui/public/visualize/loader/vis';
+import { Visualization } from '../../../../ui/public/visualize/components';
 
 export const visualization = () => ({
   name: 'visualization',
@@ -50,17 +52,27 @@ export const visualization = () => ({
         type: visType,
         params: visConfig,
       });
-      handlers.vis.eventsSubject = handlers.eventsSubject;
     }
+
+    handlers.vis.eventsSubject = { next: handlers.event };
 
     const uiState = handlers.uiState || handlers.vis.getUiState();
 
-    handlers.onDestroy(() => visualizationLoader.destroy());
+    handlers.onDestroy(() => {
+      unmountComponentAtNode(domNode);
+    });
 
-    await visualizationLoader
-      .render(domNode, handlers.vis, visData, handlers.vis.params, uiState, params)
-      .then(() => {
-        if (handlers.done) handlers.done();
-      });
+    const listenOnChange = params ? params.listenOnChange : false;
+    render(
+      <Visualization
+        vis={handlers.vis}
+        visData={visData}
+        visParams={handlers.vis.params}
+        uiState={uiState}
+        listenOnChange={listenOnChange}
+        onInit={handlers.done}
+      />,
+      domNode
+    );
   },
 });

commit 9e24ad3b8fa660b705ad53aef372ea5bab11d10a

fix data-shared-items-container on visualise page (#52370)
Addresses #52109 (comment)

Should fix #51309,
Regression was introduced in: #46910

src/legacy/core_plugins/kibana/public/visualize/embeddable/visualize_embeddable.ts
@@ -313,7 +313,6 @@ export class VisualizeEmbeddable extends Embeddable<VisualizeInput, VisualizeOut
 
     div.setAttribute('data-test-subj', 'visualizationLoader');
     div.setAttribute('data-shared-item', '');
-    div.setAttribute('data-shared-item-container', '');
     div.setAttribute('data-rendering-count', '0');
     div.setAttribute('data-render-complete', 'false');
 

src/legacy/ui/public/vis/editors/default/default.html
@@ -11,6 +11,7 @@
 
   <div
     class="visEditor__canvas"
+    data-shared-items-container
   />
 
 </div>

test/functional/page_objects/common_page.ts
@@ -288,7 +288,7 @@ export function CommonPageProvider({ getService, getPageObjects }: FtrProviderCo
     }
 
     async getSharedItemContainers() {
-      const cssSelector = '[data-shared-item-container]';
+      const cssSelector = '[data-shared-items-container]';
       return find.allByCssSelector(cssSelector);
     }
 
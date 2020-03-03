patrykkopycinski/kibana

commit 3c5b9968313104b19d1646248e597caa70082f21

[ML] Transform: Support multi-line JSON notation in advanced editor (#58015)
* [ML] use xJsonMode

* [ML] remove commented code

* [ML] move use_x_json_mode hook, disable json formatting

* [ML] mocks for shared_imports

* [ML] ts-ignore worker import

x-pack/legacy/plugins/transform/public/app/sections/create_transform/components/step_define/step_define_form.tsx
@@ -28,6 +28,7 @@ import {
   EuiSwitch,
 } from '@elastic/eui';
 
+import { useXJsonMode, xJsonMode } from '../../../../hooks/use_x_json_mode';
 import { TransformPivotConfig } from '../../../../common';
 import { dictionaryToArray, Dictionary } from '../../../../../../common/types/common';
 import { DropDown } from '../aggregation_dropdown';
@@ -383,7 +384,13 @@ export const StepDefineForm: FC<Props> = React.memo(({ overrides = {}, onChange 
   const [advancedEditorConfigLastApplied, setAdvancedEditorConfigLastApplied] = useState(
     stringifiedPivotConfig
   );
-  const [advancedEditorConfig, setAdvancedEditorConfig] = useState(stringifiedPivotConfig);
+
+  const {
+    convertToJson,
+    setXJson: setAdvancedEditorConfig,
+    xJson: advancedEditorConfig,
+  } = useXJsonMode(stringifiedPivotConfig);
+
   // source config
   const stringifiedSourceConfig = JSON.stringify(previewRequest.source.query, null, 2);
   const [
@@ -407,7 +414,7 @@ export const StepDefineForm: FC<Props> = React.memo(({ overrides = {}, onChange 
   };
 
   const applyAdvancedPivotEditorChanges = () => {
-    const pivotConfig = JSON.parse(advancedEditorConfig);
+    const pivotConfig = JSON.parse(convertToJson(advancedEditorConfig));
 
     const newGroupByList: PivotGroupByConfigDict = {};
     if (pivotConfig !== undefined && pivotConfig.group_by !== undefined) {
@@ -442,10 +449,8 @@ export const StepDefineForm: FC<Props> = React.memo(({ overrides = {}, onChange 
       });
     }
     setAggList(newAggList);
-    const prettyPivotConfig = JSON.stringify(pivotConfig, null, 2);
 
-    setAdvancedEditorConfig(prettyPivotConfig);
-    setAdvancedEditorConfigLastApplied(prettyPivotConfig);
+    setAdvancedEditorConfigLastApplied(advancedEditorConfig);
     setAdvancedPivotEditorApplyButtonEnabled(false);
   };
 
@@ -513,13 +518,11 @@ export const StepDefineForm: FC<Props> = React.memo(({ overrides = {}, onChange 
       pivotAggsArr
     );
 
-    const stringifiedPivotConfigUpdate = JSON.stringify(previewRequestUpdate.pivot, null, 2);
     const stringifiedSourceConfigUpdate = JSON.stringify(
       previewRequestUpdate.source.query,
       null,
       2
     );
-    setAdvancedEditorConfig(stringifiedPivotConfigUpdate);
     setAdvancedEditorSourceConfig(stringifiedSourceConfigUpdate);
 
     onChange({
@@ -784,7 +787,7 @@ export const StepDefineForm: FC<Props> = React.memo(({ overrides = {}, onChange 
                 >
                   <EuiPanel grow={false} paddingSize="none">
                     <EuiCodeEditor
-                      mode="json"
+                      mode={xJsonMode}
                       width="100%"
                       value={advancedEditorConfig}
                       onChange={(d: string) => {
@@ -799,7 +802,7 @@ export const StepDefineForm: FC<Props> = React.memo(({ overrides = {}, onChange 
                         // Try to parse the string passed on from the editor.
                         // If parsing fails, the "Apply"-Button will be disabled
                         try {
-                          JSON.parse(d);
+                          JSON.parse(convertToJson(d));
                           setAdvancedPivotEditorApplyButtonEnabled(true);
                         } catch (e) {
                           setAdvancedPivotEditorApplyButtonEnabled(false);

x-pack/legacy/plugins/transform/public/__mocks__/shared_imports.ts
@@ -4,10 +4,7 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-// @ts-ignore
-import src from '!!raw-loader!./worker.js';
-
-export const workerModule = {
-  id: 'ace/mode/json_worker',
-  src,
-};
+export function XJsonMode() {}
+export function setDependencyCache() {}
+export { mlInMemoryTableBasicFactory } from '../../../ml/public/application/components/ml_in_memory_table';
+export const SORT_DIRECTION = { ASC: 'asc' };

x-pack/legacy/plugins/transform/public/app/hooks/use_x_json_mode.ts
@@ -1,0 +1,20 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+import { useState } from 'react';
+import { collapseLiteralStrings, expandLiteralStrings, XJsonMode } from '../../shared_imports';
+
+export const xJsonMode = new XJsonMode();
+
+export const useXJsonMode = (json: string) => {
+  const [xJson, setXJson] = useState(expandLiteralStrings(json));
+
+  return {
+    xJson,
+    setXJson,
+    xJsonMode,
+    convertToJson: collapseLiteralStrings,
+  };
+};

x-pack/legacy/plugins/transform/public/app/sections/create_transform/components/source_index_preview/source_index_preview.test.tsx
@@ -20,6 +20,8 @@ jest.mock('react', () => {
   return { ...r, memo: (x: any) => x };
 });
 
+jest.mock('../../../../../shared_imports');
+
 describe('Transform: <SourceIndexPreview />', () => {
   test('Minimal initialization', () => {
     const props = {

x-pack/legacy/plugins/transform/public/app/sections/create_transform/components/step_create/step_create_form.test.tsx
@@ -19,6 +19,8 @@ jest.mock('react', () => {
   return { ...r, memo: (x: any) => x };
 });
 
+jest.mock('../../../../../shared_imports');
+
 describe('Transform: <StepCreateForm />', () => {
   test('Minimal initialization', () => {
     const props = {

x-pack/legacy/plugins/transform/public/app/sections/create_transform/components/step_define/pivot_preview.test.tsx
@@ -27,6 +27,8 @@ jest.mock('react', () => {
   return { ...r, memo: (x: any) => x };
 });
 
+jest.mock('../../../../../shared_imports');
+
 describe('Transform: <PivotPreview />', () => {
   test('Minimal initialization', () => {
     const groupBy: PivotGroupByConfig = {

x-pack/legacy/plugins/transform/public/app/sections/create_transform/components/step_define/step_define_form.test.tsx
@@ -25,6 +25,8 @@ jest.mock('react', () => {
   return { ...r, memo: (x: any) => x };
 });
 
+jest.mock('../../../../../shared_imports');
+
 describe('Transform: <DefinePivotForm />', () => {
   test('Minimal initialization', () => {
     // Using a wrapping <div> element because shallow() would fail

x-pack/legacy/plugins/transform/public/app/sections/create_transform/components/step_define/step_define_summary.test.tsx
@@ -26,6 +26,8 @@ jest.mock('react', () => {
   return { ...r, memo: (x: any) => x };
 });
 
+jest.mock('../../../../../shared_imports');
+
 describe('Transform: <DefinePivotSummary />', () => {
   test('Minimal initialization', () => {
     const groupBy: PivotGroupByConfig = {

commit bb008b122e0cdcf138307840a25b2a2e41bd6cd4

[ML] Transform: Fix advanced editor initialization. (#59006)
Fixes regression introduced by #58015 to correctly initialize the transform wizard advanced editor with the current configuration.

x-pack/legacy/plugins/transform/public/app/sections/create_transform/components/step_define/step_define_form.tsx
@@ -404,6 +404,10 @@ export const StepDefineForm: FC<Props> = React.memo(({ overrides = {}, onChange 
     xJson: advancedEditorConfig,
   } = useXJsonMode(stringifiedPivotConfig);
 
+  useEffect(() => {
+    setAdvancedEditorConfig(stringifiedPivotConfig);
+  }, [setAdvancedEditorConfig, stringifiedPivotConfig]);
+
   // source config
   const stringifiedSourceConfig = JSON.stringify(previewRequest.source.query, null, 2);
   const [
@@ -797,6 +801,7 @@ export const StepDefineForm: FC<Props> = React.memo(({ overrides = {}, onChange 
                 >
                   <EuiPanel grow={false} paddingSize="none">
                     <EuiCodeEditor
+                      data-test-subj="transformAdvancedPivotEditor"
                       mode={xJsonMode}
                       width="100%"
                       value={advancedEditorConfig}

x-pack/test/functional/apps/transform/creation_index_pattern.ts
@@ -57,6 +57,28 @@ export default function({ getService }: FtrProviderContext) {
           return `user-${this.transformId}`;
         },
         expected: {
+          pivotAdvancedEditorValue: {
+            group_by: {
+              'category.keyword': {
+                terms: {
+                  field: 'category.keyword',
+                },
+              },
+              order_date: {
+                date_histogram: {
+                  field: 'order_date',
+                  calendar_interval: '1m',
+                },
+              },
+            },
+            aggregations: {
+              'products.base_price.avg': {
+                avg: {
+                  field: 'products.base_price',
+                },
+              },
+            },
+          },
           pivotPreview: {
             column: 0,
             values: [`Men's Accessories`],
@@ -150,6 +172,13 @@ export default function({ getService }: FtrProviderContext) {
         it('displays the advanced pivot editor switch', async () => {
           await transform.wizard.assertAdvancedPivotEditorSwitchExists();
           await transform.wizard.assertAdvancedPivotEditorSwitchCheckState(false);
+        });
+
+        it('displays the advanced configuration', async () => {
+          await transform.wizard.enabledAdvancedPivotEditor();
+          await transform.wizard.assertAdvancedPivotEditorContent(
+            testData.expected.pivotAdvancedEditorValue
+          );
         });
 
         it('loads the pivot preview', async () => {

x-pack/test/functional/services/transform_ui/wizard.ts
@@ -8,6 +8,7 @@ import expect from '@kbn/expect';
 import { FtrProviderContext } from '../../ftr_provider_context';
 
 export function TransformWizardProvider({ getService }: FtrProviderContext) {
+  const aceEditor = getService('aceEditor');
   const testSubjects = getService('testSubjects');
   const comboBox = getService('comboBox');
   const retry = getService('retry');
@@ -273,6 +274,12 @@ export function TransformWizardProvider({ getService }: FtrProviderContext) {
       await this.assertAggregationEntryExists(index, expectedLabel);
     },
 
+    async assertAdvancedPivotEditorContent(expectedValue: Record<string, any>) {
+      const advancedEditorString = await aceEditor.getValue('transformAdvancedPivotEditor');
+      const advancedEditorValue = JSON.parse(advancedEditorString);
+      expect(advancedEditorValue).to.eql(expectedValue);
+    },
+
     async assertAdvancedPivotEditorSwitchExists() {
       await testSubjects.existOrFail(`transformAdvancedPivotEditorSwitch`, { allowHidden: true });
     },
@@ -285,6 +292,13 @@ export function TransformWizardProvider({ getService }: FtrProviderContext) {
         expectedCheckState,
         `Advanced pivot editor switch check state should be ${expectedCheckState} (got ${actualCheckState})`
       );
+    },
+
+    async enabledAdvancedPivotEditor() {
+      await this.assertAdvancedPivotEditorSwitchCheckState(false);
+      await testSubjects.click('transformAdvancedPivotEditorSwitch');
+      await this.assertAdvancedPivotEditorSwitchCheckState(true);
+      await testSubjects.existOrFail('transformAdvancedPivotEditor');
     },
 
     async assertTransformIdInputExists() {
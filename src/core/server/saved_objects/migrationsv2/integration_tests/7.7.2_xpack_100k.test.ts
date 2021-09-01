41dc461c0624c9d906b29f4a38d06a859630446f
Rudolf Meijering <skaapgif@gmail.com>
2021-09-01 12:19:02 +0200

    Migrationsv2: limit batch sizes to migrations.batchSizeBytes (= 100mb by default) (#109540)
    * Fix logging for existing integration test
    
    * First stab at limiting batches to batchSizeBytes
    
    * Fix tests
    
    * Fix batch size calculation, NDJSON needs to be terminated by an empty line
    
    * Integration tests
    
    * Fix type failures
    
    * rename migration integration tests and log files to be consistent & more descriptive
    
    * Review feedback
    
    * Remove duplication of fatal error reasons
    
    * migrations.maxBatchSizeBytes to docker environment vars
    
    * docs for migrations.maxBatchSizeBytes

--- src/core/server/saved_objects/migrationsv2/integration_tests/7.7.2_xpack_100k.test.ts
+++ src/core/server/saved_objects/migrationsv2/integration_tests/7.7.2_xpack_100k.test.ts
@@ -17,7 +17,7 @@ import { InternalCoreStart } from '../../../internal_types';
 import { Root } from '../../../root';
 
 const kibanaVersion = Env.createDefault(REPO_ROOT, getEnvOptions()).packageInfo.version;
-const logFilePath = path.join(__dirname, 'migration_test_kibana.log');
+const logFilePath = path.join(__dirname, '7.7.2_xpack_100k.log');
 
 async function removeLogFile() {
   // ignore errors if it doesn't exist
@@ -61,9 +61,12 @@ describe('migration from 7.7.2-xpack with 100k objects', () => {
               },
             },
           },
-          root: {
-            appenders: ['default', 'file'],
-          },
+          loggers: [
+            {
+              name: 'root',
+              appenders: ['file'],
+            },
+          ],
         },
       },
       {

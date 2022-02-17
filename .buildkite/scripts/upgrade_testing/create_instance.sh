3c814a964e45a7f08528383e988c2c2a2661c5ca
David Sanchez Soler <david.sanchezsoler@elastic.co>
2022-02-17 10:29:01 +0100

    Fix unbound variable

--- .buildkite/scripts/upgrade_testing/create_instance.sh
+++ .buildkite/scripts/upgrade_testing/create_instance.sh
@@ -5,4 +5,4 @@ set -euo pipefail
 .buildkite/scripts/lifecycle/pre_build.sh
 .buildkite/scripts/bootstrap.sh
 
-node .buildkite/scripts/upgrade_testing/create_instance_runner.js $1 $2
+node .buildkite/scripts/upgrade_testing/create_instance_runner.js $1

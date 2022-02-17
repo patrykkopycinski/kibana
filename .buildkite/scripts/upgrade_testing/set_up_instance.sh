ff8f5d366a9efeb6cd074599467439ec0639b99d
David Sanchez Soler <david.sanchezsoler@elastic.co>
2022-02-17 11:39:12 +0100

    Fix unbound variable in setup script

--- .buildkite/scripts/upgrade_testing/set_up_instance.sh
+++ .buildkite/scripts/upgrade_testing/set_up_instance.sh
@@ -5,4 +5,4 @@ set -euo pipefail
 .buildkite/scripts/lifecycle/pre_build.sh
 .buildkite/scripts/bootstrap.sh
 
-node .buildkite/scripts/upgrade_testing/create_instance_runner.js $1
+node .buildkite/scripts/upgrade_testing/set_up_instance.js

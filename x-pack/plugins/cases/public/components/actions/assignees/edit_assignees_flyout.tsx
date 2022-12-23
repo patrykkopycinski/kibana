062b3efa532bc1b3afcd46ec0ff4907b75a9fe2b
Christos Nasikas <christos.nasikas@elastic.co>
2022-12-23 09:40:36 +0200

    [Cases] Bulk edit assignees (#146907)
    ## Summary
    
    This PR adds the ability to bulk edit assignees on multiple cases.
    Because the tags and the assignees share common logic:
    
    - I create the `useIsUserTyping` hook which contains common logic for
    when the user is typing. Is meant to be used with debouncing functions
    - I extracted all common logic from the tags selectable about state and
    rendering and put it in two new hooks called `useItemsAction` and
    `useItemsState`. No logic was changed in the state management
    - I refactored the tag action to use the new common hooks
    
    
    
    https://user-images.githubusercontent.com/7871006/207409490-a9a41e45-bd47-42e5-ab70-f5e0f1d8a914.mov
    
    
    ### Checklist
    
    Delete any items that are not applicable to this PR.
    
    - [x] Any text added follows [EUI's writing
    guidelines](https://elastic.github.io/eui/#/guidelines/writing), uses
    sentence case text and includes [i18n
    support](https://github.com/elastic/kibana/blob/main/packages/kbn-i18n/README.md)
    - [x] [Unit or functional
    tests](https://www.elastic.co/guide/en/kibana/master/development-tests.html)
    were updated or added to match the most common scenarios
    
    ### For maintainers
    
    - [x] This was checked for breaking API changes and was [labeled
    appropriately](https://www.elastic.co/guide/en/kibana/master/contributing.html#kibana-release-notes-process)
    
    ## Release notes
    
    Add the ability to bulk edit assignees on multiple cases
    
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>

--- x-pack/plugins/cases/public/components/actions/assignees/edit_assignees_flyout.tsx
+++ x-pack/plugins/cases/public/components/actions/assignees/edit_assignees_flyout.tsx
@@ -17,21 +17,19 @@ import {
   EuiFlyoutFooter,
   EuiFlyoutHeader,
   euiFullHeight,
-  EuiLoadingSpinner,
   EuiText,
   EuiTitle,
 } from '@elastic/eui';
 
 import type { Case } from '../../../../common';
-import { useGetTags } from '../../../containers/use_get_tags';
-import { EditTagsSelectable } from './edit_tags_selectable';
+import { EditAssigneesSelectable } from './edit_assignees_selectable';
 import * as i18n from './translations';
 import type { ItemsSelectionState } from '../types';
 
 interface Props {
   selectedCases: Case[];
   onClose: () => void;
-  onSaveTags: (args: ItemsSelectionState) => void;
+  onSaveAssignees: (args: ItemsSelectionState) => void;
 }
 
 const fullHeight = css`
@@ -42,15 +40,20 @@ const fullHeight = css`
   }
 `;
 
-const EditTagsFlyoutComponent: React.FC<Props> = ({ selectedCases, onClose, onSaveTags }) => {
-  const { data: tags, isLoading } = useGetTags();
-
-  const [tagsSelection, setTagsSelection] = useState<ItemsSelectionState>({
+const EditAssigneesFlyoutComponent: React.FC<Props> = ({
+  selectedCases,
+  onClose,
+  onSaveAssignees,
+}) => {
+  const [assigneesSelection, setAssigneesSelection] = useState<ItemsSelectionState>({
     selectedItems: [],
     unSelectedItems: [],
   });
 
-  const onSave = useCallback(() => onSaveTags(tagsSelection), [onSaveTags, tagsSelection]);
+  const onSave = useCallback(
+    () => onSaveAssignees(assigneesSelection),
+    [onSaveAssignees, assigneesSelection]
+  );
 
   const headerSubtitle =
     selectedCases.length > 1 ? i18n.SELECTED_CASES(selectedCases.length) : selectedCases[0].title;
@@ -59,30 +62,24 @@ const EditTagsFlyoutComponent: React.FC<Props> = ({ selectedCases, onClose, onSa
     <EuiFlyout
       ownFocus
       onClose={onClose}
-      aria-labelledby="cases-edit-tags-flyout"
-      data-test-subj="cases-edit-tags-flyout"
+      aria-labelledby="cases-edit-assignees-flyout"
+      data-test-subj="cases-edit-assignees-flyout"
       size="s"
       paddingSize="m"
     >
       <EuiFlyoutHeader hasBorder>
         <EuiTitle size="m">
-          <h2 data-test-subj="cases-edit-tags-flyout-title">{i18n.EDIT_TAGS}</h2>
+          <h2 data-test-subj="cases-edit-assignees-flyout-title">{i18n.EDIT_ASSIGNEES}</h2>
         </EuiTitle>
         <EuiText color="subdued">
           <p>{headerSubtitle}</p>
         </EuiText>
       </EuiFlyoutHeader>
       <EuiFlyoutBody css={fullHeight}>
-        {isLoading ? (
-          <EuiLoadingSpinner />
-        ) : (
-          <EditTagsSelectable
-            selectedCases={selectedCases}
-            isLoading={isLoading}
-            tags={tags ?? []}
-            onChangeTags={setTagsSelection}
-          />
-        )}
+        <EditAssigneesSelectable
+          selectedCases={selectedCases}
+          onChangeAssignees={setAssigneesSelection}
+        />
       </EuiFlyoutBody>
       <EuiFlyoutFooter>
         <EuiFlexGroup justifyContent="spaceBetween">
@@ -90,13 +87,13 @@ const EditTagsFlyoutComponent: React.FC<Props> = ({ selectedCases, onClose, onSa
             <EuiButtonEmpty
               onClick={onClose}
               flush="left"
-              data-test-subj="cases-edit-tags-flyout-cancel"
+              data-test-subj="cases-edit-assignees-flyout-cancel"
             >
               {i18n.CANCEL}
             </EuiButtonEmpty>
           </EuiFlexItem>
           <EuiFlexItem grow={false}>
-            <EuiButton onClick={onSave} fill data-test-subj="cases-edit-tags-flyout-submit">
+            <EuiButton onClick={onSave} fill data-test-subj="cases-edit-assignees-flyout-submit">
               {i18n.SAVE_SELECTION}
             </EuiButton>
           </EuiFlexItem>
@@ -106,6 +103,6 @@ const EditTagsFlyoutComponent: React.FC<Props> = ({ selectedCases, onClose, onSa
   );
 };
 
-EditTagsFlyoutComponent.displayName = 'EditTagsFlyout';
+EditAssigneesFlyoutComponent.displayName = 'EditAssigneesFlyout';
 
-export const EditTagsFlyout = React.memo(EditTagsFlyoutComponent);
+export const EditAssigneesFlyout = React.memo(EditAssigneesFlyoutComponent);

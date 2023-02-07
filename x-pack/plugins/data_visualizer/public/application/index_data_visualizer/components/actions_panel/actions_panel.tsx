patrykkopycinski/kibana

commit ae5594849c26775e5a6207258f5fe5139bd3e5a0

[ML] Move DatePickerWrapper and related code to package (#148063)
- Moves duplicates of `DatePickerWrapper` and related code to package
`@kbn/ml-date-picker`. The duplicated components across the code base
have been consolidated and diverging features combined. Each duplicate
has been checked with a diff against the package before deletion.
- Moves duplicates of `query_utils.ts` to a package
`@kbn/ml-query_utils`.
- Some jest test were migrated from enzyme to react-testing-lib.
- `i18n` strings and data-test-subjects have been updated to be prefixes
in line with package names.
- Replaces custom code related to the `compact` flag with EUI's
breakpoints.

x-pack/plugins/data_visualizer/public/application/index_data_visualizer/components/actions_panel/actions_panel.tsx
@@ -11,7 +11,7 @@ import React, { FC, useState, useEffect } from 'react';
 
 import { FormattedMessage } from '@kbn/i18n-react';
 import { i18n } from '@kbn/i18n';
-import { EuiSpacer, EuiTitle } from '@elastic/eui';
+import { useEuiBreakpoint, EuiSpacer, EuiTitle } from '@elastic/eui';
 import { DataView } from '@kbn/data-views-plugin/public';
 import { useUrlState } from '@kbn/ml-url-state';
 import { isDefined } from '@kbn/ml-is-defined';
@@ -26,7 +26,6 @@ interface Props {
   searchString?: string | { [key: string]: any };
   searchQueryLanguage?: string;
   getAdditionalLinks?: GetAdditionalLinks;
-  compact?: boolean;
 }
 
 const ACTIONS_PANEL_WIDTH = '240px';
@@ -36,7 +35,6 @@ export const ActionsPanel: FC<Props> = ({
   searchString,
   searchQueryLanguage,
   getAdditionalLinks,
-  compact,
 }) => {
   const [globalState] = useUrlState('_g');
 
@@ -121,19 +119,17 @@ export const ActionsPanel: FC<Props> = ({
   const showActionsPanel =
     discoverLink || (Array.isArray(asyncHrefCards) && asyncHrefCards.length > 0);
 
+  const dvActionsPanel = css({
+    [useEuiBreakpoint(['xs', 's', 'm', 'l', 'xl'])]: {
+      width: ACTIONS_PANEL_WIDTH,
+    },
+  });
+
   // Note we use display:none for the DataRecognizer section as it needs to be
   // passed the recognizerResults object, and then run the recognizer check which
   // controls whether the recognizer section is ultimately displayed.
   return showActionsPanel ? (
-    <div
-      data-test-subj="dataVisualizerActionsPanel"
-      css={
-        !compact &&
-        css`
-          width: ${ACTIONS_PANEL_WIDTH};
-        `
-      }
-    >
+    <div data-test-subj="dataVisualizerActionsPanel" css={dvActionsPanel}>
       <EuiTitle size="s">
         <h2>
           <FormattedMessage

x-pack/plugins/data_visualizer/public/application/index_data_visualizer/components/index_data_visualizer_view/index_data_visualizer_view.tsx
@@ -5,10 +5,13 @@
  * 2.0.
  */
 
+import { css } from '@emotion/react';
 import React, { FC, useEffect, useMemo, useState, useCallback, useRef } from 'react';
 import type { Required } from 'utility-types';
 
 import {
+  useEuiBreakpoint,
+  useIsWithinMaxBreakpoint,
   EuiFlexGroup,
   EuiFlexItem,
   EuiPageBody,
@@ -26,15 +29,20 @@ import { Filter, FilterStateStore, Query } from '@kbn/es-query';
 import { generateFilters } from '@kbn/data-plugin/public';
 import { DataView, DataViewField } from '@kbn/data-views-plugin/public';
 import { usePageUrlState, useUrlState } from '@kbn/ml-url-state';
-
+import {
+  DatePickerWrapper,
+  FullTimeRangeSelector,
+  FROZEN_TIER_PREFERENCE,
+} from '@kbn/ml-date-picker';
 import { useStorage } from '@kbn/ml-local-storage';
+
 import { useCurrentEuiTheme } from '../../../common/hooks/use_current_eui_theme';
 import {
+  DV_FROZEN_TIER_PREFERENCE,
   DV_RANDOM_SAMPLER_PREFERENCE,
   type DVKey,
   type DVStorageMapped,
 } from '../../types/storage';
-import { FullTimeRangeSelector } from '../full_time_range_selector';
 import {
   DataVisualizerTable,
   ItemIdToExpandedRowMap,
@@ -57,7 +65,6 @@ import { OMIT_FIELDS } from '../../../../../common/constants';
 import { kbnTypeToJobType } from '../../../common/util/field_types_utils';
 import { SearchPanel } from '../search_panel';
 import { ActionsPanel } from '../actions_panel';
-import { DatePickerWrapper } from '../../../common/components/date_picker_wrapper';
 import { createMergedEsQuery } from '../../utils/saved_search_utils';
 import { DataVisualizerDataViewManagement } from '../data_view_management';
 import { GetAdditionalLinks } from '../../../common/components/results_links';
@@ -125,7 +132,6 @@ export interface IndexDataVisualizerViewProps {
   currentSavedSearch: SavedSearchSavedObject | null;
   currentSessionId?: string;
   getAdditionalLinks?: GetAdditionalLinks;
-  compact?: boolean;
 }
 
 export const IndexDataVisualizerView: FC<IndexDataVisualizerViewProps> = (dataVisualizerProps) => {
@@ -135,6 +141,15 @@ export const IndexDataVisualizerView: FC<IndexDataVisualizerViewProps> = (dataVi
     DVKey,
     DVStorageMapped<typeof DV_RANDOM_SAMPLER_PREFERENCE>
   >(DV_RANDOM_SAMPLER_PREFERENCE, RANDOM_SAMPLER_OPTION.ON_AUTOMATIC);
+
+  const [frozenDataPreference, setFrozenDataPreference] = useStorage<
+    DVKey,
+    DVStorageMapped<typeof DV_FROZEN_TIER_PREFERENCE>
+  >(
+    DV_FROZEN_TIER_PREFERENCE,
+    // By default we will exclude frozen data tier
+    FROZEN_TIER_PREFERENCE.EXCLUDE
+  );
 
   const restorableDefaults = useMemo(
     () =>
@@ -161,7 +176,7 @@ export const IndexDataVisualizerView: FC<IndexDataVisualizerViewProps> = (dataVi
     dataVisualizerProps.currentSavedSearch
   );
 
-  const { currentDataView, currentSessionId, getAdditionalLinks, compact } = dataVisualizerProps;
+  const { currentDataView, currentSessionId, getAdditionalLinks } = dataVisualizerProps;
 
   useEffect(() => {
     if (dataVisualizerProps?.currentSavedSearch !== undefined) {
@@ -458,14 +473,21 @@ export const IndexDataVisualizerView: FC<IndexDataVisualizerViewProps> = (dataVi
     () => currentDataView.timeFieldName !== undefined && currentDataView.timeFieldName !== '',
     [currentDataView.timeFieldName]
   );
+
+  const dvPageHeader = css({
+    [useEuiBreakpoint(['xs', 's', 'm', 'l', 'xl'])]: {
+      flexDirection: 'column',
+      alignItems: 'flex-start',
+    },
+  });
+
+  const isWithinXl = useIsWithinMaxBreakpoint('xl');
+
   return (
     <EuiPageBody data-test-subj="dataVisualizerIndexPage" paddingSize="none" panelled={false}>
       <EuiFlexGroup gutterSize="m">
         <EuiFlexItem>
-          <EuiPageContentHeader
-            data-test-subj="dataVisualizerPageHeader"
-            css={compact ? { flexDirection: 'column', alignItems: 'flex-start' } : null}
-          >
+          <EuiPageContentHeader data-test-subj="dataVisualizerPageHeader" css={dvPageHeader}>
             <EuiPageContentHeaderSection>
               <EuiFlexGroup
                 data-test-subj="dataViewTitleHeader"
@@ -483,7 +505,7 @@ export const IndexDataVisualizerView: FC<IndexDataVisualizerViewProps> = (dataVi
               </EuiFlexGroup>
             </EuiPageContentHeaderSection>
 
-            {compact ? <EuiSpacer size="m" /> : null}
+            {isWithinXl ? <EuiSpacer size="m" /> : null}
             <EuiFlexGroup
               alignItems="center"
               justifyContent="flexEnd"
@@ -493,6 +515,8 @@ export const IndexDataVisualizerView: FC<IndexDataVisualizerViewProps> = (dataVi
               {hasValidTimeField ? (
                 <EuiFlexItem grow={false}>
                   <FullTimeRangeSelector
+                    frozenDataPreference={frozenDataPreference}
+                    setFrozenDataPreference={setFrozenDataPreference}
                     dataView={currentDataView}
                     query={undefined}
                     disabled={false}
@@ -504,7 +528,6 @@ export const IndexDataVisualizerView: FC<IndexDataVisualizerViewProps> = (dataVi
                 <DatePickerWrapper
                   isAutoRefreshOnly={!hasValidTimeField}
                   showRefresh={!hasValidTimeField}
-                  compact={compact}
                 />
               </EuiFlexItem>
             </EuiFlexGroup>
@@ -513,7 +536,7 @@ export const IndexDataVisualizerView: FC<IndexDataVisualizerViewProps> = (dataVi
       </EuiFlexGroup>
       <EuiSpacer size="m" />
       <EuiPageContentBody>
-        <EuiFlexGroup gutterSize="m" direction={compact ? 'column' : 'row'}>
+        <EuiFlexGroup gutterSize="m" direction={isWithinXl ? 'column' : 'row'}>
           <EuiFlexItem>
             <EuiPanel hasShadow={false} hasBorder>
               <SearchPanel
@@ -530,7 +553,6 @@ export const IndexDataVisualizerView: FC<IndexDataVisualizerViewProps> = (dataVi
                 setVisibleFieldNames={setVisibleFieldNames}
                 showEmptyFields={showEmptyFields}
                 onAddFilter={onAddFilter}
-                compact={compact}
               />
 
               {overallStats?.totalCount !== undefined && (
@@ -576,14 +598,13 @@ export const IndexDataVisualizerView: FC<IndexDataVisualizerViewProps> = (dataVi
               />
             </EuiPanel>
           </EuiFlexItem>
-          {compact ? <EuiSpacer size="m" /> : null}
+          {isWithinXl ? <EuiSpacer size="m" /> : null}
           <EuiFlexItem grow={false}>
             <ActionsPanel
               dataView={currentDataView}
               searchQueryLanguage={searchQueryLanguage}
               searchString={searchString}
               getAdditionalLinks={getAdditionalLinks}
-              compact={compact}
             />
           </EuiFlexItem>
         </EuiFlexGroup>

commit 3d02c99d8dfbd33eea73e0f5bc84f1b79baab6f8

[ML] Fix Data visualizer responsive layout (#150120)
## Summary

Fix Data Visualizer responsive layout broken due to a regression
introduced in https://github.com/elastic/kibana/pull/148063

After
<img width="1771" alt="Screen Shot 2023-02-01 at 16 23 06"
src="https://user-images.githubusercontent.com/43350163/216177549-c15c4d81-045a-4540-89ec-af599ede0320.png">
<img width="838" alt="Screen Shot 2023-02-01 at 16 23 18"
src="https://user-images.githubusercontent.com/43350163/216177561-39aac97e-2fff-425b-8872-c2aa796858eb.png">

And adds key to React.Fragment for the action cards. 

### Checklist

- [x] This renders correctly on smaller devices using a responsive
layout. (You can test this [in your
browser](https://www.browserstack.com/guide/responsive-testing-on-local-server))
- [x] This was checked for [cross-browser
compatibility](https://www.elastic.co/support/matrix#matrix_browsers)

x-pack/plugins/data_visualizer/public/application/index_data_visualizer/components/actions_panel/actions_panel.tsx
@@ -7,7 +7,7 @@
 
 import { css } from '@emotion/react';
 import { flatten } from 'lodash';
-import React, { FC, useState, useEffect } from 'react';
+import React, { FC, Fragment, useState, useEffect } from 'react';
 
 import { FormattedMessage } from '@kbn/i18n-react';
 import { i18n } from '@kbn/i18n';
@@ -120,11 +120,11 @@ export const ActionsPanel: FC<Props> = ({
     discoverLink || (Array.isArray(asyncHrefCards) && asyncHrefCards.length > 0);
 
   const dvActionsPanel = css({
-    [useEuiBreakpoint(['xs', 's', 'm', 'l', 'xl'])]: {
+    [useEuiBreakpoint(['xl', 'xxl'])]: {
       width: ACTIONS_PANEL_WIDTH,
     },
+    width: '100%',
   });
-
   // Note we use display:none for the DataRecognizer section as it needs to be
   // passed the recognizerResults object, and then run the recognizer check which
   // controls whether the recognizer section is ultimately displayed.
@@ -164,16 +164,17 @@ export const ActionsPanel: FC<Props> = ({
 
       {Array.isArray(asyncHrefCards) &&
         asyncHrefCards.map((link) => (
-          <>
+          <Fragment key={`dv-action-card-${link.title}`}>
             <LinkCard
               href={link.href}
               icon={link.icon}
               description={link.description}
               title={link.title}
               data-test-subj={link['data-test-subj']}
+              key={link.href}
             />
             <EuiSpacer size="m" />
-          </>
+          </Fragment>
         ))}
     </div>
   ) : null;

x-pack/plugins/data_visualizer/public/application/index_data_visualizer/components/index_data_visualizer_view/index_data_visualizer_view.tsx
@@ -469,14 +469,13 @@ export const IndexDataVisualizerView: FC<IndexDataVisualizerViewProps> = (dataVi
     [currentDataView.timeFieldName]
   );
 
+  const isWithinLargeBreakpoint = useIsWithinMaxBreakpoint('l');
   const dvPageHeader = css({
-    [useEuiBreakpoint(['xs', 's', 'm', 'l', 'xl'])]: {
+    [useEuiBreakpoint(['xs', 's', 'm', 'l'])]: {
       flexDirection: 'column',
       alignItems: 'flex-start',
     },
   });
-
-  const isWithinXl = useIsWithinMaxBreakpoint('xl');
 
   return (
     <EuiPageBody data-test-subj="dataVisualizerIndexPage" paddingSize="none" panelled={false}>
@@ -500,7 +499,7 @@ export const IndexDataVisualizerView: FC<IndexDataVisualizerViewProps> = (dataVi
               </EuiFlexGroup>
             </EuiPageContentHeaderSection>
 
-            {isWithinXl ? <EuiSpacer size="m" /> : null}
+            {isWithinLargeBreakpoint ? <EuiSpacer size="m" /> : null}
             <EuiFlexGroup
               alignItems="center"
               justifyContent="flexEnd"
@@ -532,7 +531,7 @@ export const IndexDataVisualizerView: FC<IndexDataVisualizerViewProps> = (dataVi
       </EuiFlexGroup>
       <EuiSpacer size="m" />
       <EuiPageContentBody>
-        <EuiFlexGroup gutterSize="m" direction={isWithinXl ? 'column' : 'row'}>
+        <EuiFlexGroup gutterSize="m" direction={isWithinLargeBreakpoint ? 'column' : 'row'}>
           <EuiFlexItem>
             <EuiPanel hasShadow={false} hasBorder>
               <SearchPanel
@@ -594,7 +593,7 @@ export const IndexDataVisualizerView: FC<IndexDataVisualizerViewProps> = (dataVi
               />
             </EuiPanel>
           </EuiFlexItem>
-          {isWithinXl ? <EuiSpacer size="m" /> : null}
+          {isWithinLargeBreakpoint ? <EuiSpacer size="m" /> : null}
           <EuiFlexItem grow={false}>
             <ActionsPanel
               dataView={currentDataView}
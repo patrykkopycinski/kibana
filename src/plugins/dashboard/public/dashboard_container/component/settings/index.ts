96d89984e0e2121fecb7b5b004c3ecc16e4b6bac
Nick Peihl <nick.peihl@elastic.co>
2023-03-31 09:52:51 -0400

    [Dashboard Usability] Unified dashboard settings (#153862)
    ## Summary
    
    Adds flyout for changing individual dashboard settings such as title,
    description, tags, and save time with dashboard. This also moves the
    existing dashboard options (show panel titles, sync colors, use margins,
    sync cursor, and sync tooltips) into the flyout.
    
    Fixes #144532
    
    [Flaky test
    runner](https://buildkite.com/elastic/kibana-flaky-test-suite-runner/builds/2055)
    
    ### Checklist
    
    Delete any items that are not applicable to this PR.
    
    - [x] Any text added follows [EUI's writing
    guidelines](https://elastic.github.io/eui/#/guidelines/writing), uses
    sentence case text and includes [i18n
    support](https://github.com/elastic/kibana/blob/main/packages/kbn-i18n/README.md)
    - [x] [Unit or functional
    tests](https://www.elastic.co/guide/en/kibana/master/development-tests.html)
    were updated or added to match the most common scenarios
    - [x] Any UI touched in this PR is usable by keyboard only (learn more
    about [keyboard accessibility](https://webaim.org/techniques/keyboard/))
    - [x] Any UI touched in this PR does not create any new axe failures
    (run axe in browser:
    [FF](https://addons.mozilla.org/en-US/firefox/addon/axe-devtools/),
    [Chrome](https://chrome.google.com/webstore/detail/axe-web-accessibility-tes/lhdoppojpmngadmnindnejefpokejbdd?hl=en-US))
    - [x] This was checked for [cross-browser
    compatibility](https://www.elastic.co/support/matrix#matrix_browsers)
    
    
    ### For maintainers
    
    - [ ] This was checked for breaking API changes and was [labeled
    appropriately](https://www.elastic.co/guide/en/kibana/master/contributing.html#kibana-release-notes-process)

--- src/plugins/dashboard/public/dashboard_container/component/settings/index.ts
+++ src/plugins/dashboard/public/dashboard_container/component/settings/index.ts
@@ -6,7 +6,4 @@
  * Side Public License, v 1.
  */
 
-export { showSettings } from './show_settings';
-export { addFromLibrary } from './add_panel_from_library';
-export { runSaveAs, runQuickSave, runClone } from './run_save_functions';
-export { addOrUpdateEmbeddable, replacePanel, showPlaceholderUntil } from './panel_management';
+export { DashboardSettings } from './settings_flyout';

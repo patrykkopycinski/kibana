patrykkopycinski/kibana

commit 4854f425c8fae81464cc18ba699d3c6900fa0dce

[Lens] [metric visualization] a column normalized by unit doesn't display properly on dashboard (#142741)
* [Lens] [metric visualization] a column normalized by unit doesn't display properly on dashboard

Closes: #142615

* add test

* [CI] Auto-commit changed files from 'node scripts/build_plugin_list_docs'

* bundle size reduction

Co-authored-by: kibanamachine <42973632+kibanamachine@users.noreply.github.com>

x-pack/plugins/lens/common/expressions/time_scale/time_scale_fn.ts
@@ -24,12 +24,39 @@ const unitInMs: Record<TimeScaleUnit, number> = {
   d: 1000 * 60 * 60 * 24,
 };
 
+// the datemath plugin always parses dates by using the current default moment time zone.
+// to use the configured time zone, we are temporary switching it just for the calculation.
+
+// The code between this call and the reset in the finally block is not allowed to get async,
+// otherwise the timezone setting can leak out of this function.
+const withChangedTimeZone = <TReturnedValue = unknown>(
+  timeZone: string | undefined,
+  action: () => TReturnedValue
+): TReturnedValue => {
+  if (timeZone) {
+    const defaultTimezone = moment().zoneName();
+    try {
+      moment.tz.setDefault(timeZone);
+      return action();
+    } finally {
+      // reset default moment timezone
+      moment.tz.setDefault(defaultTimezone);
+    }
+  } else {
+    return action();
+  }
+};
+
+const getTimeBounds = (timeRange: TimeRange, timeZone?: string, getForceNow?: () => Date) =>
+  withChangedTimeZone(timeZone, () => calculateBounds(timeRange, { forceNow: getForceNow?.() }));
+
 export const timeScaleFn =
   (
     getDatatableUtilities: (
       context: ExecutionContext
     ) => DatatableUtilitiesService | Promise<DatatableUtilitiesService>,
-    getTimezone: (context: ExecutionContext) => string | Promise<string>
+    getTimezone: (context: ExecutionContext) => string | Promise<string>,
+    getForceNow?: () => Date
   ): TimeScaleExpressionFunction['fn'] =>
   async (
     input,
@@ -69,7 +96,9 @@ export const timeScaleFn =
         timeZone: contextTimeZone,
       });
       const intervalDuration = timeInfo?.interval && parseInterval(timeInfo.interval);
-      timeBounds = timeInfo?.timeRange && calculateBounds(timeInfo.timeRange);
+
+      timeBounds =
+        timeInfo?.timeRange && getTimeBounds(timeInfo.timeRange, timeInfo?.timeZone, getForceNow);
 
       getStartEndOfBucketMeta = (row) => {
         const startOfBucket = moment.tz(row[dateColumnId], timeInfo?.timeZone ?? contextTimeZone);
@@ -89,8 +118,18 @@ export const timeScaleFn =
       }
     } else {
       const timeRange = context.getSearchContext().timeRange as TimeRange;
-      const endOfBucket = moment.tz(timeRange.to, contextTimeZone);
-      let startOfBucket = moment.tz(timeRange.from, contextTimeZone);
+      timeBounds = getTimeBounds(timeRange, contextTimeZone, getForceNow);
+
+      if (!timeBounds.max || !timeBounds.min) {
+        throw new Error(
+          i18n.translate('xpack.lens.functions.timeScale.timeBoundsMissingMessage', {
+            defaultMessage: 'Could not parse "Time Range"',
+          })
+        );
+      }
+
+      const endOfBucket = timeBounds.max;
+      let startOfBucket = timeBounds.min;
 
       if (reducedTimeRange) {
         const reducedStartOfBucket = endOfBucket.clone().subtract(parseInterval(reducedTimeRange));
@@ -99,8 +138,6 @@ export const timeScaleFn =
           startOfBucket = reducedStartOfBucket;
         }
       }
-
-      timeBounds = calculateBounds(timeRange);
 
       getStartEndOfBucketMeta = () => ({
         startOfBucket,

x-pack/plugins/lens/common/expressions/time_scale/time_scale.test.ts
@@ -12,18 +12,6 @@ import type { TimeRange } from '@kbn/es-query';
 import { createDatatableUtilitiesMock } from '@kbn/data-plugin/common/mocks';
 import { functionWrapper } from '@kbn/expressions-plugin/common/expression_functions/specs/tests/utils';
 
-// mock the specific inner variable:
-// there are intra dependencies in the data plugin we might break trying to mock the whole thing
-jest.mock('@kbn/data-plugin/common/query/timefilter/get_time', () => {
-  const localMoment = jest.requireActual('moment');
-  return {
-    calculateBounds: jest.fn(({ from, to }) => ({
-      min: localMoment(from),
-      max: localMoment(to),
-    })),
-  };
-});
-
 import { getTimeScale } from './time_scale';
 import type { TimeScaleArgs } from './types';
 
@@ -423,6 +411,35 @@ describe('time_scale', () => {
     );
 
     expect(result.rows.map(({ scaledMetric }) => scaledMetric)).toEqual([75]);
+  });
+
+  it('should work with relative time range', async () => {
+    const result = await timeScaleWrapped(
+      {
+        ...emptyTable,
+        rows: [
+          {
+            date: moment().subtract('1d').valueOf(),
+            metric: 300,
+          },
+        ],
+      },
+      {
+        inputColumnId: 'metric',
+        outputColumnId: 'scaledMetric',
+        targetUnit: 'd',
+      },
+      {
+        getSearchContext: () => ({
+          timeRange: {
+            from: 'now-10d',
+            to: 'now',
+          },
+        }),
+      } as unknown as ExecutionContext
+    );
+
+    expect(result.rows.map(({ scaledMetric }) => scaledMetric)).toEqual([30]);
   });
 
   it('should apply fn for non-histogram fields (with Reduced time range)', async () => {

x-pack/plugins/lens/common/embeddable_factory/index.ts
@@ -5,8 +5,8 @@
  * 2.0.
  */
 
-import { SerializableRecord, Serializable } from '@kbn/utility-types';
-import { SavedObjectReference } from '@kbn/core/types';
+import type { SerializableRecord, Serializable } from '@kbn/utility-types';
+import type { SavedObjectReference } from '@kbn/core/types';
 import type {
   EmbeddableStateWithType,
   EmbeddableRegistryDefinition,

x-pack/plugins/lens/common/index.ts
@@ -10,7 +10,6 @@
 
 export * from './constants';
 export * from './types';
-export * from './visualizations';
 
 // Note: do not import the expression folder here or the page bundle will be bloated with all
 // the package

x-pack/plugins/lens/common/types.ts
@@ -6,12 +6,12 @@
  */
 
 import type { Filter, FilterMeta } from '@kbn/es-query';
-import { Position } from '@elastic/charts';
-import { $Values } from '@kbn/utility-types';
+import type { Position } from '@elastic/charts';
+import type { $Values } from '@kbn/utility-types';
 import type { CustomPaletteParams, PaletteOutput } from '@kbn/coloring';
 import type { IFieldFormat, SerializedFieldFormat } from '@kbn/field-formats-plugin/common';
 import type { ColorMode } from '@kbn/charts-plugin/common';
-import { LegendSize } from '@kbn/visualizations-plugin/common';
+import type { LegendSize } from '@kbn/visualizations-plugin/common';
 import {
   CategoryDisplay,
   layerTypes,

x-pack/plugins/lens/public/embeddable/embeddable_factory.ts
@@ -23,16 +23,16 @@ import {
   IContainer,
   ErrorEmbeddable,
 } from '@kbn/embeddable-plugin/public';
-import { UiActionsStart } from '@kbn/ui-actions-plugin/public';
-import { Start as InspectorStart } from '@kbn/inspector-plugin/public';
+import type { UiActionsStart } from '@kbn/ui-actions-plugin/public';
+import type { Start as InspectorStart } from '@kbn/inspector-plugin/public';
 import type { SpacesPluginStart } from '@kbn/spaces-plugin/public';
-import { LensByReferenceInput, LensEmbeddableInput } from './embeddable';
-import { Document } from '../persistence/saved_object_store';
-import { LensAttributeService } from '../lens_attribute_service';
+import type { LensByReferenceInput, LensEmbeddableInput } from './embeddable';
+import type { Document } from '../persistence/saved_object_store';
+import type { LensAttributeService } from '../lens_attribute_service';
 import { DOC_TYPE } from '../../common/constants';
-import { ErrorMessage } from '../editor_frame_service/types';
+import type { ErrorMessage } from '../editor_frame_service/types';
 import { extract, inject } from '../../common/embeddable_factory';
-import { DatasourceMap, VisualizationMap } from '../types';
+import type { DatasourceMap, VisualizationMap } from '../types';
 
 export interface LensEmbeddableStartServices {
   data: DataPublicPluginStart;

x-pack/plugins/lens/public/expressions.ts
@@ -6,6 +6,7 @@
  */
 
 import type { ExpressionsSetup } from '@kbn/expressions-plugin/public';
+
 import { getDatatable } from '../common/expressions/datatable/datatable';
 import { datatableColumn } from '../common/expressions/datatable/datatable_column';
 import { mapToColumns } from '../common/expressions/map_to_columns/map_to_columns';
@@ -14,11 +15,14 @@ import { counterRate } from '../common/expressions/counter_rate';
 import { getTimeScale } from '../common/expressions/time_scale/time_scale';
 import { collapse } from '../common/expressions';
 
+type TimeScaleArguments = Parameters<typeof getTimeScale>;
+
 export const setupExpressions = (
   expressions: ExpressionsSetup,
   formatFactory: Parameters<typeof getDatatable>[0],
-  getDatatableUtilities: Parameters<typeof getTimeScale>[0],
-  getTimeZone: Parameters<typeof getTimeScale>[1]
+  getDatatableUtilities: TimeScaleArguments[0],
+  getTimeZone: TimeScaleArguments[1],
+  getForceNow: TimeScaleArguments[2]
 ) => {
   [
     collapse,
@@ -27,6 +31,6 @@ export const setupExpressions = (
     mapToColumns,
     datatableColumn,
     getDatatable(formatFactory),
-    getTimeScale(getDatatableUtilities, getTimeZone),
+    getTimeScale(getDatatableUtilities, getTimeZone, getForceNow),
   ].forEach((expressionFn) => expressions.registerFunction(expressionFn));
 };

commit 32a29ec0a7f0100ee3095af504d00f5a75165fde

[Lens] Fix timezone used in normalize by unit (#154472)
This commit fixes an issue introduced in
https://github.com/elastic/kibana/pull/142741 where the time bounds were
computed in an expression `time_scale` that can be run on both the
server and the client.
Computing time-related information on each side (server/client), means
that we need to align temporarily with the client timezone to make an
effective calculation.
This temporary alignment had a bug when executed on the client: the
the first time it gets completed, the restored timezone was the wrong
timezone, in particular, the function used to get the current configured
timezone `moment().zoneName()` return abbreviated zone names and in some
cases also non-unique abbreviations (see
https://momentjs.com/timezone/docs/#/using-timezones/formatting/) making
the restoration a bit difficult and problematic.

The fix instead did the following:
- replace the `moment().zoneName()` with `moment.defaultZone?.name` even
this is not typed, this property is exposed by moment [since years
now](https://github.com/moment/moment-timezone/blob/2448cdcbe15875bc22ddfbc184794d0a6b568b90/moment-timezone.js#L603)
and is the only way to get the `defaultZone` configured through the
`setDefault`.
- replace the try/catch/finally block with a more readable
implementation: using a "safe" implementation to update the timezone in
moment we can get rid of the try/catch and we can make it more linear.
 

fix #154309

The unit test was firstly tested with the old implementation (and it was
failing because the `zoneName` was returning `EDT` which is not a valid
IANA timezone and the timezone were set to `undefined`. With the new
implementation, it returns the specified timezone correctly. I haven't
tested the function itself because I don't know the internal details and
this is also out of the scope of the fix.

x-pack/plugins/lens/common/expressions/time_scale/time_scale_fn.ts
@@ -24,31 +24,31 @@ const unitInMs: Record<TimeScaleUnit, number> = {
   d: 1000 * 60 * 60 * 24,
 };
 
-// the datemath plugin always parses dates by using the current default moment time zone.
-// to use the configured time zone, we are temporary switching it just for the calculation.
+function safelySetTimeZone(timeZone: string) {
+  const zone = moment.tz.zone(timeZone);
+  if (zone) moment.tz.setDefault(zone.name);
+}
 
-// The code between this call and the reset in the finally block is not allowed to get async,
-// otherwise the timezone setting can leak out of this function.
-const withChangedTimeZone = <TReturnedValue = unknown>(
-  timeZone: string | undefined,
-  action: () => TReturnedValue
-): TReturnedValue => {
+/**
+ * This function can be called both from server side and from the client side. Each of them could have
+ * a different configured timezone. To be sure the time bounds are computed relative to the same passed timezone,
+ * temporarily switch the default moment timezone to the one passed, and switch it back after the calculation is done.
+ */
+export function getTimeBounds(timeRange: TimeRange, timeZone?: string, getForceNow?: () => Date) {
   if (timeZone) {
-    const defaultTimezone = moment().zoneName();
-    try {
-      moment.tz.setDefault(timeZone);
-      return action();
-    } finally {
-      // reset default moment timezone
-      moment.tz.setDefault(defaultTimezone);
-    }
+    // the `defaultZone` property is injected by moment.timezone.
+    // If is not available is it fine to keep undefined because calling setDefault() will automatically reset to default
+    // https://github.com/moment/moment-timezone/blob/2448cdcbe15875bc22ddfbc184794d0a6b568b90/moment-timezone.js#L603
+    // @ts-expect-error because is not part of the exposed types unfortunately
+    const currentDefaultTimeZone = moment.defaultZone?.name;
+    safelySetTimeZone(timeZone);
+    const timeBounds = calculateBounds(timeRange, { forceNow: getForceNow?.() });
+    safelySetTimeZone(currentDefaultTimeZone);
+    return timeBounds;
   } else {
-    return action();
+    return calculateBounds(timeRange, { forceNow: getForceNow?.() });
   }
-};
-
-const getTimeBounds = (timeRange: TimeRange, timeZone?: string, getForceNow?: () => Date) =>
-  withChangedTimeZone(timeZone, () => calculateBounds(timeRange, { forceNow: getForceNow?.() }));
+}
 
 export const timeScaleFn =
   (

x-pack/plugins/lens/common/expressions/time_scale/time_scale.test.ts
@@ -14,6 +14,7 @@ import { functionWrapper } from '@kbn/expressions-plugin/common/expression_funct
 
 import { getTimeScale } from './time_scale';
 import type { TimeScaleArgs } from './types';
+import { getTimeBounds } from './time_scale_fn';
 
 describe('time_scale', () => {
   let timeScaleWrapped: (
@@ -502,5 +503,20 @@ describe('time_scale', () => {
     await new Promise((r) => setTimeout(r, 0));
     // should resolve now without another async dependency
     expect(timeScaleResolved).toHaveBeenCalled();
+  });
+
+  it('getTimeBounds should not alter the default moment timezone', () => {
+    // configuring an exotic timezone
+    moment.tz.setDefault('Pacific/Honolulu');
+    // @ts-ignore
+    expect(moment.defaultZone?.name).toBe('Pacific/Honolulu');
+
+    getTimeBounds(
+      { from: '2023-04-01T00:00:00.000+02:00', to: '2023-04-02T00:00:00.000+02:00' },
+      'Europe/Lisbon',
+      () => new Date('2023-04-01T00:00:00.000Z')
+    );
+    // @ts-ignore
+    expect(moment.defaultZone?.name).toBe('Pacific/Honolulu');
   });
 });
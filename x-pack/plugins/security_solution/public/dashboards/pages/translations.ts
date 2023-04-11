a6cb26b225f42dd8be685ef6434eafcc5ec76144
Angela Chuang <6295984+angorayc@users.noreply.github.com>
2023-04-11 21:15:37 +0100

    [SecuritySolution] Setup dashboard view page (#153040)
    ## Summary
    
    https://github.com/elastic/kibana/issues/152955
    
    Demo link:
    https://kibana-pr-153040.kb.us-west2.gcp.elastic-cloud.com:9243/app/security/dashboards/6294c960-ce35-11ed-b8ca-51636b04063c?sourcerer=(default:(id:security-solution-default,selectedPatterns:!(%27auditbeat-*%27,%27logs-*%27)))
    
    
    https://p.elstc.co/paste/pOFVo-fV#Zgp3hnsnijsDHbki4y9Cy5F+apet-hYEcedpDzsc+f7
    
    This create a single dashboard view under Security Solution:
    
    - Add dashboard view path: `/app/security/dashboards/:dashboardId`
    - Move the dashboards landing page to this new sub-plugin.
    - Check users' read permission to render a dashboard.
    - Render a dashboard with the given saved object id.
    - Show the dashboard name in the breadcrumbs.
    
    Dashboard not found:
    
    <img width="2363" alt="Screenshot 2023-03-23 at 13 44 01"
    src="https://user-images.githubusercontent.com/6295984/227477728-8d4984f2-3d8f-4f92-88ae-3337e6b3e5be.png">
    
    Dashboard rendered:
    
    <img width="2539" alt="Screenshot 2023-03-23 at 13 44 28"
    src="https://user-images.githubusercontent.com/6295984/227477761-b1301b5c-1c4f-4970-bf8f-e077342c317f.png">
    
    
    Interact with filters and query:
    
    
    https://user-images.githubusercontent.com/6295984/227477735-dc53bb85-31fb-4043-8355-22866296ebf9.mov
    
    
    
    
    Interact with `Open in Lens` and `Investigate in timeline`
    
    
    
    https://user-images.githubusercontent.com/6295984/228217900-5055a5d1-46f2-4d2f-98a8-289eb0f1939a.mov
    
    
    
    
    **Steps to verify**:
    1. Create a dashboard from `/app/dashboards#/list`, save it and copy the
    dashboard saved object id from url.
    2. Visit `/app/security/dashboards/:dashboardId`
    
    
    
    ### Checklist
    
    Delete any items that are not applicable to this PR.
    
    - [x] Any text added follows [EUI's writing
    guidelines](https://elastic.github.io/eui/#/guidelines/writing), uses
    sentence case text and includes [i18n
    support](https://github.com/elastic/kibana/blob/main/packages/kbn-i18n/README.md)
    - [x] [Unit or functional
    tests](https://www.elastic.co/guide/en/kibana/master/development-tests.html)
    were updated or added to match the most common scenarios
    
    ---------
    
    Co-authored-by: Stratoula Kalafateli <stratoula1@gmail.com>
    Co-authored-by: kibanamachine <42973632+kibanamachine@users.noreply.github.com>

--- x-pack/plugins/security_solution/public/dashboards/pages/translations.ts
+++ x-pack/plugins/security_solution/public/dashboards/pages/translations.ts
@@ -4,16 +4,12 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
-
 import { i18n } from '@kbn/i18n';
 
-export const EXPLORE_PAGE_TITLE = i18n.translate(
-  'xpack.securitySolution.landing.threatHunting.pageTitle',
-  {
-    defaultMessage: 'Explore',
-  }
-);
+export const DASHBOARDS_PAGE_TITLE = i18n.translate('xpack.securitySolution.dashboards.pageTitle', {
+  defaultMessage: 'Dashboards',
+});
 
-export const MANAGE_PAGE_TITLE = i18n.translate('xpack.securitySolution.landing.manage.pageTitle', {
-  defaultMessage: 'Manage',
+export const DASHBOARD_PAGE_TITLE = i18n.translate('xpack.securitySolution.dashboard.pageTitle', {
+  defaultMessage: 'Dashboard',
 });

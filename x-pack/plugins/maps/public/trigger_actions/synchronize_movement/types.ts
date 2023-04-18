7685512cb4e527024425d18224d1b3d684abea62
Nathan Reese <reese.nathan@elastic.co>
2023-04-18 06:36:58 -0600

    [maps] reduce bundle size (#154789)
    Co-authored-by: kibanamachine <42973632+kibanamachine@users.noreply.github.com>

--- x-pack/plugins/maps/public/trigger_actions/synchronize_movement/types.ts
+++ x-pack/plugins/maps/public/trigger_actions/synchronize_movement/types.ts
@@ -5,6 +5,8 @@
  * 2.0.
  */
 
-export { GEOHASH_GRID, getGeoHashBucketAgg } from './tile_map';
-export { createRegionMapFn, regionMapRenderer, regionMapVisType } from './region_map';
-export { createTileMapFn, tileMapRenderer, tileMapVisType } from './tile_map';
+import type { Embeddable, EmbeddableInput } from '@kbn/embeddable-plugin/public';
+
+export interface SynchronizeMovementActionContext {
+  embeddable: Embeddable<EmbeddableInput>;
+}

809fff151d6b59d5acd5fb446f205d8962a4576d
Patryk Kopycinski <contact@patrykkopycinski.com>
2023-05-25 19:11:24 +0200

    WIP

--- x-pack/plugins/security_solution/cypress/cypress_ci.config.ts
+++ x-pack/plugins/security_solution/cypress/cypress_ci.config.ts
@@ -4,8 +4,35 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
+import {
+  EsVersion,
+  FunctionalTestRunner,
+  readConfigFile,
+  runElasticsearch,
+  runKibanaServer,
+} from '@kbn/test';
+import _ from 'lodash';
+import fs from 'fs';
+import * as parser from '@babel/parser';
+import type {
+  ExpressionStatement,
+  Identifier,
+  ObjectExpression,
+  ObjectProperty,
+} from '@babel/types';
+import deepmerge from 'deepmerge';
+
+import {
+  Lifecycle,
+  ProviderCollection,
+  readProviderSpec,
+} from '@kbn/test/src/functional_test_runner/lib';
+import { ProcRunner } from '@kbn/dev-proc-runner';
 
 import { defineCypressConfig } from '@kbn/cypress-config';
+import { ToolingLog } from '@kbn/tooling-log';
+import getPort from 'get-port';
+import { getLocalhostRealIp } from '../scripts/endpoint/common/localhost_services';
 
 // eslint-disable-next-line import/no-default-export
 export default defineCypressConfig({
@@ -26,5 +53,236 @@ export default defineCypressConfig({
     baseUrl: 'http://localhost:5601',
     experimentalMemoryManagement: true,
     specPattern: './cypress/e2e/**/*.cy.ts',
+    setupNodeEvents(on, globalConfig) {
+      const processes = [];
+      on('before:spec', (...arguments) => {
+        console.log('details', arguments);
+        require('@kbn/babel-register').install();
+        return Promise.resolve().then(async () => {
+          const abortCtrl = new AbortController();
+
+          const argv = {
+            ftrConfigFile: '../../../../../../x-pack/test/security_solution_cypress/cli_config',
+          };
+
+          const log = new ToolingLog({
+            level: 'info',
+            writeTo: process.stdout,
+          });
+
+          const onEarlyExit = (msg: string) => {
+            log.error(msg);
+            abortCtrl.abort();
+          };
+
+          const parseTestFileConfig = (
+            filePath: string
+          ): Record<string, string | number | Record<string, string | number>> | undefined => {
+            const testFile = fs.readFileSync(filePath, { encoding: 'utf8' });
+
+            const ast = parser.parse(testFile, {
+              sourceType: 'module',
+              plugins: ['typescript'],
+            });
+
+            const expressionStatement = _.find(ast.program.body, [
+              'type',
+              'ExpressionStatement',
+            ]) as ExpressionStatement | undefined;
+
+            const callExpression = expressionStatement?.expression;
+            // @ts-expect-error
+            if (expressionStatement?.expression?.arguments?.length === 3) {
+              // @ts-expect-error
+              const callExpressionArguments = _.find(callExpression?.arguments, [
+                'type',
+                'ObjectExpression',
+              ]) as ObjectExpression | undefined;
+
+              const callExpressionProperties = _.find(callExpressionArguments?.properties, [
+                'key.name',
+                'env',
+              ]) as ObjectProperty[] | undefined;
+              // @ts-expect-error
+              const ftrConfig = _.find(callExpressionProperties?.value?.properties, [
+                'key.name',
+                'ftrConfig',
+              ]);
+
+              if (!ftrConfig) {
+                return {};
+              }
+
+              return _.reduce(
+                ftrConfig.value.properties,
+                (acc: Record<string, string | number | Record<string, string>>, property) => {
+                  const key = (property.key as Identifier).name;
+                  let value;
+                  if (property.value.type === 'ArrayExpression') {
+                    value = _.map(property.value.elements, (element) => {
+                      if (element.type === 'StringLiteral') {
+                        return element.value as string;
+                      }
+                      return element.value as string;
+                    });
+                  }
+                  if (key && value) {
+                    // @ts-expect-error
+                    acc[key] = value;
+                  }
+                  return acc;
+                },
+                {}
+              );
+            }
+            return undefined;
+          };
+
+          const esPort: number = await getPort({
+            port: getPort.makeRange(9201, 9299),
+          });
+          const kibanaPort: number = await getPort({
+            port: getPort.makeRange(5602, 5699),
+          });
+          const fleetServerPort: number = await getPort({
+            port: getPort.makeRange(8081, 8099),
+          });
+
+          const configFromTestFile = undefined; // parseTestFileConfig(filePath);
+
+          const config = await readConfigFile(
+            log,
+            EsVersion.getDefault(),
+            _.isArray(argv.ftrConfigFile) ? _.last(argv.ftrConfigFile) : argv.ftrConfigFile,
+            {
+              servers: {
+                elasticsearch: {
+                  port: esPort,
+                },
+                kibana: {
+                  port: kibanaPort,
+                },
+                // fleetserver: {
+                //   port: fleetServerPort,
+                // },
+              },
+              kbnTestServer: {
+                serverArgs: [
+                  `--server.port=${kibanaPort}`,
+                  `--elasticsearch.hosts=http://localhost:${esPort}`,
+                ],
+              },
+            },
+            (vars) => {
+              const hasFleetServerArgs = _.some(
+                vars.kbnTestServer.serverArgs,
+                (value) =>
+                  value.includes('--xpack.fleet.agents.fleet_server.hosts') ||
+                  value.includes('--xpack.fleet.agents.elasticsearch.host')
+              );
+
+              vars.kbnTestServer.serverArgs = _.filter(
+                vars.kbnTestServer.serverArgs,
+                (value) =>
+                  !(
+                    value.includes('--elasticsearch.hosts=http://localhost:9220') ||
+                    value.includes('--xpack.fleet.agents.fleet_server.hosts') ||
+                    value.includes('--xpack.fleet.agents.elasticsearch.host')
+                  )
+              );
+
+              if (
+                // @ts-expect-error
+                configFromTestFile?.enableExperimental?.length &&
+                _.some(vars.kbnTestServer.serverArgs, (value) =>
+                  value.includes('--xpack.securitySolution.enableExperimental')
+                )
+              ) {
+                vars.kbnTestServer.serverArgs = _.filter(
+                  vars.kbnTestServer.serverArgs,
+                  (value) => !value.includes('--xpack.securitySolution.enableExperimental')
+                );
+                vars.kbnTestServer.serverArgs.push(
+                  `--xpack.securitySolution.enableExperimental=${JSON.stringify(
+                    configFromTestFile?.enableExperimental
+                  )}`
+                );
+              }
+              const hostRealIp = getLocalhostRealIp();
+
+              if (hasFleetServerArgs) {
+                vars.kbnTestServer.serverArgs.push(
+                  `--xpack.fleet.agents.fleet_server.hosts=["https://${hostRealIp}:${fleetServerPort}"]`,
+                  `--xpack.fleet.agents.elasticsearch.host=http://${hostRealIp}:${esPort}`
+                );
+              }
+
+              return vars;
+            }
+          );
+
+          const lifecycle = new Lifecycle(log);
+
+          const providers = new ProviderCollection(log, [
+            ...readProviderSpec('Service', {
+              lifecycle: () => lifecycle,
+              log: () => log,
+              config: () => config,
+            }),
+            ...readProviderSpec('Service', config.get('services')),
+          ]);
+
+          const options = {
+            installDir: process.env.KIBANA_INSTALL_DIR,
+          };
+
+          await runElasticsearch({
+            config,
+            log,
+            name: `ftr-${esPort}`,
+            esFrom: 'snapshot',
+            onEarlyExit,
+          });
+
+          const procs = new ProcRunner(log);
+
+          await runKibanaServer({
+            procs,
+            config,
+            installDir: options?.installDir,
+            extraKbnOpts: options?.installDir
+              ? []
+              : ['--dev', '--no-dev-config', '--no-dev-credentials'],
+            onEarlyExit,
+          });
+
+          await providers.loadAll();
+
+          const functionalTestRunner = new FunctionalTestRunner(
+            log,
+            config,
+            EsVersion.getDefault()
+          );
+
+          const customEnv = await functionalTestRunner.run(abortCtrl.signal);
+
+          pro;
+          return deepmerge(globalConfig, {
+            e2e: {
+              baseUrl: `http://localhost:${kibanaPort}`,
+            },
+            env: {
+              ...customEnv,
+              ftrConfig: {
+                kibanaPort,
+                esPort,
+                fleetServerPort,
+              },
+            },
+          });
+        });
+        // },
+      });
+    },
   },
 });
--- x-pack/plugins/security_solution/scripts/run_cypress/parallel.ts
+++ x-pack/plugins/security_solution/scripts/run_cypress/parallel.ts
@@ -8,37 +8,14 @@
 import { run } from '@kbn/dev-cli-runner';
 import yargs from 'yargs';
 import _ from 'lodash';
-import * as fs from 'fs';
 import globby from 'globby';
 import pMap from 'p-map';
 import { ToolingLog } from '@kbn/tooling-log';
 import { withProcRunner } from '@kbn/dev-proc-runner';
 import cypress from 'cypress';
 
-import {
-  EsVersion,
-  FunctionalTestRunner,
-  readConfigFile,
-  runElasticsearch,
-  runKibanaServer,
-} from '@kbn/test';
-
-import {
-  Lifecycle,
-  ProviderCollection,
-  readProviderSpec,
-} from '@kbn/test/src/functional_test_runner/lib';
-import * as parser from '@babel/parser';
-import type {
-  ExpressionStatement,
-  Identifier,
-  ObjectExpression,
-  ObjectProperty,
-} from '@babel/types';
-
 import { createFailError } from '@kbn/dev-cli-errors';
 import { renderSummaryTable } from './print_run';
-import { getLocalhostRealIp } from '../endpoint/common/localhost_services';
 
 const retrieveIntegrations = (
   specPattern: string[],
@@ -83,265 +60,29 @@ export const cli = () => {
         throw new Error('No files found');
       }
 
-      const esPorts: number[] = [9200, 9220];
-      const kibanaPorts: number[] = [5601, 5620];
-      const fleetServerPorts: number[] = [8220];
-
-      const getEsPort = <T>(): T | number => {
-        const esPort = parseInt(`92${Math.floor(Math.random() * 89) + 10}`, 10);
-        if (esPorts.includes(esPort)) {
-          return getEsPort();
-        }
-        esPorts.push(esPort);
-        return esPort;
-      };
-
-      const getKibanaPort = <T>(): T | number => {
-        const kibanaPort = parseInt(`56${Math.floor(Math.random() * 89) + 10}`, 10);
-        if (kibanaPorts.includes(kibanaPort)) {
-          return getKibanaPort();
-        }
-        kibanaPorts.push(kibanaPort);
-        return kibanaPort;
-      };
-
-      const getFleetServerPort = <T>(): T | number => {
-        const fleetServerPort = parseInt(`82${Math.floor(Math.random() * 89) + 10}`, 10);
-        if (fleetServerPorts.includes(fleetServerPort)) {
-          return getFleetServerPort();
-        }
-        fleetServerPorts.push(fleetServerPort);
-        return fleetServerPort;
-      };
-
-      const cleanupServerPorts = ({
-        esPort,
-        kibanaPort,
-        fleetServerPort,
-      }: {
-        esPort: number;
-        kibanaPort: number;
-        fleetServerPort: number;
-      }) => {
-        _.pull(esPorts, esPort);
-        _.pull(kibanaPorts, kibanaPort);
-        _.pull(fleetServerPorts, fleetServerPort);
-      };
-
-      const parseTestFileConfig = (
-        filePath: string
-      ): Record<string, string | number | Record<string, string | number>> | undefined => {
-        const testFile = fs.readFileSync(filePath, { encoding: 'utf8' });
-
-        const ast = parser.parse(testFile, {
-          sourceType: 'module',
-          plugins: ['typescript'],
-        });
-
-        const expressionStatement = _.find(ast.program.body, ['type', 'ExpressionStatement']) as
-          | ExpressionStatement
-          | undefined;
-
-        const callExpression = expressionStatement?.expression;
-        // @ts-expect-error
-        if (expressionStatement?.expression?.arguments?.length === 3) {
-          // @ts-expect-error
-          const callExpressionArguments = _.find(callExpression?.arguments, [
-            'type',
-            'ObjectExpression',
-          ]) as ObjectExpression | undefined;
-
-          const callExpressionProperties = _.find(callExpressionArguments?.properties, [
-            'key.name',
-            'env',
-          ]) as ObjectProperty[] | undefined;
-          // @ts-expect-error
-          const ftrConfig = _.find(callExpressionProperties?.value?.properties, [
-            'key.name',
-            'ftrConfig',
-          ]);
-
-          if (!ftrConfig) {
-            return {};
-          }
-
-          return _.reduce(
-            ftrConfig.value.properties,
-            (acc: Record<string, string | number | Record<string, string>>, property) => {
-              const key = (property.key as Identifier).name;
-              let value;
-              if (property.value.type === 'ArrayExpression') {
-                value = _.map(property.value.elements, (element) => {
-                  if (element.type === 'StringLiteral') {
-                    return element.value as string;
-                  }
-                  return element.value as string;
-                });
-              }
-              if (key && value) {
-                // @ts-expect-error
-                acc[key] = value;
-              }
-              return acc;
-            },
-            {}
-          );
-        }
-        return undefined;
-      };
-
       const log = new ToolingLog({
         level: 'info',
         writeTo: process.stdout,
       });
 
-      const hostRealIp = getLocalhostRealIp();
-
       const isOpen = argv._[0] === 'open';
 
       await pMap(
-        files,
+        [files[0]],
         async (filePath) => {
           let result:
             | CypressCommandLine.CypressRunResult
             | CypressCommandLine.CypressFailedRunResult
             | undefined;
           await withProcRunner(log, async (procs) => {
-            const abortCtrl = new AbortController();
-
-            const onEarlyExit = (msg: string) => {
-              log.error(msg);
-              abortCtrl.abort();
-            };
-
-            const esPort: number = getEsPort();
-            const kibanaPort: number = getKibanaPort();
-            const fleetServerPort: number = getFleetServerPort();
-            const configFromTestFile = parseTestFileConfig(filePath);
-
-            const config = await readConfigFile(
-              log,
-              EsVersion.getDefault(),
-              _.isArray(argv.ftrConfigFile) ? _.last(argv.ftrConfigFile) : argv.ftrConfigFile,
-              {
-                servers: {
-                  elasticsearch: {
-                    port: esPort,
-                  },
-                  kibana: {
-                    port: kibanaPort,
-                  },
-                  // fleetserver: {
-                  //   port: fleetServerPort,
-                  // },
-                },
-                kbnTestServer: {
-                  serverArgs: [
-                    `--server.port=${kibanaPort}`,
-                    `--elasticsearch.hosts=http://localhost:${esPort}`,
-                  ],
-                },
-              },
-              (vars) => {
-                const hasFleetServerArgs = _.some(
-                  vars.kbnTestServer.serverArgs,
-                  (value) =>
-                    value.includes('--xpack.fleet.agents.fleet_server.hosts') ||
-                    value.includes('--xpack.fleet.agents.elasticsearch.host')
-                );
-
-                vars.kbnTestServer.serverArgs = _.filter(
-                  vars.kbnTestServer.serverArgs,
-                  (value) =>
-                    !(
-                      value.includes('--elasticsearch.hosts=http://localhost:9220') ||
-                      value.includes('--xpack.fleet.agents.fleet_server.hosts') ||
-                      value.includes('--xpack.fleet.agents.elasticsearch.host')
-                    )
-                );
-
-                if (
-                  // @ts-expect-error
-                  configFromTestFile?.enableExperimental?.length &&
-                  _.some(vars.kbnTestServer.serverArgs, (value) =>
-                    value.includes('--xpack.securitySolution.enableExperimental')
-                  )
-                ) {
-                  vars.kbnTestServer.serverArgs = _.filter(
-                    vars.kbnTestServer.serverArgs,
-                    (value) => !value.includes('--xpack.securitySolution.enableExperimental')
-                  );
-                  vars.kbnTestServer.serverArgs.push(
-                    `--xpack.securitySolution.enableExperimental=${JSON.stringify(
-                      configFromTestFile?.enableExperimental
-                    )}`
-                  );
-                }
-
-                if (hasFleetServerArgs) {
-                  vars.kbnTestServer.serverArgs.push(
-                    `--xpack.fleet.agents.fleet_server.hosts=["https://${hostRealIp}:${fleetServerPort}"]`,
-                    `--xpack.fleet.agents.elasticsearch.host=http://${hostRealIp}:${esPort}`
-                  );
-                }
-
-                return vars;
-              }
-            );
-
-            const lifecycle = new Lifecycle(log);
-
-            const providers = new ProviderCollection(log, [
-              ...readProviderSpec('Service', {
-                lifecycle: () => lifecycle,
-                log: () => log,
-                config: () => config,
-              }),
-              ...readProviderSpec('Service', config.get('services')),
-            ]);
-
-            const options = {
-              installDir: process.env.KIBANA_INSTALL_DIR,
-            };
-
-            const shutdownEs = await runElasticsearch({
-              config,
-              log,
-              name: `ftr-${esPort}`,
-              esFrom: 'snapshot',
-              onEarlyExit,
-            });
-
-            await runKibanaServer({
-              procs,
-              config,
-              installDir: options?.installDir,
-              extraKbnOpts: options?.installDir
-                ? []
-                : ['--dev', '--no-dev-config', '--no-dev-credentials'],
-              onEarlyExit,
-            });
-
-            await providers.loadAll();
-
-            const functionalTestRunner = new FunctionalTestRunner(
-              log,
-              config,
-              EsVersion.getDefault()
-            );
-
-            const customEnv = await functionalTestRunner.run(abortCtrl.signal);
-
             if (isOpen) {
               await cypress.open({
                 configFile: require.resolve(`../../${argv.configFile}`),
                 config: {
                   e2e: {
-                    baseUrl: `http://localhost:${kibanaPort}`,
+                    baseUrl: `http://google.com`,
                   },
-                  env: customEnv,
                 },
-                // ...commonCypressConfig,
               });
             } else {
               try {
@@ -353,10 +94,9 @@ export const cli = () => {
                   reporterOptions: argv.reporterOptions,
                   config: {
                     e2e: {
-                      baseUrl: `http://localhost:${kibanaPort}`,
+                      baseUrl: 'http://google.com',
                     },
                     numTestsKeptInMemory: 0,
-                    env: customEnv,
                   },
                 });
               } catch (error) {
@@ -364,55 +104,11 @@ export const cli = () => {
               }
             }
 
-            await procs.stop('kibana');
-            shutdownEs();
-            cleanupServerPorts({ esPort, kibanaPort, fleetServerPort });
-
-            // return pRetry(
-            //   () =>
-            //     cypress
-            //       .run({
-            //         browser: 'chrome',
-            //         spec: filePath,
-            //         headed: true,
-            //         configFile: argv.configFile,
-            //         config: {
-            //           env: customEnv,
-            //           baseUrl: `http://localhost:${kibanaPort}`,
-            //         },
-            //       })
-            //       .then((results) => {
-            //         if (results.status === 'finished') {
-            //           _.forEach(results.runs, (run) => {
-            //             _.forEach(run.tests, (test) => {
-            //               _.forEach(test.attempts, (attempt) => {
-            //                 if (
-            //                   attempt.state === 'failed' &&
-            //                   attempt.error &&
-            //                   attempt.error.name !== 'AssertionError'
-            //                 ) {
-            //                   throw new Error(
-            //                     `Non AssertionError in ${filePath}, retrying test. Error message: ${attempt.error.message}`
-            //                   );
-            //                 }
-            //               });
-            //             });
-            //           });
-            //         }
-            //         return results;
-            //       }),
-            //   {
-            //     retries: 1,
-            //   }
-            // ).finally(() => {
-            //   cleanupServerPorts({ esPort, kibanaPort });
-            // });
-
             return result;
           });
           return result;
         },
-        { concurrency: !isOpen ? 3 : 1 }
+        { concurrency: !isOpen ? 1 : 1 }
       ).then((results) => {
         renderSummaryTable(results as CypressCommandLine.CypressRunResult[]);
         const hasFailedTests = _.some(

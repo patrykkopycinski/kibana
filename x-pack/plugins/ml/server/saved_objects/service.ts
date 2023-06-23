patrykkopycinski/kibana

commit 9a050573641935386ba832f27f84a574fdf6478d

[ML] ML client and shared services optimizations (#146155)
Closes https://github.com/elastic/kibana/issues/144897

Improvements to the way we are using the saved objects client when
interacting loading ML saved objects:

memoizes the calls to the saved object client's find method, based on
request for the duration of the request. some calls make multiple
requests for the full ml saved object list, and each one checks
privileges, so by caching the list we reduce the total number of calls
to `_has_privileges` triggered by saved object client interaction.

Improvements to the way we are resolving the capabilities per request:

Memoizes calls to `resolveCapabilities` for the duration of the request.
Our shared function providers receive the request object and can share
out multiple functions, each using the same request object. This allows
us to cache the capabilities check used for each function. e.g.
`getAnomalyDetectorsProvider` shares `jobs`, `jobStats`, `datafeeds` an
`datafeedStats`, if a plugin calls all of these in the same request,
only one call to `resolveCapabilities` will be made.
This is what the APM plugin was originally doing, calling `jobs`,
`jobStats` and `datafeedStats` however this has also been changed to a
single ML shared function `getJobsState` which provides the minimum job
state information.


Before:
31 calls

![image](https://user-images.githubusercontent.com/22172091/203587122-8ffed69a-daa3-4e47-839d-d8d1d43d5473.png)

After:
13 calls

![Untitled33](https://user-images.githubusercontent.com/22172091/203773476-213f4cab-33d6-4911-a9f0-3edc73b51b6d.jpg)

The red highlighted section above are all calls that take place inside
the `capabilities` plugin's `resolveCapabilities` method.
These are caused by the various switchers registered by plugins which
are called to build up the capabilities list.
We need these capabilities to ensure the user triggering the calls to

x-pack/plugins/ml/server/saved_objects/service.ts
@@ -6,6 +6,7 @@
  */
 
 import RE2 from 're2';
+import { memoize } from 'lodash';
 import {
   KibanaRequest,
   SavedObjectsClientContract,
@@ -57,6 +58,11 @@ export function mlSavedObjectServiceFactory(
   client: IScopedClusterClient,
   isMlReady: () => Promise<void>
 ) {
+  const _savedObjectsClientFindMemo = memoize(
+    async <T>(options: SavedObjectsFindOptions) => savedObjectsClient.find<T>(options),
+    (options: SavedObjectsFindOptions) => JSON.stringify(options)
+  );
+
   async function _getJobObjects(
     jobType?: JobType,
     jobId?: string,
@@ -87,8 +93,7 @@ export function mlSavedObjectServiceFactory(
       filter,
     };
 
-    const jobs = await savedObjectsClient.find<JobObject>(options);
-
+    const jobs = await _savedObjectsClientFindMemo<JobObject>(options);
     return jobs.saved_objects;
   }
 
@@ -231,7 +236,8 @@ export function mlSavedObjectServiceFactory(
       filter,
     };
 
-    return (await internalSavedObjectsClient.find<JobObject>(options)).saved_objects;
+    const jobs = await _savedObjectsClientFindMemo<JobObject>(options);
+    return jobs.saved_objects;
   }
 
   async function addDatafeed(datafeedId: string, jobId: string) {
@@ -462,8 +468,7 @@ export function mlSavedObjectServiceFactory(
       filter,
     };
 
-    const models = await savedObjectsClient.find<TrainedModelObject>(options);
-
+    const models = await _savedObjectsClientFindMemo<TrainedModelObject>(options);
     return models.saved_objects;
   }
 
@@ -677,7 +682,7 @@ export function mlSavedObjectServiceFactory(
         searchFields,
         filter,
       };
-      return savedObjectsClient.find<TrainedModelObject>(options);
+      return _savedObjectsClientFindMemo<TrainedModelObject>(options);
     });
 
     const finedResult = await Promise.all(searches);

x-pack/plugins/apm/server/lib/anomaly_detection/get_ml_jobs_with_apm_group.ts
@@ -5,7 +5,8 @@
  * 2.0.
  */
 
-import { MlPluginSetup } from '@kbn/ml-plugin/server';
+import { DATAFEED_STATE, JOB_STATE } from '@kbn/ml-plugin/common';
+import { MlAnomalyDetectors } from '@kbn/ml-plugin/server';
 import { ApmMlJob } from '../../../common/anomaly_detection/apm_ml_job';
 import { Environment } from '../../../common/environment_rt';
 import { withApmSpan } from '../../utils/with_apm_span';
@@ -23,11 +24,11 @@ function catch404(e: any) {
 }
 
 export function getMlJobsWithAPMGroup(
-  anomalyDetectors: ReturnType<MlPluginSetup['anomalyDetectorsProvider']>
+  anomalyDetectors: MlAnomalyDetectors
 ): Promise<ApmMlJob[]> {
   return withApmSpan('get_ml_jobs_with_apm_group', async () => {
     try {
-      const [jobs, allJobStats, allDatafeedStats] = await Promise.all([
+      const [jobs, jobStats, datafeedStats] = await Promise.all([
         anomalyDetectors
           .jobs(APM_ML_JOB_GROUP)
           .then((response) => response.jobs),
@@ -41,26 +42,27 @@ export function getMlJobsWithAPMGroup(
           .catch(catch404),
       ]);
 
+      const datafeedStateMap = Object.fromEntries(
+        datafeedStats.map((d) => [d.datafeed_id, d.state as DATAFEED_STATE])
+      );
+
+      const jobStateMap = Object.fromEntries(
+        jobStats.map((j) => [j.job_id, j.state as JOB_STATE])
+      );
+
       return jobs.map((job): ApmMlJob => {
-        const jobStats = allJobStats.find(
-          (stats) => stats.job_id === job.job_id
-        );
-
-        const datafeedStats = allDatafeedStats.find(
-          (stats) => stats.datafeed_id === job.datafeed_config?.datafeed_id
-        );
-
+        const jobId = job.job_id;
+        const datafeedId = job.datafeed_config?.datafeed_id;
         return {
+          jobId,
+          jobState: jobStateMap[jobId],
+          datafeedId,
+          datafeedState: datafeedId ? datafeedStateMap[datafeedId] : undefined,
+          version: Number(job?.custom_settings?.job_tags?.apm_ml_version ?? 1),
           environment: String(
-            job.custom_settings?.job_tags?.environment
+            job?.custom_settings?.job_tags?.environment
           ) as Environment,
-          jobId: job.job_id,
-          jobState: jobStats?.state as ApmMlJob['jobState'],
-          version: Number(job.custom_settings?.job_tags?.apm_ml_version ?? 1),
-          datafeedId: datafeedStats?.datafeed_id,
-          datafeedState: datafeedStats?.state as ApmMlJob['datafeedState'],
-          // @ts-expect-error bucket_span is of type `estypes.Duration`
-          bucketSpan: job.analysis_config?.bucket_span,
+          bucketSpan: job?.analysis_config.bucket_span as string,
         };
       });
     } catch (e) {

x-pack/plugins/apm/server/lib/helpers/get_ml_client.ts
@@ -5,13 +5,18 @@
  * 2.0.
  */
 
+import {
+  MlAnomalyDetectors,
+  MlMlSystem,
+  MlModules,
+} from '@kbn/ml-plugin/server';
 import { isActivePlatinumLicense } from '../../../common/license_check';
 import { APMRouteHandlerResources } from '../../routes/typings';
 
 export interface MlClient {
-  mlSystem: any;
-  anomalyDetectors: any;
-  modules: any;
+  mlSystem: MlMlSystem;
+  anomalyDetectors: MlAnomalyDetectors;
+  modules: MlModules;
 }
 
 export async function getMlClient({

x-pack/plugins/apm/server/routes/service_map/get_service_anomalies.ts
@@ -9,7 +9,7 @@ import Boom from '@hapi/boom';
 import { sortBy, uniqBy } from 'lodash';
 import type * as estypes from '@elastic/elasticsearch/lib/api/typesWithBodyKey';
 import type { ESSearchResponse } from '@kbn/es-types';
-import { MlPluginSetup } from '@kbn/ml-plugin/server';
+import type { MlAnomalyDetectors } from '@kbn/ml-plugin/server';
 import { rangeQuery } from '@kbn/observability-plugin/server';
 import { getSeverity, ML_ERRORS } from '../../../common/anomaly_detection';
 import { ENVIRONMENT_ALL } from '../../../common/environment_filter_values';
@@ -154,7 +154,7 @@ export async function getServiceAnomalies({
 }
 
 export async function getMLJobs(
-  anomalyDetectors: ReturnType<MlPluginSetup['anomalyDetectorsProvider']>,
+  anomalyDetectors: MlAnomalyDetectors,
   environment?: string
 ) {
   const jobs = await getMlJobsWithAPMGroup(anomalyDetectors);
@@ -173,7 +173,7 @@ export async function getMLJobs(
 }
 
 export async function getMLJobIds(
-  anomalyDetectors: ReturnType<MlPluginSetup['anomalyDetectorsProvider']>,
+  anomalyDetectors: MlAnomalyDetectors,
   environment?: string
 ) {
   const mlJobs = await getMLJobs(anomalyDetectors, environment);

x-pack/plugins/ml/server/lib/capabilities/check_capabilities.ts
@@ -6,6 +6,7 @@
  */
 
 import { KibanaRequest } from '@kbn/core/server';
+import { once } from 'lodash';
 import type { MlClient } from '../ml_client';
 import { mlLog } from '../log';
 import {
@@ -60,24 +61,28 @@ function disableAdminPrivileges(capabilities: MlCapabilities) {
 
 export type HasMlCapabilities = (capabilities: MlCapabilitiesKey[]) => Promise<void>;
 
-export function hasMlCapabilitiesProvider(resolveMlCapabilities: ResolveMlCapabilities) {
-  return (request: KibanaRequest): HasMlCapabilities => {
-    let mlCapabilities: MlCapabilities | null = null;
-    return async (capabilities: MlCapabilitiesKey[]) => {
-      try {
-        mlCapabilities = await resolveMlCapabilities(request);
-      } catch (e) {
-        mlLog.error(e);
-        throw new UnknownMLCapabilitiesError(`Unable to perform ML capabilities check ${e}`);
-      }
+export function hasMlCapabilitiesProvider(
+  resolveMlCapabilities: ResolveMlCapabilities,
+  request: KibanaRequest
+) {
+  let mlCapabilities: MlCapabilities | null = null;
 
-      if (mlCapabilities === null) {
-        throw new MLPrivilegesUninitialized('ML capabilities have not been initialized');
-      }
+  const resolveMlCapabilitiesOnce = once(resolveMlCapabilities);
 
-      if (capabilities.every((c) => mlCapabilities![c] === true) === false) {
-        throw new InsufficientMLCapabilities('Insufficient privileges to access feature');
-      }
-    };
+  return async (capabilities: MlCapabilitiesKey[]) => {
+    try {
+      mlCapabilities = await resolveMlCapabilitiesOnce(request);
+    } catch (e) {
+      mlLog.error(e);
+      throw new UnknownMLCapabilitiesError(`Unable to perform ML capabilities check ${e}`);
+    }
+
+    if (mlCapabilities === null) {
+      throw new MLPrivilegesUninitialized('ML capabilities have not been initialized');
+    }
+
+    if (capabilities.every((c) => mlCapabilities![c] === true) === false) {
+      throw new InsufficientMLCapabilities('Insufficient privileges to access feature');
+    }
   };
 }

x-pack/plugins/ml/server/shared_services/providers/alerting_service.ts
@@ -16,9 +16,10 @@ export function getAlertingServiceProvider(getGuards: GetGuards) {
       savedObjectsClient: SavedObjectsClientContract,
       request: KibanaRequest
     ) {
+      const guards = getGuards(request, savedObjectsClient);
       return {
         preview: async (...args: Parameters<MlAlertingService['preview']>) => {
-          return await getGuards(request, savedObjectsClient)
+          return await guards
             .isFullLicense()
             .hasMlCapabilities(['canGetJobs'])
             .ok(({ mlClient, scopedClient, getFieldsFormatRegistry, getDataViewsService }) =>
@@ -33,7 +34,7 @@ export function getAlertingServiceProvider(getGuards: GetGuards) {
         execute: async (
           ...args: Parameters<MlAlertingService['execute']>
         ): ReturnType<MlAlertingService['execute']> => {
-          return await getGuards(request, savedObjectsClient)
+          return await guards
             .isFullLicense()
             .hasMlCapabilities(['canGetJobs'])
             .ok(({ mlClient, scopedClient, getFieldsFormatRegistry, getDataViewsService }) =>

x-pack/plugins/ml/server/shared_services/providers/anomaly_detectors.ts
@@ -32,9 +32,10 @@ export function getAnomalyDetectorsProvider(getGuards: GetGuards): AnomalyDetect
       request: KibanaRequest,
       savedObjectsClient: SavedObjectsClientContract
     ) {
+      const guards = getGuards(request, savedObjectsClient);
       return {
         async jobs(jobId?: string) {
-          return await getGuards(request, savedObjectsClient)
+          return await guards
             .isFullLicense()
             .hasMlCapabilities(['canGetJobs'])
             .ok(async ({ mlClient }) => {
@@ -45,7 +46,7 @@ export function getAnomalyDetectorsProvider(getGuards: GetGuards): AnomalyDetect
             });
         },
         async jobStats(jobId?: string) {
-          return await getGuards(request, savedObjectsClient)
+          return await guards
             .isFullLicense()
             .hasMlCapabilities(['canGetJobs'])
             .ok(async ({ mlClient }) => {
@@ -56,7 +57,7 @@ export function getAnomalyDetectorsProvider(getGuards: GetGuards): AnomalyDetect
             });
         },
         async datafeeds(datafeedId?: string) {
-          return await getGuards(request, savedObjectsClient)
+          return await guards
             .isFullLicense()
             .hasMlCapabilities(['canGetDatafeeds'])
             .ok(async ({ mlClient }) => {
@@ -67,7 +68,7 @@ export function getAnomalyDetectorsProvider(getGuards: GetGuards): AnomalyDetect
             });
         },
         async datafeedStats(datafeedId?: string) {
-          return await getGuards(request, savedObjectsClient)
+          return await guards
             .isFullLicense()
             .hasMlCapabilities(['canGetDatafeeds'])
             .ok(async ({ mlClient }) => {

x-pack/plugins/ml/server/shared_services/providers/index.ts
@@ -1,0 +1,20 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+export type { MlSystemProvider } from './system';
+export type { JobServiceProvider } from './job_service';
+export type { ModuleSetupPayload, ModulesProvider } from './modules';
+export type { ResultsServiceProvider } from './results_service';
+export type { TrainedModelsProvider } from './trained_models';
+export type { AnomalyDetectorsProvider } from './anomaly_detectors';
+
+export { getMlSystemProvider } from './system';
+export { getJobServiceProvider } from './job_service';
+export { getModulesProvider } from './modules';
+export { getResultsServiceProvider } from './results_service';
+export { getTrainedModelsProvider } from './trained_models';
+export { getAnomalyDetectorsProvider } from './anomaly_detectors';

commit 06a8ec659d3f897dcfbf69070a8d89624daa976b

[ML] Fix saved object sync check for jobs which are hidden from the user (#160266)
Fixes a bug introduced in PR
https://github.com/elastic/kibana/pull/146155
A user who cannot see all spaces will incorrectly be told that jobs
which only exist in spaces they cannot see are in need of
synchronisation.
The problem was caused by an accident replacement of the
`internalSavedObjectsClient` function (which can see all spaces) with
the cached saved objects client which can only see the user's allowed
spaces.
The fix is to revert to the original code.

This particular scenario was not covered by API tests. The tests have
also been updated in this PR.


- [x] [Unit or functional
tests](https://www.elastic.co/guide/en/kibana/master/development-tests.html)
were updated or added to match the most common scenarios

x-pack/plugins/ml/server/saved_objects/service.ts
@@ -248,8 +248,7 @@ export function mlSavedObjectServiceFactory(
       filter,
     };
 
-    const jobs = await _savedObjectsClientFindMemo<JobObject>(options);
-    return jobs.saved_objects;
+    return (await internalSavedObjectsClient.find<JobObject>(options)).saved_objects;
   }
 
   async function addDatafeed(datafeedId: string, jobId: string) {

x-pack/test/api_integration/apis/ml/jobs/reset.ts
@@ -84,6 +84,7 @@ export default ({ getService }: FtrProviderContext) => {
         await ml.api.deleteAnomalyDetectionJobES(job.job_id);
       }
       await ml.api.cleanMlIndices();
+      await ml.testResources.cleanMLSavedObjects();
     });
 
     it('succeeds for ML Poweruser and keeps user annotations', async () => {

x-pack/test/api_integration/apis/ml/saved_objects/sync_jobs.ts
@@ -22,10 +22,16 @@ export default ({ getService }: FtrProviderContext) => {
   const adJobId3 = 'fq_single_3';
   const adJobIdES = 'fq_single_es';
   const idSpace1 = 'space1';
+  const idSpace2 = 'space2';
 
-  async function runSyncRequest(user: USER, expectedStatusCode: number) {
+  async function runSyncRequest(
+    user: USER,
+    expectedStatusCode: number,
+    simulate: boolean = false,
+    space: string = idSpace1
+  ) {
     const { body, status } = await supertest
-      .get(`/s/${idSpace1}/api/ml/saved_objects/sync`)
+      .get(`/s/${space}/api/ml/saved_objects/sync?simulate=${simulate}`)
       .auth(user, ml.securityCommon.getPasswordForUser(user))
       .set(getCommonRequestHeader('2023-10-31'));
     ml.api.assertResponseStatusCode(expectedStatusCode, status, body);
@@ -36,10 +42,11 @@ export default ({ getService }: FtrProviderContext) => {
   async function runSyncCheckRequest(
     user: USER,
     mlSavedObjectType: MlSavedObjectType,
-    expectedStatusCode: number
+    expectedStatusCode: number,
+    space: string = idSpace1
   ) {
     const { body, status } = await supertest
-      .post(`/s/${idSpace1}/internal/ml/saved_objects/sync_check`)
+      .post(`/s/${space}/internal/ml/saved_objects/sync_check`)
       .auth(user, ml.securityCommon.getPasswordForUser(user))
       .set(getCommonRequestHeader('1'))
       .send({ mlSavedObjectType });
@@ -52,11 +59,13 @@ export default ({ getService }: FtrProviderContext) => {
     beforeEach(async () => {
       await ml.api.initSavedObjects();
       await spacesService.create({ id: idSpace1, name: 'space_one', disabledFeatures: [] });
+      await spacesService.create({ id: idSpace2, name: 'space_two', disabledFeatures: [] });
       await ml.testResources.setKibanaTimeZoneToUTC();
     });
 
     afterEach(async () => {
       await spacesService.delete(idSpace1);
+      await spacesService.delete(idSpace2);
       await ml.api.cleanMlIndices();
       await ml.testResources.cleanMLSavedObjects();
     });
@@ -128,6 +137,122 @@ export default ({ getService }: FtrProviderContext) => {
         savedObjectsDeleted: {
           'anomaly-detector': { [adJobId1]: { success: true } },
         },
+      });
+    });
+
+    it('should simulate syncing saved objects', async () => {
+      // check to see if a sync is needed
+      const syncNeeded = await runSyncCheckRequest(
+        USER.ML_POWERUSER_ALL_SPACES,
+        'anomaly-detector',
+        200
+      );
+      expect(syncNeeded.result).to.eql(false, 'sync should not be needed');
+
+      // prepare test data
+      await ml.api.createAnomalyDetectionJobES(
+        ml.commonConfig.getADFqSingleMetricJobConfig(adJobId1)
+      );
+      await ml.api.createDatafeedES(ml.commonConfig.getADFqDatafeedConfig(adJobId1));
+
+      // check to see if a sync is needed
+      const syncNeeded2 = await runSyncCheckRequest(
+        USER.ML_POWERUSER_ALL_SPACES,
+        'anomaly-detector',
+        200
+      );
+      expect(syncNeeded2.result).to.eql(true, 'sync should be needed');
+
+      // run the sync request and verify the response
+      const body = await runSyncRequest(USER.ML_POWERUSER_ALL_SPACES, 200, true);
+
+      expect(body).to.eql({
+        datafeedsAdded: {},
+        datafeedsRemoved: {},
+        savedObjectsCreated: {
+          'anomaly-detector': {
+            [adJobId1]: { success: true },
+          },
+        },
+        savedObjectsDeleted: {},
+      });
+
+      const syncNeeded3 = await runSyncCheckRequest(
+        USER.ML_POWERUSER_ALL_SPACES,
+        'anomaly-detector',
+        200
+      );
+      expect(syncNeeded3.result).to.eql(true, 'sync should still be needed');
+
+      const body2 = await runSyncRequest(USER.ML_POWERUSER_ALL_SPACES, 200, true);
+
+      expect(body2).to.eql({
+        datafeedsAdded: {},
+        datafeedsRemoved: {},
+        savedObjectsCreated: {
+          'anomaly-detector': {
+            [adJobId1]: { success: true },
+          },
+        },
+        savedObjectsDeleted: {},
+      });
+    });
+
+    // add a job to space 2, then check that it is not synced from space 1
+    it('should not sync saved objects in different space', async () => {
+      // check to see if a sync is needed
+      const syncNeeded = await runSyncCheckRequest(
+        USER.ML_POWERUSER_ALL_SPACES,
+        'anomaly-detector',
+        200
+      );
+      expect(syncNeeded.result).to.eql(false, 'sync should not be needed');
+
+      // prepare test data
+      await ml.api.createAnomalyDetectionJob(
+        ml.commonConfig.getADFqSingleMetricJobConfig(adJobId1),
+        idSpace2
+      );
+      await ml.api.createDatafeed(ml.commonConfig.getADFqDatafeedConfig(adJobId1), idSpace2);
+
+      // check to see if a sync is needed
+      const syncNeeded2 = await runSyncCheckRequest(
+        USER.ML_POWERUSER_ALL_SPACES,
+        'anomaly-detector',
+        200
+      );
+      expect(syncNeeded2.result).to.eql(
+        false,
+        'sync should be needed for a user who can see all spaces'
+      );
+
+      const syncNeeded3 = await runSyncCheckRequest(
+        USER.ML_POWERUSER_SPACE1,
+        'anomaly-detector',
+        200
+      );
+      expect(syncNeeded3.result).to.eql(
+        false,
+        'sync should be needed for a user who can only see space 1'
+      );
+
+      // check sync outputs
+      const body = await runSyncRequest(USER.ML_POWERUSER_ALL_SPACES, 200, true);
+
+      expect(body).to.eql({
+        datafeedsAdded: {},
+        datafeedsRemoved: {},
+        savedObjectsCreated: {},
+        savedObjectsDeleted: {},
+      });
+
+      const body2 = await runSyncRequest(USER.ML_POWERUSER_SPACE1, 200, true);
+
+      expect(body2).to.eql({
+        datafeedsAdded: {},
+        datafeedsRemoved: {},
+        savedObjectsCreated: {},
+        savedObjectsDeleted: {},
       });
     });
 
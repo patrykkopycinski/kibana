f1e7e3781f40411e4a65a37ad4ca5967282e94bd
Pablo Machado <machadoum@gmail.com>
2023-08-16 16:20:25 +0200

    [Security Solutions] Add upselling service to security solutions ESS plugin (#163406)
    ## Summary
    
    * Use Serverless upsell architecture on ESS. But check for the required
    license instead of capabilities.
    * Covert Investigation Guide and Entity Analytics upsell to the new
    architecture.
    * Update upsell registering functions always to clear the state when
    registering a new value. It fits perfectly ESS because the license is
    observable.
    ### Checklist
    
    - [x] Any text added follows [EUI's writing
    guidelines](https://elastic.github.io/eui/#/guidelines/writing), uses
    sentence case text and includes [i18n
    support](https://github.com/elastic/kibana/blob/main/packages/kbn-i18n/README.md)
    - [x] [Unit or functional
    tests](https://www.elastic.co/guide/en/kibana/master/development-tests.html)
    were updated or added to match the most common scenarios
    
    ---------
    
    Co-authored-by: kibanamachine <42973632+kibanamachine@users.noreply.github.com>

--- x-pack/plugins/security_solution_ess/public/upselling/register_upsellings.tsx
+++ x-pack/plugins/security_solution_ess/public/upselling/register_upsellings.tsx
@@ -4,34 +4,31 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
-import { SecurityPageName, AppFeatureKey } from '@kbn/security-solution-plugin/common';
-import type {
-  UpsellingService,
-  PageUpsellings,
-  SectionUpsellings,
-  UpsellingSectionId,
-} from '@kbn/security-solution-plugin/public';
+
+import { SecurityPageName } from '@kbn/security-solution-plugin/common';
+import type { UpsellingService } from '@kbn/security-solution-plugin/public';
 import type {
   MessageUpsellings,
+  PageUpsellings,
+  SectionUpsellings,
   UpsellingMessageId,
+  UpsellingSectionId,
 } from '@kbn/security-solution-plugin/public/common/lib/upsellings/types';
-import React from 'react';
-import { EndpointPolicyProtectionsLazy } from './sections/endpoint_management';
-import type { SecurityProductTypes } from '../../common/config';
-import { getProductAppFeatures } from '../../common/pli/pli_features';
-import { investigationGuideUpselling } from './messages/investigation_guide_upselling';
-import {
-  OsqueryResponseActionsUpsellingSectionLazy,
-  ThreatIntelligencePaywallLazy,
-} from './lazy_upselling';
+import type { ILicense, LicenseType } from '@kbn/licensing-plugin/public';
+import { lazy } from 'react';
+import type React from 'react';
+import { UPGRADE_INVESTIGATION_GUIDE } from './messages/investigation_guide_upselling';
+import type { Services } from '../common/services';
+import { withServicesProvider } from '../common/services';
+const EntityAnalyticsUpsellingLazy = lazy(() => import('./pages/entity_analytics_upselling'));
 
 interface UpsellingsConfig {
-  pli: AppFeatureKey;
+  minimumLicenseRequired: LicenseType;
   component: React.ComponentType;
 }
 
 interface UpsellingsMessageConfig {
-  pli: AppFeatureKey;
+  minimumLicenseRequired: LicenseType;
   message: string;
   id: UpsellingMessageId;
 }
@@ -42,14 +39,13 @@ type UpsellingMessages = UpsellingsMessageConfig[];
 
 export const registerUpsellings = (
   upselling: UpsellingService,
-  productTypes: SecurityProductTypes
+  license: ILicense,
+  services: Services
 ) => {
-  const enabledPLIsSet = new Set(getProductAppFeatures(productTypes));
-
   const upsellingPagesToRegister = upsellingPages.reduce<PageUpsellings>(
-    (pageUpsellings, { pageName, pli, component }) => {
-      if (!enabledPLIsSet.has(pli)) {
-        pageUpsellings[pageName] = component;
+    (pageUpsellings, { pageName, minimumLicenseRequired, component }) => {
+      if (!license.hasAtLeast(minimumLicenseRequired)) {
+        pageUpsellings[pageName] = withServicesProvider(component, services);
       }
       return pageUpsellings;
     },
@@ -57,8 +53,8 @@ export const registerUpsellings = (
   );
 
   const upsellingSectionsToRegister = upsellingSections.reduce<SectionUpsellings>(
-    (sectionUpsellings, { id, pli, component }) => {
-      if (!enabledPLIsSet.has(pli)) {
+    (sectionUpsellings, { id, minimumLicenseRequired, component }) => {
+      if (!license.hasAtLeast(minimumLicenseRequired)) {
         sectionUpsellings[id] = component;
       }
       return sectionUpsellings;
@@ -67,8 +63,8 @@ export const registerUpsellings = (
   );
 
   const upsellingMessagesToRegister = upsellingMessages.reduce<MessageUpsellings>(
-    (messagesUpsellings, { id, pli, message }) => {
-      if (!enabledPLIsSet.has(pli)) {
+    (messagesUpsellings, { id, minimumLicenseRequired, message }) => {
+      if (!license.hasAtLeast(minimumLicenseRequired)) {
         messagesUpsellings[id] = message;
       }
       return messagesUpsellings;
@@ -85,38 +81,22 @@ export const registerUpsellings = (
 export const upsellingPages: UpsellingPages = [
   // It is highly advisable to make use of lazy loaded components to minimize bundle size.
   {
-    pageName: SecurityPageName.threatIntelligence,
-    pli: AppFeatureKey.threatIntelligence,
-    component: () => (
-      <ThreatIntelligencePaywallLazy requiredPLI={AppFeatureKey.threatIntelligence} />
-    ),
+    pageName: SecurityPageName.entityAnalytics,
+    minimumLicenseRequired: 'platinum',
+    component: EntityAnalyticsUpsellingLazy,
   },
 ];
 
 // Upsellings for sections, linked by arbitrary ids
 export const upsellingSections: UpsellingSections = [
   // It is highly advisable to make use of lazy loaded components to minimize bundle size.
-  {
-    id: 'osquery_automated_response_actions',
-    pli: AppFeatureKey.osqueryAutomatedResponseActions,
-    component: () => (
-      <OsqueryResponseActionsUpsellingSectionLazy
-        requiredPLI={AppFeatureKey.osqueryAutomatedResponseActions}
-      />
-    ),
-  },
-  {
-    id: 'endpointPolicyProtections',
-    pli: AppFeatureKey.endpointPolicyProtections,
-    component: EndpointPolicyProtectionsLazy,
-  },
 ];
 
 // Upsellings for sections, linked by arbitrary ids
 export const upsellingMessages: UpsellingMessages = [
   {
     id: 'investigation_guide',
-    pli: AppFeatureKey.investigationGuide,
-    message: investigationGuideUpselling(AppFeatureKey.investigationGuide),
+    minimumLicenseRequired: 'platinum',
+    message: UPGRADE_INVESTIGATION_GUIDE('platinum'),
   },
 ];

cfabc6fce3dc8d59d85c4e833287c4252b717ab5
Sander Philipse <94373878+sphilipse@users.noreply.github.com>
2023-10-04 16:35:27 +0200

    [Search] Add connectors table (#167900)
    ## Summary
    
    This adds a connectors table to the Serverless Search plugin. Actions
    for the table will be added after [this related
    PR](https://github.com/elastic/kibana/pull/167804) has merged.
    <img width="1318" alt="Screenshot 2023-10-03 at 17 24 22"
    src="https://github.com/elastic/kibana/assets/94373878/62383a52-e7fd-4458-8db9-d6e433f3f94c">

--- x-pack/plugins/serverless_search/public/application/hooks/api/use_connectors.tsx
+++ x-pack/plugins/serverless_search/public/application/hooks/api/use_connectors.tsx
@@ -5,18 +5,17 @@
  * 2.0.
  */
 
-import { ConnectorServerSideDefinition } from '@kbn/search-connectors';
+import { Connector } from '@kbn/search-connectors';
 import { useQuery } from '@tanstack/react-query';
 import { useKibanaServices } from '../use_kibana';
 
-export const useConnectorTypes = () => {
+export const useConnectors = (from: number = 0, size: number = 10) => {
   const { http } = useKibanaServices();
-
   return useQuery({
-    queryKey: ['fetchConnectorTypes'],
+    queryKey: ['fetchConnectors', from, size],
     queryFn: () =>
-      http.fetch<{ connectors: ConnectorServerSideDefinition[] }>(
-        '/internal/serverless_search/connector_types'
-      ),
+      http.fetch<{ connectors: Connector[] }>('/internal/serverless_search/connectors', {
+        query: { from, size },
+      }),
   });
 };

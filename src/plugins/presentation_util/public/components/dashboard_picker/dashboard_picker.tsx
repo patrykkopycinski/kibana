patrykkopycinski/kibana

commit 01f4d61d0031687012659e6037d6dc88a05f0244

[Dashboard] Copy panel refactor (#166991)
Redesigns the copy to dashboard modal and the Dashboard picker element to fix some UX issues they were causing.
Makes panels copied with the `copy panel to` dialog retain their original sizes.

src/plugins/presentation_util/public/components/dashboard_picker/dashboard_picker.tsx
@@ -1,0 +1,182 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+import { debounce } from 'lodash';
+import { css } from '@emotion/react';
+import React, { useState, useEffect, useMemo } from 'react';
+
+import {
+  EuiText,
+  useEuiTheme,
+  EuiSelectable,
+  EuiInputPopover,
+  EuiPopoverTitle,
+  EuiFieldSearch,
+  EuiHighlight,
+} from '@elastic/eui';
+import { i18n } from '@kbn/i18n';
+import { ToolbarButton } from '@kbn/kibana-react-plugin/public';
+import { SavedObjectCommon } from '@kbn/saved-objects-finder-plugin/common';
+
+import { pluginServices } from '../../services';
+
+export interface DashboardPickerProps {
+  onChange: (dashboard: { name: string; id: string } | null) => void;
+  isDisabled: boolean;
+  idsToOmit?: string[];
+}
+
+interface DashboardOption {
+  label: string;
+  value: string;
+}
+
+type DashboardHit = SavedObjectCommon<{ title: string }>;
+
+export function DashboardPicker({ isDisabled, onChange, idsToOmit }: DashboardPickerProps) {
+  const { euiTheme } = useEuiTheme();
+
+  const [isLoading, setIsLoading] = useState(true);
+  const [isPopoverOpen, setIsPopoverOpen] = useState(false);
+
+  const [dashboardHits, setDashboardHits] = useState<DashboardHit[]>([]);
+  const [dashboardOptions, setDashboardOptions] = useState<DashboardOption[]>([]);
+
+  const [query, setQuery] = useState('');
+  const [debouncedQuery, setDebouncedQuery] = useState('');
+
+  const [selectedDashboard, setSelectedDashboard] = useState<DashboardOption | null>(null);
+
+  const {
+    contentManagement: { client: cmClient },
+  } = pluginServices.getServices();
+
+  /**
+   * Debounce the query to avoid many calls to content management.
+   */
+  const debouncedSetQuery = useMemo(
+    () => debounce((latestQuery) => setDebouncedQuery(latestQuery), 150),
+    []
+  );
+  useEffect(() => {
+    debouncedSetQuery(query);
+  }, [debouncedSetQuery, query]);
+
+  /**
+   * Run query to search for Dashboards when debounced query changes.
+   */
+  useEffect(() => {
+    let canceled = false;
+
+    (async () => {
+      setIsLoading(true);
+
+      const response = await cmClient.mSearch<DashboardHit>({
+        contentTypes: [{ contentTypeId: 'dashboard' }],
+        query: {
+          text: debouncedQuery ? `${debouncedQuery}*` : undefined,
+          limit: 30,
+        },
+      });
+      if (canceled) return;
+      if (response && response.hits) {
+        setDashboardHits(response.hits);
+      }
+
+      setIsLoading(false);
+    })();
+
+    return () => {
+      canceled = true;
+    };
+  }, [debouncedQuery, cmClient]);
+
+  /**
+   * Format items with dashboard hits and selected option
+   */
+  useEffect(() => {
+    setDashboardOptions(
+      dashboardHits
+        .filter((d) => !d.managed && !(idsToOmit ?? []).includes(d.id))
+        .map((d) => ({
+          value: d.id,
+          label: d.attributes.title,
+          checked: d.id === selectedDashboard?.value ? 'on' : undefined,
+          'data-test-subj': `dashboard-picker-option-${d.attributes.title.replaceAll(' ', '-')}`,
+        }))
+    );
+  }, [dashboardHits, idsToOmit, selectedDashboard]);
+
+  return (
+    <EuiInputPopover
+      panelPaddingSize="none"
+      input={
+        <ToolbarButton
+          fullWidth
+          isLoading={isLoading}
+          data-test-subj="open-dashboard-picker"
+          onClick={() => setIsPopoverOpen(!isPopoverOpen)}
+        >
+          <EuiText
+            size="s"
+            css={css`
+              color: ${selectedDashboard ? euiTheme.colors.text : euiTheme.colors.disabledText};
+            `}
+          >
+            {selectedDashboard?.label ??
+              i18n.translate('presentationUtil.dashboardPicker.noDashboardOptionLabel', {
+                defaultMessage: 'Select dashboard',
+              })}
+          </EuiText>
+        </ToolbarButton>
+      }
+      isOpen={isPopoverOpen}
+      closePopover={() => setIsPopoverOpen(false)}
+    >
+      <EuiPopoverTitle paddingSize="s">
+        <EuiFieldSearch
+          compressed
+          fullWidth
+          onChange={(event) => setQuery(event.target.value)}
+          value={query}
+          data-test-subj="dashboard-picker-search"
+          placeholder={i18n.translate(
+            'presentationUtil.dashboardPicker.searchDashboardPlaceholder',
+            {
+              defaultMessage: 'Search dashboards...',
+            }
+          )}
+        />
+      </EuiPopoverTitle>
+
+      <EuiSelectable
+        singleSelection={true}
+        options={dashboardOptions}
+        onChange={(newOptions, event, selected) => {
+          setIsPopoverOpen(false);
+
+          const nextSelectedDashboard: DashboardOption | null =
+            selected.value === selectedDashboard?.value ? null : selected;
+          setSelectedDashboard(nextSelectedDashboard);
+          onChange(
+            nextSelectedDashboard
+              ? { name: nextSelectedDashboard.label, id: nextSelectedDashboard.value }
+              : null
+          );
+        }}
+        renderOption={(option) => <EuiHighlight search={query}>{option.label}</EuiHighlight>}
+      >
+        {(list) => <div>{list}</div>}
+      </EuiSelectable>
+    </EuiInputPopover>
+  );
+}
+
+// required for dynamic import using React.lazy()
+// eslint-disable-next-line import/no-default-export
+export default DashboardPicker;

src/plugins/presentation_util/public/components/saved_object_save_modal_dashboard.tsx
@@ -16,8 +16,6 @@ import { pluginServices } from '../services';
 import { SaveModalDashboardProps } from './types';
 import { SaveModalDashboardSelector } from './saved_object_save_modal_dashboard_selector';
 
-import './saved_object_save_modal_dashboard.scss';
-
 function SavedObjectSaveModalDashboard(props: SaveModalDashboardProps) {
   const { documentInfo, tagOptions, objectType, onClose, canSaveByReference } = props;
   const { id: documentId } = documentInfo;

src/plugins/presentation_util/public/components/saved_object_save_modal_dashboard_selector.tsx
@@ -22,9 +22,7 @@ import {
   EuiCheckbox,
 } from '@elastic/eui';
 
-import DashboardPicker, { DashboardPickerProps } from './dashboard_picker';
-
-import './saved_object_save_modal_dashboard.scss';
+import DashboardPicker, { DashboardPickerProps } from './dashboard_picker/dashboard_picker';
 
 export interface SaveModalDashboardSelectorProps {
   copyOnSave: boolean;

test/functional/page_objects/time_to_visualize_page.ts
@@ -75,8 +75,11 @@ export class TimeToVisualizePageObject extends FtrService {
       await label.click();
 
       if (dashboardId) {
-        await this.testSubjects.setValue('dashboardPickerInput', dashboardId);
-        await this.find.clickByButtonText(dashboardId);
+        await this.testSubjects.click('open-dashboard-picker');
+        await this.testSubjects.setValue('dashboard-picker-search', dashboardId);
+        await this.testSubjects.click(
+          `dashboard-picker-option-${dashboardId.replaceAll(' ', '-')}`
+        );
       }
     }
 

src/plugins/presentation_util/public/components/dashboard_picker.tsx
@@ -1,105 +1,0 @@
-/*
- * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
- * or more contributor license agreements. Licensed under the Elastic License
- * 2.0 and the Server Side Public License, v 1; you may not use this file except
- * in compliance with, at your election, the Elastic License 2.0 or the Server
- * Side Public License, v 1.
- */
-
-import React, { useState, useEffect } from 'react';
-
-import { i18n } from '@kbn/i18n';
-
-import { EuiComboBox } from '@elastic/eui';
-import { pluginServices } from '../services';
-
-export interface DashboardPickerProps {
-  onChange: (dashboard: { name: string; id: string } | null) => void;
-  isDisabled: boolean;
-  idsToOmit?: string[];
-}
-
-interface DashboardOption {
-  label: string;
-  value: string;
-}
-
-export function DashboardPicker(props: DashboardPickerProps) {
-  const [dashboardOptions, setDashboardOptions] = useState<DashboardOption[]>([]);
-  const [isLoadingDashboards, setIsLoadingDashboards] = useState(true);
-  const [selectedDashboard, setSelectedDashboard] = useState<DashboardOption | null>(null);
-  const [query, setQuery] = useState('');
-
-  const { isDisabled, onChange } = props;
-  const { dashboards } = pluginServices.getHooks();
-  const { findDashboardsByTitle } = dashboards.useService();
-
-  useEffect(() => {
-    // We don't want to manipulate the React state if the component has been unmounted
-    // while we wait for the saved objects to return.
-    let cleanedUp = false;
-
-    const fetchDashboards = async () => {
-      setIsLoadingDashboards(true);
-      setDashboardOptions([]);
-
-      const objects = await findDashboardsByTitle(query ? `${query}*` : '');
-
-      if (cleanedUp) {
-        return;
-      }
-
-      if (objects) {
-        setDashboardOptions(
-          objects
-            .filter((d) => !d.managed && !(props.idsToOmit ?? []).includes(d.id))
-            .map((d) => ({
-              value: d.id,
-              label: d.attributes.title,
-              'data-test-subj': `dashboard-picker-option-${d.attributes.title.replaceAll(
-                ' ',
-                '-'
-              )}`,
-            }))
-        );
-      }
-
-      setIsLoadingDashboards(false);
-    };
-
-    fetchDashboards();
-
-    return () => {
-      cleanedUp = true;
-    };
-  }, [findDashboardsByTitle, query, props.idsToOmit]);
-
-  return (
-    <EuiComboBox
-      data-test-subj="dashboardPickerInput"
-      placeholder={i18n.translate('presentationUtil.dashboardPicker.searchDashboardPlaceholder', {
-        defaultMessage: 'Search dashboards...',
-      })}
-      singleSelection={{ asPlainText: true }}
-      options={dashboardOptions || []}
-      selectedOptions={!!selectedDashboard ? [selectedDashboard] : undefined}
-      onChange={(e) => {
-        if (e.length) {
-          setSelectedDashboard({ value: e[0].value || '', label: e[0].label });
-          onChange({ name: e[0].label, id: e[0].value || '' });
-        } else {
-          setSelectedDashboard(null);
-          onChange(null);
-        }
-      }}
-      onSearchChange={setQuery}
-      isDisabled={isDisabled}
-      isLoading={isLoadingDashboards}
-      compressed={true}
-    />
-  );
-}
-
-// required for dynamic import using React.lazy()
-// eslint-disable-next-line import/no-default-export
-export default DashboardPicker;

src/plugins/dashboard/public/dashboard_actions/_dashboard_actions_strings.ts
@@ -29,10 +29,6 @@ export const dashboardCopyToDashboardActionStrings = {
     i18n.translate('dashboard.panel.copyToDashboard.existingDashboardOptionLabel', {
       defaultMessage: 'Existing dashboard',
     }),
-  getDescription: () =>
-    i18n.translate('dashboard.panel.copyToDashboard.description', {
-      defaultMessage: 'Choose the destination dashboard.',
-    }),
 };
 
 export const dashboardAddToLibraryActionStrings = {

src/plugins/dashboard/public/dashboard_actions/copy_to_dashboard_action.tsx
@@ -8,10 +8,10 @@
 
 import React from 'react';
 
-import { toMountPoint } from '@kbn/kibana-react-plugin/public';
+import { toMountPoint } from '@kbn/react-kibana-mount';
+import { CoreStart } from '@kbn/core-lifecycle-browser';
 import type { IEmbeddable } from '@kbn/embeddable-plugin/public';
 import { Action, IncompatibleActionError } from '@kbn/ui-actions-plugin/public';
-import type { PresentationUtilPluginStart } from '@kbn/presentation-util-plugin/public';
 
 import { pluginServices } from '../services/plugin_services';
 import { CopyToDashboardModal } from './copy_to_dashboard_modal';
@@ -39,16 +39,12 @@ export class CopyToDashboardAction implements Action<CopyToDashboardActionContex
   public order = 1;
 
   private dashboardCapabilities;
-  private theme$;
   private openModal;
 
-  constructor(private PresentationUtilContext: PresentationUtilPluginStart['ContextProvider']) {
+  constructor(private core: CoreStart) {
     ({
       dashboardCapabilities: this.dashboardCapabilities,
       overlays: { openModal: this.openModal },
-      settings: {
-        theme: { theme$: this.theme$ },
-      },
     } = pluginServices.getServices());
   }
 
@@ -81,15 +77,15 @@ export class CopyToDashboardAction implements Action<CopyToDashboardActionContex
       throw new IncompatibleActionError();
     }
 
+    const { theme, i18n } = this.core;
     const session = this.openModal(
       toMountPoint(
         <CopyToDashboardModal
-          PresentationUtilContext={this.PresentationUtilContext}
           closeModal={() => session.close()}
           dashboardId={(embeddable.parent as DashboardContainer).getDashboardSavedObjectId()}
           embeddable={embeddable}
         />,
-        { theme$: this.theme$ }
+        { theme, i18n }
       ),
       {
         maxWidth: 400,

src/plugins/dashboard/public/dashboard_actions/copy_to_dashboard_modal.tsx
@@ -9,21 +9,21 @@ import React, { useCallback, useState } from 'react';
 import { omit } from 'lodash';
 
 import {
-  EuiText,
   EuiRadio,
-  EuiPanel,
   EuiButton,
   EuiSpacer,
   EuiFormRow,
-  EuiFocusTrap,
   EuiModalBody,
   EuiButtonEmpty,
   EuiModalFooter,
   EuiModalHeader,
   EuiModalHeaderTitle,
-  EuiOutsideClickDetector,
 } from '@elastic/eui';
-import { IEmbeddable, PanelNotFoundError } from '@kbn/embeddable-plugin/public';
+import {
+  EmbeddablePackageState,
+  IEmbeddable,
+  PanelNotFoundError,
+} from '@kbn/embeddable-plugin/public';
 import { LazyDashboardPicker, withSuspense } from '@kbn/presentation-util-plugin/public';
 
 import { DashboardPanelState } from '../../common';
@@ -33,7 +33,6 @@ import { dashboardCopyToDashboardActionStrings } from './_dashboard_actions_stri
 import { createDashboardEditUrl, CREATE_NEW_DASHBOARD_URL } from '../dashboard_constants';
 
 interface CopyToDashboardModalProps {
-  PresentationUtilContext: React.FC;
   embeddable: IEmbeddable;
   dashboardId?: string;
   closeModal: () => void;
@@ -42,7 +41,6 @@ interface CopyToDashboardModalProps {
 const DashboardPicker = withSuspense(LazyDashboardPicker);
 
 export function CopyToDashboardModal({
-  PresentationUtilContext,
   dashboardId,
   embeddable,
   closeModal,
@@ -65,10 +63,14 @@ export function CopyToDashboardModal({
       throw new PanelNotFoundError();
     }
 
-    const state = {
+    const state: EmbeddablePackageState = {
       type: embeddable.type,
       input: {
         ...omit(panelToCopy.explicitInput, 'id'),
+      },
+      size: {
+        width: panelToCopy.gridData.w,
+        height: panelToCopy.gridData.h,
       },
     };
 
@@ -88,90 +90,73 @@ export function CopyToDashboardModal({
   const descriptionId = 'copyToDashboardDescription';
 
   return (
-    <EuiFocusTrap clickOutsideDisables={true}>
-      <EuiOutsideClickDetector onOutsideClick={closeModal}>
-        <div
-          role="dialog"
-          aria-modal="true"
-          aria-labelledby={titleId}
-          aria-describedby={descriptionId}
+    <div role="dialog" aria-modal="true" aria-labelledby={titleId} aria-describedby={descriptionId}>
+      <EuiModalHeader>
+        <EuiModalHeaderTitle id={titleId} component="h2">
+          {dashboardCopyToDashboardActionStrings.getDisplayName()}
+        </EuiModalHeaderTitle>
+      </EuiModalHeader>
+
+      <EuiModalBody>
+        <>
+          <EuiFormRow hasChildLabel={false}>
+            <div data-test-subj="add-to-dashboard-options">
+              {canEditExisting && (
+                <>
+                  <EuiRadio
+                    checked={dashboardOption === 'existing'}
+                    data-test-subj="add-to-existing-dashboard-option"
+                    id="existing-dashboard-option"
+                    name="dashboard-option"
+                    label={dashboardCopyToDashboardActionStrings.getExistingDashboardOption()}
+                    onChange={() => setDashboardOption('existing')}
+                  />
+                  <EuiSpacer size="s" />
+                  <div>
+                    <DashboardPicker
+                      isDisabled={dashboardOption !== 'existing'}
+                      idsToOmit={dashboardId ? [dashboardId] : undefined}
+                      onChange={(dashboard) => {
+                        setSelectedDashboard(dashboard);
+                        setDashboardOption('existing');
+                      }}
+                    />
+                  </div>
+                  <EuiSpacer size="s" />
+                </>
+              )}
+              {canCreateNew && (
+                <>
+                  <EuiRadio
+                    checked={dashboardOption === 'new'}
+                    data-test-subj="add-to-new-dashboard-option"
+                    id="new-dashboard-option"
+                    name="dashboard-option"
+                    disabled={!dashboardId}
+                    label={dashboardCopyToDashboardActionStrings.getNewDashboardOption()}
+                    onChange={() => setDashboardOption('new')}
+                  />
+                  <EuiSpacer size="s" />
+                </>
+              )}
+            </div>
+          </EuiFormRow>
+        </>
+      </EuiModalBody>
+
+      <EuiModalFooter>
+        <EuiButtonEmpty data-test-subj="cancelCopyToButton" onClick={() => closeModal()}>
+          {dashboardCopyToDashboardActionStrings.getCancelButtonName()}
+        </EuiButtonEmpty>
+        <EuiButton
+          fill
+          data-test-subj="confirmCopyToButton"
+          onClick={onSubmit}
+          disabled={dashboardOption === 'existing' && !selectedDashboard}
         >
-          <PresentationUtilContext>
-            <EuiModalHeader>
-              <EuiModalHeaderTitle id={titleId} component="h2">
-                {dashboardCopyToDashboardActionStrings.getDisplayName()}
-              </EuiModalHeaderTitle>
-            </EuiModalHeader>
-
-            <EuiModalBody>
-              <>
-                <EuiText>
-                  <p id={descriptionId}>{dashboardCopyToDashboardActionStrings.getDescription()}</p>
-                </EuiText>
-                <EuiSpacer />
-                <EuiFormRow hasChildLabel={false}>
-                  <EuiPanel
-                    color="subdued"
-                    hasShadow={false}
-                    data-test-subj="add-to-dashboard-options"
-                  >
-                    <div>
-                      {canEditExisting && (
-                        <>
-                          <EuiRadio
-                            checked={dashboardOption === 'existing'}
-                            data-test-subj="add-to-existing-dashboard-option"
-                            id="existing-dashboard-option"
-                            name="dashboard-option"
-                            label={dashboardCopyToDashboardActionStrings.getExistingDashboardOption()}
-                            onChange={() => setDashboardOption('existing')}
-                          />
-                          <div className="savAddDashboard__searchDashboards">
-                            <DashboardPicker
-                              isDisabled={dashboardOption !== 'existing'}
-                              idsToOmit={dashboardId ? [dashboardId] : undefined}
-                              onChange={(dashboard) => setSelectedDashboard(dashboard)}
-                            />
-                          </div>
-                          <EuiSpacer size="s" />
-                        </>
-                      )}
-                      {canCreateNew && (
-                        <>
-                          <EuiRadio
-                            checked={dashboardOption === 'new'}
-                            data-test-subj="add-to-new-dashboard-option"
-                            id="new-dashboard-option"
-                            name="dashboard-option"
-                            disabled={!dashboardId}
-                            label={dashboardCopyToDashboardActionStrings.getNewDashboardOption()}
-                            onChange={() => setDashboardOption('new')}
-                          />
-                          <EuiSpacer size="s" />
-                        </>
-                      )}
-                    </div>
-                  </EuiPanel>
-                </EuiFormRow>
-              </>
-            </EuiModalBody>
-
-            <EuiModalFooter>
-              <EuiButtonEmpty data-test-subj="cancelCopyToButton" onClick={() => closeModal()}>
-                {dashboardCopyToDashboardActionStrings.getCancelButtonName()}
-              </EuiButtonEmpty>
-              <EuiButton
-                fill
-                data-test-subj="confirmCopyToButton"
-                onClick={onSubmit}
-                disabled={dashboardOption === 'existing' && !selectedDashboard}
-              >
-                {dashboardCopyToDashboardActionStrings.getAcceptButtonName()}
-              </EuiButton>
-            </EuiModalFooter>
-          </PresentationUtilContext>
-        </div>
-      </EuiOutsideClickDetector>
-    </EuiFocusTrap>
+          {dashboardCopyToDashboardActionStrings.getAcceptButtonName()}
+        </EuiButton>
+      </EuiModalFooter>
+    </div>
   );
 }

commit 31fbcdedd19b6e23ba57a1c37864c25809412e19

unskip Failing test: Chrome X-Pack UI Functional Tests.x-pack/test/functional/apps/maps/group2/embeddable/add_to_dashboard·js (#167566)
Closes https://github.com/elastic/kibana/issues/167320

flaky test runner
https://buildkite.com/elastic/kibana-flaky-test-suite-runner/builds/3278

Flakiness caused by https://github.com/elastic/kibana/pull/166991. Cause
of flakiness is not waiting for dashboard-picker to finish loading
options after setting the search value.

Flaky test highlights 3 other problems with
`SavedObjectSaveModalDashboard` component that are resolved in this PR:
1) Dashboard select not disabled when "New" or "None" is selected.
Regression from https://github.com/elastic/kibana/pull/166991
2) There is no form validation message giving visual feedback when users
click "Save" with "Existing" and no selected dashboard.
3) https://github.com/elastic/kibana/pull/166991 switched dashboard
selector from `EuiComboBox` to `EuiSelectable`. This changed the
behavior of selecting the same item. With the `EuiComboBox`
implemenation, selecting the same item retained the selection while
selecting the same item with the `EuiSelectable` implementation
unselected the item.

<img width="300" alt="Screenshot 2023-09-29 at 12 51 29 PM"
src="https://github.com/elastic/kibana/assets/373691/edd8d647-b8b6-4ecc-8f91-d1ad9e916a95">

---------

Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>

src/plugins/presentation_util/public/components/dashboard_picker/dashboard_picker.tsx
@@ -117,6 +117,7 @@ export function DashboardPicker({ isDisabled, onChange, idsToOmit }: DashboardPi
       panelPaddingSize="none"
       input={
         <ToolbarButton
+          isDisabled={isDisabled}
           fullWidth
           isLoading={isLoading}
           data-test-subj="open-dashboard-picker"
@@ -160,14 +161,9 @@ export function DashboardPicker({ isDisabled, onChange, idsToOmit }: DashboardPi
         onChange={(newOptions, event, selected) => {
           setIsPopoverOpen(false);
 
-          const nextSelectedDashboard: DashboardOption | null =
-            selected.value === selectedDashboard?.value ? null : selected;
-          setSelectedDashboard(nextSelectedDashboard);
-          onChange(
-            nextSelectedDashboard
-              ? { name: nextSelectedDashboard.label, id: nextSelectedDashboard.value }
-              : null
-          );
+          if (!selected || selected.value === selectedDashboard?.value) return;
+          setSelectedDashboard(selected);
+          onChange({ name: selected.label, id: selected.value });
         }}
         renderOption={(option) => <EuiHighlight search={query}>{option.label}</EuiHighlight>}
       >

src/plugins/presentation_util/public/components/saved_object_save_modal_dashboard.tsx
@@ -10,7 +10,11 @@ import React, { useState } from 'react';
 
 import { i18n } from '@kbn/i18n';
 
-import { OnSaveProps, SavedObjectSaveModal } from '@kbn/saved-objects-plugin/public';
+import {
+  OnSaveProps,
+  SavedObjectSaveModal,
+  type SaveModalState,
+} from '@kbn/saved-objects-plugin/public';
 
 import { pluginServices } from '../services';
 import { SaveModalDashboardProps } from './types';
@@ -39,7 +43,7 @@ function SavedObjectSaveModalDashboard(props: SaveModalDashboardProps) {
   const [copyOnSave, setCopyOnSave] = useState<boolean>(initialCopyOnSave);
 
   const rightOptions = !disableDashboardOptions
-    ? () => (
+    ? ({ hasAttemptedSubmit }: SaveModalState) => (
         <SaveModalDashboardSelector
           onSelectDashboard={(dash) => {
             setSelectedDashboard(dash);
@@ -48,7 +52,15 @@ function SavedObjectSaveModalDashboard(props: SaveModalDashboardProps) {
             setDashboardOption(option);
           }}
           canSaveByReference={canSaveByReference}
-          {...{ copyOnSave, documentId, dashboardOption, setAddToLibrary, isAddToLibrarySelected }}
+          {...{
+            copyOnSave,
+            documentId,
+            dashboardOption,
+            setAddToLibrary,
+            isAddToLibrarySelected,
+            hasAttemptedSubmit,
+            hasSelectedDashboard: Boolean(selectedDashboard),
+          }}
         />
       )
     : null;

src/plugins/presentation_util/public/components/saved_object_save_modal_dashboard_selector.stories.tsx
@@ -62,6 +62,8 @@ export function Example({
       documentId={hasDocumentId ? 'abc' : undefined}
       isAddToLibrarySelected={isAddToLibrarySelected}
       setAddToLibrary={setAddToLibrary}
+      hasAttemptedSubmit={false}
+      hasSelectedDashboard={false}
     />
   );
 }

src/plugins/presentation_util/public/components/saved_object_save_modal_dashboard_selector.tsx
@@ -33,6 +33,8 @@ export interface SaveModalDashboardSelectorProps {
   isAddToLibrarySelected: boolean;
   dashboardOption: 'new' | 'existing' | null;
   onChange: (dashboardOption: 'new' | 'existing' | null) => void;
+  hasAttemptedSubmit: boolean;
+  hasSelectedDashboard: boolean;
 }
 
 export function SaveModalDashboardSelector(props: SaveModalDashboardSelectorProps) {
@@ -45,6 +47,8 @@ export function SaveModalDashboardSelector(props: SaveModalDashboardSelectorProp
     dashboardOption,
     onChange,
     copyOnSave,
+    hasAttemptedSubmit,
+    hasSelectedDashboard,
   } = props;
   const isDisabled = !copyOnSave && !!documentId;
 
@@ -81,6 +85,16 @@ export function SaveModalDashboardSelector(props: SaveModalDashboardSelectorProp
                     isDisabled={dashboardOption !== 'existing'}
                     onChange={onSelectDashboard}
                   />
+                  {hasAttemptedSubmit && dashboardOption === 'existing' && !hasSelectedDashboard ? (
+                    <div className="euiFormErrorText euiFormRow__text">
+                      {i18n.translate(
+                        'presentationUtil.saveModalDashboard.existingDashboardRequiredMessage',
+                        {
+                          defaultMessage: 'Dashboard is required',
+                        }
+                      )}
+                    </div>
+                  ) : null}
                 </div>
                 <EuiSpacer size="s" />
               </>

test/functional/page_objects/time_to_visualize_page.ts
@@ -75,8 +75,11 @@ export class TimeToVisualizePageObject extends FtrService {
       await label.click();
 
       if (dashboardId) {
+        await this.testSubjects.waitForEnabled('open-dashboard-picker');
         await this.testSubjects.click('open-dashboard-picker');
         await this.testSubjects.setValue('dashboard-picker-search', dashboardId);
+        await this.common.sleep(150); // wait for input debounce so loading starts
+        await this.testSubjects.waitForEnabled('open-dashboard-picker');
         await this.testSubjects.click(
           `dashboard-picker-option-${dashboardId.replaceAll(' ', '-')}`
         );

x-pack/test/functional/apps/maps/group2/embeddable/add_to_dashboard.js
@@ -21,8 +21,7 @@ export default function ({ getPageObjects, getService }) {
   const testSubjects = getService('testSubjects');
   const security = getService('security');
 
-  // Failing: See https://github.com/elastic/kibana/issues/167320
-  describe.skip('maps add-to-dashboard save flow', () => {
+  describe('maps add-to-dashboard save flow', () => {
     before(async () => {
       await security.testUser.setRoles(
         [
patrykkopycinski/kibana

commit 7d024a7b8640488454dad0fe236efcf9f3f941c3

[Controls] Add numeric options list (#172106)
Closes https://github.com/elastic/kibana/issues/143587
Closes https://github.com/elastic/kibana/issues/126795

## Summary

This PR adds support for numeric options lists - i.e. options list
controls that are created with a numeric field. In order to make this
possible, I had to add support for fields to have multiple, overlapping
compatible control types (however, it is currently only number fields
where this applies). When selecting a field that is compatible with
multiple control types, the user is given the option to select which
control type they want, like so:

<p align="center"><img alt="GIF of multiple control types being
available for a single field"
src="https://github.com/elastic/kibana/assets/8698078/9ebb6795-1206-47c4-aaef-32437d27cf59"/></p>

> [!NOTE]
> This system currently defaults to options list controls, since these
are the most common - this is backed up by our telemetry, which shows
that (in the last 30 days), clusters with at least one options list
control occured **ten times more often** than clusters with at least one
range slider control. However, if we decide to introduce more control
types (such as, for example, a date picker control), this assumption may
no longer be the case - at that point, we would need to reevaluate
whether the default should **always** be options list.

### Video


https://github.com/elastic/kibana/assets/8698078/4a876c94-a041-4228-aab8-7c2c1c871071

### Exact Match Searching

Since numeric fields do not have a "prefix" query equivalent, the only
possibility for searching these fields is either (1) a range query or
(2) an exact match / equality query. In this PR, I added support for
equality search - i.e. in order to find a value in a numeric options
list, you must enter the *full*, exact term in order for it to be found;
in the future, we could extend this to include a range search. This is
the exact match searching in action:

src/plugins/controls/server/options_list/suggestion_queries/options_list_all_suggestions.ts
@@ -1,0 +1,90 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+import { getFieldSubtypeNested } from '@kbn/data-views-plugin/common';
+import { get } from 'lodash';
+
+import { OptionsListRequestBody, OptionsListSuggestions } from '../../../common/options_list/types';
+import { EsBucket, OptionsListSuggestionAggregationBuilder } from '../types';
+import { getSortType } from './options_list_suggestion_query_helpers';
+
+/**
+ * Fetch all suggestions without any additional searching/filtering.
+ * This query will be more-or-less the same for **all** field types,
+ */
+export const getAllSuggestionsAggregationBuilder: () => OptionsListSuggestionAggregationBuilder =
+  () => allSuggestionsAggregationBuilder;
+
+const allSuggestionsAggregationBuilder: OptionsListSuggestionAggregationBuilder = {
+  buildAggregation: ({
+    fieldName,
+    fieldSpec,
+    sort,
+    size,
+    allowExpensiveQueries,
+  }: OptionsListRequestBody) => {
+    let suggestionsAgg: { suggestions: any; unique_terms?: any } = {
+      suggestions: {
+        terms: {
+          size,
+          field: fieldName,
+          shard_size: 10,
+          order: getSortType(sort),
+        },
+      },
+    };
+
+    if (allowExpensiveQueries) {
+      suggestionsAgg = {
+        ...suggestionsAgg,
+        unique_terms: {
+          cardinality: {
+            field: fieldName,
+          },
+        },
+      };
+    }
+
+    const subTypeNested = fieldSpec && getFieldSubtypeNested(fieldSpec);
+    if (subTypeNested) {
+      return {
+        nestedSuggestions: {
+          nested: {
+            path: subTypeNested.nested.path,
+          },
+          aggs: {
+            ...suggestionsAgg,
+          },
+        },
+      };
+    }
+
+    return suggestionsAgg;
+  },
+  parse: (rawEsResult, { fieldSpec, allowExpensiveQueries }: OptionsListRequestBody) => {
+    const subTypeNested = fieldSpec && getFieldSubtypeNested(fieldSpec);
+    const suggestions = get(
+      rawEsResult,
+      `aggregations.${subTypeNested ? 'nestedSuggestions.suggestions' : 'suggestions'}.buckets`
+    )?.reduce((acc: OptionsListSuggestions, suggestion: EsBucket) => {
+      acc.push({ value: suggestion.key, docCount: suggestion.doc_count });
+      return acc;
+    }, []);
+    return {
+      suggestions,
+      totalCardinality: allowExpensiveQueries
+        ? get(
+            rawEsResult,
+            `aggregations.${
+              subTypeNested ? 'nestedSuggestions.unique_terms' : 'unique_terms'
+            }.value`
+          )
+        : undefined,
+    };
+  },
+};

src/plugins/controls/server/options_list/suggestion_queries/options_list_all_suggestions.test.ts
@@ -1,0 +1,142 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+import { FieldSpec } from '@kbn/data-views-plugin/common';
+import { OptionsListRequestBody } from '../../../common/options_list/types';
+import { getAllSuggestionsAggregationBuilder } from './options_list_all_suggestions';
+
+describe('options list fetch all suggestions query', () => {
+  describe('suggestion aggregation', () => {
+    test('number field', () => {
+      const optionsListRequestBodyMock: OptionsListRequestBody = {
+        size: 10,
+        fieldName: 'bytes',
+        allowExpensiveQueries: true,
+        fieldSpec: {
+          type: 'number',
+        } as unknown as FieldSpec,
+        sort: {
+          by: '_key',
+          direction: 'asc',
+        },
+      };
+      const aggregationBuilder = getAllSuggestionsAggregationBuilder();
+      const aggregation = aggregationBuilder.buildAggregation(optionsListRequestBodyMock);
+
+      expect(aggregation).toMatchObject({
+        suggestions: {
+          terms: {
+            size: 10,
+            shard_size: 10,
+            field: 'bytes',
+            order: {
+              _key: 'asc',
+            },
+          },
+        },
+        unique_terms: {
+          cardinality: {
+            field: 'bytes',
+          },
+        },
+      });
+    });
+
+    test('nested string (keyword, text+keyword) field', () => {
+      const optionsListRequestBodyMock: OptionsListRequestBody = {
+        size: 10,
+        fieldName: 'testField',
+        allowExpensiveQueries: true,
+        fieldSpec: {
+          type: 'string',
+          subType: { nested: { path: 'path.to.nested' } },
+        } as unknown as FieldSpec,
+      };
+      const aggregationBuilder = getAllSuggestionsAggregationBuilder();
+      const aggregation = aggregationBuilder.buildAggregation(optionsListRequestBodyMock);
+
+      expect(aggregation).toMatchObject({
+        nestedSuggestions: {
+          nested: {
+            path: 'path.to.nested',
+          },
+          aggs: {
+            suggestions: {
+              terms: {
+                size: 10,
+                shard_size: 10,
+                field: 'testField',
+                order: {
+                  _count: 'desc',
+                },
+              },
+            },
+            unique_terms: {
+              cardinality: {
+                field: 'testField',
+              },
+            },
+          },
+        },
+      });
+    });
+  });
+
+  test('suggestion parsing', () => {
+    const optionsListRequestBodyMock: OptionsListRequestBody = {
+      size: 10,
+      fieldName: 'bytes',
+      allowExpensiveQueries: true,
+      fieldSpec: {
+        type: 'number',
+      } as unknown as FieldSpec,
+      sort: {
+        by: '_key',
+        direction: 'asc',
+      },
+    };
+    const aggregationBuilder = getAllSuggestionsAggregationBuilder();
+    const searchResponseMock = {
+      hits: {
+        total: 10,
+        max_score: 10,
+        hits: [],
+      },
+      took: 10,
+      timed_out: false,
+      _shards: {
+        failed: 0,
+        successful: 1,
+        total: 1,
+        skipped: 0,
+      },
+      aggregations: {
+        suggestions: {
+          buckets: [
+            { doc_count: 5, key: '1' },
+            { doc_count: 4, key: '2' },
+            { doc_count: 3, key: '3' },
+          ],
+        },
+        unique_terms: {
+          value: 3,
+        },
+      },
+    };
+
+    const parsed = aggregationBuilder.parse(searchResponseMock, optionsListRequestBodyMock);
+    expect(parsed).toMatchObject({
+      suggestions: [
+        { value: '1', docCount: 5 },
+        { value: '2', docCount: 4 },
+        { value: '3', docCount: 3 },
+      ],
+      totalCardinality: 3,
+    });
+  });
+});

src/plugins/controls/common/options_list/types.ts
@@ -6,16 +6,14 @@
  * Side Public License, v 1.
  */
 
-import { FieldSpec, DataView, RuntimeFieldSpec } from '@kbn/data-views-plugin/common';
-import type { Filter, Query, BoolQuery, TimeRange } from '@kbn/es-query';
+import { DataView, FieldSpec, RuntimeFieldSpec } from '@kbn/data-views-plugin/common';
+import type { BoolQuery, Filter, Query, TimeRange } from '@kbn/es-query';
 
-import type { OptionsListSortingType } from './suggestions_sorting';
 import type { DataControlInput } from '../types';
+import { OptionsListSearchTechnique } from './suggestions_searching';
+import type { OptionsListSortingType } from './suggestions_sorting';
 
 export const OPTIONS_LIST_CONTROL = 'optionsListControl';
-
-export type OptionsListSearchTechnique = 'prefix' | 'wildcard';
-export const OPTIONS_LIST_DEFAULT_SEARCH_TECHNIQUE: OptionsListSearchTechnique = 'prefix';
 
 export interface OptionsListEmbeddableInput extends DataControlInput {
   searchTechnique?: OptionsListSearchTechnique;

packages/kbn-optimizer/limits.yml
@@ -23,7 +23,7 @@ pageLoadAssetSize:
   cloudSecurityPosture: 19109
   console: 46091
   contentManagement: 16254
-  controls: 40000
+  controls: 55082
   core: 435325
   crossClusterReplication: 65408
   customIntegrations: 22034

src/plugins/controls/common/options_list/ip_search.ts
@@ -17,6 +17,10 @@ interface IpSegments {
   type: 'ipv4' | 'ipv6' | 'unknown';
 }
 
+export const getIsValidFullIp = (searchString: string) => {
+  return ipaddr.IPv4.isValidFourPartDecimal(searchString) || ipaddr.IPv6.isValid(searchString);
+};
+
 export const getIpSegments = (searchString: string): IpSegments => {
   if (searchString.indexOf('.') !== -1) {
     // ipv4 takes priority - so if search string contains both `.` and `:` then it will just be an invalid ipv4 search

src/plugins/controls/common/options_list/is_valid_search.test.ts
@@ -1,0 +1,118 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+import { isValidSearch } from './is_valid_search';
+
+describe('test validity of search strings', () => {
+  describe('number field', () => {
+    it('valid search - basic integer', () => {
+      expect(isValidSearch({ searchString: '123', fieldType: 'number' })).toBe(true);
+    });
+
+    it('valid search - floating point number', () => {
+      expect(isValidSearch({ searchString: '12.34', fieldType: 'number' })).toBe(true);
+    });
+
+    it('valid search - negative number', () => {
+      expect(isValidSearch({ searchString: '-42', fieldType: 'number' })).toBe(true);
+    });
+
+    it('invalid search - invalid character search string', () => {
+      expect(isValidSearch({ searchString: '1!a23', fieldType: 'number' })).toBe(false);
+    });
+  });
+
+  // we do not currently support searching date fields, so they will always be invalid
+  describe('date field', () => {
+    it('invalid search - formatted date', () => {
+      expect(isValidSearch({ searchString: 'December 12, 2023', fieldType: 'date' })).toBe(false);
+    });
+
+    it('invalid search - invalid character search string', () => {
+      expect(isValidSearch({ searchString: '!!12/12/23?', fieldType: 'date' })).toBe(false);
+    });
+  });
+
+  // only testing exact match validity here - the remainder of testing is covered by ./ip_search.test.ts
+  describe('ip field', () => {
+    it('valid search - ipv4', () => {
+      expect(
+        isValidSearch({
+          searchString: '1.2.3.4',
+          fieldType: 'ip',
+          searchTechnique: 'exact',
+        })
+      ).toBe(true);
+    });
+
+    it('valid search - full ipv6', () => {
+      expect(
+        isValidSearch({
+          searchString: 'fbbe:a363:9e14:987c:49cf:d4d0:d8c8:bc42',
+          fieldType: 'ip',
+          searchTechnique: 'exact',
+        })
+      ).toBe(true);
+    });
+
+    it('valid search - partial ipv6', () => {
+      expect(
+        isValidSearch({
+          searchString: 'fbbe:a363::',
+          fieldType: 'ip',
+          searchTechnique: 'exact',
+        })
+      ).toBe(true);
+    });
+
+    it('invalid search - invalid character search string', () => {
+      expect(
+        isValidSearch({
+          searchString: '!!123.abc?',
+          fieldType: 'ip',
+          searchTechnique: 'exact',
+        })
+      ).toBe(false);
+    });
+
+    it('invalid search - ipv4', () => {
+      expect(
+        isValidSearch({
+          searchString: '1.2.3.256',
+          fieldType: 'ip',
+          searchTechnique: 'exact',
+        })
+      ).toBe(false);
+    });
+
+    it('invalid search - ipv6', () => {
+      expect(
+        isValidSearch({
+          searchString: '::fbbe:a363::',
+          fieldType: 'ip',
+          searchTechnique: 'exact',
+        })
+      ).toBe(false);
+    });
+  });
+
+  // string field searches can never be invalid
+  describe('string field', () => {
+    it('valid search - basic search string', () => {
+      expect(isValidSearch({ searchString: 'abc', fieldType: 'string' })).toBe(true);
+    });
+
+    it('valid search - numeric search string', () => {
+      expect(isValidSearch({ searchString: '123', fieldType: 'string' })).toBe(true);
+    });
+
+    it('valid search - complex search string', () => {
+      expect(isValidSearch({ searchString: '!+@abc*&[]', fieldType: 'string' })).toBe(true);
+    });
+  });
+});

src/plugins/controls/common/options_list/is_valid_search.ts
@@ -1,0 +1,52 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+import { getIpRangeQuery, getIsValidFullIp } from './ip_search';
+import { OptionsListSearchTechnique } from './suggestions_searching';
+
+/**
+ * ipaddr is a fairly large library - therefore, this function needs to be separate from
+ * the `suggestions_searching` file (which is used in the OptionsListEditorOptions component,
+ * which is in the factory and not async imported)
+ */
+
+export const isValidSearch = ({
+  searchString,
+  fieldType,
+  searchTechnique,
+}: {
+  searchString?: string;
+  fieldType?: string;
+  searchTechnique?: OptionsListSearchTechnique;
+}): boolean => {
+  if (!searchString || searchString.length === 0) return true;
+
+  switch (fieldType) {
+    case 'number': {
+      return !isNaN(Number(searchString));
+    }
+    case 'date': {
+      /** searching is not currently supported for date fields */
+      return false;
+    }
+    case 'ip': {
+      if (searchTechnique === 'exact') {
+        /**
+         * exact match searching will throw an error if the search string isn't a **full** IP,
+         * so we need a slightly different validity check here than for other search techniques
+         */
+        return getIsValidFullIp(searchString);
+      }
+      return getIpRangeQuery(searchString).validSearch;
+    }
+    default: {
+      /** string searches are always considered to be valid */
+      return true;
+    }
+  }
+};

src/plugins/controls/common/options_list/suggestions_searching.ts
@@ -1,0 +1,31 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+export type OptionsListSearchTechnique = 'prefix' | 'wildcard' | 'exact';
+
+export const getDefaultSearchTechnique = (type: string): OptionsListSearchTechnique | undefined => {
+  const compatibleSearchTechniques = getCompatibleSearchTechniques(type);
+  return compatibleSearchTechniques.length > 0 ? compatibleSearchTechniques[0] : undefined;
+};
+
+export const getCompatibleSearchTechniques = (type?: string): OptionsListSearchTechnique[] => {
+  switch (type) {
+    case 'string': {
+      return ['prefix', 'wildcard', 'exact'];
+    }
+    case 'ip': {
+      return ['prefix', 'exact'];
+    }
+    case 'number': {
+      return ['exact'];
+    }
+    default: {
+      return [];
+    }
+  }
+};

commit 61824bddadecd1bf66262afabd5630d3eb677fac

[Controls] Add back support for `key_as_string` (#176859)
Closes https://github.com/elastic/kibana/issues/176645

## Summary

This is PR fixes a regression caused by
https://github.com/elastic/kibana/pull/172106 - specifically, in the
major refactor of the options list queries, I accidentally dropped
support for the `key_as_string` key in the ES response. This broke
`boolean` fields because, in the ES response, the `key` is the numeric
representation of booleans (`0 / 1`) while `key_as_string` is the string
representation `"false" / "true"` - this caused an error to be thrown
when selections were included in the fetch request because we began
storing **selections** as `0 / 1` due to this but the ES client expected
only `"false" / "true"`.

This PR fixes this by re-introducing support for `key_as_string`, so
boolean selections are once again stored as `"false" / "true"`.


### Checklist

- [x] [Unit or functional
tests](https://www.elastic.co/guide/en/kibana/master/development-tests.html)
were updated or added to match the most common scenarios

### For maintainers

- [ ] This was checked for breaking API changes and was [labeled
appropriately](https://www.elastic.co/guide/en/kibana/master/contributing.html#kibana-release-notes-process)

src/plugins/controls/server/options_list/suggestion_queries/options_list_all_suggestions.ts
@@ -72,7 +72,13 @@ const allSuggestionsAggregationBuilder: OptionsListSuggestionAggregationBuilder 
       rawEsResult,
       `aggregations.${subTypeNested ? 'nestedSuggestions.suggestions' : 'suggestions'}.buckets`
     )?.reduce((acc: OptionsListSuggestions, suggestion: EsBucket) => {
-      acc.push({ value: suggestion.key, docCount: suggestion.doc_count });
+      acc.push({
+        value:
+          fieldSpec?.type === 'boolean' && suggestion.key_as_string
+            ? suggestion.key_as_string
+            : suggestion.key,
+        docCount: suggestion.doc_count,
+      });
       return acc;
     }, []);
     return {

src/plugins/controls/server/options_list/types.ts
@@ -13,7 +13,8 @@ import {
 } from '../../common/options_list/types';
 
 export interface EsBucket {
-  key: string;
+  key: any;
+  key_as_string?: string;
   doc_count: number;
 }
 

src/plugins/controls/server/options_list/suggestion_queries/options_list_all_suggestions.test.ts
@@ -87,56 +87,162 @@ describe('options list fetch all suggestions query', () => {
     });
   });
 
-  test('suggestion parsing', () => {
-    const optionsListRequestBodyMock: OptionsListRequestBody = {
-      size: 10,
-      fieldName: 'bytes',
-      allowExpensiveQueries: true,
-      fieldSpec: {
-        type: 'number',
-      } as unknown as FieldSpec,
-      sort: {
-        by: '_key',
-        direction: 'asc',
-      },
-    };
-    const aggregationBuilder = getAllSuggestionsAggregationBuilder();
-    const searchResponseMock = {
-      hits: {
-        total: 10,
-        max_score: 10,
-        hits: [],
-      },
-      took: 10,
-      timed_out: false,
-      _shards: {
-        failed: 0,
-        successful: 1,
-        total: 1,
-        skipped: 0,
-      },
-      aggregations: {
-        suggestions: {
-          buckets: [
-            { doc_count: 5, key: '1' },
-            { doc_count: 4, key: '2' },
-            { doc_count: 3, key: '3' },
-          ],
+  describe('suggestion parsing', () => {
+    test('test parsing for number field', () => {
+      const optionsListRequestBodyMock: OptionsListRequestBody = {
+        size: 10,
+        fieldName: 'bytes',
+        allowExpensiveQueries: true,
+        fieldSpec: {
+          type: 'number',
+        } as unknown as FieldSpec,
+        sort: {
+          by: '_key',
+          direction: 'asc',
         },
-        unique_terms: {
-          value: 3,
+      };
+      const aggregationBuilder = getAllSuggestionsAggregationBuilder();
+      const searchResponseMock = {
+        hits: {
+          total: 10,
+          max_score: 10,
+          hits: [],
         },
-      },
-    };
+        took: 10,
+        timed_out: false,
+        _shards: {
+          failed: 0,
+          successful: 1,
+          total: 1,
+          skipped: 0,
+        },
+        aggregations: {
+          suggestions: {
+            buckets: [
+              { doc_count: 5, key: '1' },
+              { doc_count: 4, key: '2' },
+              { doc_count: 3, key: '3' },
+            ],
+          },
+          unique_terms: {
+            value: 3,
+          },
+        },
+      };
 
-    const parsed = aggregationBuilder.parse(searchResponseMock, optionsListRequestBodyMock);
-    expect(parsed).toMatchObject({
-      suggestions: [
-        { value: '1', docCount: 5 },
-        { value: '2', docCount: 4 },
-        { value: '3', docCount: 3 },
-      ],
-      totalCardinality: 3,
+      const parsed = aggregationBuilder.parse(searchResponseMock, optionsListRequestBodyMock);
+      expect(parsed).toMatchObject({
+        suggestions: [
+          { value: '1', docCount: 5 },
+          { value: '2', docCount: 4 },
+          { value: '3', docCount: 3 },
+        ],
+        totalCardinality: 3,
+      });
+    });
+
+    test('test parsing for boolean field', () => {
+      const optionsListRequestBodyMock: OptionsListRequestBody = {
+        size: 10,
+        fieldName: 'cancelled',
+        allowExpensiveQueries: true,
+        fieldSpec: {
+          type: 'boolean',
+        } as unknown as FieldSpec,
+        sort: {
+          by: '_key',
+          direction: 'desc',
+        },
+      };
+      const aggregationBuilder = getAllSuggestionsAggregationBuilder();
+      const searchResponseMock = {
+        hits: {
+          total: 10,
+          max_score: 10,
+          hits: [],
+        },
+        took: 10,
+        timed_out: false,
+        _shards: {
+          failed: 0,
+          successful: 1,
+          total: 1,
+          skipped: 0,
+        },
+        aggregations: {
+          suggestions: {
+            buckets: [
+              { doc_count: 54, key: 0, key_as_string: 'false' },
+              { doc_count: 46, key: 1, key_as_string: 'true' },
+            ],
+          },
+          unique_terms: {
+            value: 2,
+          },
+        },
+      };
+
+      const parsed = aggregationBuilder.parse(searchResponseMock, optionsListRequestBodyMock);
+      expect(parsed).toMatchObject({
+        suggestions: [
+          { value: 'false', docCount: 54 },
+          { value: 'true', docCount: 46 },
+        ],
+        totalCardinality: 2,
+      });
+    });
+
+    test('test parsing for date field', () => {
+      const optionsListRequestBodyMock: OptionsListRequestBody = {
+        size: 10,
+        fieldName: '@timestamp',
+        allowExpensiveQueries: true,
+        fieldSpec: {
+          type: 'date',
+        } as unknown as FieldSpec,
+        sort: {
+          by: '_key',
+          direction: 'desc',
+        },
+      };
+      const aggregationBuilder = getAllSuggestionsAggregationBuilder();
+      const searchResponseMock = {
+        hits: {
+          total: 10,
+          max_score: 10,
+          hits: [],
+        },
+        took: 10,
+        timed_out: false,
+        _shards: {
+          failed: 0,
+          successful: 1,
+          total: 1,
+          skipped: 0,
+        },
+        aggregations: {
+          suggestions: {
+            buckets: [
+              { doc_count: 5, key: 1707810859000, key_as_string: 'Feb 13, 2024 @ 00:54:19.000' },
+              { doc_count: 4, key: 1707728532000, key_as_string: 'Feb 12, 2024 @ 02:02:12.000' },
+              { doc_count: 2, key: 1707216874000, key_as_string: 'Feb 6, 2024 @ 03:54:34.000' },
+            ],
+          },
+          unique_terms: {
+            value: 3,
+          },
+        },
+      };
+
+      const parsed = aggregationBuilder.parse(searchResponseMock, optionsListRequestBodyMock);
+      expect(parsed).toMatchObject({
+        suggestions: [
+          { value: 1707810859000, docCount: 5 },
+          { value: 1707728532000, docCount: 4 },
+          { value: 1707216874000, docCount: 2 },
+        ],
+        totalCardinality: 3,
+      });
     });
   });
 });
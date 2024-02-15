patrykkopycinski/kibana

commit 9357bd4714ef0af35d75534b5d19d33684e0aaaf

[RAM] Stack Management::Rules loses user selections when navigating back (#174954)
## Summary

FIX => https://github.com/elastic/kibana/issues/173967


![image](https://github.com/elastic/kibana/assets/189600/dd6a2b83-7460-4d90-8ee4-39c40534c247)


### Checklist

- [ ] [Unit or functional
tests](https://www.elastic.co/guide/en/kibana/master/development-tests.html)
were updated or added to match the most common scenarios

---------

Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>

x-pack/plugins/triggers_actions_ui/public/application/sections/rules_list/components/rules_list.tsx
@@ -54,7 +54,6 @@ import {
   Pagination,
   Percentiles,
   SnoozeSchedule,
-  RulesListFilters,
   UpdateFiltersProps,
   BulkEditActions,
   UpdateRulesToBulkEditProps,
@@ -107,6 +106,7 @@ import {
 import { useBulkOperationToast } from '../../../hooks/use_bulk_operation_toast';
 import { RulesSettingsLink } from '../../../components/rules_setting/rules_settings_link';
 import { useRulesListUiState as useUiState } from '../../../hooks/use_rules_list_ui_state';
+import { useRulesListFilterStore } from './hooks/use_rules_list_filter_store';
 
 // Directly lazy import the flyouts because the suspendedComponentWithProps component
 // cause a visual hitch due to the loading spinner
@@ -190,22 +190,11 @@ export const RulesList = ({
     notifications: { toasts },
     ruleTypeRegistry,
   } = kibanaServices;
+
   const canExecuteActions = hasExecuteActionsCapability(capabilities);
   const [isPerformingAction, setIsPerformingAction] = useState<boolean>(false);
   const [page, setPage] = useState<Pagination>({ index: 0, size: DEFAULT_SEARCH_PAGE_SIZE });
   const [inputText, setInputText] = useState<string>(searchFilter);
-
-  const [filters, setFilters] = useState<RulesListFilters>({
-    actionTypes: [],
-    ruleExecutionStatuses: lastResponseFilter || [],
-    ruleLastRunOutcomes: lastRunOutcomeFilter || [],
-    ruleParams: ruleParamFilter || {},
-    ruleStatuses: statusFilter || [],
-    searchText: searchFilter || '',
-    tags: [],
-    types: typeFilter || [],
-    kueryNode: undefined,
-  });
 
   const [ruleFlyoutVisible, setRuleFlyoutVisibility] = useState<boolean>(false);
   const [editFlyoutVisible, setEditFlyoutVisibility] = useState<boolean>(false);
@@ -258,6 +247,17 @@ export const RulesList = ({
   } = useLoadRuleTypesQuery({ filteredRuleTypes });
   // Fetch action types
   const { actionTypes } = useLoadActionTypesQuery();
+
+  const { filters, setFiltersStore, numberOfFiltersStore, resetFiltersStore } =
+    useRulesListFilterStore({
+      lastResponseFilter,
+      lastRunOutcomeFilter,
+      rulesListKey,
+      ruleParamFilter,
+      statusFilter,
+      searchFilter,
+      typeFilter,
+    });
 
   const rulesTypesFilter = isEmpty(filters.types)
     ? authorizedRuleTypes.map((art) => art.id)
@@ -406,14 +406,10 @@ export const RulesList = ({
 
   const updateFilters = useCallback(
     (updateFiltersProps: UpdateFiltersProps) => {
-      const { filter, value } = updateFiltersProps;
-      setFilters((prev) => ({
-        ...prev,
-        [filter]: value,
-      }));
+      setFiltersStore(updateFiltersProps);
       handleUpdateFiltersEffect(updateFiltersProps);
     },
-    [setFilters, handleUpdateFiltersEffect]
+    [setFiltersStore, handleUpdateFiltersEffect]
   );
 
   const handleClearRuleParamFilter = () => updateFilters({ filter: 'ruleParams', value: {} });
@@ -982,6 +978,8 @@ export const RulesList = ({
               rulesListKey={rulesListKey}
               config={config}
               visibleColumns={visibleColumns}
+              numberOfFilters={numberOfFiltersStore}
+              resetFilters={resetFiltersStore}
             />
             {manageLicenseModalOpts && (
               <ManageLicenseModal

x-pack/plugins/triggers_actions_ui/public/application/sections/rules_list/components/hooks/use_rules_list_filter_store.test.tsx
@@ -1,0 +1,267 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { act, renderHook } from '@testing-library/react-hooks';
+import * as useLocalStorage from 'react-use/lib/useLocalStorage';
+import { useRulesListFilterStore } from './use_rules_list_filter_store';
+
+jest.mock('@kbn/kibana-utils-plugin/public');
+const { createKbnUrlStateStorage } = jest.requireMock('@kbn/kibana-utils-plugin/public');
+
+const useUrlStateStorageGetMock = jest.fn();
+const useUrlStateStorageSetMock = jest.fn();
+const setRulesListFilterLocalMock = jest.fn();
+const LOCAL_STORAGE_KEY = 'test_local';
+describe('useRulesListFilterStore', () => {
+  beforeAll(() => {
+    createKbnUrlStateStorage.mockReturnValue({
+      get: useUrlStateStorageGetMock,
+      set: useUrlStateStorageSetMock,
+    });
+  });
+
+  beforeEach(() => {
+    jest
+      .spyOn(useLocalStorage, 'default')
+      .mockImplementation(() => [null, setRulesListFilterLocalMock, () => {}]);
+    useUrlStateStorageGetMock.mockReturnValue(null);
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('Should return empty filter when url query param and local storage and props are empty', async () => {
+    const { result } = renderHook(() =>
+      useRulesListFilterStore({
+        rulesListKey: LOCAL_STORAGE_KEY,
+      })
+    );
+    expect(result.current.filters).toEqual({
+      actionTypes: [],
+      kueryNode: undefined,
+      ruleExecutionStatuses: [],
+      ruleLastRunOutcomes: [],
+      ruleParams: {},
+      ruleStatuses: [],
+      searchText: '',
+      tags: [],
+      types: [],
+    });
+    expect(result.current.numberOfFiltersStore).toEqual(0);
+  });
+
+  it('Should return the props as filter when url query param and local storage are empty', () => {
+    const { result } = renderHook(() =>
+      useRulesListFilterStore({
+        lastResponseFilter: ['props-lastResponse-filter'],
+        lastRunOutcomeFilter: ['props-lastRunOutcome-filter'],
+        rulesListKey: LOCAL_STORAGE_KEY,
+        ruleParamFilter: { propsRuleParams: 'props-ruleParams-filter' },
+        statusFilter: ['enabled'],
+        searchFilter: 'props-search-filter',
+        typeFilter: ['props-ruleType-filter'],
+      })
+    );
+    expect(result.current.filters).toEqual({
+      actionTypes: [],
+      kueryNode: undefined,
+      ruleExecutionStatuses: ['props-lastResponse-filter'],
+      ruleLastRunOutcomes: ['props-lastRunOutcome-filter'],
+      ruleParams: {
+        propsRuleParams: 'props-ruleParams-filter',
+      },
+      ruleStatuses: ['enabled'],
+      searchText: 'props-search-filter',
+      tags: [],
+      types: ['props-ruleType-filter'],
+    });
+    expect(result.current.numberOfFiltersStore).toEqual(6);
+  });
+
+  it('Should return the local storage params as filter when url query param is empty', () => {
+    jest.spyOn(useLocalStorage, 'default').mockImplementation(() => [
+      {
+        actionTypes: ['localStorage-actionType-filter'],
+        lastResponse: ['localStorage-lastResponse-filter'],
+        params: { localStorageRuleParams: 'localStorage-ruleParams-filter' },
+        search: 'localStorage-search-filter',
+        status: ['disabled'],
+        tags: ['localStorage-tag-filter'],
+        type: ['localStorage-ruleType-filter'],
+      },
+      () => null,
+      () => {},
+    ]);
+    const { result } = renderHook(() =>
+      useRulesListFilterStore({
+        lastResponseFilter: ['props-lastResponse-filter'],
+        lastRunOutcomeFilter: ['props-lastRunOutcome-filter'],
+        rulesListKey: LOCAL_STORAGE_KEY,
+        ruleParamFilter: { propsRuleParams: 'props-ruleParams-filter' },
+        statusFilter: ['enabled'],
+        searchFilter: 'props-search-filter',
+        typeFilter: ['ruleType-filter'],
+      })
+    );
+    expect(result.current.filters).toEqual({
+      actionTypes: ['localStorage-actionType-filter'],
+      kueryNode: undefined,
+      // THIS is valid because we are not using this param in local storage
+      ruleExecutionStatuses: ['props-lastResponse-filter'],
+      ruleLastRunOutcomes: ['localStorage-lastResponse-filter'],
+      ruleParams: {
+        localStorageRuleParams: 'localStorage-ruleParams-filter',
+      },
+      ruleStatuses: ['disabled'],
+      searchText: 'localStorage-search-filter',
+      tags: ['localStorage-tag-filter'],
+      types: ['localStorage-ruleType-filter'],
+    });
+    expect(result.current.numberOfFiltersStore).toEqual(8);
+  });
+
+  it('Should return the url params as filter when url query param is empty', () => {
+    jest.spyOn(useLocalStorage, 'default').mockImplementation(() => [
+      {
+        actionTypes: ['localStorage-actionType-filter'],
+        lastResponse: ['localStorage-lastResponse-filter'],
+        params: { localStorageRuleParams: 'localStorage-ruleParams-filter' },
+        search: 'localStorage-search-filter',
+        status: ['disabled'],
+        tags: ['localStorage-tag-filter'],
+        type: ['localStorage-ruleType-filter'],
+      },
+      () => null,
+      () => {},
+    ]);
+    useUrlStateStorageGetMock.mockReturnValue({
+      actionTypes: ['urlQueryParams-actionType-filter'],
+      lastResponse: ['urlQueryParams-lastResponse-filter'],
+      params: { urlQueryParamsRuleParams: 'urlQueryParams-ruleParams-filter' },
+      search: 'urlQueryParams-search-filter',
+      status: ['snoozed'],
+      tags: ['urlQueryParams-tag-filter'],
+      type: ['urlQueryParams-ruleType-filter'],
+    });
+    const { result } = renderHook(() =>
+      useRulesListFilterStore({
+        lastResponseFilter: ['props-lastResponse-filter'],
+        lastRunOutcomeFilter: ['props-lastRunOutcome-filter'],
+        rulesListKey: LOCAL_STORAGE_KEY,
+        ruleParamFilter: { propsRuleParams: 'props-ruleParams-filter' },
+        statusFilter: ['enabled'],
+        searchFilter: 'props-search-filter',
+        typeFilter: ['ruleType-filter'],
+      })
+    );
+    expect(result.current.filters).toEqual({
+      actionTypes: ['urlQueryParams-actionType-filter'],
+      kueryNode: undefined,
+      // THIS is valid because we are not using this param in url query params
+      ruleExecutionStatuses: ['props-lastResponse-filter'],
+      ruleLastRunOutcomes: ['urlQueryParams-lastResponse-filter'],
+      ruleParams: {
+        urlQueryParamsRuleParams: 'urlQueryParams-ruleParams-filter',
+      },
+      ruleStatuses: ['snoozed'],
+      searchText: 'urlQueryParams-search-filter',
+      tags: ['urlQueryParams-tag-filter'],
+      types: ['urlQueryParams-ruleType-filter'],
+    });
+    expect(result.current.numberOfFiltersStore).toEqual(8);
+  });
+
+  it('Should clear filter when resetFiltersStore has been called', async () => {
+    useUrlStateStorageGetMock.mockReturnValue({
+      actionTypes: ['urlQueryParams-actionType-filter'],
+      lastResponse: ['urlQueryParams-lastResponse-filter'],
+      params: { urlQueryParamsRuleParams: 'urlQueryParams-ruleParams-filter' },
+      search: 'urlQueryParams-search-filter',
+      status: ['snoozed'],
+      tags: ['urlQueryParams-tag-filter'],
+      type: ['urlQueryParams-ruleType-filter'],
+    });
+    const { result } = renderHook(() =>
+      useRulesListFilterStore({
+        rulesListKey: LOCAL_STORAGE_KEY,
+      })
+    );
+    expect(result.current.filters).toEqual({
+      actionTypes: ['urlQueryParams-actionType-filter'],
+      kueryNode: undefined,
+      ruleExecutionStatuses: [],
+      ruleLastRunOutcomes: ['urlQueryParams-lastResponse-filter'],
+      ruleParams: {
+        urlQueryParamsRuleParams: 'urlQueryParams-ruleParams-filter',
+      },
+      ruleStatuses: ['snoozed'],
+      searchText: 'urlQueryParams-search-filter',
+      tags: ['urlQueryParams-tag-filter'],
+      types: ['urlQueryParams-ruleType-filter'],
+    });
+    expect(result.current.numberOfFiltersStore).toEqual(7);
+
+    act(() => {
+      result.current.resetFiltersStore();
+    });
+
+    expect(result.current.filters).toEqual({
+      actionTypes: [],
+      kueryNode: undefined,
+      ruleExecutionStatuses: [],
+      ruleLastRunOutcomes: [],
+      ruleParams: {},
+      ruleStatuses: [],
+      searchText: '',
+      tags: [],
+      types: [],
+    });
+    expect(result.current.numberOfFiltersStore).toEqual(0);
+    expect(useUrlStateStorageSetMock).toBeCalledTimes(1);
+    expect(setRulesListFilterLocalMock).toBeCalledTimes(1);
+  });
+
+  it('Should set filter when setFiltersStore has been called', async () => {
+    const { result } = renderHook(() =>
+      useRulesListFilterStore({
+        rulesListKey: LOCAL_STORAGE_KEY,
+      })
+    );
+    expect(result.current.filters).toEqual({
+      actionTypes: [],
+      kueryNode: undefined,
+      ruleExecutionStatuses: [],
+      ruleLastRunOutcomes: [],
+      ruleParams: {},
+      ruleStatuses: [],
+      searchText: '',
+      tags: [],
+      types: [],
+    });
+    expect(result.current.numberOfFiltersStore).toEqual(0);
+
+    act(() => {
+      result.current.setFiltersStore({ filter: 'tags', value: ['my-tags'] });
+    });
+
+    expect(result.current.filters).toEqual({
+      actionTypes: [],
+      kueryNode: undefined,
+      ruleExecutionStatuses: [],
+      ruleLastRunOutcomes: [],
+      ruleParams: {},
+      ruleStatuses: [],
+      searchText: '',
+      tags: ['my-tags'],
+      types: [],
+    });
+    expect(result.current.numberOfFiltersStore).toEqual(1);
+    expect(useUrlStateStorageSetMock).toBeCalledTimes(1);
+    expect(setRulesListFilterLocalMock).toBeCalledTimes(1);
+  });
+});

x-pack/plugins/triggers_actions_ui/public/application/sections/rules_list/components/hooks/use_rules_list_filter_store.tsx
@@ -1,0 +1,188 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { useHistory } from 'react-router-dom';
+import { createKbnUrlStateStorage } from '@kbn/kibana-utils-plugin/public';
+import { useCallback, useEffect, useMemo, useState } from 'react';
+import useLocalStorage from 'react-use/lib/useLocalStorage';
+import { isEmpty } from 'lodash';
+import { RuleStatus } from '../../../../../common';
+import { RulesListFilters, RulesListProps, UpdateFiltersProps } from '../../../../../types';
+
+type FilterStoreProps = Pick<
+  RulesListProps,
+  | 'lastResponseFilter'
+  | 'lastRunOutcomeFilter'
+  | 'rulesListKey'
+  | 'ruleParamFilter'
+  | 'statusFilter'
+  | 'searchFilter'
+  | 'typeFilter'
+>;
+const RULES_LIST_FILTERS_KEY = 'triggersActionsUi_rulesList';
+
+interface FilterParameters {
+  actionTypes?: string[];
+  lastResponse?: string[];
+  params?: Record<string, string | number | object>;
+  search?: string;
+  status?: RuleStatus[];
+  tags?: string[];
+  type?: string[];
+}
+
+export const convertRulesListFiltersToFilterAttributes = (
+  rulesListFilter: RulesListFilters
+): FilterParameters => {
+  return {
+    actionTypes: rulesListFilter.actionTypes,
+    lastResponse: rulesListFilter.ruleLastRunOutcomes,
+    params: rulesListFilter.ruleParams,
+    search: rulesListFilter.searchText,
+    status: rulesListFilter.ruleStatuses,
+    tags: rulesListFilter.tags,
+    type: rulesListFilter.types,
+  };
+};
+
+export const useRulesListFilterStore = ({
+  lastResponseFilter,
+  lastRunOutcomeFilter,
+  rulesListKey = RULES_LIST_FILTERS_KEY,
+  ruleParamFilter,
+  statusFilter,
+  searchFilter,
+  typeFilter,
+}: FilterStoreProps): {
+  filters: RulesListFilters;
+  setFiltersStore: (params: UpdateFiltersProps) => void;
+  numberOfFiltersStore: number;
+  resetFiltersStore: () => void;
+} => {
+  const history = useHistory();
+  const urlStateStorage = createKbnUrlStateStorage({
+    history,
+    useHash: false,
+    useHashQuery: false,
+  });
+
+  const [rulesListFilterLocal, setRulesListFilterLocal] = useLocalStorage<FilterParameters>(
+    `${RULES_LIST_FILTERS_KEY}_filters`,
+    {}
+  );
+  const hasFilterFromLocalStorage = useMemo(
+    () =>
+      rulesListFilterLocal
+        ? !Object.values(rulesListFilterLocal).every((filters) => isEmpty(filters))
+        : false,
+    [rulesListFilterLocal]
+  );
+
+  const rulesListFilterUrl = useMemo(
+    () => urlStateStorage.get<FilterParameters>('_a') ?? {},
+    [urlStateStorage]
+  );
+
+  const hasFilterFromUrl = useMemo(
+    () =>
+      rulesListFilterUrl
+        ? !Object.values(rulesListFilterUrl).every((filters) => isEmpty(filters))
+        : false,
+    [rulesListFilterUrl]
+  );
+
+  const filtersStore = useMemo(
+    () =>
+      hasFilterFromUrl ? rulesListFilterUrl : hasFilterFromLocalStorage ? rulesListFilterLocal : {},
+    [hasFilterFromLocalStorage, hasFilterFromUrl, rulesListFilterLocal, rulesListFilterUrl]
+  );
+  const [filters, setFilters] = useState<RulesListFilters>({
+    actionTypes: filtersStore?.actionTypes ?? [],
+    ruleExecutionStatuses: lastResponseFilter ?? [],
+    ruleLastRunOutcomes: filtersStore?.lastResponse ?? lastRunOutcomeFilter ?? [],
+    ruleParams: filtersStore?.params ?? ruleParamFilter ?? {},
+    ruleStatuses: filtersStore?.status ?? statusFilter ?? [],
+    searchText: filtersStore?.search ?? searchFilter ?? '',
+    tags: filtersStore?.tags ?? [],
+    types: filtersStore?.type ?? typeFilter ?? [],
+    kueryNode: undefined,
+  });
+
+  const updateUrlFilters = useCallback(
+    (updatedParams: RulesListFilters) => {
+      urlStateStorage.set('_a', convertRulesListFiltersToFilterAttributes(updatedParams));
+    },
+    [urlStateStorage]
+  );
+
+  const updateLocalFilters = useCallback(
+    (updatedParams: RulesListFilters) => {
+      setRulesListFilterLocal(convertRulesListFiltersToFilterAttributes(updatedParams));
+    },
+    [setRulesListFilterLocal]
+  );
+
+  const setFiltersStore = useCallback(
+    (updateFiltersProps: UpdateFiltersProps) => {
+      const { filter, value } = updateFiltersProps;
+      setFilters((prev) => {
+        const newFilters = {
+          ...prev,
+          [filter]: value,
+        };
+        updateUrlFilters(newFilters);
+        updateLocalFilters(newFilters);
+        return newFilters;
+      });
+    },
+    [updateLocalFilters, updateUrlFilters]
+  );
+
+  const resetFiltersStore = useCallback(() => {
+    const resetFilter = {
+      actionTypes: [],
+      ruleExecutionStatuses: [],
+      ruleLastRunOutcomes: [],
+      ruleParams: {},
+      ruleStatuses: [],
+      searchText: '',
+      tags: [],
+      types: [],
+      kueryNode: undefined,
+    };
+    setFilters(resetFilter);
+    updateUrlFilters(resetFilter);
+    updateLocalFilters(resetFilter);
+  }, [updateLocalFilters, updateUrlFilters]);
+
+  useEffect(() => {
+    if (hasFilterFromUrl || hasFilterFromLocalStorage) {
+      setFilters({
+        actionTypes: filtersStore?.actionTypes ?? [],
+        ruleExecutionStatuses: lastResponseFilter ?? [],
+        ruleLastRunOutcomes: filtersStore?.lastResponse ?? lastRunOutcomeFilter ?? [],
+        ruleParams: filtersStore?.params ?? ruleParamFilter ?? {},
+        ruleStatuses: filtersStore?.status ?? statusFilter ?? [],
+        searchText: filtersStore?.search ?? searchFilter ?? '',
+        tags: filtersStore?.tags ?? [],
+        types: filtersStore?.type ?? typeFilter ?? [],
+        kueryNode: undefined,
+      });
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return useMemo(
+    () => ({
+      filters,
+      setFiltersStore,
+      numberOfFiltersStore: Object.values(filters).filter((filter) => !isEmpty(filter)).length,
+      resetFiltersStore,
+    }),
+    [filters, resetFiltersStore, setFiltersStore]
+  );
+};

commit cdfdd4e0f6548e32e005acb1c29e76d41f8a3e5c

[RAM] Fix bug where select all rules bypasses filters (#176962)
## Summary

Fixes #176867 

A bug introduced in https://github.com/elastic/kibana/pull/174954
bypassed most filters when using Select All on the Rules List. This was
because the names of the filter properties changed, and no longer
matched what the `useBulkEditSelect` hook was expecting.

Because all of these types were optional, it didn't trigger any type
errors.

This syncs up the type definitions with the new filter store hook, and
adds a functional test to make sure filters are working on bulk actions
when clicking the select all button.

### Checklist

- [x] [Unit or functional
tests](https://www.elastic.co/guide/en/kibana/master/development-tests.html)
were updated or added to match the most common scenarios

x-pack/plugins/triggers_actions_ui/public/application/hooks/use_bulk_edit_select.tsx
@@ -7,7 +7,7 @@
 import { useReducer, useMemo, useCallback } from 'react';
 import { fromKueryExpression, nodeBuilder } from '@kbn/es-query';
 import { mapFiltersToKueryNode } from '../lib/rule_api/map_filters_to_kuery_node';
-import { RuleTableItem, RuleStatus } from '../../types';
+import { RuleTableItem, RulesListFilters } from '../../types';
 
 interface BulkEditSelectionState {
   selectedIds: Set<string>;
@@ -73,27 +73,11 @@ const reducer = (state: BulkEditSelectionState, action: Action) => {
 interface UseBulkEditSelectProps {
   totalItemCount: number;
   items: RuleTableItem[];
-  typesFilter?: string[];
-  actionTypesFilter?: string[];
-  tagsFilter?: string[];
-  ruleExecutionStatusesFilter?: string[];
-  ruleLastRunOutcomesFilter?: string[];
-  ruleStatusesFilter?: RuleStatus[];
-  searchText?: string;
+  filters?: RulesListFilters;
 }
 
 export function useBulkEditSelect(props: UseBulkEditSelectProps) {
-  const {
-    totalItemCount = 0,
-    items = [],
-    typesFilter,
-    actionTypesFilter,
-    tagsFilter,
-    ruleExecutionStatusesFilter,
-    ruleLastRunOutcomesFilter,
-    ruleStatusesFilter,
-    searchText,
-  } = props;
+  const { totalItemCount = 0, items = [], filters } = props;
 
   const [state, dispatch] = useReducer(reducer, {
     ...initialState,
@@ -187,15 +171,20 @@ export function useBulkEditSelect(props: UseBulkEditSelectProps) {
 
   const getFilterKueryNode = useCallback(
     (idsToExclude?: string[]) => {
-      const ruleFilterKueryNode = mapFiltersToKueryNode({
-        typesFilter,
-        actionTypesFilter,
-        tagsFilter,
-        ruleExecutionStatusesFilter,
-        ruleLastRunOutcomesFilter,
-        ruleStatusesFilter,
-        searchText,
-      });
+      const ruleFilterKueryNode = mapFiltersToKueryNode(
+        filters
+          ? {
+              typesFilter: filters.types,
+              actionTypesFilter: filters.actionTypes,
+              tagsFilter: filters.tags,
+              ruleExecutionStatusesFilter: filters.ruleExecutionStatuses,
+              ruleLastRunOutcomesFilter: filters.ruleLastRunOutcomes,
+              ruleParamsFilter: filters.ruleParams,
+              ruleStatusesFilter: filters.ruleStatuses,
+              searchText: filters.searchText,
+            }
+          : {}
+      );
 
       if (idsToExclude && idsToExclude.length) {
         const excludeFilter = fromKueryExpression(
@@ -209,15 +198,7 @@ export function useBulkEditSelect(props: UseBulkEditSelectProps) {
 
       return ruleFilterKueryNode;
     },
-    [
-      typesFilter,
-      actionTypesFilter,
-      tagsFilter,
-      ruleExecutionStatusesFilter,
-      ruleLastRunOutcomesFilter,
-      ruleStatusesFilter,
-      searchText,
-    ]
+    [filters]
   );
 
   const getFilter = useCallback(() => {

x-pack/plugins/triggers_actions_ui/public/application/sections/rules_list/components/rules_list.tsx
@@ -363,8 +363,7 @@ export const RulesList = ({
   } = useBulkEditSelect({
     totalItemCount: rulesState.totalItemCount,
     items: tableItems,
-    ...filters,
-    typesFilter: rulesTypesFilter,
+    filters: { ...filters, types: rulesTypesFilter },
   });
 
   const handleUpdateFiltersEffect = useCallback(

x-pack/plugins/triggers_actions_ui/public/application/hooks/use_bulk_edit_select.test.tsx
@@ -45,8 +45,7 @@ describe('useBulkEditSelectTest', () => {
       useBulkEditSelect({
         items,
         totalItemCount: 4,
-        tagsFilter: ['test: 123'],
-        searchText: 'rules*',
+        filters: { tags: ['test: 123'], searchText: 'rules*' },
       })
     );
 
@@ -58,8 +57,7 @@ describe('useBulkEditSelectTest', () => {
       useBulkEditSelect({
         items,
         totalItemCount: 4,
-        tagsFilter: ['test: 123'],
-        searchText: 'rules*',
+        filters: { tags: ['test: 123'], searchText: 'rules*' },
       })
     );
 
@@ -107,8 +105,7 @@ describe('useBulkEditSelectTest', () => {
       useBulkEditSelect({
         items,
         totalItemCount: 4,
-        tagsFilter: ['test: 123'],
-        searchText: 'rules*',
+        filters: { tags: ['test: 123'], searchText: 'rules*' },
       })
     );
 
@@ -124,8 +121,10 @@ describe('useBulkEditSelectTest', () => {
       useBulkEditSelect({
         items,
         totalItemCount: 4,
-        tagsFilter: ['test: 123'],
-        searchText: 'rules*',
+        filters: {
+          tags: ['test: 123'],
+          searchText: 'rules*',
+        },
       })
     );
 

x-pack/test/functional_with_es_ssl/apps/triggers_actions_ui/rules_list/bulk_actions.ts
@@ -14,7 +14,12 @@
 
 import expect from '@kbn/expect';
 import { FtrProviderContext } from '../../../ftr_provider_context';
-import { createAlert, scheduleRule, snoozeAlert } from '../../../lib/alert_api_actions';
+import {
+  createAlert,
+  createAlertManualCleanup,
+  scheduleRule,
+  snoozeAlert,
+} from '../../../lib/alert_api_actions';
 import { ObjectRemover } from '../../../lib/object_remover';
 
 export default ({ getPageObjects, getService }: FtrProviderContext) => {
@@ -30,7 +35,7 @@ export default ({ getPageObjects, getService }: FtrProviderContext) => {
     await testSubjects.click('rulesTab');
   }
 
-  describe('rules list', () => {
+  describe('rules list bulk actions', () => {
     before(async () => {
       await pageObjects.common.navigateToApp('triggersActions');
       await testSubjects.click('rulesTab');
@@ -205,6 +210,84 @@ export default ({ getPageObjects, getService }: FtrProviderContext) => {
         const toastTitle = await toasts.getTitleAndDismiss();
         expect(toastTitle).to.eql('Updated API key for 1 rule.');
       });
+    });
+
+    it('should apply filters to bulk actions when using the select all button', async () => {
+      const rule1 = await createAlert({
+        supertest,
+        objectRemover,
+        overwrites: { name: 'a' },
+      });
+      const rule2 = await createAlertManualCleanup({
+        supertest,
+        overwrites: { name: 'b', rule_type_id: 'test.always-firing' },
+      });
+      const rule3 = await createAlert({
+        supertest,
+        objectRemover,
+        overwrites: { name: 'c' },
+      });
+
+      await refreshAlertsList();
+      expect(await testSubjects.getVisibleText('totalRulesCount')).to.be('3 rules');
+
+      await testSubjects.click('ruleTypeFilterButton');
+      await testSubjects.existOrFail('ruleTypetest.noopFilterOption');
+      await testSubjects.click('ruleTypetest.noopFilterOption');
+      await testSubjects.click(`checkboxSelectRow-${rule1.id}`);
+      await testSubjects.click('selectAllRulesButton');
+
+      await testSubjects.click('showBulkActionButton');
+      await testSubjects.click('bulkDisable');
+      await testSubjects.existOrFail('untrackAlertsModal');
+      await testSubjects.click('confirmModalConfirmButton');
+
+      await retry.try(async () => {
+        const toastTitle = await toasts.getTitleAndDismiss();
+        expect(toastTitle).to.eql('Disabled 2 rules');
+      });
+
+      await testSubjects.click('rules-list-clear-filter');
+      await refreshAlertsList();
+
+      await pageObjects.triggersActionsUI.searchAlerts(rule1.name);
+      expect(await testSubjects.getVisibleText('statusDropdown')).to.be('Disabled');
+      await pageObjects.triggersActionsUI.searchAlerts(rule2.name);
+      expect(await testSubjects.getVisibleText('statusDropdown')).to.be('Enabled');
+      await pageObjects.triggersActionsUI.searchAlerts(rule3.name);
+      expect(await testSubjects.getVisibleText('statusDropdown')).to.be('Disabled');
+
+      await testSubjects.click('rules-list-clear-filter');
+      await refreshAlertsList();
+
+      await testSubjects.click('ruleStatusFilterButton');
+      await testSubjects.existOrFail('ruleStatusFilterOption-enabled');
+      await testSubjects.click('ruleStatusFilterOption-enabled');
+      await testSubjects.click(`checkboxSelectRow-${rule2.id}`);
+      await testSubjects.click('selectAllRulesButton');
+
+      await testSubjects.click('showBulkActionButton');
+      await testSubjects.click('bulkDelete');
+      await testSubjects.existOrFail('rulesDeleteConfirmation');
+      await testSubjects.click('confirmModalConfirmButton');
+
+      await retry.try(async () => {
+        const toastTitle = await toasts.getTitleAndDismiss();
+        expect(toastTitle).to.eql('Deleted 1 rule');
+      });
+
+      await testSubjects.click('rules-list-clear-filter');
+      await refreshAlertsList();
+
+      await retry.try(
+        async () => {
+          expect(await testSubjects.getVisibleText('totalRulesCount')).to.be('2 rules');
+        },
+        async () => {
+          // If the delete fails, make sure rule2 gets cleaned up
+          objectRemover.add(rule2.id, 'alert', 'alerts');
+        }
+      );
     });
   });
 };
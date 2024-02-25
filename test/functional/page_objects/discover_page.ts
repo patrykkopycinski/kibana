patrykkopycinski/kibana

commit 967f18333d1164a3f565bca5c957bc407301b132

Fix flaky test from #173292 and #173784 (#176978)
## Summary

This PR fixes the flaky test from #173292 and #173784.

Flaky test runner x95:
https://buildkite.com/elastic/kibana-flaky-test-suite-runner/builds/5193.

Resolves #173292.
Resolves #173784.

### Checklist

- [ ] Any text added follows [EUI's writing
guidelines](https://elastic.github.io/eui/#/guidelines/writing), uses
sentence case text and includes [i18n
support](https://github.com/elastic/kibana/blob/main/packages/kbn-i18n/README.md)
- [ ]
[Documentation](https://www.elastic.co/guide/en/kibana/master/development-documentation.html)
was added for features that require explanation or tutorials
- [x] [Unit or functional
tests](https://www.elastic.co/guide/en/kibana/master/development-tests.html)
were updated or added to match the most common scenarios
- [x] [Flaky Test
Runner](https://ci-stats.kibana.dev/trigger_flaky_test_runner/1) was
used on any tests changed
- [ ] Any UI touched in this PR is usable by keyboard only (learn more
about [keyboard accessibility](https://webaim.org/techniques/keyboard/))
- [ ] Any UI touched in this PR does not create any new axe failures
(run axe in browser:
[FF](https://addons.mozilla.org/en-US/firefox/addon/axe-devtools/),
[Chrome](https://chrome.google.com/webstore/detail/axe-web-accessibility-tes/lhdoppojpmngadmnindnejefpokejbdd?hl=en-US))
- [ ] If a plugin configuration key changed, check if it needs to be
allowlisted in the cloud and added to the [docker
list](https://github.com/elastic/kibana/blob/main/src/dev/build/tasks/os_packages/docker_generator/resources/base/bin/kibana-docker)
- [ ] This renders correctly on smaller devices using a responsive
layout. (You can test this [in your
browser](https://www.browserstack.com/guide/responsive-testing-on-local-server))
- [ ] This was checked for [cross-browser
compatibility](https://www.elastic.co/support/matrix#matrix_browsers)

### For maintainers

x-pack/test_serverless/functional/test_suites/common/discover/group1/_discover.ts
@@ -152,9 +152,7 @@ export default function ({ getService, getPageObjects }: FtrProviderContext) {
       });
     });
 
-    // FLAKY: https://github.com/elastic/kibana/issues/173292
-    // FLAKY: https://github.com/elastic/kibana/issues/173784
-    describe.skip('query #2, which has an empty time range', () => {
+    describe('query #2, which has an empty time range', () => {
       const fromTime = 'Jun 11, 1999 @ 09:22:11.000';
       const toTime = 'Jun 12, 1999 @ 11:21:04.000';
 
@@ -177,8 +175,11 @@ export default function ({ getService, getPageObjects }: FtrProviderContext) {
       });
 
       it('should show matches when time range is expanded', async () => {
-        await PageObjects.discover.expandTimeRangeAsSuggestedInNoResultsMessage();
-        await PageObjects.discover.waitUntilSearchingHasFinished();
+        await retry.waitFor('view all matches to load', async () => {
+          await PageObjects.discover.expandTimeRangeAsSuggestedInNoResultsMessage();
+          await PageObjects.discover.waitUntilSearchingHasFinished();
+          return !(await testSubjects.exists('discoverNoResultsViewAllMatches'));
+        });
         await retry.try(async function () {
           expect(await PageObjects.discover.hasNoResults()).to.be(false);
           expect(await PageObjects.discover.getHitCountInt()).to.be.above(0);

test/functional/apps/discover/group1/_discover.ts
@@ -170,8 +170,11 @@ export default function ({ getService, getPageObjects }: FtrProviderContext) {
       });
 
       it('should show matches when time range is expanded', async () => {
-        await PageObjects.discover.expandTimeRangeAsSuggestedInNoResultsMessage();
-        await PageObjects.discover.waitUntilSearchingHasFinished();
+        await retry.waitFor('view all matches to load', async () => {
+          await PageObjects.discover.expandTimeRangeAsSuggestedInNoResultsMessage();
+          await PageObjects.discover.waitUntilSearchingHasFinished();
+          return !(await testSubjects.exists('discoverNoResultsViewAllMatches'));
+        });
         await retry.try(async function () {
           expect(await PageObjects.discover.hasNoResults()).to.be(false);
           expect(await PageObjects.discover.getHitCountInt()).to.be.above(0);

src/plugins/discover/public/application/main/components/no_results/no_results_suggestions/use_fetch_occurances_range.ts
@@ -66,7 +66,10 @@ export const useFetchOccurrencesRange = (params: Params): Result => {
             abortSignal: abortControllerRef.current?.signal,
           });
         } catch (error) {
-          //
+          if (error.name !== 'AbortError') {
+            // eslint-disable-next-line no-console
+            console.error(error);
+          }
         }
       }
 

commit b6859fc278c0c5ea86896ab4581b5a2cc643e0a1

Fix flaky test from #173292 and #173784 part 2 (#177530)
## Summary

Let's try this again... This PR again attempts to fix the flaky test
from #173292 and #173784 (and now #175047 too), which was still failing
after #176978 was merged.

The latest failures are due to the "View all matches" button
disappearing between when it was last checked in the retry loop and when
we attempt to click it again, so I've updated the test to handle
exceptions thrown when we attempt to re-click the button.

Hopefully this will eliminate the flakiness, but I'm open to any
suggestions for alternative approaches if someone has a better idea.

~Flaky test runner x95:
https://buildkite.com/elastic/kibana-flaky-test-suite-runner/builds/5266.~
~Flaky test runner x95:
https://buildkite.com/elastic/kibana-flaky-test-suite-runner/builds/5284.~
Flaky test runner x95:
https://buildkite.com/elastic/kibana-flaky-test-suite-runner/builds/5286.
Flaky test runner x95:
https://buildkite.com/elastic/kibana-flaky-test-suite-runner/builds/5305.

Resolves #173292.
Resolves #173784.
Resolves #175047.

### Checklist

- [ ] Any text added follows [EUI's writing
guidelines](https://elastic.github.io/eui/#/guidelines/writing), uses
sentence case text and includes [i18n
support](https://github.com/elastic/kibana/blob/main/packages/kbn-i18n/README.md)
- [ ]
[Documentation](https://www.elastic.co/guide/en/kibana/master/development-documentation.html)
was added for features that require explanation or tutorials
- [x] [Unit or functional
tests](https://www.elastic.co/guide/en/kibana/master/development-tests.html)
were updated or added to match the most common scenarios
- [x] [Flaky Test
Runner](https://ci-stats.kibana.dev/trigger_flaky_test_runner/1) was
used on any tests changed
- [ ] Any UI touched in this PR is usable by keyboard only (learn more
about [keyboard accessibility](https://webaim.org/techniques/keyboard/))

x-pack/test_serverless/functional/test_suites/common/discover/group1/_discover.ts
@@ -31,8 +31,7 @@ export default function ({ getService, getPageObjects }: FtrProviderContext) {
     defaultIndex: 'logstash-*',
   };
 
-  // Failing: See https://github.com/elastic/kibana/issues/173784
-  describe.skip('discover test', function describeIndexTests() {
+  describe('discover test', function describeIndexTests() {
     before(async function () {
       log.debug('load kibana index with default index pattern');
       await kibanaServer.importExport.load('test/functional/fixtures/kbn_archiver/discover');
@@ -176,11 +175,7 @@ export default function ({ getService, getPageObjects }: FtrProviderContext) {
       });
 
       it('should show matches when time range is expanded', async () => {
-        await retry.waitFor('view all matches to load', async () => {
-          await PageObjects.discover.expandTimeRangeAsSuggestedInNoResultsMessage();
-          await PageObjects.discover.waitUntilSearchingHasFinished();
-          return !(await testSubjects.exists('discoverNoResultsViewAllMatches'));
-        });
+        await PageObjects.discover.expandTimeRangeAsSuggestedInNoResultsMessage();
         await retry.try(async function () {
           expect(await PageObjects.discover.hasNoResults()).to.be(false);
           expect(await PageObjects.discover.getHitCountInt()).to.be.above(0);

test/functional/apps/discover/group1/_discover.ts
@@ -170,11 +170,7 @@ export default function ({ getService, getPageObjects }: FtrProviderContext) {
       });
 
       it('should show matches when time range is expanded', async () => {
-        await retry.waitFor('view all matches to load', async () => {
-          await PageObjects.discover.expandTimeRangeAsSuggestedInNoResultsMessage();
-          await PageObjects.discover.waitUntilSearchingHasFinished();
-          return !(await testSubjects.exists('discoverNoResultsViewAllMatches'));
-        });
+        await PageObjects.discover.expandTimeRangeAsSuggestedInNoResultsMessage();
         await retry.try(async function () {
           expect(await PageObjects.discover.hasNoResults()).to.be(false);
           expect(await PageObjects.discover.getHitCountInt()).to.be.above(0);

test/functional/page_objects/discover_page.ts
@@ -532,7 +532,25 @@ export class DiscoverPageObject extends FtrService {
     await this.retry.waitFor('the button before pressing it', async () => {
       return await this.testSubjects.exists('discoverNoResultsViewAllMatches');
     });
-    return await this.testSubjects.click('discoverNoResultsViewAllMatches');
+    await this.retry.waitForWithTimeout('view all matches to load', 60000, async () => {
+      try {
+        // We need to manually click the button since testSubjects.click will
+        // use a retry, but we want this to throw if the click fails since it
+        // means the button disappeared before we could click it
+        const button = await this.testSubjects.find('discoverNoResultsViewAllMatches', 1000);
+        // Don't click the button if it's disabled since it means the previous
+        // click succeeded and the request is still loading
+        if (await button.isEnabled()) {
+          await button.click();
+        }
+      } catch {
+        // We could get an exception here if the button isn't found or isn't in
+        // the DOM by the time we try to click it, so just ignore it and move on
+      }
+      await this.waitUntilSearchingHasFinished();
+      await this.header.waitUntilLoadingHasFinished();
+      return !(await this.testSubjects.exists('discoverNoResultsViewAllMatches'));
+    });
   }
 
   public async clickFieldSort(field: string, text = 'Sort New-Old') {
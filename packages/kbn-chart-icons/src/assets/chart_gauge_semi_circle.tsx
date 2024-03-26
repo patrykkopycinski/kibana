636766a198b9580119c78a80f8fc9b3967056917
Nick Partridge <nicholas.partridge@elastic.co>
2024-03-26 08:42:13 -0700

    [Lens] Replace `expression_gauge` from `Goal` to `Bullet` (#177766)
    ## Summary
    
    Migrates the Lens `Bullet Horizontal` and `Bullet Vertical` charts to the new design. Adds the `Semi-circular Gauge`, `Arc Gauge` and `Circular Gauge` chart types to Lens. See comparison between old and new designs below.
    
    ### Horizontal Bullet
    
    <img width="891" alt="image"
    src="https://github.com/elastic/kibana/assets/19007109/18c3ec03-203f-4058-a543-4c7bde58dfd2">
    
    ### Vertical Bullet
    
    <img width="736" alt="image"
    src="https://github.com/elastic/kibana/assets/19007109/f3da410a-8003-4647-8027-f6a13416aa38">
    
    ### Semi-circular Gauge
    
    <img width="870" alt="image"
    src="https://github.com/elastic/kibana/assets/19007109/b1b3cc1b-1155-4fac-8ae7-fb1992fdfa99">
    
    ### Arc Gauge
    
    <img width="849" alt="image"
    src="https://github.com/elastic/kibana/assets/19007109/bf552604-44cf-4db8-819d-a4d1558fc679">
    
    ### Circular Gauge
    
    <img width="846" alt="image"
    src="https://github.com/elastic/kibana/assets/19007109/19ee5dc3-fd86-434c-ad62-6063361f60e5">

--- packages/kbn-chart-icons/src/assets/chart_gauge_semi_circle.tsx
+++ packages/kbn-chart-icons/src/assets/chart_gauge_semi_circle.tsx
@@ -10,7 +10,7 @@ import React from 'react';
 import type { EuiIconProps } from '@elastic/eui';
 import { colors } from './common_styles';
 
-export const IconChartVerticalBullet = ({
+export const IconChartGaugeSemiCircle = ({
   title,
   titleId,
   ...props
@@ -28,12 +28,13 @@ export const IconChartVerticalBullet = ({
       {title ? <title id={titleId}>{title}</title> : null}
       <path
         className={colors.accent}
-        d="M16 22a1 1 0 01-1-1V4a1 1 0 011-1h4a1 1 0 011 1v17a1 1 0 01-1 1h-4z"
+        d="M3 17.9c-.8 0-1.5-.7-1.5-1.5 0-3.1 1.1-6.1 3-8.5 2.6-3.2 6.5-5 10.6-5 3.4 0 6.7 1.3 9.2 3.6.6.6.7 1.5.1 2.1-.6.6-1.5.6-2.1.1-2-1.8-4.5-2.8-7.2-2.8-3.2 0-6.2 1.4-8.2 3.9-1.5 1.9-2.3 4.2-2.3 6.6-.1.9-.7 1.5-1.6 1.5z"
       />
-      <path
-        className={colors.subdued}
-        d="M10 0h2a1 1 0 011 1v20a1 1 0 01-1 1h-2a1 1 0 110-2h1v-3h-1a1 1 0 110-2h1v-3h-1a1 1 0 110-2h1V7h-1a1 1 0 010-2h1V2h-1a1 1 0 010-2z"
-      />
+      <g className={colors.subdued}>
+        <path d="M24.3 16.7h-2c0-4-3.3-7.3-7.3-7.3-2.2 0-4.3 1-5.7 2.7-1.1 1.3-1.6 2.9-1.6 4.6h-2c0-2.1.7-4.2 2.1-5.8 1.8-2.2 4.4-3.5 7.3-3.5 5-.1 9.2 4.1 9.2 9.3z" />
+        <path d="M7.5 17.6h1c.6 0 1-.4 1-1s-.4-1-1-1h-1m6.7-6.8v1c0 .6.4 1 1 1s1-.4 1-1v-1m-7.3 3.1.7.7c.4.4 1 .4 1.4 0 .4-.4.4-1 0-1.4l-.7-.7m9.3 0-.7.7c-.4.4-.4 1 0 1.4.4.4 1 .4 1.4 0l.7-.7m1.55 3.7h-1c-.6 0-1 .4-1 1s.4 1 1 1h1" />
+        <path d="M6.6 17.6h1v-1H5.7c0 .5.4 1 .9 1zm15.8 0h1c.5 0 1-.4 1-1h-1.9v1z" />
+      </g>
     </svg>
   );
 };

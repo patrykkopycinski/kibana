patrykkopycinski/kibana

commit cdfa465faa812894579cbeb957cebca2fcc54ce5

[UII] Add unprivileged vs privileged agent count to Fleet UI (#183077)
## Summary

Resolves https://github.com/elastic/ingest-dev/issues/3249

This PR:

- Adds `unprivileged_agents: number` to agent policy object(s) returned
by agent policy list, get, and bulk get APIs
- Adds breakdown of Unprivileged vs Privileged vs total agent count to
agent policy table and agent policy details header
- Removes Description column from agent policy table and adds the
description below agent policy name
- Adds `Privilege mode` to agent details page


![Image](https://github.com/elastic/ingest-dev/assets/1965714/79c2b57d-2946-4a98-9735-4c42a02676d1)


![Image](https://github.com/elastic/ingest-dev/assets/1965714/6ddc8c3a-175b-4d43-87b5-155193295558)

<img width="1352" alt="image"
src="https://github.com/elastic/kibana/assets/1965714/f4e49c3b-7bb2-411a-892f-a6a38650b820">

#### Testing
To load unprivileged agents, run them in docker. Local agents or agents
running in VM will be privileged.

### Checklist

Delete any items that are not applicable to this PR.

- [x] Any text added follows [EUI's writing
guidelines](https://elastic.github.io/eui/#/guidelines/writing), uses
sentence case text and includes [i18n
support](https://github.com/elastic/kibana/blob/main/packages/kbn-i18n/README.md)
- [x]
[Documentation](https://www.elastic.co/guide/en/kibana/master/development-documentation.html)
was added for features that require explanation or tutorials
- [x] [Unit or functional
tests](https://www.elastic.co/guide/en/kibana/master/development-tests.html)
were updated or added to match the most common scenarios

x-pack/plugins/fleet/public/components/linked_agent_count.tsx
@@ -11,7 +11,7 @@ import type { EuiLinkAnchorProps } from '@elastic/eui';
 import { EuiLink } from '@elastic/eui';
 
 import { useLink } from '../hooks';
-import { AGENTS_PREFIX } from '../constants';
+import { AGENTS_PREFIX, UNPRIVILEGED_AGENT_KUERY, PRIVILEGED_AGENT_KUERY } from '../constants';
 
 /**
  * Displays the provided `count` number as a link to the Agents list if it is greater than zero
@@ -21,8 +21,9 @@ export const LinkedAgentCount = memo<
     count: number;
     agentPolicyId: string;
     showAgentText?: boolean;
+    privilegeMode?: 'privileged' | 'unprivileged';
   }
->(({ count, agentPolicyId, showAgentText, ...otherEuiLinkProps }) => {
+>(({ count, agentPolicyId, showAgentText, privilegeMode, ...otherEuiLinkProps }) => {
   const { getHref } = useLink();
   const displayValue = showAgentText ? (
     <FormattedMessage
@@ -33,13 +34,13 @@ export const LinkedAgentCount = memo<
   ) : (
     count
   );
+  const kuery = `${AGENTS_PREFIX}.policy_id : ${agentPolicyId}${
+    privilegeMode &&
+    ` and ${privilegeMode === 'unprivileged' ? UNPRIVILEGED_AGENT_KUERY : PRIVILEGED_AGENT_KUERY}`
+  }`;
+
   return count > 0 ? (
-    <EuiLink
-      {...otherEuiLinkProps}
-      href={getHref('agent_list', {
-        kuery: `${AGENTS_PREFIX}.policy_id : ${agentPolicyId}`,
-      })}
-    >
+    <EuiLink {...otherEuiLinkProps} href={getHref('agent_list', { kuery })}>
       {displayValue}
     </EuiLink>
   ) : (

x-pack/plugins/fleet/common/constants/agent.ts
@@ -47,3 +47,8 @@ export const AgentStatuses = [
   'updating',
   'degraded',
 ] as const;
+
+// Kueries for finding unprivileged and privileged agents
+// Privileged is `not` because the metadata field can be undefined
+export const UNPRIVILEGED_AGENT_KUERY = `${AGENTS_PREFIX}.local_metadata.elastic.agent.unprivileged: true`;
+export const PRIVILEGED_AGENT_KUERY = `not ${AGENTS_PREFIX}.local_metadata.elastic.agent.unprivileged: true`;

x-pack/plugins/fleet/common/openapi/bundled.yaml
@@ -4864,6 +4864,8 @@ components:
           type: number
         agents:
           type: number
+        unprivileged_agents:
+          type: number
         agent_features:
           type: array
           items:

x-pack/plugins/fleet/common/openapi/components/schemas/agent_policy.yaml
@@ -50,6 +50,8 @@ properties:
     type: number
   agents:
     type: number
+  unprivileged_agents:
+    type: number
   agent_features:
     type: array
     items:

x-pack/plugins/fleet/common/types/models/agent_policy.ts
@@ -54,6 +54,7 @@ export interface AgentPolicy extends Omit<NewAgentPolicy, 'id'> {
   updated_by: string;
   revision: number;
   agents?: number;
+  unprivileged_agents?: number;
   is_protected: boolean;
 }
 

x-pack/plugins/fleet/common/types/rest_spec/agent_policy.ts
@@ -16,7 +16,7 @@ export interface GetAgentPoliciesRequest {
   };
 }
 
-export type GetAgentPoliciesResponseItem = AgentPolicy & { agents?: number };
+export type GetAgentPoliciesResponseItem = AgentPolicy;
 
 export type BulkGetAgentPoliciesResponse = BulkGetResult<GetAgentPoliciesResponseItem>;
 export type GetAgentPoliciesResponse = ListResult<GetAgentPoliciesResponseItem>;

x-pack/plugins/fleet/public/applications/fleet/sections/agent_policy/details_page/components/header/right_content.stories.tsx
@@ -18,7 +18,6 @@ export const HeaderRightContent = () => {
   return (
     <div style={{ maxWidth: '800px' }}>
       <Component
-        policyId="test123"
         isLoading={false}
         addAgent={() => {}}
         isAddAgentHelpPopoverOpen={false}
@@ -29,11 +28,6 @@ export const HeaderRightContent = () => {
             revision: 1,
             updated_at: new Date().toISOString(),
             package_policies: ['test1', 'test2'],
-          } as any
-        }
-        agentStatus={
-          {
-            total: 0,
           } as any
         }
       />
@@ -45,7 +39,6 @@ export const HeaderRightContentWithManagedPolicy = () => {
   return (
     <div style={{ maxWidth: '800px' }}>
       <Component
-        policyId="test123"
         isLoading={false}
         addAgent={() => {}}
         isAddAgentHelpPopoverOpen={false}
@@ -57,11 +50,6 @@ export const HeaderRightContentWithManagedPolicy = () => {
             updated_at: new Date().toISOString(),
             package_policies: ['test1', 'test2'],
             is_managed: true,
-          } as any
-        }
-        agentStatus={
-          {
-            total: 0,
           } as any
         }
       />

x-pack/plugins/fleet/public/applications/fleet/sections/agent_policy/details_page/components/header/right_content.tsx
@@ -18,19 +18,18 @@ import {
   EuiDescriptionListTitle,
   EuiDescriptionListDescription,
   EuiLink,
+  EuiToolTip,
 } from '@elastic/eui';
 
 import { useAuthz, useLink } from '../../../../../hooks';
-import type { AgentPolicy, GetAgentStatusResponse } from '../../../../../types';
+import type { AgentPolicy } from '../../../../../types';
 import { AgentPolicyActionMenu, LinkedAgentCount } from '../../../components';
 import { AddAgentHelpPopover } from '../../../../../components';
 import { FLEET_SERVER_PACKAGE } from '../../../../../../../../common/constants';
 
 export interface HeaderRightContentProps {
   isLoading: boolean;
-  policyId: string;
   agentPolicy?: AgentPolicy | null;
-  agentStatus?: GetAgentStatusResponse['results'];
   addAgent: () => void;
   onCancelEnrollment?: () => void;
   isAddAgentHelpPopoverOpen: boolean;
@@ -45,9 +44,7 @@ const Divider = styled.div`
 
 export const HeaderRightContent: React.FunctionComponent<HeaderRightContentProps> = ({
   isLoading,
-  policyId,
   agentPolicy,
-  agentStatus,
   addAgent,
   onCancelEnrollment,
   isAddAgentHelpPopoverOpen,
@@ -86,118 +83,134 @@ export const HeaderRightContent: React.FunctionComponent<HeaderRightContentProps
 
   return (
     <EuiFlexGroup justifyContent={'flexEnd'} direction="row">
-      {[
-        {
-          label: i18n.translate('xpack.fleet.policyDetails.summary.revision', {
-            defaultMessage: 'Revision',
-          }),
-          content: agentPolicy?.revision ?? 0,
-        },
-        { isDivider: true },
-        {
-          label: i18n.translate('xpack.fleet.policyDetails.summary.integrations', {
-            defaultMessage: 'Integrations',
-          }),
-          content: (
-            <EuiI18nNumber
-              value={
-                (agentPolicy &&
-                  agentPolicy.package_policies &&
-                  agentPolicy.package_policies.length) ||
-                0
-              }
-            />
-          ),
-        },
-        { isDivider: true },
-        ...(authz.fleet.readAgents
-          ? [
-              {
-                label: i18n.translate('xpack.fleet.policyDetails.summary.usedBy', {
-                  defaultMessage: 'Agents',
-                }),
-                content:
-                  agentStatus && agentStatus!.total ? (
-                    <LinkedAgentCount
-                      count={agentStatus.total}
-                      agentPolicyId={(agentPolicy && agentPolicy.id) || ''}
-                      showAgentText
-                    />
-                  ) : isFleetServerPolicy && authz.fleet.addFleetServers ? (
-                    <AddAgentHelpPopover
-                      button={addFleetServerLink}
-                      isOpen={isAddAgentHelpPopoverOpen}
-                      offset={15}
-                      closePopover={() => {
-                        setIsAddAgentHelpPopoverOpen(false);
-                      }}
-                    />
-                  ) : !isFleetServerPolicy && authz.fleet.addAgents ? (
-                    <AddAgentHelpPopover
-                      button={addAgentLink}
-                      isOpen={isAddAgentHelpPopoverOpen}
-                      offset={15}
-                      closePopover={() => {
-                        setIsAddAgentHelpPopoverOpen(false);
-                      }}
-                    />
-                  ) : (
-                    <LinkedAgentCount
-                      count={0}
-                      agentPolicyId={(agentPolicy && agentPolicy.id) || ''}
-                      showAgentText
-                    />
-                  ),
-              },
-              { isDivider: true },
-            ]
-          : []),
-        {
-          label: i18n.translate('xpack.fleet.policyDetails.summary.lastUpdated', {
-            defaultMessage: 'Last updated on',
-          }),
-          content:
-            (agentPolicy && (
-              <FormattedDate
-                value={agentPolicy?.updated_at}
-                year="numeric"
-                month="short"
-                day="2-digit"
-              />
-            )) ||
-            '',
-        },
-        { isDivider: true },
-        {
-          content: agentPolicy && (
-            <AgentPolicyActionMenu
-              agentPolicy={agentPolicy}
-              fullButton={true}
-              onCopySuccess={(newAgentPolicy: AgentPolicy) => {
-                history.push(getPath('policy_details', { policyId: newAgentPolicy.id }));
-              }}
-              onCancelEnrollment={onCancelEnrollment}
-            />
-          ),
-        },
-      ].map((item, index) => (
-        <EuiFlexItem grow={false} key={index}>
-          {item.isDivider ?? false ? (
-            <Divider />
-          ) : item.label ? (
-            <EuiDescriptionList compressed textStyle="reverse" style={{ textAlign: 'right' }}>
-              <EuiDescriptionListTitle className="eui-textNoWrap">
-                {item.label}
-              </EuiDescriptionListTitle>
-              <EuiDescriptionListDescription className="eui-textNoWrap">
-                {item.content}
-              </EuiDescriptionListDescription>
-            </EuiDescriptionList>
-          ) : (
-            item.content
-          )}
-        </EuiFlexItem>
-      ))}
+      {isLoading || !agentPolicy
+        ? null
+        : [
+            {
+              label: i18n.translate('xpack.fleet.policyDetails.summary.revision', {
+                defaultMessage: 'Revision',
+              }),
+              content: agentPolicy.revision ?? 0,
+            },
+            { isDivider: true },
+            {
+              label: i18n.translate('xpack.fleet.policyDetails.summary.integrations', {
+                defaultMessage: 'Integrations',
+              }),
+              content: (
+                <EuiI18nNumber
+                  value={(agentPolicy.package_policies && agentPolicy.package_policies.length) || 0}
+                />
+              ),
+            },
+            { isDivider: true },
+            ...(authz.fleet.readAgents
+              ? [
+                  {
+                    label: i18n.translate('xpack.fleet.policyDetails.summary.usedBy', {
+                      defaultMessage: 'Agents',
+                    }),
+                    content:
+                      !agentPolicy.agents && isFleetServerPolicy && authz.fleet.addFleetServers ? (
+                        <AddAgentHelpPopover
+                          button={addFleetServerLink}
+                          isOpen={isAddAgentHelpPopoverOpen}
+                          offset={15}
+                          closePopover={() => {
+                            setIsAddAgentHelpPopoverOpen(false);
+                          }}
+                        />
+                      ) : !agentPolicy.agents && !isFleetServerPolicy && authz.fleet.addAgents ? (
+                        <AddAgentHelpPopover
+                          button={addAgentLink}
+                          isOpen={isAddAgentHelpPopoverOpen}
+                          offset={15}
+                          closePopover={() => {
+                            setIsAddAgentHelpPopoverOpen(false);
+                          }}
+                        />
+                      ) : (
+                        <EuiToolTip
+                          content={
+                            <EuiFlexGroup direction="column" gutterSize="xs">
+                              <EuiFlexItem>
+                                <FormattedMessage
+                                  id="xpack.fleet.policyDetails.summary.usedByUnprivilegedTooltip"
+                                  defaultMessage="{count, plural, one {# unprivileged agent} other {# unprivileged agents}}"
+                                  values={{ count: agentPolicy.unprivileged_agents || 0 }}
+                                />
+                              </EuiFlexItem>
+                              <EuiFlexItem>
+                                <FormattedMessage
+                                  id="xpack.fleet.policyDetails.summary.usedByPrivilegedTooltip"
+                                  defaultMessage="{count, plural, one {# privileged agent} other {# privileged agents}}"
+                                  values={{
+                                    count:
+                                      (agentPolicy.agents || 0) -
+                                      (agentPolicy.unprivileged_agents || 0),
+                                  }}
+                                />
+                              </EuiFlexItem>
+                            </EuiFlexGroup>
+                          }
+                        >
+                          <LinkedAgentCount
+                            count={agentPolicy.agents || 0}
+                            agentPolicyId={agentPolicy.id}
+                            showAgentText={true}
+                          />
+                        </EuiToolTip>
+                      ),
+                  },
+                  { isDivider: true },
+                ]
+              : []),
+            {
+              label: i18n.translate('xpack.fleet.policyDetails.summary.lastUpdated', {
+                defaultMessage: 'Last updated on',
+              }),
+              content:
+                (agentPolicy && (
+                  <FormattedDate
+                    value={agentPolicy?.updated_at}
+                    year="numeric"
+                    month="short"
+                    day="2-digit"
+                  />
+                )) ||
+                '',
+            },
+            { isDivider: true },
+            {
+              content: agentPolicy && (
+                <AgentPolicyActionMenu
+                  agentPolicy={agentPolicy}
+                  fullButton={true}
+                  onCopySuccess={(newAgentPolicy: AgentPolicy) => {
+                    history.push(getPath('policy_details', { policyId: newAgentPolicy.id }));
+                  }}
+                  onCancelEnrollment={onCancelEnrollment}
+                />
+              ),
+            },
+          ].map((item, index) => (
+            <EuiFlexItem grow={false} key={index}>
+              {item.isDivider ?? false ? (
+                <Divider />
+              ) : item.label ? (
+                <EuiDescriptionList compressed textStyle="reverse" style={{ textAlign: 'right' }}>
+                  <EuiDescriptionListTitle className="eui-textNoWrap">
+                    {item.label}
+                  </EuiDescriptionListTitle>
+                  <EuiDescriptionListDescription className="eui-textNoWrap">
+                    {item.content}
+                  </EuiDescriptionListDescription>
+                </EuiDescriptionList>
+              ) : (
+                item.content
+              )}
+            </EuiFlexItem>
+          ))}
     </EuiFlexGroup>
   );
 };

commit 654d548f5f68443b8ef892ee8d940e392a2e7bcc

[UII] Fix all agent link kuery showing as undefined (#183333)
## Summary

Fixes issue introduced in https://github.com/elastic/kibana/pull/183077
where linking to all agents had `undefined` added to the end of the
kuery 🫠

Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>

x-pack/plugins/fleet/public/components/linked_agent_count.tsx
@@ -35,8 +35,11 @@ export const LinkedAgentCount = memo<
     count
   );
   const kuery = `${AGENTS_PREFIX}.policy_id : ${agentPolicyId}${
-    privilegeMode &&
-    ` and ${privilegeMode === 'unprivileged' ? UNPRIVILEGED_AGENT_KUERY : PRIVILEGED_AGENT_KUERY}`
+    privilegeMode
+      ? ` and ${
+          privilegeMode === 'unprivileged' ? UNPRIVILEGED_AGENT_KUERY : PRIVILEGED_AGENT_KUERY
+        }`
+      : ''
   }`;
 
   return count > 0 ? (
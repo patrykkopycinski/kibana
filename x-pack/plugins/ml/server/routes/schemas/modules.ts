patrykkopycinski/kibana

commit f9c43f61c987ec0b236f87012fda1fd4c22c7f2a

[ML] Replace apiDoc annotations with routes definitions for OpenAPI spec generation   (#190840)
## Summary

- Removes `apidoc-markdown` dependency and custom scripts for generating
internal documentation for ML Kibana endpoints
- Replaces apidoc annotations with `summary` and `description` props for
route handlers and kbn-schemas to generate an OpenAPI spec file
- `/internal/ml/jobs/delete_jobs` route was not versioned for some
reason, I changed that


#### How to test 

1. Enable OAS endpoint in `kibana.dev.yml`

```yaml
server.oas.enabled: true
```
2. Start Kibana dev server 
```bash
yarn start --no-base-path
```
3. Call the OAS endpoint for ML _internal_ routes 
```bash
curl -s -u <USERNAME>:<PASSWORD> http://localhost:5601/api/oas\?pathStartsWith\=/internal/ml\&access\=internal -o ml_kibana_openapi.json
```

x-pack/plugins/ml/server/routes/schemas/modules.ts
@@ -8,98 +8,169 @@
 import { schema } from '@kbn/config-schema';
 
 export const setupModuleBodySchema = schema.object({
-  /**
-   * Job ID prefix. This will be added to the start of the ID every job created by the module (optional).
-   */
-  prefix: schema.maybe(schema.string()),
-  /**
-   * List of group IDs. This will override the groups assigned to each job created by the module (optional).
-   */
-  groups: schema.maybe(schema.arrayOf(schema.string())),
-  /**
-   * Name of kibana index pattern. Overrides the index used in each datafeed and each index pattern
-   * used in the custom urls and saved objects created by the module. A matching index pattern must
-   * exist in kibana if the module contains custom urls or saved objects which rely on an index pattern ID.
-   * If the module does not contain custom urls or saved objects which require an index pattern ID, the
-   * indexPatternName can be any index name or pattern that will match an ES index. It can also be a comma
-   * separated list of names. If no indexPatternName is supplied, the default index pattern specified in
-   * the manifest.json will be used (optional).
-   */
-  indexPatternName: schema.maybe(schema.string()),
-  /**
-   * ES Query DSL object. Overrides the query object for each datafeed created by the module (optional).
-   */
-  query: schema.maybe(schema.any()),
-  /**
-   * Flag to specify that each job created by the module uses a dedicated index (optional).
-   */
-  useDedicatedIndex: schema.maybe(schema.boolean()),
-  /**
-   * Flag to specify that each datafeed created by the module is started once saved. Defaults to <code>false</code> (optional).
-   */
-  startDatafeed: schema.maybe(schema.boolean()),
-  /**
-   * Start date for datafeed. Specified in epoch seconds. Only used if startDatafeed is true.
-   * If not specified, a value of 0 is used i.e. start at the beginning of the data (optional).
-   */
-  start: schema.maybe(schema.number()),
-  /**
-   * End date for datafeed. Specified in epoch seconds. Only used if startDatafeed is true.
-   * If not specified, the datafeed will continue to run in real time (optional).
-   */
-  end: schema.maybe(schema.number()),
-  /**
-   * Partial job configuration which will override jobs contained in the module. Can be an array of objects.
-   * If a <code>job_id</code> is specified, only that job in the module will be overridden.
-   * Applied before any of the existing
-   * overridable options (e.g. useDedicatedIndex, groups, indexPatternName etc)
-   * and so can be overridden themselves (optional).
-   */
-  jobOverrides: schema.maybe(schema.any()),
-  /**
-   * Partial datafeed configuration which will override datafeeds contained in the module.
-   * Can be an array of objects.
-   * If a datafeed_id or a job_id is specified,
-   * only that datafeed in the module will be overridden. Applied before any of the existing
-   * overridable options (e.g. useDedicatedIndex, groups, indexPatternName etc)
-   * and so can be overridden themselves (optional).
-   */
-  datafeedOverrides: schema.maybe(schema.any()),
-  /**
-   * Indicates whether an estimate of the model memory limit
-   * should be made by checking the cardinality of fields in the job configurations (optional).
-   */
-  estimateModelMemory: schema.maybe(schema.boolean()),
-
-  /**
-   * Add each job created to the * space (optional)
-   */
-  applyToAllSpaces: schema.maybe(schema.boolean()),
+  prefix: schema.maybe(
+    schema.string({
+      meta: {
+        description:
+          'Job ID prefix. This will be added to the start of the ID every job created by the module (optional).',
+      },
+    })
+  ),
+  groups: schema.maybe(
+    schema.arrayOf(schema.string(), {
+      meta: {
+        description:
+          'List of group IDs. This will override the groups assigned to each job created by the module (optional).',
+      },
+    })
+  ),
+  indexPatternName: schema.maybe(
+    schema.string({
+      meta: {
+        description: `Name of kibana index pattern. Overrides the index used in each datafeed and each index pattern used in the custom urls and saved objects created by the module. A matching index pattern must exist in kibana if the module contains custom urls or saved objects which rely on an index pattern ID. If the module does not contain custom urls or saved objects which require an index pattern ID, the indexPatternName can be any index name or pattern that will match an ES index. It can also be a comma separated list of names. If no indexPatternName is supplied, the default index pattern specified in the manifest.json will be used (optional).`,
+      },
+    })
+  ),
+  query: schema.maybe(
+    schema.any({
+      meta: {
+        description:
+          'ES Query DSL object. Overrides the query object for each datafeed created by the module (optional).',
+      },
+    })
+  ),
+  useDedicatedIndex: schema.maybe(
+    schema.boolean({
+      meta: {
+        description:
+          'Flag to specify that each job created by the module uses a dedicated index (optional).',
+      },
+    })
+  ),
+  startDatafeed: schema.maybe(
+    schema.boolean({
+      meta: {
+        description:
+          'Flag to specify that each datafeed created by the module is started once saved. Defaults to <code>false</code> (optional).',
+      },
+    })
+  ),
+  start: schema.maybe(
+    schema.number({
+      meta: {
+        description:
+          'Start date for datafeed. Specified in epoch seconds. Only used if startDatafeed is true. If not specified, a value of 0 is used i.e. start at the beginning of the data (optional).',
+      },
+    })
+  ),
+  end: schema.maybe(
+    schema.number({
+      meta: {
+        description: `End date for datafeed. Specified in epoch seconds. Only used if startDatafeed is true. If not specified, the datafeed will continue to run in real time (optional).`,
+      },
+    })
+  ),
+  jobOverrides: schema.maybe(
+    schema.any({
+      meta: {
+        description:
+          'Partial job configuration which will override jobs contained in the module. Can be an array of objects. If a <code>job_id</code> is specified, only that job in the module will be overridden. Applied before any of the existing overridable options (e.g. useDedicatedIndex, groups, indexPatternName etc) and so can be overridden themselves (optional).',
+      },
+    })
+  ),
+  datafeedOverrides: schema.maybe(
+    schema.any({
+      meta: {
+        description:
+          'Partial datafeed configuration which will override datafeeds contained in the module. Can be an array of objects. If a datafeed_id or a job_id is specified, only that datafeed in the module will be overridden. Applied before any of the existing overridable options (e.g. useDedicatedIndex, groups, indexPatternName etc) and so can be overridden themselves (optional).',
+      },
+    })
+  ),
+  estimateModelMemory: schema.maybe(
+    schema.boolean({
+      meta: {
+        description:
+          'Indicates whether an estimate of the model memory limit should be made by checking the cardinality of fields in the job configurations (optional).',
+      },
+    })
+  ),
+  applyToAllSpaces: schema.maybe(
+    schema.boolean({ meta: { description: 'Add each job created to the * space (optional)' } })
+  ),
 });
 
 export const optionalModuleIdParamSchema = schema.object({
-  /**
-   * ID of the module.
-   */
-  moduleId: schema.maybe(schema.string()),
+  moduleId: schema.maybe(schema.string({ meta: { description: 'ID of the module' } })),
 });
 
 export const moduleIdParamSchema = schema.object({
-  /**
-   * ID of the module.
-   */
-  moduleId: schema.string(),
+  moduleId: schema.string({ meta: { description: 'ID of the module' } }),
 });
 
 export const recognizeModulesSchema = schema.object({
-  /**
-   * Index pattern to recognize. Note that this does not need to be a Kibana
-   * index pattern, and can be the name of a single Elasticsearch index,
-   * or include a wildcard (*) to match multiple indices.
-   */
-  indexPatternTitle: schema.string(),
+  indexPatternTitle: schema.string({
+    meta: {
+      description:
+        'Index pattern to recognize. Note that this does not need to be a Kibana index pattern, and can be the name of a single Elasticsearch index, or include a wildcard (*) to match multiple indices.',
+    },
+  }),
 });
 
 export const moduleFilterSchema = schema.object({
   filter: schema.maybe(schema.string()),
 });
+
+export const recognizeModulesSchemaResponse = () =>
+  schema.arrayOf(
+    schema.object({
+      id: schema.string(),
+      title: schema.string(),
+      query: schema.any(),
+      description: schema.string(),
+      logo: schema.any(),
+    })
+  );
+
+const moduleSchema = schema.object({
+  id: schema.string(),
+  title: schema.string(),
+  description: schema.string(),
+  type: schema.string(),
+  logo: schema.maybe(schema.any()),
+  logoFile: schema.maybe(schema.string()),
+  defaultIndexPattern: schema.string(),
+  query: schema.any(),
+  jobs: schema.arrayOf(schema.any()),
+  datafeeds: schema.arrayOf(schema.any()),
+  kibana: schema.any(),
+  tags: schema.maybe(schema.arrayOf(schema.string())),
+});
+
+export const getModulesSchemaResponse = () =>
+  schema.oneOf([moduleSchema, schema.arrayOf(moduleSchema)]);
+
+/**
+ *
+ * @link DataRecognizerConfigResponse
+ */
+export const dataRecognizerConfigResponse = () =>
+  schema.object({
+    datafeeds: schema.arrayOf(schema.any()),
+    jobs: schema.arrayOf(schema.any()),
+    kibana: schema.any(),
+  });
+
+export const jobExistsResponse = () =>
+  schema.object({
+    jobsExist: schema.boolean(),
+    jobs: schema.maybe(
+      schema.arrayOf(
+        schema.object({
+          id: schema.string(),
+          earliestTimestampMs: schema.number(),
+          latestTimestampMs: schema.number(),
+          latestResultsTimestampMs: schema.maybe(schema.number()),
+        })
+      )
+    ),
+  });

commit affa2046e284d2139731357a50d1e4c31f45102b

[ML] Fix schema definition for the module endpoint  (#191633)
## Summary

Fixes an issue introduced in
https://github.com/elastic/kibana/pull/190840 where the module endpoint
had a wrong type definition for validation the response causing the
Rules page in Security plugin to crash.

x-pack/plugins/ml/server/routes/schemas/modules.ts
@@ -138,11 +138,11 @@ const moduleSchema = schema.object({
   type: schema.string(),
   logo: schema.maybe(schema.any()),
   logoFile: schema.maybe(schema.string()),
-  defaultIndexPattern: schema.string(),
-  query: schema.any(),
+  defaultIndexPattern: schema.maybe(schema.string()),
+  query: schema.maybe(schema.any()),
   jobs: schema.arrayOf(schema.any()),
   datafeeds: schema.arrayOf(schema.any()),
-  kibana: schema.any(),
+  kibana: schema.maybe(schema.any()),
   tags: schema.maybe(schema.arrayOf(schema.string())),
 });
 
@@ -157,7 +157,7 @@ export const dataRecognizerConfigResponse = () =>
   schema.object({
     datafeeds: schema.arrayOf(schema.any()),
     jobs: schema.arrayOf(schema.any()),
-    kibana: schema.any(),
+    kibana: schema.maybe(schema.any()),
   });
 
 export const jobExistsResponse = () =>
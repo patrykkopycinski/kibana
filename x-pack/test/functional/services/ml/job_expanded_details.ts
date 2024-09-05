patrykkopycinski/kibana

commit 245f24b1b8c1566770224b69b4e234d628cf4141

[ML] Remove dependency cache. (#189729)
## Summary

Fixes #153477.
Fixes #153476.
Part of #187772 (technical debt).
Part of #153288 (migrate enzyme tests to react-testing-lib).

Removes dependency cache. The major culprit making this PR large and not
easy to split is that `getHttp()` from the dependency cache was used
throughout the code base for services like `mlJobService` and
`ml/mlApiServices` which then themselves were directly imported and not
part of React component lifecycles.

- For functional components this means mostly migrating to hooks that
allow accessing services.
- We still have a bit of a mix of usage of `withKibana` and `context`
for class based React components. This was not consolidated in this PR,
I took what's there and adjusted how services get used. These components
access services via `this.props.kibana.services.*` or
`this.context.services.*`.
- Functions no longer access the global services provided via dependency
cache but were updated to receive services via arguments.
- Stateful services like `mlJobService` are exposed now via a factory
that makes sure the service gets instantiated only once.
- Some tests where the mocks needed quite some refactoring were ported
to `react-testing-lib`. They no longer make use of snapshots or call
component methods which should be considered implementation details.
- We have a mix of usage of the plain `toasts` via `useMlKibana` and our
own `toastNotificationServices` that wraps `toasts`. I didn't
consolidate this in this PR but used what's available for the given
code.
- For class based components, service initializations were moved from
`componentDidMount()` to `constructor()` where I spotted it.
- We have a bit of a mix of naming: `ml`, `mlApiServices`,
`useMlApiContext()` for the same thing. I didn't consolidate the naming
in this PR, to avoid making this PR even larger. This can be done in a
follow up, once this PR is in this should be more straightforward and

x-pack/plugins/ml/public/application/components/annotations/annotations_table/annotations_table.js
@@ -29,10 +29,11 @@ import {
 } from '@elastic/eui';
 import { i18n } from '@kbn/i18n';
 import { FormattedMessage } from '@kbn/i18n-react';
+import { withKibana } from '@kbn/kibana-react-plugin/public';
 
 import { addItemToRecentlyAccessed } from '../../../util/recently_accessed';
-import { ml } from '../../../services/ml_api_service';
-import { mlJobService } from '../../../services/job_service';
+import { mlJobServiceFactory } from '../../../services/job_service';
+import { toastNotificationServiceProvider } from '../../../services/toast_notification_service';
 import { mlTableService } from '../../../services/table_service';
 import { ANNOTATIONS_TABLE_DEFAULT_QUERY_SIZE } from '../../../../../common/constants/search';
 import {
@@ -45,7 +46,6 @@ import {
   ANNOTATION_EVENT_USER,
   ANNOTATION_EVENT_DELAYED_DATA,
 } from '../../../../../common/constants/annotations';
-import { withKibana } from '@kbn/kibana-react-plugin/public';
 import { ML_APP_LOCATOR, ML_PAGES } from '../../../../../common/constants/locator';
 import { timeFormatter } from '@kbn/ml-date-utils';
 import { MlAnnotationUpdatesContext } from '../../../contexts/ml/ml_annotation_updates_context';
@@ -90,10 +90,8 @@ class AnnotationsTableUI extends Component {
       queryText: `event:(${ANNOTATION_EVENT_USER} or ${ANNOTATION_EVENT_DELAYED_DATA})`,
       searchError: undefined,
       jobId:
-        Array.isArray(this.props.jobs) &&
-        this.props.jobs.length > 0 &&
-        this.props.jobs[0] !== undefined
-          ? this.props.jobs[0].job_id
+        Array.isArray(props.jobs) && props.jobs.length > 0 && props.jobs[0] !== undefined
+          ? props.jobs[0].job_id
           : undefined,
       datafeedFlyoutVisible: false,
       modelSnapshot: null,
@@ -103,6 +101,10 @@ class AnnotationsTableUI extends Component {
     this.sorting = {
       sort: { field: 'timestamp', direction: 'asc' },
     };
+    this.mlJobService = mlJobServiceFactory(
+      toastNotificationServiceProvider(props.kibana.services.notifications.toasts),
+      props.kibana.services.mlServices.mlApiServices
+    );
   }
 
   getAnnotations() {
@@ -112,6 +114,8 @@ class AnnotationsTableUI extends Component {
     this.setState({
       isLoading: true,
     });
+
+    const ml = this.props.kibana.services.mlServices.mlApiServices;
 
     if (dataCounts.processed_record_count > 0) {
       // Load annotations for the selected job.
@@ -177,7 +181,7 @@ class AnnotationsTableUI extends Component {
       }
     }
 
-    return mlJobService.getJob(jobId);
+    return this.mlJobService.getJob(jobId);
   }
 
   annotationsRefreshSubscription = null;

x-pack/plugins/ml/public/application/jobs/jobs_list/components/job_details/forecasts_table/forecasts_table.js
@@ -39,13 +39,15 @@ const MAX_FORECASTS = 500;
  * Table component for rendering the lists of forecasts run on an ML job.
  */
 export class ForecastsTable extends Component {
-  constructor(props) {
-    super(props);
+  constructor(props, constructorContext) {
+    super(props, constructorContext);
     this.state = {
       isLoading: props.job.data_counts.processed_record_count !== 0,
       forecasts: [],
     };
-    this.mlForecastService;
+    this.mlForecastService = forecastServiceFactory(
+      constructorContext.services.mlServices.mlApiServices
+    );
   }
 
   /**
@@ -54,7 +56,6 @@ export class ForecastsTable extends Component {
   static contextType = context;
 
   componentDidMount() {
-    this.mlForecastService = forecastServiceFactory(this.context.services.mlServices.mlApiServices);
     const dataCounts = this.props.job.data_counts;
     if (dataCounts.processed_record_count > 0) {
       // Get the list of all the forecasts with results at or later than the specified 'from' time.

commit 703887834e0f2ced97efdc4e71289b10cbcecfe0

[ML] Links to the Single Metric Viewer from the Annotations and Forecasts table fix (#192000)
## Summary

Fix for: [#191936](https://github.com/elastic/kibana/issues/191936)
Fixes navigation from the Forecasts & Annotations table to the Single
Metric Viewer. Added functional tests.
Tested on 8.15 - issue doesn't exist, it was probably introduced in
[#189729](https://github.com/elastic/kibana/pull/189729)

### Checklist

- [X] [Unit or functional
tests](https://www.elastic.co/guide/en/kibana/master/development-tests.html)
were updated or added to match the most common scenarios

x-pack/plugins/ml/public/application/components/annotations/annotations_table/annotations_table.js
@@ -223,6 +223,7 @@ class AnnotationsTableUI extends Component {
   openSingleMetricView = async (annotation = {}) => {
     const {
       services: {
+        chrome: { recentlyAccessed },
         application: { navigateToUrl },
         share,
       },
@@ -307,7 +308,12 @@ class AnnotationsTableUI extends Component {
       { absolute: true }
     );
 
-    addItemToRecentlyAccessed('timeseriesexplorer', job.job_id, singleMetricViewerLink);
+    addItemToRecentlyAccessed(
+      'timeseriesexplorer',
+      job.job_id,
+      singleMetricViewerLink,
+      recentlyAccessed
+    );
     await navigateToUrl(singleMetricViewerLink);
   };
 

x-pack/plugins/ml/public/application/jobs/jobs_list/components/job_details/forecasts_table/forecasts_table.js
@@ -89,6 +89,7 @@ export class ForecastsTable extends Component {
   async openSingleMetricView(forecast) {
     const {
       services: {
+        chrome: { recentlyAccessed },
         application: { navigateToUrl },
         share,
       },
@@ -156,7 +157,8 @@ export class ForecastsTable extends Component {
     addItemToRecentlyAccessed(
       'timeseriesexplorer',
       this.props.job.job_id,
-      singleMetricViewerForecastLink
+      singleMetricViewerForecastLink,
+      recentlyAccessed
     );
     await navigateToUrl(singleMetricViewerForecastLink);
   }

x-pack/test/functional/apps/ml/anomaly_detection_jobs/job_expanded_details.ts
@@ -83,11 +83,21 @@ export default function ({ getService }: FtrProviderContext) {
       await ml.jobExpandedDetails.assertJobRowCalendars(jobId, [calendarId]);
     });
 
-    it('expanded row with forecast should display open forecast button', async () => {
-      await ml.jobExpandedDetails.assertForecastElements(jobId);
+    it('expanded row with forecast should navigate to Single Metric Viewer on button click', async () => {
+      await ml.jobExpandedDetails.openForecastJob(jobId);
+    });
+
+    it('expanded row with annotations should navigate to Single Metric Viewer on button click', async () => {
+      await ml.navigation.navigateToJobManagement();
+
+      const annotationsFromApi = await ml.api.getAnnotations(jobId);
+
+      await ml.jobExpandedDetails.openAnnotationInSingleMetricViewer(jobId, annotationsFromApi);
     });
 
     it('expanded row with annotations can be edited', async () => {
+      await ml.navigation.navigateToJobManagement();
+
       const annotationsFromApi = await ml.api.getAnnotations(jobId);
 
       await ml.jobExpandedDetails.editAnnotation(jobId, 'edited annotation', annotationsFromApi);

x-pack/test/functional/services/ml/job_expanded_details.ts
@@ -46,12 +46,11 @@ export function MachineLearningJobExpandedDetailsProvider(
       });
     },
 
-    async assertForecastElements(jobId: string): Promise<void> {
+    async openForecastJob(jobId: string): Promise<void> {
       await jobTable.ensureDetailsOpen(jobId);
       await this.openForecastTab(jobId);
-      await testSubjects.existOrFail('mlJobListForecastTabOpenSingleMetricViewButton', {
-        timeout: 5_000,
-      });
+      await testSubjects.click('mlJobListForecastTabOpenSingleMetricViewButton', 5000);
+      await testSubjects.existOrFail('mlSingleMetricViewerChart');
     },
 
     async clearSearchButton() {
@@ -61,16 +60,36 @@ export function MachineLearningJobExpandedDetailsProvider(
       }
     },
 
+    async assertAnnotationsFromApi(annotationsFromApi: any) {
+      const length = annotationsFromApi.length;
+      expect(length).to.eql(
+        1,
+        `Expect annotations from api to have length of 1, but got [${length}]`
+      );
+    },
+
+    async openAnnotationInSingleMetricViewer(
+      jobId: string,
+      annotationsFromApi: any
+    ): Promise<void> {
+      await this.assertAnnotationsFromApi(annotationsFromApi);
+
+      const { _id: annotationId }: { _id: string } = annotationsFromApi[0];
+
+      await jobTable.ensureDetailsOpen(jobId);
+      await jobTable.openAnnotationsTab(jobId);
+      await this.clearSearchButton();
+      await jobAnnotationsTable.ensureAnnotationsActionsMenuOpen(annotationId);
+      await testSubjects.click('mlAnnotationsActionOpenInSingleMetricViewer');
+      await testSubjects.existOrFail('mlSingleMetricViewerChart');
+    },
+
     async editAnnotation(
       jobId: string,
       newAnnotationText: string,
       annotationsFromApi: any
     ): Promise<void> {
-      const length = annotationsFromApi.length;
-      expect(length).to.eql(
-        1,
-        `Expect annotions from api to have length of 1, but got [${length}]`
-      );
+      await this.assertAnnotationsFromApi(annotationsFromApi);
 
       await jobTable.ensureDetailsOpen(jobId);
       await jobTable.openAnnotationsTab(jobId);
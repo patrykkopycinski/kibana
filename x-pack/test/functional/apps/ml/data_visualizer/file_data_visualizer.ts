patrykkopycinski/kibana

commit ee1a147baca52dca5703663d35b66e7c44f3b676

[ML] Fix file upload with no ingest pipeline (#193744)
With some datasets the find structure api will not generate an ingest
pipeline. A recent
[change](https://github.com/elastic/kibana/pull/186956) to how we catch
and display errors during file upload means an upload with no pipeline
now produces an error which aborts the upload.
Previously all pipeline creation errors were ignored and hidden from the
user.

This PR changes changes the file upload endpoint to allow it to receive
no ingest pipeline and also changes the UI to not display the pipeline
creation step during upload.

This file can be used to test the fix.
https://github.com/elastic/eland/blob/main/tests/flights.json.gz

x-pack/plugins/file_upload/server/import_data.ts
@@ -21,7 +21,7 @@ export function importDataProvider({ asCurrentUser }: IScopedClusterClient) {
     index: string,
     settings: IndicesIndexSettings,
     mappings: MappingTypeMapping,
-    ingestPipeline: IngestPipelineWrapper,
+    ingestPipeline: IngestPipelineWrapper | undefined,
     data: InputData
   ): Promise<ImportResponse> {
     let createdIndex;
@@ -29,7 +29,8 @@ export function importDataProvider({ asCurrentUser }: IScopedClusterClient) {
     const docCount = data.length;
 
     try {
-      const { id: pipelineId, pipeline } = ingestPipeline;
+      const pipelineId = ingestPipeline?.id;
+      const pipeline = ingestPipeline?.pipeline;
 
       if (id === undefined) {
         // first chunk of data, create the index and id to return
@@ -48,7 +49,6 @@ export function importDataProvider({ asCurrentUser }: IScopedClusterClient) {
         createdPipelineId = pipelineId;
       } else {
         createdIndex = index;
-        createdPipelineId = pipelineId;
       }
 
       let failures: ImportFailure[] = [];
@@ -109,7 +109,7 @@ export function importDataProvider({ asCurrentUser }: IScopedClusterClient) {
     await asCurrentUser.indices.create({ index, body }, { maxRetries: 0 });
   }
 
-  async function indexData(index: string, pipelineId: string, data: InputData) {
+  async function indexData(index: string, pipelineId: string | undefined, data: InputData) {
     try {
       const body = [];
       for (let i = 0; i < data.length; i++) {

x-pack/test/functional/apps/ml/data_visualizer/file_data_visualizer.ts
@@ -180,6 +180,42 @@ export default function ({ getService }: FtrProviderContext) {
         ingestedDocCount: 3,
       },
     },
+    {
+      suiteSuffix: 'with a file which does not generate a ingest pipeline',
+      filePath: require.resolve('./files_to_import/flights_small.json'),
+      indexName: 'user-import_4',
+      createIndexPattern: false,
+      fieldTypeFilters: [ML_JOB_FIELD_TYPES.KEYWORD],
+      fieldNameFilters: ['timestamp'],
+      expected: {
+        results: {
+          title: 'flights_small.json',
+          highlightedText: false,
+        },
+        metricFields: [],
+        nonMetricFields: [
+          {
+            fieldName: 'Carrier',
+            type: ML_JOB_FIELD_TYPES.KEYWORD,
+            docCountFormatted: '20 (100%)',
+            exampleCount: 4,
+          },
+          {
+            fieldName: 'timestamp',
+            type: ML_JOB_FIELD_TYPES.KEYWORD,
+            docCountFormatted: '20 (100%)',
+            exampleCount: 11,
+          },
+        ],
+        visibleMetricFieldsCount: 0,
+        totalMetricFieldsCount: 0,
+        populatedFieldsCount: 3,
+        totalFieldsCount: 25,
+        fieldTypeFiltersResultCount: 16,
+        fieldNameFiltersResultCount: 1,
+        ingestedDocCount: 20,
+      },
+    },
   ];
 
   const testDataListNegative = [

x-pack/plugins/data_visualizer/public/application/file_data_visualizer/components/import_view/import.ts
@@ -29,6 +29,7 @@ interface Config {
   mappingsString: string;
   pipelineString: string;
   pipelineId: string | null;
+  createPipeline: boolean;
 }
 
 export async function importData(props: Props, config: Config, setState: (state: unknown) => void) {
@@ -41,6 +42,7 @@ export async function importData(props: Props, config: Config, setState: (state:
     mappingsString,
     pipelineString,
     pipelineId,
+    createPipeline,
   } = config;
   const { format } = results;
 
@@ -86,7 +88,7 @@ export async function importData(props: Props, config: Config, setState: (state:
 
   let settings = {};
   let mappings = {};
-  let pipeline = {};
+  let pipeline;
 
   try {
     settings = JSON.parse(indexSettingsString);
@@ -109,7 +111,9 @@ export async function importData(props: Props, config: Config, setState: (state:
   }
 
   try {
-    pipeline = JSON.parse(pipelineString);
+    if (createPipeline) {
+      pipeline = JSON.parse(pipelineString) as IngestPipeline;
+    }
   } catch (error) {
     success = false;
     const parseError = i18n.translate('xpack.dataVisualizer.file.importView.parsePipelineError', {
@@ -143,12 +147,7 @@ export async function importData(props: Props, config: Config, setState: (state:
     return;
   }
 
-  const initializeImportResp = await importer.initializeImport(
-    index,
-    settings,
-    mappings,
-    pipeline as IngestPipeline
-  );
+  const initializeImportResp = await importer.initializeImport(index, settings, mappings, pipeline);
 
   const timeFieldName = importer.getTimeField();
   setState({ timeFieldName });
@@ -158,14 +157,20 @@ export async function importData(props: Props, config: Config, setState: (state:
     indexCreatedStatus: getSuccess(indexCreated),
   });
 
-  const pipelineCreated = initializeImportResp.pipelineId !== undefined;
-  if (indexCreated) {
-    setState({
-      ingestPipelineCreatedStatus: pipelineCreated ? IMPORT_STATUS.COMPLETE : IMPORT_STATUS.FAILED,
-      pipelineId: pipelineCreated ? initializeImportResp.pipelineId : '',
-    });
+  if (createPipeline) {
+    const pipelineCreated = initializeImportResp.pipelineId !== undefined;
+    if (indexCreated) {
+      setState({
+        ingestPipelineCreatedStatus: pipelineCreated
+          ? IMPORT_STATUS.COMPLETE
+          : IMPORT_STATUS.FAILED,
+        pipelineId: pipelineCreated ? initializeImportResp.pipelineId : '',
+      });
+    }
+    success = indexCreated && pipelineCreated;
+  } else {
+    success = indexCreated;
   }
-  success = indexCreated && pipelineCreated;
 
   if (success === false) {
     errors.push(initializeImportResp.error);

x-pack/plugins/data_visualizer/public/application/file_data_visualizer/components/import_view/import_view.js
@@ -109,6 +109,11 @@ export class ImportView extends Component {
       pipelineId,
     } = this.state;
 
+    const createPipeline = pipelineString !== '';
+    this.setState({
+      createPipeline,
+    });
+
     importData(
       { data, results, dataViewsContract, fileUpload },
       {
@@ -119,6 +124,7 @@ export class ImportView extends Component {
         mappingsString,
         pipelineString,
         pipelineId,
+        createPipeline,
       },
       (state) => this.setState(state)
     );

x-pack/plugins/file_upload/public/importer/geo/abstract_geo_file_importer.tsx
@@ -196,9 +196,12 @@ export class AbstractGeoFileImporter extends Importer implements GeoFileImporter
             data: chunks[i],
             settings: {},
             mappings: {},
-            ingestPipeline: {
-              id: pipelineId,
-            },
+            ingestPipeline:
+              pipelineId !== undefined
+                ? {
+                    id: pipelineId,
+                  }
+                : undefined,
           });
 
           if (!this._isActive) {

x-pack/plugins/file_upload/public/importer/importer.ts
@@ -15,7 +15,13 @@ import { i18n } from '@kbn/i18n';
 import { isPopulatedObject } from '@kbn/ml-is-populated-object';
 import { getHttp } from '../kibana_services';
 import { MB } from '../../common/constants';
-import type { ImportDoc, ImportFailure, ImportResponse, IngestPipeline } from '../../common/types';
+import type {
+  ImportDoc,
+  ImportFailure,
+  ImportResponse,
+  IngestPipeline,
+  IngestPipelineWrapper,
+} from '../../common/types';
 import { CreateDocsResponse, IImporter, ImportResults } from './types';
 
 const CHUNK_SIZE = 5000;
@@ -79,25 +85,24 @@ export abstract class Importer implements IImporter {
     index: string,
     settings: IndicesIndexSettings,
     mappings: MappingTypeMapping,
-    pipeline: IngestPipeline
+    pipeline: IngestPipeline | undefined
   ) {
-    updatePipelineTimezone(pipeline);
+    let ingestPipeline: IngestPipelineWrapper | undefined;
+    if (pipeline !== undefined) {
+      updatePipelineTimezone(pipeline);
 
-    if (pipelineContainsSpecialProcessors(pipeline)) {
-      // pipeline contains processors which we know are slow
-      // so reduce the chunk size significantly to avoid timeouts
-      this._chunkSize = REDUCED_CHUNK_SIZE;
+      if (pipelineContainsSpecialProcessors(pipeline)) {
+        // pipeline contains processors which we know are slow
+        // so reduce the chunk size significantly to avoid timeouts
+        this._chunkSize = REDUCED_CHUNK_SIZE;
+      }
+      // if no pipeline has been supplied,
+      // send an empty object
+      ingestPipeline = {
+        id: `${index}-pipeline`,
+        pipeline,
+      };
     }
-
-    // if no pipeline has been supplied,
-    // send an empty object
-    const ingestPipeline =
-      pipeline !== undefined
-        ? {
-            id: `${index}-pipeline`,
-            pipeline,
-          }
-        : {};
 
     this._index = index;
     this._pipeline = pipeline;
@@ -139,9 +144,11 @@ export abstract class Importer implements IImporter {
 
     const chunks = createDocumentChunks(this._docArray, this._chunkSize);
 
-    const ingestPipeline = {
-      id: pipelineId,
-    };
+    const ingestPipeline: IngestPipelineWrapper | undefined = pipelineId
+      ? {
+          id: pipelineId,
+        }
+      : undefined;
 
     let success = true;
     const failures: ImportFailure[] = [];
@@ -345,10 +352,7 @@ export function callImportRoute({
   data: ImportDoc[];
   settings: IndicesIndexSettings;
   mappings: MappingTypeMapping;
-  ingestPipeline: {
-    id?: string;
-    pipeline?: IngestPipeline;
-  };
+  ingestPipeline: IngestPipelineWrapper | undefined;
 }) {
   const query = id !== undefined ? { id } : {};
   const body = JSON.stringify({

x-pack/plugins/file_upload/public/importer/types.ts
@@ -43,7 +43,7 @@ export interface IImporter {
     index: string,
     settings: IndicesIndexSettings,
     mappings: MappingTypeMapping,
-    pipeline: IngestPipeline
+    pipeline: IngestPipeline | undefined
   ): Promise<ImportResponse>;
   import(
     id: string,

x-pack/plugins/file_upload/server/schemas.ts
@@ -37,10 +37,12 @@ export const importFileBodySchema = schema.object({
   /** Mappings */
   mappings: schema.any(),
   /** Ingest pipeline definition */
-  ingestPipeline: schema.object({
-    id: schema.maybe(schema.string()),
-    pipeline: schema.maybe(schema.any()),
-  }),
+  ingestPipeline: schema.maybe(
+    schema.object({
+      id: schema.maybe(schema.string()),
+      pipeline: schema.maybe(schema.any()),
+    })
+  ),
 });
 
 export const runtimeMappingsSchema = schema.object(

commit 6ec07ccbeb31c8bb90a0847f422e7121599b40e0

[ML] Fix file upload when pipline is used (#194273)
Fixes bug introduced in https://github.com/elastic/kibana/pull/193744

If a file is uploaded with an ingest pipeline, the pipeline is created,
but not used when uploading the data.

Adds tests to check that the data exists in the index and the expected
fields exist and are populated.

x-pack/plugins/file_upload/server/import_data.ts
@@ -49,6 +49,7 @@ export function importDataProvider({ asCurrentUser }: IScopedClusterClient) {
         createdPipelineId = pipelineId;
       } else {
         createdIndex = index;
+        createdPipelineId = pipelineId;
       }
 
       let failures: ImportFailure[] = [];

x-pack/test/functional/apps/ml/data_visualizer/file_data_visualizer.ts
@@ -85,6 +85,24 @@ export default function ({ getService }: FtrProviderContext) {
             docCountFormatted: '19 (100%)',
           },
         ],
+        allFields: [
+          '@timestamp',
+          'http',
+          'http.request',
+          'http.request.method',
+          'http.response',
+          'http.response.body',
+          'http.response.body.bytes',
+          'http.response.status_code',
+          'http.version',
+          'message',
+          'source',
+          'source.address',
+          'url',
+          'url.original',
+          'user_agent',
+          'user_agent.original',
+        ],
         visibleMetricFieldsCount: 3,
         totalMetricFieldsCount: 3,
         populatedFieldsCount: 9,
@@ -127,6 +145,7 @@ export default function ({ getService }: FtrProviderContext) {
             exampleCount: 7,
           },
         ],
+        allFields: ['Coordinates', 'Location'],
         visibleMetricFieldsCount: 0,
         totalMetricFieldsCount: 0,
         populatedFieldsCount: 3,
@@ -171,6 +190,7 @@ export default function ({ getService }: FtrProviderContext) {
             exampleCount: 3,
           },
         ],
+        allFields: ['description', 'title', 'value'],
         visibleMetricFieldsCount: 0,
         totalMetricFieldsCount: 0,
         populatedFieldsCount: 3,
@@ -206,6 +226,41 @@ export default function ({ getService }: FtrProviderContext) {
             docCountFormatted: '20 (100%)',
             exampleCount: 11,
           },
+        ],
+        allFields: [
+          'AvgTicketPrice',
+          'Cancelled',
+          'Carrier',
+          'Dest',
+          'DestAirportID',
+          'DestCityName',
+          'DestCountry',
+          'DestLocation',
+          'DestLocation.lat',
+          'DestLocation.lat.keyword',
+          'DestLocation.lon',
+          'DestLocation.lon.keyword',
+          'DestRegion',
+          'DestWeather',
+          'DistanceKilometers',
+          'FlightDelayMin',
+          'FlightDelayType',
+          'FlightNum',
+          'FlightTimeHour',
+          'FlightTimeMin',
+          'Origin',
+          'OriginAirportID',
+          'OriginCityName',
+          'OriginCountry',
+          'OriginLocation',
+          'OriginLocation.lat',
+          'OriginLocation.lat.keyword',
+          'OriginLocation.lon',
+          'OriginLocation.lon.keyword',
+          'OriginRegion',
+          'OriginWeather',
+          'dayOfWeek',
+          'timestamp',
         ],
         visibleMetricFieldsCount: 0,
         totalMetricFieldsCount: 0,
@@ -349,6 +404,16 @@ export default function ({ getService }: FtrProviderContext) {
 
           await ml.testExecution.logTestStep('closes filebeat config');
           await ml.dataVisualizerFileBased.closeCreateFilebeatConfig();
+
+          await ml.dataVisualizerFileBased.assertDocCountInIndex(
+            testData.indexName,
+            testData.expected.ingestedDocCount
+          );
+
+          await ml.dataVisualizerFileBased.assertFieldsFromIndex(
+            testData.indexName,
+            testData.expected.allFields
+          );
         });
       });
     }

x-pack/test/functional/services/ml/data_visualizer_file_based.ts
@@ -16,6 +16,7 @@ export function MachineLearningDataVisualizerFileBasedProvider(
   { getService, getPageObjects }: FtrProviderContext,
   mlCommonUI: MlCommonUI
 ) {
+  const es = getService('es');
   const log = getService('log');
   const retry = getService('retry');
   const testSubjects = getService('testSubjects');
@@ -176,6 +177,47 @@ export function MachineLearningDataVisualizerFileBasedProvider(
     async closeCreateFilebeatConfig() {
       await testSubjects.click('fileBeatConfigFlyoutCloseButton');
       await testSubjects.missingOrFail('fileDataVisFilebeatConfigPanel');
+    },
+
+    async assertDocCountInIndex(index: string, expectedCount: number) {
+      await retry.tryForTime(60 * 1000, async () => {
+        const count = await this.getDocCountFromIndex(index);
+        expect(count).to.eql(
+          expectedCount,
+          `Expected document count in index '${index}' to be '${expectedCount}' (got '${count}')`
+        );
+      });
+    },
+
+    async getDocCountFromIndex(index: string) {
+      const resp = await es.search({
+        index,
+        body: {
+          size: 0,
+          query: {
+            match_all: {},
+          },
+        },
+      });
+      // @ts-expect-error incorrect type definition
+      return resp.hits.total?.value;
+    },
+
+    async assertFieldsFromIndex(index: string, fields: string[]) {
+      await retry.tryForTime(60 * 1000, async () => {
+        const sortedFields = fields.sort();
+        const fieldCaps = await es.fieldCaps({
+          index,
+          fields: '*',
+          filters: '-metadata',
+          include_empty_fields: false,
+        });
+        const fieldsFromIndex = Object.keys(fieldCaps.fields).sort();
+        expect(fieldsFromIndex).to.eql(
+          sortedFields,
+          `Expected fields to be ${sortedFields} (got ${fieldsFromIndex})`
+        );
+      });
     },
   };
 }
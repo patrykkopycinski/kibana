patrykkopycinski/kibana

commit 9975fd63d38a4a6baa27ec34c32d49dab53ec854

[Inventory][ECO] Return metadata values (#195204)
https://github.com/elastic/kibana/issues/194131

Use `entity.identityFields` instead of host, container and service
specific ones. Get the first environment available.

x-pack/plugins/observability_solution/inventory/common/entities.ts
@@ -6,15 +6,16 @@
  */
 import { ENTITY_LATEST, entitiesAliasPattern } from '@kbn/entities-schema';
 import {
-  CONTAINER_ID,
-  HOST_NAME,
   AGENT_NAME,
   CLOUD_PROVIDER,
+  CONTAINER_ID,
   ENTITY_DEFINITION_ID,
   ENTITY_DISPLAY_NAME,
   ENTITY_ID,
+  ENTITY_IDENTITY_FIELDS,
   ENTITY_LAST_SEEN,
   ENTITY_TYPE,
+  HOST_NAME,
   SERVICE_ENVIRONMENT,
   SERVICE_NAME,
 } from '@kbn/observability-shared-plugin/common';
@@ -77,6 +78,7 @@ interface BaseEntity {
   [ENTITY_TYPE]: EntityType;
   [ENTITY_DISPLAY_NAME]: string;
   [ENTITY_DEFINITION_ID]: string;
+  [ENTITY_IDENTITY_FIELDS]: string[];
 }
 
 /**
@@ -85,7 +87,7 @@ interface BaseEntity {
 interface ServiceEntity extends BaseEntity {
   [ENTITY_TYPE]: 'service';
   [SERVICE_NAME]: string;
-  [SERVICE_ENVIRONMENT]?: string | null;
+  [SERVICE_ENVIRONMENT]?: string | string[] | null;
   [AGENT_NAME]: string | string[] | null;
 }
 

x-pack/plugins/observability_solution/inventory/public/components/entities_grid/entity_name/index.tsx
@@ -5,19 +5,20 @@
  * 2.0.
  */
 
-import { EuiLink, EuiFlexGroup, EuiFlexItem } from '@elastic/eui';
-import React, { useCallback } from 'react';
+import { EuiFlexGroup, EuiFlexItem, EuiLink } from '@elastic/eui';
 import {
-  AssetDetailsLocatorParams,
   ASSET_DETAILS_LOCATOR_ID,
-  ServiceOverviewParams,
-  ENTITY_TYPE,
+  AssetDetailsLocatorParams,
   ENTITY_DISPLAY_NAME,
+  ENTITY_IDENTITY_FIELDS,
+  ENTITY_TYPE,
+  SERVICE_ENVIRONMENT,
+  ServiceOverviewParams,
 } from '@kbn/observability-shared-plugin/common';
+import React, { useCallback } from 'react';
+import { Entity } from '../../../../common/entities';
 import { useKibana } from '../../../hooks/use_kibana';
 import { EntityIcon } from '../../entity_icon';
-import { Entity } from '../../../../common/entities';
-import { parseServiceParams } from '../../../utils/parse_service_params';
 
 interface EntityNameProps {
   entity: Entity;
@@ -34,23 +35,23 @@ export function EntityName({ entity }: EntityNameProps) {
 
   const getEntityRedirectUrl = useCallback(() => {
     const type = entity[ENTITY_TYPE];
+    // For service, host and container type there is only one identity field
+    const identityField = entity[ENTITY_IDENTITY_FIELDS][0];
 
     // Any unrecognised types will always return undefined
     switch (type) {
       case 'host':
       case 'container':
         return assetDetailsLocator?.getRedirectUrl({
-          assetId: entity[ENTITY_DISPLAY_NAME],
+          assetId: identityField,
           assetType: type,
         });
 
       case 'service':
-        // For services, the format of the display name is `service.name:service.environment`.
-        // We just want the first part of the name for the locator.
-        // TODO: Replace this with a better approach for handling service names. See https://github.com/elastic/kibana/issues/194131
-        return serviceOverviewLocator?.getRedirectUrl(
-          parseServiceParams(entity[ENTITY_DISPLAY_NAME])
-        );
+        return serviceOverviewLocator?.getRedirectUrl({
+          serviceName: identityField,
+          environment: [entity[SERVICE_ENVIRONMENT] || undefined].flat()[0],
+        });
     }
   }, [entity, assetDetailsLocator, serviceOverviewLocator]);
 

x-pack/plugins/observability_solution/inventory/public/utils/parse_service_params.test.ts
@@ -1,39 +1,0 @@
-/*
- * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
- * or more contributor license agreements. Licensed under the Elastic License
- * 2.0; you may not use this file except in compliance with the Elastic License
- * 2.0.
- */
-
-import { parseServiceParams } from './parse_service_params';
-
-describe('parseServiceParams', () => {
-  it('should return only serviceName with a simple name string', () => {
-    const params = parseServiceParams('service.name');
-
-    expect(params).toEqual({ serviceName: 'service.name' });
-  });
-
-  it('should return both serviceName and environment with a full name string', () => {
-    const params = parseServiceParams('service.name:service.environment');
-
-    expect(params).toEqual({ serviceName: 'service.name', environment: 'service.environment' });
-  });
-
-  it('should ignore multiple colons in the environment portion of the displayName', () => {
-    const params = parseServiceParams('service.name:synthtrace: service.environment');
-
-    expect(params).toEqual({
-      serviceName: 'service.name',
-      environment: 'synthtrace: service.environment',
-    });
-  });
-
-  it('should ignore empty environment names and return only the service.name', () => {
-    const params = parseServiceParams('service.name:');
-
-    expect(params).toEqual({
-      serviceName: 'service.name',
-    });
-  });
-});

x-pack/plugins/observability_solution/inventory/public/utils/parse_service_params.ts
@@ -1,30 +1,0 @@
-/*
- * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
- * or more contributor license agreements. Licensed under the Elastic License
- * 2.0; you may not use this file except in compliance with the Elastic License
- * 2.0.
- */
-
-import type { ServiceOverviewParams } from '@kbn/observability-shared-plugin/common';
-
-/**
- * Parses a displayName string with the format `service.name:service.environment`,
- * returning a valid `ServiceOverviewParams` object.
- * @param displayName A string from a `entity.displayName` field.
- * @returns
- */
-export const parseServiceParams = (displayName: string): ServiceOverviewParams => {
-  const separatorIndex = displayName.indexOf(':');
-
-  const hasEnvironmentName = separatorIndex !== -1;
-
-  const serviceName = hasEnvironmentName ? displayName.slice(0, separatorIndex) : displayName;
-  // Exclude the separator from the sliced string for the environment name.
-  // If the string is empty however, then we default to undefined.
-  const environment = (hasEnvironmentName && displayName.slice(separatorIndex + 1)) || undefined;
-
-  return {
-    serviceName,
-    environment,
-  };
-};

x-pack/plugins/observability_solution/observability_shared/common/field_names/elasticsearch.ts
@@ -153,4 +153,5 @@ export const ENTITY_LAST_SEEN = 'entity.lastSeenTimestamp';
 export const ENTITY_FIRST_SEEN = 'entity.firstSeenTimestamp';
 export const ENTITY_DISPLAY_NAME = 'entity.displayName';
 export const ENTITY_DEFINITION_ID = 'entity.definitionId';
+export const ENTITY_IDENTITY_FIELDS = 'entity.identityFields';
 export const SOURCE_DATA_STREAM_TYPE = 'source_data_stream.type';

x-pack/plugins/observability_solution/observability_shared/common/index.ts
@@ -136,6 +136,7 @@ export {
   ENTITY_LAST_SEEN,
   ENTITY_TYPE,
   SOURCE_DATA_STREAM_TYPE,
+  ENTITY_IDENTITY_FIELDS,
 } from './field_names/elasticsearch';
 
 export {

commit 199e762142b95eeb92c08c738ad3c3558d1b15cf

[Inventory] Fixing entity links (#195625)
Regression from https://github.com/elastic/kibana/pull/195204

x-pack/plugins/observability_solution/inventory/common/entities.ts
@@ -78,26 +78,27 @@ interface BaseEntity {
   [ENTITY_TYPE]: EntityType;
   [ENTITY_DISPLAY_NAME]: string;
   [ENTITY_DEFINITION_ID]: string;
-  [ENTITY_IDENTITY_FIELDS]: string[];
+  [ENTITY_IDENTITY_FIELDS]: string | string[];
+  [key: string]: any;
 }
 
 /**
  * These types are based on service, host and container from the built in definition.
  */
-interface ServiceEntity extends BaseEntity {
+export interface ServiceEntity extends BaseEntity {
   [ENTITY_TYPE]: 'service';
   [SERVICE_NAME]: string;
   [SERVICE_ENVIRONMENT]?: string | string[] | null;
   [AGENT_NAME]: string | string[] | null;
 }
 
-interface HostEntity extends BaseEntity {
+export interface HostEntity extends BaseEntity {
   [ENTITY_TYPE]: 'host';
   [HOST_NAME]: string;
   [CLOUD_PROVIDER]: string | string[] | null;
 }
 
-interface ContainerEntity extends BaseEntity {
+export interface ContainerEntity extends BaseEntity {
   [ENTITY_TYPE]: 'container';
   [CONTAINER_ID]: string;
   [CLOUD_PROVIDER]: string | string[] | null;

x-pack/plugins/observability_solution/inventory/public/components/entities_grid/entity_name/index.tsx
@@ -36,33 +36,37 @@ export function EntityName({ entity }: EntityNameProps) {
   const getEntityRedirectUrl = useCallback(() => {
     const type = entity[ENTITY_TYPE];
     // For service, host and container type there is only one identity field
-    const identityField = entity[ENTITY_IDENTITY_FIELDS][0];
+    const identityField = Array.isArray(entity[ENTITY_IDENTITY_FIELDS])
+      ? entity[ENTITY_IDENTITY_FIELDS][0]
+      : entity[ENTITY_IDENTITY_FIELDS];
+    const identityValue = entity[identityField];
 
-    // Any unrecognised types will always return undefined
     switch (type) {
       case 'host':
       case 'container':
         return assetDetailsLocator?.getRedirectUrl({
-          assetId: identityField,
+          assetId: identityValue,
           assetType: type,
         });
 
       case 'service':
         return serviceOverviewLocator?.getRedirectUrl({
-          serviceName: identityField,
+          serviceName: identityValue,
           environment: [entity[SERVICE_ENVIRONMENT] || undefined].flat()[0],
         });
     }
   }, [entity, assetDetailsLocator, serviceOverviewLocator]);
 
   return (
-    <EuiLink data-test-subj="inventoryCellValueLink" href={getEntityRedirectUrl()}>
+    <EuiLink data-test-subj="entityNameLink" href={getEntityRedirectUrl()}>
       <EuiFlexGroup gutterSize="s" alignItems="center">
         <EuiFlexItem grow={0}>
           <EntityIcon entity={entity} />
         </EuiFlexItem>
         <EuiFlexItem className="eui-textTruncate">
-          <span className="eui-textTruncate">{entity[ENTITY_DISPLAY_NAME]}</span>
+          <span className="eui-textTruncate" data-test-subj="entityNameDisplayName">
+            {entity[ENTITY_DISPLAY_NAME]}
+          </span>
         </EuiFlexItem>
       </EuiFlexGroup>
     </EuiLink>

x-pack/plugins/observability_solution/inventory/public/components/entities_grid/entity_name/entity_name.test.tsx
@@ -1,0 +1,152 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { type KibanaReactContextValue } from '@kbn/kibana-react-plugin/public';
+import * as useKibana from '../../../hooks/use_kibana';
+import { EntityName } from '.';
+import { ContainerEntity, HostEntity, ServiceEntity } from '../../../../common/entities';
+import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { ASSET_DETAILS_LOCATOR_ID } from '@kbn/observability-shared-plugin/common/locators/infra/asset_details_locator';
+
+describe('EntityName', () => {
+  jest.spyOn(useKibana, 'useKibana').mockReturnValue({
+    services: {
+      share: {
+        url: {
+          locators: {
+            get: (locatorId: string) => {
+              return {
+                getRedirectUrl: (params: { [key: string]: any }) => {
+                  if (locatorId === ASSET_DETAILS_LOCATOR_ID) {
+                    return `assets_url/${params.assetType}/${params.assetId}`;
+                  }
+                  return `services_url/${params.serviceName}?environment=${params.environment}`;
+                },
+              };
+            },
+          },
+        },
+      },
+    },
+  } as unknown as KibanaReactContextValue<useKibana.InventoryKibanaContext>);
+
+  afterAll(() => {
+    jest.clearAllMocks();
+  });
+
+  it('returns host link', () => {
+    const entity: HostEntity = {
+      'entity.lastSeenTimestamp': 'foo',
+      'entity.id': '1',
+      'entity.type': 'host',
+      'entity.displayName': 'foo',
+      'entity.identityFields': 'host.name',
+      'host.name': 'foo',
+      'entity.definitionId': 'host',
+      'cloud.provider': null,
+    };
+    render(<EntityName entity={entity} />);
+    expect(screen.queryByTestId('entityNameLink')?.getAttribute('href')).toEqual(
+      'assets_url/host/foo'
+    );
+    expect(screen.queryByTestId('entityNameDisplayName')?.textContent).toEqual('foo');
+  });
+
+  it('returns container link', () => {
+    const entity: ContainerEntity = {
+      'entity.lastSeenTimestamp': 'foo',
+      'entity.id': '1',
+      'entity.type': 'container',
+      'entity.displayName': 'foo',
+      'entity.identityFields': 'container.id',
+      'container.id': 'foo',
+      'entity.definitionId': 'container',
+      'cloud.provider': null,
+    };
+    render(<EntityName entity={entity} />);
+    expect(screen.queryByTestId('entityNameLink')?.getAttribute('href')).toEqual(
+      'assets_url/container/foo'
+    );
+    expect(screen.queryByTestId('entityNameDisplayName')?.textContent).toEqual('foo');
+  });
+
+  it('returns service link without environment', () => {
+    const entity: ServiceEntity = {
+      'entity.lastSeenTimestamp': 'foo',
+      'entity.id': '1',
+      'entity.type': 'service',
+      'entity.displayName': 'foo',
+      'entity.identityFields': 'service.name',
+      'service.name': 'foo',
+      'entity.definitionId': 'service',
+      'agent.name': 'bar',
+    };
+    render(<EntityName entity={entity} />);
+    expect(screen.queryByTestId('entityNameLink')?.getAttribute('href')).toEqual(
+      'services_url/foo?environment=undefined'
+    );
+    expect(screen.queryByTestId('entityNameDisplayName')?.textContent).toEqual('foo');
+  });
+
+  it('returns service link with environment', () => {
+    const entity: ServiceEntity = {
+      'entity.lastSeenTimestamp': 'foo',
+      'entity.id': '1',
+      'entity.type': 'service',
+      'entity.displayName': 'foo',
+      'entity.identityFields': 'service.name',
+      'service.name': 'foo',
+      'entity.definitionId': 'service',
+      'agent.name': 'bar',
+      'service.environment': 'baz',
+    };
+    render(<EntityName entity={entity} />);
+    expect(screen.queryByTestId('entityNameLink')?.getAttribute('href')).toEqual(
+      'services_url/foo?environment=baz'
+    );
+    expect(screen.queryByTestId('entityNameDisplayName')?.textContent).toEqual('foo');
+  });
+
+  it('returns service link with first environment when it is an array', () => {
+    const entity: ServiceEntity = {
+      'entity.lastSeenTimestamp': 'foo',
+      'entity.id': '1',
+      'entity.type': 'service',
+      'entity.displayName': 'foo',
+      'entity.identityFields': 'service.name',
+      'service.name': 'foo',
+      'entity.definitionId': 'service',
+      'agent.name': 'bar',
+      'service.environment': ['baz', 'bar', 'foo'],
+    };
+    render(<EntityName entity={entity} />);
+    expect(screen.queryByTestId('entityNameLink')?.getAttribute('href')).toEqual(
+      'services_url/foo?environment=baz'
+    );
+    expect(screen.queryByTestId('entityNameDisplayName')?.textContent).toEqual('foo');
+  });
+
+  it('returns service link identity fields is an array', () => {
+    const entity: ServiceEntity = {
+      'entity.lastSeenTimestamp': 'foo',
+      'entity.id': '1',
+      'entity.type': 'service',
+      'entity.displayName': 'foo',
+      'entity.identityFields': ['service.name', 'service.environment'],
+      'service.name': 'foo',
+      'entity.definitionId': 'service',
+      'agent.name': 'bar',
+      'service.environment': 'baz',
+    };
+    render(<EntityName entity={entity} />);
+    expect(screen.queryByTestId('entityNameLink')?.getAttribute('href')).toEqual(
+      'services_url/foo?environment=baz'
+    );
+    expect(screen.queryByTestId('entityNameDisplayName')?.textContent).toEqual('foo');
+  });
+});
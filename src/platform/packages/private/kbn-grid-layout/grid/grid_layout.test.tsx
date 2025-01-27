patrykkopycinski/kibana

commit 02455ffe3a6e684540226817d716166a7ead8f73

[Dashboard][Collapsable panels] Enable touch between panel sections and refactor events flow  (#206941)
# Summary

## New Functionality

- **Touch Interactions Across Rows**: Adds support for touch
interactions to move panels between sections.

## Refactoring Highlights

### Unified Event Logic and Simplified Event Flow

**Refactored event handling (drag, resize) to originate solely from the
drag/resize handles when the user initiates dragging.**

#### Previous Approach:
- Events were attached in scattered locations:
  - `mousedown` from the handle.
  - `mousemove` in the `GridLayout`
  - `mouseup` on the panel.

#### New Approach:
- All interaction events (`mousemove` and `mouseup` for mouse) are
dynamically attached in a unified location in the code, simplifying
event flow.
- Benefits:
  - Easier to add new sensors (e.g., keyboard).
  - Cleaner, more maintainable code structure.

### Sensor-Specific Logic: Facilitates integration of additional input
methods (like in the near future keyboard navigation), alongside mouse
and touch interactions.
Two examples illustrating the need for this flexibility:

1. Mousemove event has to be bind to document to handle interactions
across the viewport, while touch events depend on the originating
element due to their dependency on the [touch start
context.](https://developer.mozilla.org/en-US/docs/Web/API/Touch/target)

2. Mouse and touch follow a flow of - pickup, dragging, and drop -.
Keyboard interactions, however, rely on distinct keydown events for
initiating actions, navigating elements, and confirming selections. As
this flow is defined on the sensors level, it's very easy to add
different flows.

### Split files that became too bulky (`drag_handle`,
`use_layout_grid_events`).

I've added some comments specific to the changes in the code.

---------

Co-authored-by: kibanamachine <42973632+kibanamachine@users.noreply.github.com>

src/platform/packages/private/kbn-grid-layout/grid/grid_panel/grid_panel.tsx
@@ -13,7 +13,7 @@ import { combineLatest, skip } from 'rxjs';
 import { useEuiTheme } from '@elastic/eui';
 import { css } from '@emotion/react';
 
-import { GridLayoutStateManager, PanelInteractionEvent, UserInteractionEvent } from '../types';
+import { GridLayoutStateManager } from '../types';
 import { DragHandle, DragHandleApi } from './drag_handle';
 import { ResizeHandle } from './resize_handle';
 
@@ -24,50 +24,18 @@ export interface GridPanelProps {
     panelId: string,
     setDragHandles?: (refs: Array<HTMLElement | null>) => void
   ) => React.ReactNode;
-  interactionStart: (type: PanelInteractionEvent['type'] | 'drop', e: UserInteractionEvent) => void;
   gridLayoutStateManager: GridLayoutStateManager;
 }
 
 export const GridPanel = forwardRef<HTMLDivElement, GridPanelProps>(
-  (
-    { panelId, rowIndex, renderPanelContents, interactionStart, gridLayoutStateManager },
-    panelRef
-  ) => {
+  ({ panelId, rowIndex, renderPanelContents, gridLayoutStateManager }, panelRef) => {
     const [dragHandleApi, setDragHandleApi] = useState<DragHandleApi | null>(null);
     const { euiTheme } = useEuiTheme();
 
-    useEffect(() => {
-      const onDropEventHandler = (dropEvent: MouseEvent) => interactionStart('drop', dropEvent);
-      /**
-       * Subscription to add a singular "drop" event handler whenever an interaction starts -
-       * this is handled in a subscription so that it is not lost when the component gets remounted
-       * (which happens when a panel gets dragged from one grid row to another)
-       */
-      const dropEventSubscription = gridLayoutStateManager.interactionEvent$.subscribe((event) => {
-        if (!event || event.id !== panelId) return;
-
-        /**
-         * By adding the "drop" event listener to the document rather than the drag/resize event handler,
-         * we prevent the element from getting "stuck" in an interaction; however, we only attach this event
-         * listener **when the drag/resize event starts**, and it only executes once (i.e. it removes itself
-         * once it executes, so we don't have to manually remove it outside of the unmount condition)
-         */
-        document.addEventListener('mouseup', onDropEventHandler, {
-          once: true,
-          passive: true,
-        });
-      });
-
-      return () => {
-        dropEventSubscription.unsubscribe();
-        document.removeEventListener('mouseup', onDropEventHandler); // removes the event listener on row change
-      };
-      // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
-
     /** Set initial styles based on state at mount to prevent styles from "blipping" */
     const initialStyles = useMemo(() => {
-      const initialPanel = gridLayoutStateManager.gridLayout$.getValue()[rowIndex].panels[panelId];
+      const initialPanel = (gridLayoutStateManager.proposedGridLayout$.getValue() ??
+        gridLayoutStateManager.gridLayout$.getValue())[rowIndex].panels[panelId];
       return css`
         position: relative;
         height: calc(
@@ -89,12 +57,12 @@ export const GridPanel = forwardRef<HTMLDivElement, GridPanelProps>(
         /** Update the styles of the panel via a subscription to prevent re-renders */
         const activePanelStyleSubscription = combineLatest([
           gridLayoutStateManager.activePanel$,
-          gridLayoutStateManager.gridLayout$,
+          gridLayoutStateManager.proposedGridLayout$,
         ])
           .pipe(skip(1)) // skip the first emit because the `initialStyles` will take care of it
-          .subscribe(([activePanel, gridLayout]) => {
+          .subscribe(([activePanel, proposedGridLayout]) => {
             const ref = gridLayoutStateManager.panelRefs.current[rowIndex][panelId];
-            const panel = gridLayout[rowIndex].panels[panelId];
+            const panel = proposedGridLayout?.[rowIndex].panels[panelId];
             if (!ref || !panel) return;
 
             const currentInteractionEvent = gridLayoutStateManager.interactionEvent$.getValue();
@@ -196,10 +164,15 @@ export const GridPanel = forwardRef<HTMLDivElement, GridPanelProps>(
         <DragHandle
           ref={setDragHandleApi}
           gridLayoutStateManager={gridLayoutStateManager}
-          interactionStart={interactionStart}
+          panelId={panelId}
+          rowIndex={rowIndex}
         />
         {panelContents}
-        <ResizeHandle interactionStart={interactionStart} />
+        <ResizeHandle
+          gridLayoutStateManager={gridLayoutStateManager}
+          panelId={panelId}
+          rowIndex={rowIndex}
+        />
       </div>
     );
   }

src/platform/packages/private/kbn-grid-layout/grid/grid_layout.test.tsx
@@ -7,77 +7,59 @@
  * License v3.0 only", or the "Server Side Public License, v 1".
  */
 import React from 'react';
-import { fireEvent, render, screen } from '@testing-library/react';
+import { render, screen, within } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { getSampleLayout } from './test_utils/sample_layout';
 import { GridLayout, GridLayoutProps } from './grid_layout';
 import { gridSettings, mockRenderPanelContents } from './test_utils/mocks';
 import { cloneDeep } from 'lodash';
+import {
+  mouseDrop,
+  mouseMoveTo,
+  mouseStartDragging,
+  touchEnd,
+  touchMoveTo,
+  touchStart,
+} from './test_utils/events';
 
-class TouchEventFake extends Event {
-  constructor(public touches: Array<{ clientX: number; clientY: number }>) {
-    super('touchmove');
-    this.touches = [{ clientX: 256, clientY: 128 }];
-  }
-}
+const renderGridLayout = (propsOverrides: Partial<GridLayoutProps> = {}) => {
+  const defaultProps: GridLayoutProps = {
+    accessMode: 'EDIT',
+    layout: getSampleLayout(),
+    gridSettings,
+    renderPanelContents: mockRenderPanelContents,
+    onLayoutChange: jest.fn(),
+  };
+
+  const { rerender, ...rtlRest } = render(<GridLayout {...defaultProps} {...propsOverrides} />);
+
+  return {
+    ...rtlRest,
+    rerender: (overrides: Partial<GridLayoutProps>) =>
+      rerender(<GridLayout {...defaultProps} {...overrides} />),
+  };
+};
+
+const getAllThePanelIds = () =>
+  screen
+    .getAllByRole('button', { name: /panelId:panel/i })
+    .map((el) => el.getAttribute('aria-label')?.replace(/panelId:/g, ''));
+
+const assertTabThroughPanel = async (panelId: string) => {
+  await userEvent.tab(); // tab to drag handle
+  await userEvent.tab(); // tab to the panel
+  expect(screen.getByLabelText(`panelId:${panelId}`)).toHaveFocus();
+  await userEvent.tab(); // tab to the resize handle
+};
+
+const getPanelHandle = (panelId: string, interactionType: 'resize' | 'drag' = 'drag') => {
+  const gridPanel = screen.getByText(`panel content ${panelId}`).closest('div')!;
+  const handleText = new RegExp(interactionType === 'resize' ? /resize to move/i : /drag to move/i);
+  return within(gridPanel).getByRole('button', { name: handleText });
+};
 
 describe('GridLayout', () => {
-  const renderGridLayout = (propsOverrides: Partial<GridLayoutProps> = {}) => {
-    const defaultProps: GridLayoutProps = {
-      accessMode: 'EDIT',
-      layout: getSampleLayout(),
-      gridSettings,
-      renderPanelContents: mockRenderPanelContents,
-      onLayoutChange: jest.fn(),
-    };
-
-    const { rerender, ...rtlRest } = render(<GridLayout {...defaultProps} {...propsOverrides} />);
-
-    return {
-      ...rtlRest,
-      rerender: (overrides: Partial<GridLayoutProps>) =>
-        rerender(<GridLayout {...defaultProps} {...overrides} />),
-    };
-  };
-
-  const getAllThePanelIds = () =>
-    screen
-      .getAllByRole('button', { name: /panelId:panel/i })
-      .map((el) => el.getAttribute('aria-label')?.replace(/panelId:/g, ''));
-
-  const mouseStartDragging = (handle: HTMLElement, options = { clientX: 0, clientY: 0 }) => {
-    fireEvent.mouseDown(handle, options);
-  };
-
-  const mouseMoveTo = (options = { clientX: 256, clientY: 128 }) => {
-    fireEvent.mouseMove(document, options);
-  };
-
-  const mouseDrop = (handle: HTMLElement) => {
-    fireEvent.mouseUp(handle);
-  };
-  const touchStart = (handle: HTMLElement, options = { touches: [{ clientX: 0, clientY: 0 }] }) => {
-    fireEvent.touchStart(handle, options);
-  };
-  const touchMoveTo = (options = { touches: [{ clientX: 256, clientY: 128 }] }) => {
-    const realTouchEvent = window.TouchEvent;
-    // @ts-expect-error
-    window.TouchEvent = TouchEventFake;
-    fireEvent.touchMove(document, new TouchEventFake(options.touches));
-    window.TouchEvent = realTouchEvent;
-  };
-  const touchEnd = (handle: HTMLElement) => {
-    fireEvent.touchEnd(handle);
-  };
-
-  const assertTabThroughPanel = async (panelId: string) => {
-    await userEvent.tab(); // tab to drag handle
-    await userEvent.tab(); // tab to the panel
-    expect(screen.getByLabelText(`panelId:${panelId}`)).toHaveFocus();
-    await userEvent.tab(); // tab to the resize handle
-  };
-
-  const expectedInitialOrder = [
+  const expectedInitPanelIdsInOrder = [
     'panel1',
     'panel5',
     'panel2',
@@ -97,16 +79,38 @@ describe('GridLayout', () => {
   it(`'renderPanelContents' is not called during dragging`, () => {
     renderGridLayout();
 
-    expect(mockRenderPanelContents).toHaveBeenCalledTimes(10); // renderPanelContents is called for each of 10 panels
+    // assert that renderPanelContents has been called ONLY ONCE for each of 10 panels on initial render
+    expect(mockRenderPanelContents).toHaveBeenCalledTimes(expectedInitPanelIdsInOrder.length);
     jest.clearAllMocks();
 
-    const panel1DragHandle = screen.getAllByRole('button', { name: /drag to move/i })[0];
-    mouseStartDragging(panel1DragHandle);
+    const panelHandle = getPanelHandle('panel1');
+    mouseStartDragging(panelHandle);
     mouseMoveTo({ clientX: 256, clientY: 128 });
-    expect(mockRenderPanelContents).toHaveBeenCalledTimes(0); // renderPanelContents should not be called during dragging
 
-    mouseDrop(panel1DragHandle);
-    expect(mockRenderPanelContents).toHaveBeenCalledTimes(0); // renderPanelContents should not be called after reordering
+    // assert that renderPanelContents has not been called during dragging
+    expect(mockRenderPanelContents).toHaveBeenCalledTimes(0);
+
+    mouseDrop(panelHandle);
+    // assert that renderPanelContents has not been called after reordering
+    expect(mockRenderPanelContents).toHaveBeenCalledTimes(0);
+  });
+
+  it('panel gets active when dragged', () => {
+    renderGridLayout();
+    const panelHandle = getPanelHandle('panel1');
+    expect(screen.getByLabelText('panelId:panel1').closest('div')).toHaveClass('kbnGridPanel', {
+      exact: true,
+    });
+    mouseStartDragging(panelHandle);
+    mouseMoveTo({ clientX: 256, clientY: 128 });
+    expect(screen.getByLabelText('panelId:panel1').closest('div')).toHaveClass(
+      'kbnGridPanel kbnGridPanel--active',
+      { exact: true }
+    );
+    mouseDrop(panelHandle);
+    expect(screen.getByLabelText('panelId:panel1').closest('div')).toHaveClass('kbnGridPanel', {
+      exact: true,
+    });
   });
 
   describe('panels order: panels are rendered from left to right, from top to bottom', () => {
@@ -120,19 +124,19 @@ describe('GridLayout', () => {
     });
     it('on initializing', () => {
       renderGridLayout();
-      expect(getAllThePanelIds()).toEqual(expectedInitialOrder);
+      expect(getAllThePanelIds()).toEqual(expectedInitPanelIdsInOrder);
     });
 
     it('after reordering some panels', async () => {
       renderGridLayout();
 
-      const panel1DragHandle = screen.getAllByRole('button', { name: /drag to move/i })[0];
-      mouseStartDragging(panel1DragHandle);
+      const panelHandle = getPanelHandle('panel1');
+      mouseStartDragging(panelHandle);
 
       mouseMoveTo({ clientX: 256, clientY: 128 });
-      expect(getAllThePanelIds()).toEqual(expectedInitialOrder); // the panels shouldn't be reordered till we mouseDrop
+      expect(getAllThePanelIds()).toEqual(expectedInitPanelIdsInOrder); // the panels shouldn't be reordered till we mouseDrop
 
-      mouseDrop(panel1DragHandle);
+      mouseDrop(panelHandle);
       expect(getAllThePanelIds()).toEqual([
         'panel2',
         'panel5',
@@ -149,12 +153,12 @@ describe('GridLayout', () => {
     it('after reordering some panels via touch events', async () => {
       renderGridLayout();
 
-      const panel1DragHandle = screen.getAllByRole('button', { name: /drag to move/i })[0];
-      touchStart(panel1DragHandle);
-      touchMoveTo({ touches: [{ clientX: 256, clientY: 128 }] });
-      expect(getAllThePanelIds()).toEqual(expectedInitialOrder); // the panels shouldn't be reordered till we mouseDrop
+      const panelHandle = getPanelHandle('panel1');
+      touchStart(panelHandle);
+      touchMoveTo(panelHandle, { touches: [{ clientX: 256, clientY: 128 }] });
+      expect(getAllThePanelIds()).toEqual(expectedInitPanelIdsInOrder); // the panels shouldn't be reordered till we mouseDrop
 
-      touchEnd(panel1DragHandle);
+      touchEnd(panelHandle);
       expect(getAllThePanelIds()).toEqual([
         'panel2',
         'panel5',

src/platform/packages/private/kbn-grid-layout/grid/drag_preview.tsx
@@ -28,16 +28,16 @@ export const DragPreview = ({
       /** Update the styles of the drag preview via a subscription to prevent re-renders */
       const styleSubscription = combineLatest([
         gridLayoutStateManager.activePanel$,
-        gridLayoutStateManager.gridLayout$,
+        gridLayoutStateManager.proposedGridLayout$,
       ])
         .pipe(skip(1)) // skip the first emit because the drag preview is only rendered after a user action
-        .subscribe(([activePanel, gridLayout]) => {
+        .subscribe(([activePanel, proposedGridLayout]) => {
           if (!dragPreviewRef.current) return;
 
-          if (!activePanel || !gridLayout[rowIndex].panels[activePanel.id]) {
+          if (!activePanel || !proposedGridLayout?.[rowIndex].panels[activePanel.id]) {
             dragPreviewRef.current.style.display = 'none';
           } else {
-            const panel = gridLayout[rowIndex].panels[activePanel.id];
+            const panel = proposedGridLayout[rowIndex].panels[activePanel.id];
             dragPreviewRef.current.style.display = 'block';
             dragPreviewRef.current.style.gridColumnStart = `${panel.column + 1}`;
             dragPreviewRef.current.style.gridColumnEnd = `${panel.column + 1 + panel.width}`;

src/platform/packages/private/kbn-grid-layout/grid/grid_layout.tsx
@@ -10,14 +10,13 @@
 import classNames from 'classnames';
 import { cloneDeep } from 'lodash';
 import React, { useEffect, useMemo, useRef, useState } from 'react';
-import { combineLatest, distinctUntilChanged, filter, map, pairwise, skip } from 'rxjs';
+import { combineLatest, distinctUntilChanged, map, pairwise, skip } from 'rxjs';
 
 import { css } from '@emotion/react';
 
 import { GridHeightSmoother } from './grid_height_smoother';
 import { GridRow } from './grid_row';
 import { GridAccessMode, GridLayoutData, GridSettings } from './types';
-import { useGridLayoutEvents } from './use_grid_layout_events';
 import { useGridLayoutState } from './use_grid_layout_state';
 import { isLayoutEqual } from './utils/equality_checks';
 import { resolveGridRow } from './utils/resolve_grid_row';
@@ -52,7 +51,6 @@ export const GridLayout = ({
     expandedPanelId,
     accessMode,
   });
-  useGridLayoutEvents({ gridLayoutStateManager });
 
   const [rowCount, setRowCount] = useState<number>(
     gridLayoutStateManager.gridLayout$.getValue().length
@@ -97,17 +95,8 @@ export const GridLayout = ({
     /**
      * This subscription calls the passed `onLayoutChange` callback when the layout changes
      */
-    const onLayoutChangeSubscription = combineLatest([
-      gridLayoutStateManager.gridLayout$,
-      gridLayoutStateManager.interactionEvent$,
-    ])
-      .pipe(
-        // if an interaction event is happening, then ignore any "draft" layout changes
-        filter(([_, event]) => !Boolean(event)),
-        // once no interaction event, create pairs of "old" and "new" layouts for comparison
-        map(([newLayout]) => newLayout),
-        pairwise()
-      )
+    const onLayoutChangeSubscription = gridLayoutStateManager.gridLayout$
+      .pipe(pairwise())
       .subscribe(([layoutBefore, layoutAfter]) => {
         if (!isLayoutEqual(layoutBefore, layoutAfter)) {
           onLayoutChange(layoutAfter);
@@ -156,12 +145,6 @@ export const GridLayout = ({
           rowIndex={rowIndex}
           renderPanelContents={renderPanelContents}
           gridLayoutStateManager={gridLayoutStateManager}
-          setInteractionEvent={(nextInteractionEvent) => {
-            if (!nextInteractionEvent) {
-              gridLayoutStateManager.activePanel$.next(undefined);
-            }
-            gridLayoutStateManager.interactionEvent$.next(nextInteractionEvent);
-          }}
           ref={(element: HTMLDivElement | null) =>
             (gridLayoutStateManager.rowRefs.current[rowIndex] = element)
           }

src/platform/packages/private/kbn-grid-layout/grid/grid_panel/drag_handle.tsx
@@ -1,148 +1,0 @@
-/*
- * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
- * or more contributor license agreements. Licensed under the "Elastic License
- * 2.0", the "GNU Affero General Public License v3.0 only", and the "Server Side
- * Public License v 1"; you may not use this file except in compliance with, at
- * your election, the "Elastic License 2.0", the "GNU Affero General Public
- * License v3.0 only", or the "Server Side Public License, v 1".
- */
-
-import React, { useCallback, useEffect, useImperativeHandle, useRef, useState } from 'react';
-
-import { EuiIcon, useEuiTheme } from '@elastic/eui';
-import { css } from '@emotion/react';
-import { i18n } from '@kbn/i18n';
-import {
-  GridLayoutStateManager,
-  PanelInteractionEvent,
-  UserInteractionEvent,
-  UserMouseEvent,
-  UserTouchEvent,
-} from '../types';
-
-export interface DragHandleApi {
-  setDragHandles: (refs: Array<HTMLElement | null>) => void;
-}
-
-export const DragHandle = React.forwardRef<
-  DragHandleApi,
-  {
-    gridLayoutStateManager: GridLayoutStateManager;
-    interactionStart: (
-      type: PanelInteractionEvent['type'] | 'drop',
-      e: UserInteractionEvent
-    ) => void;
-  }
->(({ gridLayoutStateManager, interactionStart }, ref) => {
-  const { euiTheme } = useEuiTheme();
-
-  const removeEventListenersRef = useRef<(() => void) | null>(null);
-  const [dragHandleCount, setDragHandleCount] = useState<number>(0);
-  const dragHandleRefs = useRef<Array<HTMLElement | null>>([]);
-
-  /**
-   * We need to memoize the `onDragStart` and `onDragEnd` callbacks so that we don't assign a new event handler
-   * every time `setDragHandles` is called
-   */
-  const onDragStart = useCallback(
-    (e: UserMouseEvent | UserTouchEvent) => {
-      interactionStart('drag', e);
-    },
-    [interactionStart]
-  );
-
-  const onDragEnd = useCallback(
-    (e: UserTouchEvent | UserMouseEvent) => {
-      interactionStart('drop', e);
-    },
-    [interactionStart]
-  );
-
-  const setDragHandles = useCallback(
-    (dragHandles: Array<HTMLElement | null>) => {
-      setDragHandleCount(dragHandles.length);
-      dragHandleRefs.current = dragHandles;
-
-      for (const handle of dragHandles) {
-        if (handle === null) return;
-        handle.addEventListener('mousedown', onDragStart, { passive: true });
-        handle.addEventListener('touchstart', onDragStart, { passive: false });
-        handle.addEventListener('touchend', onDragEnd, { passive: true });
-      }
-
-      removeEventListenersRef.current = () => {
-        for (const handle of dragHandles) {
-          if (handle === null) return;
-          handle.removeEventListener('mousedown', onDragStart);
-          handle.removeEventListener('touchstart', onDragStart);
-          handle.removeEventListener('touchend', onDragEnd);
-        }
-      };
-    },
-    [onDragStart, onDragEnd]
-  );
-
-  useEffect(() => {
-    return () => {
-      // on unmount, remove all drag handle event listeners
-      if (removeEventListenersRef.current) {
-        removeEventListenersRef.current();
-      }
-    };
-  }, []);
-
-  useImperativeHandle(
-    ref,
-    () => {
-      return { setDragHandles };
-    },
-    [setDragHandles]
-  );
-
-  return Boolean(dragHandleCount) ? null : (
-    <button
-      aria-label={i18n.translate('kbnGridLayout.dragHandle.ariaLabel', {
-        defaultMessage: 'Drag to move',
-      })}
-      className="kbnGridPanel--dragHandle"
-      css={css`
-        opacity: 0;
-        display: flex;
-        cursor: move;
-        position: absolute;
-        align-items: center;
-        justify-content: center;
-        top: -${euiTheme.size.l};
-        width: ${euiTheme.size.l};
-        height: ${euiTheme.size.l};
-        z-index: ${euiTheme.levels.modal};
-        margin-left: ${euiTheme.size.s};
-        border: 1px solid ${euiTheme.border.color};
-        border-bottom: none;
-        background-color: ${euiTheme.colors.backgroundBasePlain};
-        border-radius: ${euiTheme.border.radius} ${euiTheme.border.radius} 0 0;
-        cursor: grab;
-        transition: ${euiTheme.animation.slow} opacity;
-        .kbnGridPanel:hover &,
-        .kbnGridPanel:focus-within &,
-        &:active,
-        &:focus {
-          opacity: 1 !important;
-        }
-        &:active {
-          cursor: grabbing;
-        }
-        .kbnGrid--static &,
-        .kbnGridPanel--expanded & {
-          display: none;
-        }
-      `}
-      onMouseDown={onDragStart}
-      onMouseUp={onDragEnd}
-      onTouchStart={onDragStart}
-      onTouchEnd={onDragEnd}
-    >
-      <EuiIcon type="grabOmnidirectional" />
-    </button>
-  );
-});

commit 255c7a8fe1175c8effc11df09050cf5e7c0d7087

[Dashboard] Fix panel reset (#208288)
## Summary

This PR fixes a bug that was introduced in
https://github.com/elastic/kibana/pull/206941 (by my recommendation,
oops) where panels can no longer be reset on Dashboard.

**Before**


https://github.com/user-attachments/assets/90bfce89-e188-45c6-937d-9ad48982d387

**After**


https://github.com/user-attachments/assets/25c9b9c8-7ad5-4808-89a1-0dcf7921b52e


### Checklist

- [x] [Unit or functional
tests](https://www.elastic.co/guide/en/kibana/master/development-tests.html)
were updated or added to match the most common scenarios
- [x] The PR description includes the appropriate Release Notes section,
and the correct `release_note:*` label is applied per the
[guidelines](https://www.elastic.co/guide/en/kibana/master/contributing.html#kibana-release-notes-process)

src/platform/packages/private/kbn-grid-layout/grid/grid_panel/grid_panel.tsx
@@ -57,12 +57,13 @@ export const GridPanel = forwardRef<HTMLDivElement, GridPanelProps>(
         /** Update the styles of the panel via a subscription to prevent re-renders */
         const activePanelStyleSubscription = combineLatest([
           gridLayoutStateManager.activePanel$,
+          gridLayoutStateManager.gridLayout$,
           gridLayoutStateManager.proposedGridLayout$,
         ])
           .pipe(skip(1)) // skip the first emit because the `initialStyles` will take care of it
-          .subscribe(([activePanel, proposedGridLayout]) => {
+          .subscribe(([activePanel, gridLayout, proposedGridLayout]) => {
             const ref = gridLayoutStateManager.panelRefs.current[rowIndex][panelId];
-            const panel = proposedGridLayout?.[rowIndex].panels[panelId];
+            const panel = (proposedGridLayout ?? gridLayout)[rowIndex].panels[panelId];
             if (!ref || !panel) return;
 
             const currentInteractionEvent = gridLayoutStateManager.interactionEvent$.getValue();

src/platform/packages/private/kbn-grid-layout/grid/grid_layout.test.tsx
@@ -22,13 +22,15 @@ import {
   touchStart,
 } from './test_utils/events';
 
+const onLayoutChange = jest.fn();
+
 const renderGridLayout = (propsOverrides: Partial<GridLayoutProps> = {}) => {
   const defaultProps: GridLayoutProps = {
     accessMode: 'EDIT',
     layout: getSampleLayout(),
     gridSettings,
     renderPanelContents: mockRenderPanelContents,
-    onLayoutChange: jest.fn(),
+    onLayoutChange,
   };
 
   const { rerender, ...rtlRest } = render(<GridLayout {...defaultProps} {...propsOverrides} />);
@@ -74,6 +76,41 @@ describe('GridLayout', () => {
 
   beforeEach(() => {
     jest.clearAllMocks();
+  });
+
+  it('`onLayoutChange` gets called when layout prop changes', () => {
+    const layoutComponent = renderGridLayout();
+    onLayoutChange.mockClear();
+
+    const layout = getSampleLayout();
+
+    // if layout hasn't changed, don't call `onLayoutChange`
+    layoutComponent.rerender({
+      layout,
+    });
+    expect(onLayoutChange).not.toBeCalled();
+
+    // if layout **has** changed, call `onLayoutChange`
+    const newLayout = cloneDeep(layout);
+    newLayout[0] = {
+      ...newLayout[0],
+      panels: {
+        ...newLayout[0].panels,
+        panel1: {
+          id: 'panel1',
+          row: 100,
+          column: 0,
+          width: 12,
+          height: 6,
+        },
+      },
+    };
+
+    layoutComponent.rerender({
+      layout: newLayout,
+    });
+
+    expect(onLayoutChange).toBeCalledTimes(1);
   });
 
   it(`'renderPanelContents' is not called during dragging`, () => {
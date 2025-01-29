patrykkopycinski/kibana

commit 59e81ee2be5b8b9082dfc9ac06898cbe6cc8e011

[Fleet] Disallow some egress-specific inputs for agentless integrations (#206074)
Closes https://github.com/elastic/kibana/issues/202091

## Summary

Disallow some egress-specific inputs for agentless integrations. 
- In the policy editor, when Setup technology dropdown is set to
Agentless, hide the rendering of configuration for inputs that have type
matching the blocklist and ensure that these inputs are set to `enabled:
false`
- `tcp, udp, winlog, http_endpoint, filestream` should be disabled when
`supports_agentless: true`
- At the API level, throw an error if attempting to enable a disallowed
input type


### Testing
Simulate agentless env with following setup in `kibana.dev.yml`:
```
xpack.cloud.id: 'anything-to-pass-cloud-validation-checks'
xpack.fleet.agentless.enabled: true
xpack.fleet.agentless.api.url: 'https://localhost:8443'
xpack.fleet.agentless.api.tls.certificate: './config/certs/ess-client.crt'
xpack.fleet.agentless.api.tls.key: './config/certs/ess-client.key'
xpack.fleet.agentless.api.tls.ca: './config/certs/ca.crt'
```
-Apply [this
patch](https://gist.github.com/jen-huang/dfc3e02ceb63976ad54bd1f50c524cb4)
to prevent attempt to create agentless pod (the agentless policy
creation fails without the patch)
- Install the following test integration, that has a bunch of different
inputs to simulate this specific case and is enabled for agentless (it
shows the setup technology as well)

[agentless_package_links-0.0.2.zip](https://github.com/user-attachments/files/18425895/agentless_package_links-0.0.2.zip)

```
curl -XPOST -H 'content-type: application/zip' -H 'kbn-xsrf: true' http://localhost:5601/YOUR_PATH/api/fleet/epm/packages -u elastic:changeme --data-binary @agentless_package_links-0.0.2.zip
```
- Navigate to the integrations page, find the above integration and test
that switching between agent-based/agentless the enabled inputs change
as follows:

x-pack/platform/plugins/shared/fleet/common/services/agentless_policy_helper.ts
@@ -5,7 +5,11 @@
  * 2.0.
  */
 
-import type { PackageInfo, RegistryPolicyTemplate } from '../types';
+import { AGENTLESS_DISABLED_INPUTS } from '../constants';
+import { PackagePolicyValidationError } from '../errors';
+import type { NewPackagePolicyInput, PackageInfo, RegistryPolicyTemplate } from '../types';
+
+import type { SimplifiedInputs } from './simplified_package_policy_helper';
 
 export const isAgentlessIntegration = (
   packageInfo: Pick<PackageInfo, 'policy_templates'> | undefined
@@ -49,3 +53,45 @@ export const isOnlyAgentlessPolicyTemplate = (policyTemplate: RegistryPolicyTemp
         policyTemplate.deployment_modes.default.enabled === false)
   );
 };
+
+/*
+ * Check if the package policy inputs is not allowed in agentless
+ */
+export function inputNotAllowedInAgentless(inputType: string, supportsAgentless?: boolean | null) {
+  return supportsAgentless === true && AGENTLESS_DISABLED_INPUTS.includes(inputType);
+}
+
+/*
+ * Throw error if trying to enabling an input that is not allowed in agentless
+ */
+export function validateAgentlessInputs(
+  packagePolicyInputs: NewPackagePolicyInput[] | SimplifiedInputs,
+  supportsAgentless?: boolean | null
+) {
+  if (Array.isArray(packagePolicyInputs)) {
+    return packagePolicyInputs.forEach((input) => {
+      throwIfInputNotAllowed(input.type, input.enabled, supportsAgentless);
+    });
+  } else {
+    Object.keys(packagePolicyInputs).forEach((inputName) => {
+      const input = packagePolicyInputs[inputName];
+      const match = inputName.match(/\-(\w*)$/);
+      const inputType = match && match.length > 0 ? match[1] : '';
+      throwIfInputNotAllowed(inputType, input?.enabled ?? false, supportsAgentless);
+    });
+  }
+}
+
+function throwIfInputNotAllowed(
+  inputType: string,
+  inputEnabled: boolean,
+  supportsAgentless?: boolean | null
+) {
+  if (inputNotAllowedInAgentless(inputType, supportsAgentless) && inputEnabled === true) {
+    throw new PackagePolicyValidationError(
+      `Input ${inputType} is not allowed: types '${AGENTLESS_DISABLED_INPUTS.map(
+        (name) => name
+      ).join(', ')}' cannot be enabled for an Agentless integration`
+    );
+  }
+}

x-pack/platform/plugins/shared/fleet/public/applications/fleet/sections/agent_policy/create_package_policy_page/single_page_layout/hooks/form.tsx
@@ -5,20 +5,21 @@
  * 2.0.
  */
 
-import { useCallback, useEffect, useRef, useState } from 'react';
+import { useCallback, useEffect, useMemo, useRef, useState } from 'react';
 import { i18n } from '@kbn/i18n';
 import { load } from 'js-yaml';
 
 import { isEqual } from 'lodash';
 
 import { useSpaceSettingsContext } from '../../../../../../../hooks/use_space_settings_context';
-import type {
-  AgentPolicy,
-  NewPackagePolicy,
-  NewAgentPolicy,
-  CreatePackagePolicyRequest,
-  PackagePolicy,
-  PackageInfo,
+import {
+  type AgentPolicy,
+  type NewPackagePolicy,
+  type NewAgentPolicy,
+  type CreatePackagePolicyRequest,
+  type PackagePolicy,
+  type PackageInfo,
+  SetupTechnology,
 } from '../../../../../types';
 import {
   useStartServices,
@@ -49,7 +50,9 @@ import {
   getCloudShellUrlFromPackagePolicy,
 } from '../../../../../../../components/cloud_security_posture/services';
 
-import { useAgentless } from './setup_technology';
+import { AGENTLESS_DISABLED_INPUTS } from '../../../../../../../../common/constants';
+
+import { useAgentless, useSetupTechnology } from './setup_technology';
 
 export async function createAgentPolicy({
   packagePolicy,
@@ -142,6 +145,8 @@ export function useOnSubmit({
   packageInfo,
   integrationToEnable,
   hasFleetAddAgentsPrivileges,
+  setNewAgentPolicy,
+  setSelectedPolicyTab,
 }: {
   packageInfo?: PackageInfo;
   newAgentPolicy: NewAgentPolicy;
@@ -151,6 +156,8 @@ export function useOnSubmit({
   queryParamsPolicyId: string | undefined;
   integrationToEnable?: string;
   hasFleetAddAgentsPrivileges: boolean;
+  setNewAgentPolicy: (policy: NewAgentPolicy) => void;
+  setSelectedPolicyTab: (tab: SelectedPolicyTab) => void;
 }) {
   const { notifications } = useStartServices();
   const confirmForceInstall = useConfirmForceInstall();
@@ -166,6 +173,11 @@ export function useOnSubmit({
   const [isInitialized, setIsInitialized] = useState<boolean>(false);
   // Used to initialize the package policy once
   const isInitializedRef = useRef(false);
+
+  // only used to save the initial value of the package policy
+  const [initialPackagePolicy, setInitialPackagePolicy] = useState<NewPackagePolicy>({
+    ...DEFAULT_PACKAGE_POLICY,
+  });
 
   const [agentPolicies, setAgentPolicies] = useState<AgentPolicy[]>([]);
   // New package policy state
@@ -255,20 +267,27 @@ export function useOnSubmit({
       const incrementedName = getMaxPackageName(packageInfo.name, packagePolicyData?.items);
 
       isInitializedRef.current = true;
-      updatePackagePolicy(
-        packageToPackagePolicy(
-          packageInfo,
-          agentPolicies.map((policy) => policy.id),
-          '',
-          DEFAULT_PACKAGE_POLICY.name || incrementedName,
-          DEFAULT_PACKAGE_POLICY.description,
-          integrationToEnable
-        )
+      const basePackagePolicy = packageToPackagePolicy(
+        packageInfo,
+        agentPolicies.map((policy) => policy.id),
+        '',
+        DEFAULT_PACKAGE_POLICY.name || incrementedName,
+        DEFAULT_PACKAGE_POLICY.description,
+        integrationToEnable
       );
+      setInitialPackagePolicy(basePackagePolicy);
+      updatePackagePolicy(basePackagePolicy);
       setIsInitialized(true);
     }
     init();
-  }, [packageInfo, agentPolicies, updatePackagePolicy, integrationToEnable, isInitialized]);
+  }, [
+    packageInfo,
+    agentPolicies,
+    updatePackagePolicy,
+    integrationToEnable,
+    isInitialized,
+    initialPackagePolicy,
+  ]);
 
   useEffect(() => {
     if (
@@ -283,6 +302,42 @@ export function useOnSubmit({
       });
     }
   }, [packagePolicy, agentPolicies, updatePackagePolicy, canUseMultipleAgentPolicies]);
+
+  const { handleSetupTechnologyChange, selectedSetupTechnology, defaultSetupTechnology } =
+    useSetupTechnology({
+      newAgentPolicy,
+      setNewAgentPolicy,
+      updateAgentPolicies,
+      updatePackagePolicy,
+      setSelectedPolicyTab,
+      packageInfo,
+      packagePolicy,
+    });
+  const setupTechnologyRef = useRef<SetupTechnology | undefined>(selectedSetupTechnology);
+  // sync the inputs with the agentless selector change
+  useEffect(() => {
+    setupTechnologyRef.current = selectedSetupTechnology;
+  });
+  const prevSetupTechnology = setupTechnologyRef.current;
+  const isAgentlessSelected =
+    isAgentlessIntegration(packageInfo) && selectedSetupTechnology === SetupTechnology.AGENTLESS;
+
+  const newInputs = useMemo(() => {
+    return packagePolicy.inputs.map((input, i) => {
+      if (isAgentlessSelected && AGENTLESS_DISABLED_INPUTS.includes(input.type)) {
+        return { ...input, enabled: false };
+      }
+      return initialPackagePolicy.inputs[i];
+    });
+  }, [initialPackagePolicy?.inputs, isAgentlessSelected, packagePolicy.inputs]);
+
+  useEffect(() => {
+    if (prevSetupTechnology !== selectedSetupTechnology) {
+      updatePackagePolicy({
+        inputs: newInputs,
+      });
+    }
+  }, [newInputs, prevSetupTechnology, selectedSetupTechnology, updatePackagePolicy, packagePolicy]);
 
   const onSaveNavigate = useOnSaveNavigate({
     packagePolicy,
@@ -495,5 +550,9 @@ export function useOnSubmit({
     // TODO check
     navigateAddAgent,
     navigateAddAgentHelp,
+    handleSetupTechnologyChange,
+    selectedSetupTechnology,
+    defaultSetupTechnology,
+    isAgentlessSelected,
   };
 }

x-pack/platform/plugins/shared/fleet/common/constants/agentless.ts
@@ -12,3 +12,6 @@ export const AGENTLESS_AGENT_POLICY_MONITORING: MonitoringType = ['logs', 'metri
 export const AGENTLESS_GLOBAL_TAG_NAME_ORGANIZATION = 'organization';
 export const AGENTLESS_GLOBAL_TAG_NAME_DIVISION = 'division';
 export const AGENTLESS_GLOBAL_TAG_NAME_TEAM = 'team';
+
+// Input types to disable for agentless integrations
+export const AGENTLESS_DISABLED_INPUTS = ['tcp', 'udp', 'filestream', 'http_endpoint', 'winlog'];

x-pack/platform/plugins/shared/fleet/common/services/simplified_package_policy_helper.ts
@@ -15,10 +15,11 @@ import type {
   PackageInfo,
   ExperimentalDataStreamFeature,
 } from '../types';
-import { DATASET_VAR_NAME } from '../constants';
+import { AGENTLESS_DISABLED_INPUTS, DATASET_VAR_NAME } from '../constants';
 import { PackagePolicyValidationError } from '../errors';
 
 import { packageToPackagePolicy } from '.';
+import { inputNotAllowedInAgentless } from './agentless_policy_helper';
 
 export type SimplifiedVars = Record<string, string | string[] | boolean | number | number[] | null>;
 
@@ -75,14 +76,18 @@ export function generateInputId(input: NewPackagePolicyInput) {
   return `${input.policy_template ? `${input.policy_template}-` : ''}${input.type}`;
 }
 
-export function formatInputs(inputs: NewPackagePolicy['inputs']) {
+export function formatInputs(inputs: NewPackagePolicy['inputs'], supportsAgentless?: boolean) {
   return inputs.reduce((acc, input) => {
     const inputId = generateInputId(input);
     if (!acc) {
       acc = {};
     }
+    const enabled =
+      supportsAgentless === true && AGENTLESS_DISABLED_INPUTS.includes(input.type)
+        ? false
+        : input.enabled;
     acc[inputId] = {
-      enabled: input.enabled,
+      enabled,
       vars: formatVars(input.vars),
       streams: formatStreams(input.streams),
     };
@@ -196,7 +201,10 @@ export function simplifiedPackagePolicytoNewPackagePolicy(
       throw new PackagePolicyValidationError(`Input not found: ${inputId}`);
     }
 
-    if (enabled === false) {
+    if (
+      inputNotAllowedInAgentless(packagePolicyInput.type, packagePolicy?.supports_agentless) ||
+      enabled === false
+    ) {
       packagePolicyInput.enabled = false;
     } else {
       packagePolicyInput.enabled = true;
@@ -213,7 +221,10 @@ export function simplifiedPackagePolicytoNewPackagePolicy(
         throw new PackagePolicyValidationError(`Stream not found ${inputId}: ${streamId}`);
       }
 
-      if (streamEnabled === false) {
+      if (
+        streamEnabled === false ||
+        inputNotAllowedInAgentless(packagePolicyInput.type, packagePolicy?.supports_agentless)
+      ) {
         packagePolicyStream.enabled = false;
       } else {
         packagePolicyStream.enabled = true;

x-pack/platform/plugins/shared/fleet/public/applications/fleet/sections/agent_policy/create_package_policy_page/components/steps/step_configure_package.tsx
@@ -27,6 +27,8 @@ import { doesPackageHaveIntegrations } from '../../../../../services';
 
 import type { PackagePolicyValidationResults } from '../../services';
 
+import { AGENTLESS_DISABLED_INPUTS } from '../../../../../../../../common/constants';
+
 import { PackagePolicyInputPanel } from './components';
 
 export const StepConfigurePackagePolicy: React.FunctionComponent<{
@@ -38,6 +40,7 @@ export const StepConfigurePackagePolicy: React.FunctionComponent<{
   submitAttempted: boolean;
   noTopRule?: boolean;
   isEditPage?: boolean;
+  isAgentlessSelected?: boolean;
 }> = ({
   packageInfo,
   showOnlyIntegration,
@@ -47,6 +50,7 @@ export const StepConfigurePackagePolicy: React.FunctionComponent<{
   submitAttempted,
   noTopRule = false,
   isEditPage = false,
+  isAgentlessSelected = false,
 }) => {
   const hasIntegrations = useMemo(() => doesPackageHaveIntegrations(packageInfo), [packageInfo]);
   const packagePolicyTemplates = useMemo(
@@ -66,8 +70,9 @@ export const StepConfigurePackagePolicy: React.FunctionComponent<{
         <EuiFlexGroup direction="column" gutterSize="none">
           {packagePolicyTemplates.map((policyTemplate) => {
             const inputs = getNormalizedInputs(policyTemplate);
+            const packagePolicyInputs = packagePolicy.inputs;
             return inputs.map((packageInput) => {
-              const packagePolicyInput = packagePolicy.inputs.find(
+              const packagePolicyInput = packagePolicyInputs.find(
                 (input) =>
                   input.type === packageInput.type &&
                   (hasIntegrations ? input.policy_template === policyTemplate.name : true)
@@ -79,28 +84,35 @@ export const StepConfigurePackagePolicy: React.FunctionComponent<{
                   ? policyTemplate.data_streams
                   : []
               );
-              return packagePolicyInput ? (
+
+              const updatePackagePolicyInput = (updatedInput: Partial<NewPackagePolicyInput>) => {
+                const indexOfUpdatedInput = packagePolicyInputs.findIndex(
+                  (input) =>
+                    input.type === packageInput.type &&
+                    (hasIntegrations ? input.policy_template === policyTemplate.name : true)
+                );
+                const newInputs = [...packagePolicyInputs];
+                newInputs[indexOfUpdatedInput] = {
+                  ...newInputs[indexOfUpdatedInput],
+                  ...updatedInput,
+                };
+                updatePackagePolicy({
+                  inputs: newInputs,
+                });
+              };
+
+              return packagePolicyInput &&
+                !(
+                  (isAgentlessSelected || packagePolicy.supports_agentless === true) &&
+                  AGENTLESS_DISABLED_INPUTS.includes(packagePolicyInput.type)
+                ) ? (
                 <EuiFlexItem key={packageInput.type}>
                   <PackagePolicyInputPanel
                     packageInput={packageInput}
                     packageInfo={packageInfo}
                     packageInputStreams={packageInputStreams}
                     packagePolicyInput={packagePolicyInput}
-                    updatePackagePolicyInput={(updatedInput: Partial<NewPackagePolicyInput>) => {
-                      const indexOfUpdatedInput = packagePolicy.inputs.findIndex(
-                        (input) =>
-                          input.type === packageInput.type &&
-                          (hasIntegrations ? input.policy_template === policyTemplate.name : true)
-                      );
-                      const newInputs = [...packagePolicy.inputs];
-                      newInputs[indexOfUpdatedInput] = {
-                        ...newInputs[indexOfUpdatedInput],
-                        ...updatedInput,
-                      };
-                      updatePackagePolicy({
-                        inputs: newInputs,
-                      });
-                    }}
+                    updatePackagePolicyInput={updatePackagePolicyInput}
                     inputValidationResults={
                       validationResults?.inputs?.[
                         hasIntegrations

x-pack/platform/plugins/shared/fleet/public/applications/fleet/sections/agent_policy/create_package_policy_page/single_page_layout/hooks/form.test.tsx
@@ -84,6 +84,8 @@ describe('useOnSubmit', () => {
         newAgentPolicy: { name: 'test', namespace: '' },
         queryParamsPolicyId: undefined,
         hasFleetAddAgentsPrivileges: true,
+        setNewAgentPolicy: jest.fn(),
+        setSelectedPolicyTab: jest.fn(),
       })
     );
 

x-pack/platform/plugins/shared/fleet/public/applications/fleet/sections/agent_policy/create_package_policy_page/single_page_layout/index.tsx
@@ -77,13 +77,13 @@ import { generateNewAgentPolicyWithDefaults } from '../../../../../../../common/
 import { packageHasAtLeastOneSecret } from '../utils';
 
 import { CreatePackagePolicySinglePageLayout, PostInstallAddAgentModal } from './components';
-import { useDevToolsRequest, useOnSubmit, useSetupTechnology } from './hooks';
+import { useDevToolsRequest, useOnSubmit } from './hooks';
 import { PostInstallCloudFormationModal } from './components/cloud_security_posture/post_install_cloud_formation_modal';
 import { PostInstallGoogleCloudShellModal } from './components/cloud_security_posture/post_install_google_cloud_shell_modal';
 import { PostInstallAzureArmTemplateModal } from './components/cloud_security_posture/post_install_azure_arm_template_modal';
 import { RootPrivilegesCallout } from './root_callout';
-import { useAgentless } from './hooks/setup_technology';
 import { SetupTechnologySelector } from './components/setup_technology_selector';
+import { useAgentless } from './hooks/setup_technology';
 
 export const StepsWithLessPadding = styled(EuiSteps)`
   .euiStep__content {
@@ -176,6 +176,10 @@ export const CreatePackagePolicySinglePage: CreatePackagePolicyParams = ({
     validationResults,
     hasAgentPolicyError,
     isInitialized,
+    handleSetupTechnologyChange,
+    selectedSetupTechnology,
+    defaultSetupTechnology,
+    isAgentlessSelected,
   } = useOnSubmit({
     agentCount,
     packageInfo,
@@ -185,6 +189,8 @@ export const CreatePackagePolicySinglePage: CreatePackagePolicyParams = ({
     queryParamsPolicyId,
     integrationToEnable: integrationInfo?.name,
     hasFleetAddAgentsPrivileges,
+    setNewAgentPolicy,
+    setSelectedPolicyTab,
   });
 
   const setPolicyValidation = useCallback(
@@ -341,7 +347,7 @@ export const CreatePackagePolicySinglePage: CreatePackagePolicyParams = ({
     () =>
       pliAuthBlockView?.Component && !isPackageInfoLoading
         ? pliAuthBlockView.Component
-        : ({ children }) => <>{children}</>, // when no UI Extension is registered, render children
+        : ({ children }: { children?: React.ReactNode }) => <>{children}</>, // when no UI Extension is registered, render children
     [isPackageInfoLoading, pliAuthBlockView?.Component]
   );
 
@@ -351,16 +357,6 @@ export const CreatePackagePolicySinglePage: CreatePackagePolicyParams = ({
     );
   }
   const { isAgentlessIntegration } = useAgentless();
-  const { handleSetupTechnologyChange, selectedSetupTechnology, defaultSetupTechnology } =
-    useSetupTechnology({
-      newAgentPolicy,
-      setNewAgentPolicy,
-      updateAgentPolicies,
-      updatePackagePolicy,
-      setSelectedPolicyTab,
-      packageInfo,
-      packagePolicy,
-    });
 
   const replaceStepConfigurePackagePolicy =
     replaceDefineStepView && packageInfo?.name ? (
@@ -423,6 +419,7 @@ export const CreatePackagePolicySinglePage: CreatePackagePolicyParams = ({
               updatePackagePolicy={updatePackagePolicy}
               validationResults={validationResults}
               submitAttempted={formState === 'INVALID'}
+              isAgentlessSelected={isAgentlessSelected}
             />
           )}
 
@@ -440,21 +437,22 @@ export const CreatePackagePolicySinglePage: CreatePackagePolicyParams = ({
         <div />
       ),
     [
-      isInitialized,
       isPackageInfoLoading,
-      agentPolicies,
+      isInitialized,
       packageInfo,
+      agentPolicies,
+      spaceSettings?.allowedNamespacePrefixes,
       packagePolicy,
       updatePackagePolicy,
       validationResults,
       formState,
-      integrationInfo?.name,
       extensionView,
-      handleExtensionViewOnChange,
-      spaceSettings?.allowedNamespacePrefixes,
-      handleSetupTechnologyChange,
       isAgentlessIntegration,
       selectedSetupTechnology,
+      integrationInfo?.name,
+      isAgentlessSelected,
+      handleExtensionViewOnChange,
+      handleSetupTechnologyChange,
     ]
   );
 

x-pack/platform/plugins/shared/fleet/public/applications/fleet/sections/agent_policy/services/devtools_request.tsx
@@ -80,7 +80,7 @@ export function generateUpdatePackagePolicyDevToolsRequest(
     {
       package: formatPackage(packagePolicy.package),
       ...omit(packagePolicy, 'version', 'package', 'enabled', 'secret_references'),
-      inputs: formatInputs(packagePolicy.inputs),
+      inputs: formatInputs(packagePolicy.inputs, packagePolicy?.supports_agentless ?? false),
       vars: formatVars(packagePolicy.vars),
     }
   );

commit e4ff0425939e801f0a6ddf3a0edebe570024bd45

[Fleet] Fix uncorrect override of cspm inputs (#208562)
## Summary
Fixes a bug introduced with
https://github.com/elastic/kibana/pull/206074

When a user selected a single account and switched the deployment method
to `agentless`, it reverted to the organization selection. This PR
removes[ the
code](https://github.com/elastic/kibana/pull/206074/files#diff-a169e222e1116859e05821170ab592903c2943613dfe0035c32a4b9d42e5634fR330)
that was causing the bug.


https://github.com/user-attachments/assets/9007c35b-8454-4f7f-8811-bd39ba7ac99e


⚠️ **Important** ⚠️
One side effect of this change is that when we switch back from
agentless to agent-based, the inputs that were previously hidden and
disabled are now visible but they are disabled. This is not ideal but
offers the user the option to re enable them, but at least doesn't try
to forcefully switch them to initial state, that was the cause of the
current bug.



https://github.com/user-attachments/assets/520e361b-2789-4081-bb91-c75db88d5431


### Checklist
- [ ] [Unit or functional
tests](https://www.elastic.co/guide/en/kibana/master/development-tests.html)
were updated or added to match the most common scenarios
- [ ] The PR description includes the appropriate Release Notes section,
and the correct `release_note:*` label is applied per the
[guidelines](https://www.elastic.co/guide/en/kibana/master/contributing.html#kibana-release-notes-process)

---------

Co-authored-by: Elastic Machine <elasticmachine@users.noreply.github.com>

x-pack/platform/plugins/shared/fleet/common/services/agentless_policy_helper.ts
@@ -73,7 +73,7 @@ export function validateAgentlessInputs(
   supportsAgentless?: boolean | null
 ) {
   if (Array.isArray(packagePolicyInputs)) {
-    return packagePolicyInputs.forEach((input) => {
+    packagePolicyInputs.forEach((input) => {
       throwIfInputNotAllowed(input.type, input.enabled, supportsAgentless);
     });
   } else {

x-pack/platform/plugins/shared/fleet/public/applications/fleet/sections/agent_policy/create_package_policy_page/single_page_layout/hooks/form.tsx
@@ -174,11 +174,6 @@ export function useOnSubmit({
   // Used to initialize the package policy once
   const isInitializedRef = useRef(false);
 
-  // only used to save the initial value of the package policy
-  const [initialPackagePolicy, setInitialPackagePolicy] = useState<NewPackagePolicy>({
-    ...DEFAULT_PACKAGE_POLICY,
-  });
-
   const [agentPolicies, setAgentPolicies] = useState<AgentPolicy[]>([]);
   // New package policy state
   const [packagePolicy, setPackagePolicy] = useState<NewPackagePolicy>({
@@ -276,19 +271,11 @@ export function useOnSubmit({
         DEFAULT_PACKAGE_POLICY.description,
         integrationToEnable
       );
-      setInitialPackagePolicy(basePackagePolicy);
       updatePackagePolicy(basePackagePolicy);
       setIsInitialized(true);
     }
     init();
-  }, [
-    packageInfo,
-    agentPolicies,
-    updatePackagePolicy,
-    integrationToEnable,
-    isInitialized,
-    initialPackagePolicy,
-  ]);
+  }, [packageInfo, agentPolicies, updatePackagePolicy, integrationToEnable, isInitialized]);
 
   useEffect(() => {
     if (
@@ -328,9 +315,9 @@ export function useOnSubmit({
       if (isAgentlessSelected && AGENTLESS_DISABLED_INPUTS.includes(input.type)) {
         return { ...input, enabled: false };
       }
-      return initialPackagePolicy.inputs[i];
+      return packagePolicy.inputs[i];
     });
-  }, [initialPackagePolicy?.inputs, isAgentlessSelected, packagePolicy.inputs]);
+  }, [packagePolicy.inputs, isAgentlessSelected]);
 
   useEffect(() => {
     if (prevSetupTechnology !== selectedSetupTechnology) {
patrykkopycinski/kibana

commit 77e4728a34b107902dd1236231b4b033d8e077c0

[Embeddable Rebuild] Migrate `ControlGroupRenderer`  to new embeddable framework (#190561)
Closes https://github.com/elastic/kibana/issues/189820

### Summary

This PR converts the `ControlGroupRenderer` to use the new control group
embeddable, which is built on the new React embeddable framework. With
this conversion, there should not be **any** changes in user-facing
behaviour - therefore, testing of this PR should be focused on ensuring
that no behaviour is changed and/or broken with this refactor.

**Notes to Solution Reviewers:**
- There should be minimal changes to your uses of `ControlGroupRenderer`
- our goal here was to keep the exposed API more-or-less consistent with
this refactor. Therefore, most changes are simply renames + changes of
imports.
- That being said, `updateInput` and `getInput$` are **very much** tied
to the old embeddable infrastructure - so while they will continue to
work for now, they have been deprecated in favour of adding
setters/getters for the parts of the control group state that you need
to update / respond to.

**Notes to Presentation Reviewer:**
- The bundle size was originally being increased by this PR, so I
decided to remove a bunch of the public exports that are no longer
necessary as a final cleanup - this resulted in changes to imports in a
few files, but it was worth doing in this PR IMO so that we didn't have
to increase the Controls bundle limit. Now, this PR shrinks the bundle
size :tada:
- I fixed a small bug with the default value of `showApplySelections` in
this PR - since it was a one-line change, if felt like overkill to
separate it out. See
https://github.com/elastic/kibana/pull/190561/files#r1733253015

### Checklist

- [x] [Unit or functional
tests](https://www.elastic.co/guide/en/kibana/master/development-tests.html)
were updated or added to match the most common scenarios

### For maintainers

- [ ] This was checked for breaking API changes and was [labeled

packages/kbn-alerts-ui-shared/src/alert_filter_controls/constants.ts
@@ -7,8 +7,8 @@
  * License v3.0 only", or the "Server Side Public License, v 1".
  */
 
-import type { AddOptionsListControlProps } from '@kbn/controls-plugin/public';
 import { ALERT_RULE_NAME, ALERT_STATUS } from '@kbn/rule-data-utils';
+import { OptionsListControlState } from '@kbn/controls-plugin/public';
 import { i18n } from '@kbn/i18n';
 import { FilterControlConfig } from './types';
 
@@ -65,14 +65,11 @@ export const TEST_IDS = {
   },
 };
 
-export const COMMON_OPTIONS_LIST_CONTROL_INPUTS: Partial<AddOptionsListControlProps> = {
+export const COMMON_OPTIONS_LIST_CONTROL_INPUTS: Partial<OptionsListControlState> = {
   hideExclude: true,
   hideSort: true,
-  hidePanelTitles: true,
   placeholder: '',
-  ignoreParentSettings: {
-    ignoreValidations: true,
-  },
+  width: 'small',
 };
 
 export const TIMEOUTS = {

commit 335e092b83e4e19b44068230be0e50f6064146e7

[ResponseOps][Alerts] Wrap Stack Alerts page filter controls in error boundary with fix call-to-action (#209559)
## Summary

Wraps the stack alerts page search bar's filter controls embeddable into
an `ErrorBoundary`, showing a fallback callout with a call-to-action to
reset the persisted state of the filters. This prevents the whole page
from crashing in case of errors in the embeddable, and provides a more
user-friendly way to gracefully recover from the error caused by
https://github.com/elastic/kibana/pull/190561 in the condition that
makes [our fix](https://github.com/elastic/kibana/pull/194785)
ineffective ([visiting the page on `8.15` and then updating to
`8.16+`](https://github.com/elastic/sdh-kibana/issues/5219#issuecomment-2633560380)).

<img width="1007" alt="Alert filter controls error callout"
src="https://github.com/user-attachments/assets/0c447f89-24f6-4d07-b7a1-97b13a267121"
/>

## Release Notes

Provides a fallback view to recover from Stack Alerts page filters bar
errors.

### Checklist

- [x] Any text added follows [EUI's writing
guidelines](https://elastic.github.io/eui/#/guidelines/writing), uses
sentence case text and includes [i18n
support](https://github.com/elastic/kibana/blob/main/src/platform/packages/shared/kbn-i18n/README.md)
- [x] The PR description includes the appropriate Release Notes section,
and the correct `release_note:*` label is applied per the
[guidelines](https://www.elastic.co/guide/en/kibana/master/contributing.html#kibana-release-notes-process)

---------

Co-authored-by: kibanamachine <42973632+kibanamachine@users.noreply.github.com>

x-pack/platform/plugins/shared/triggers_actions_ui/public/application/sections/alerts_search_bar/alerts_search_bar.tsx
@@ -6,7 +6,6 @@
  */
 
 import React, { useCallback, useMemo, useState } from 'react';
-import { useKibana } from '@kbn/kibana-react-plugin/public';
 import { compareFilters, Query, TimeRange } from '@kbn/es-query';
 import { SuggestionsAbstraction } from '@kbn/unified-search-plugin/public/typeahead/suggestions_component';
 import { isSiemRuleType } from '@kbn/rule-data-utils';
@@ -16,7 +15,7 @@ import { isQuickFiltersGroup, QuickFiltersMenuItem } from './quick_filters';
 import { NO_INDEX_PATTERNS } from './constants';
 import { SEARCH_BAR_PLACEHOLDER } from './translations';
 import { AlertsSearchBarProps, QueryLanguageType } from './types';
-import { TriggersAndActionsUiServices } from '../../..';
+import { useKibana } from '../../../common/lib/kibana';
 
 const SA_ALERTS = { type: 'alerts', fields: {} } as SuggestionsAbstraction;
 
@@ -50,7 +49,7 @@ export function AlertsSearchBar({
       ui: { SearchBar },
     },
     data: dataService,
-  } = useKibana<TriggersAndActionsUiServices>().services;
+  } = useKibana().services;
 
   const [queryLanguage, setQueryLanguage] = useState<QueryLanguageType>('kuery');
   const { dataView } = useAlertsDataView({

x-pack/platform/plugins/shared/triggers_actions_ui/public/application/sections/alerts_search_bar/constants.ts
@@ -13,3 +13,4 @@ export const ALERTS_SEARCH_BAR_PARAMS_URL_STORAGE_KEY = 'searchBarParams';
 export const NON_SIEM_CONSUMERS = Object.values(AlertConsumers).filter(
   (fid) => fid !== AlertConsumers.SIEM
 );
+export const RESET_FILTER_CONTROLS_TEST_SUBJ = 'resetFilterControlsButton';

x-pack/platform/plugins/shared/triggers_actions_ui/public/application/sections/alerts_search_bar/url_synced_alerts_search_bar.tsx
@@ -5,18 +5,23 @@
  * 2.0.
  */
 
-import React, { useCallback, useEffect, useState, useMemo } from 'react';
+import React, { useCallback, useEffect, useState, useMemo, memo } from 'react';
 import { BoolQuery, Filter } from '@kbn/es-query';
 import { i18n } from '@kbn/i18n';
 import { AlertFilterControls } from '@kbn/alerts-ui-shared/src/alert_filter_controls';
 import { ControlGroupRenderer } from '@kbn/controls-plugin/public';
 import { Storage } from '@kbn/kibana-utils-plugin/public';
+import { EuiButton, EuiCallOut } from '@elastic/eui';
 import { useKibana } from '../../..';
 import { useAlertSearchBarStateContainer } from './use_alert_search_bar_state_container';
-import { ALERTS_SEARCH_BAR_PARAMS_URL_STORAGE_KEY } from './constants';
+import {
+  ALERTS_SEARCH_BAR_PARAMS_URL_STORAGE_KEY,
+  RESET_FILTER_CONTROLS_TEST_SUBJ,
+} from './constants';
 import { AlertsSearchBarProps } from './types';
 import AlertsSearchBar from './alerts_search_bar';
 import { buildEsQuery } from './build_es_query';
+import { ErrorBoundary } from '../common/components/error_boundary';
 
 const INVALID_QUERY_STRING_TOAST_TITLE = i18n.translate(
   'xpack.triggersActionsUI.urlSyncedAlertsSearchBar.invalidQueryTitle',
@@ -24,6 +29,43 @@ const INVALID_QUERY_STRING_TOAST_TITLE = i18n.translate(
     defaultMessage: 'Invalid query string',
   }
 );
+
+const FILTER_CONTROLS_ERROR_VIEW_TITLE = i18n.translate(
+  'xpack.triggersActionsUI.urlSyncedAlertsSearchBar.filterControlsErrorTitle',
+  {
+    defaultMessage: 'Cannot render alert filters',
+  }
+);
+
+const FILTER_CONTROLS_ERROR_VIEW_DESCRIPTION = i18n.translate(
+  'xpack.triggersActionsUI.urlSyncedAlertsSearchBar.filterControlsErrorDescription',
+  {
+    defaultMessage: 'Try resetting them to fix the issue.',
+  }
+);
+
+const RESET_FILTERS_BUTTON_LABEL = i18n.translate(
+  'xpack.triggersActionsUI.urlSyncedAlertsSearchBar.resetFiltersButtonLabel',
+  {
+    defaultMessage: 'Reset filters',
+  }
+);
+
+const FilterControlsErrorView = memo(({ resetFilters }: { resetFilters: () => void }) => {
+  return (
+    <EuiCallOut title={FILTER_CONTROLS_ERROR_VIEW_TITLE} color="danger" iconType="error">
+      <p>{FILTER_CONTROLS_ERROR_VIEW_DESCRIPTION}</p>
+      <EuiButton
+        onClick={resetFilters}
+        color="danger"
+        fill
+        data-test-subj={RESET_FILTER_CONTROLS_TEST_SUBJ}
+      >
+        {RESET_FILTERS_BUTTON_LABEL}
+      </EuiButton>
+    </EuiCallOut>
+  );
+});
 
 export interface UrlSyncedAlertsSearchBarProps
   extends Omit<
@@ -135,6 +177,11 @@ export const UrlSyncedAlertsSearchBar = ({
     [spaceId]
   );
 
+  const resetFilters = useCallback(() => {
+    new Storage(window.localStorage).remove(filterControlsStorageKey);
+    window.location.reload();
+  }, [filterControlsStorageKey]);
+
   return (
     <>
       <AlertsSearchBar
@@ -151,25 +198,27 @@ export const UrlSyncedAlertsSearchBar = ({
         {...rest}
       />
       {showFilterControls && (
-        <AlertFilterControls
-          dataViewSpec={{
-            id: 'unified-alerts-dv',
-            title: '.alerts-*',
-          }}
-          spaceId={spaceId}
-          chainingSystem="HIERARCHICAL"
-          controlsUrlState={filterControls}
-          filters={controlFilters}
-          onFiltersChange={onControlFiltersChange}
-          storageKey={filterControlsStorageKey}
-          services={{
-            http,
-            notifications,
-            dataViews,
-            storage: Storage,
-          }}
-          ControlGroupRenderer={ControlGroupRenderer}
-        />
+        <ErrorBoundary fallback={() => <FilterControlsErrorView resetFilters={resetFilters} />}>
+          <AlertFilterControls
+            dataViewSpec={{
+              id: 'unified-alerts-dv',
+              title: '.alerts-*',
+            }}
+            spaceId={spaceId}
+            chainingSystem="HIERARCHICAL"
+            controlsUrlState={filterControls}
+            filters={controlFilters}
+            onFiltersChange={onControlFiltersChange}
+            storageKey={filterControlsStorageKey}
+            ControlGroupRenderer={ControlGroupRenderer}
+            services={{
+              http,
+              notifications,
+              dataViews,
+              storage: Storage,
+            }}
+          />
+        </ErrorBoundary>
       )}
     </>
   );

x-pack/platform/plugins/shared/triggers_actions_ui/public/application/sections/alerts_search_bar/url_synced_alerts_search_bar.test.tsx
@@ -1,0 +1,81 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import React from 'react';
+import { screen, render } from '@testing-library/react';
+import { AlertFilterControls } from '@kbn/alerts-ui-shared/src/alert_filter_controls';
+import { notificationServiceMock } from '@kbn/core-notifications-browser-mocks';
+import {
+  UrlSyncedAlertsSearchBar,
+  UrlSyncedAlertsSearchBarProps,
+} from './url_synced_alerts_search_bar';
+import { useKibana } from '../../../common/lib/kibana';
+import { alertSearchBarStateContainer, Provider } from './use_alert_search_bar_state_container';
+import { createStartServicesMock } from '../../../common/lib/kibana/kibana_react.mock';
+import { AlertsSearchBar } from './alerts_search_bar';
+import userEvent from '@testing-library/user-event';
+import { RESET_FILTER_CONTROLS_TEST_SUBJ } from './constants';
+
+const FILTER_CONTROLS_LOCAL_STORAGE_KEY = 'alertsSearchBar.filterControls';
+
+jest.mock('@kbn/alerts-ui-shared/src/alert_filter_controls');
+jest.mock('./alerts_search_bar');
+jest.mock('../../../common/lib/kibana');
+
+jest.mocked(useKibana).mockReturnValue({
+  services: {
+    ...createStartServicesMock(),
+    notifications: notificationServiceMock.createStartContract(),
+  },
+} as unknown as ReturnType<typeof useKibana>);
+
+jest.mocked(AlertsSearchBar).mockReturnValue(<div>AlertsSearchBar</div>);
+
+const defaultProps = {
+  appName: 'test',
+  onEsQueryChange: jest.fn(),
+};
+
+const TestComponent = (propOverrides: Partial<UrlSyncedAlertsSearchBarProps>) => (
+  <Provider value={alertSearchBarStateContainer}>
+    <UrlSyncedAlertsSearchBar {...defaultProps} {...propOverrides} />
+  </Provider>
+);
+
+describe('UrlSyncedAlertsSearchBar', () => {
+  it('should not show the filter controls when the showFilterControls toggle is off', () => {
+    jest.mocked(AlertFilterControls).mockImplementation(() => <div>AlertFilterControls</div>);
+    render(<TestComponent />);
+    expect(screen.queryByText('AlertFilterControls')).not.toBeInTheDocument();
+  });
+
+  it('should show the filter controls when the showFilterControls toggle is on', () => {
+    jest.mocked(AlertFilterControls).mockImplementation(() => <div>AlertFilterControls</div>);
+    render(<TestComponent showFilterControls />);
+    expect(screen.getByText('AlertFilterControls')).toBeInTheDocument();
+  });
+
+  describe('when the filter controls bar throws an error', () => {
+    beforeAll(() => {
+      jest.mocked(AlertFilterControls).mockImplementation(() => {
+        throw new Error('test error');
+      });
+    });
+
+    it('should catch filter control errors locally and show a fallback view', () => {
+      render(<TestComponent showFilterControls />);
+      expect(screen.getByText('Cannot render alert filters')).toBeInTheDocument();
+    });
+
+    it('should remove the correct localStorage item when resetting filter controls', async () => {
+      window.localStorage.setItem(FILTER_CONTROLS_LOCAL_STORAGE_KEY, '{}');
+      render(<TestComponent showFilterControls />);
+      await userEvent.click(await screen.findByTestId(RESET_FILTER_CONTROLS_TEST_SUBJ));
+      expect(window.localStorage.getItem(FILTER_CONTROLS_LOCAL_STORAGE_KEY)).toBeNull();
+    });
+  });
+});

x-pack/test/functional_with_es_ssl/apps/triggers_actions_ui/stack_alerts_page.ts
@@ -26,6 +26,12 @@ export default ({ getPageObjects, getService }: FtrProviderContext) => {
   const pageObjects = getPageObjects(['common', 'triggersActionsUI', 'header']);
   const log = getService('log');
   const retry = getService('retry');
+  const browser = getService('browser');
+
+  const loadAlertsPage = () =>
+    pageObjects.common.navigateToUrl('management', 'insightsAndAlerting/triggersActionsAlerts', {
+      shouldUseHashForSubUrl: false,
+    });
 
   describe('Stack alerts page', function () {
     describe('Loads the page with limited privileges', () => {
@@ -39,27 +45,26 @@ export default ({ getPageObjects, getService }: FtrProviderContext) => {
       });
 
       it('Loads the page', async () => {
-        await pageObjects.common.navigateToUrl(
-          'management',
-          'insightsAndAlerting/triggersActionsAlerts',
-          {
-            shouldUseHashForSubUrl: false,
-          }
-        );
+        await loadAlertsPage();
         const headingText = await pageObjects.triggersActionsUI.getSectionHeadingText();
         expect(headingText).to.be('Alerts');
+      });
+
+      it('Loads the page with a pre-saved filters configuration', async () => {
+        await pageObjects.common.navigateToUrl('management');
+        await browser.setLocalStorageItem(
+          'alertsSearchBar.default.filterControls',
+          `{"initialChildControlState":{"0":{"type":"optionsListControl","order":0,"hideExclude":true,"hideSort":true,"placeholder":"","width":"small","grow":true,"dataViewId":"unified-alerts-dv","title":"Status","fieldName":"kibana.alert.status","selectedOptions":["active"],"hideActionBar":true,"persist":true,"hideExists":true},"1":{"type":"optionsListControl","order":1,"hideExclude":true,"hideSort":true,"placeholder":"","width":"small","grow":true,"dataViewId":"unified-alerts-dv","title":"Rule","fieldName":"kibana.alert.rule.name","hideExists":true},"2":{"type":"optionsListControl","order":2,"hideExclude":true,"hideSort":true,"placeholder":"","width":"small","grow":true,"dataViewId":"unified-alerts-dv","title":"Group","fieldName":"kibana.alert.group.value"},"3":{"type":"optionsListControl","order":3,"hideExclude":true,"hideSort":true,"placeholder":"","width":"small","grow":true,"dataViewId":"unified-alerts-dv","title":"Tags","fieldName":"tags"}},"labelPosition":"oneLine","chainingSystem":"HIERARCHICAL","autoApplySelections":true,"ignoreParentSettings":{"ignoreValidations":true},"editorConfig":{"hideWidthSettings":true,"hideDataViewSelector":true,"hideAdditionalSettings":true}}`
+        );
+        await loadAlertsPage();
+        const filtersBar = await pageObjects.triggersActionsUI.getFilterGroupWrapper();
+        expect(filtersBar).to.not.be(null);
       });
 
       describe('feature filters', function () {
         this.tags('skipFIPS');
         it('Shows only allowed feature filters', async () => {
-          await pageObjects.common.navigateToUrl(
-            'management',
-            'insightsAndAlerting/triggersActionsAlerts',
-            {
-              shouldUseHashForSubUrl: false,
-            }
-          );
+          await loadAlertsPage();
 
           await pageObjects.header.waitUntilLoadingHasFinished();
 
@@ -90,13 +95,7 @@ export default ({ getPageObjects, getService }: FtrProviderContext) => {
       });
 
       it('Loads the page but shows missing permission prompt', async () => {
-        await pageObjects.common.navigateToUrl(
-          'management',
-          'insightsAndAlerting/triggersActionsAlerts',
-          {
-            shouldUseHashForSubUrl: false,
-          }
-        );
+        await loadAlertsPage();
         const exists = await testSubjects.exists('noPermissionPrompt');
         expect(exists).to.be(true);
       });
@@ -105,13 +104,7 @@ export default ({ getPageObjects, getService }: FtrProviderContext) => {
     describe('Loads the page', () => {
       beforeEach(async () => {
         await security.testUser.restoreDefaults();
-        await pageObjects.common.navigateToUrl(
-          'management',
-          'insightsAndAlerting/triggersActionsAlerts',
-          {
-            shouldUseHashForSubUrl: false,
-          }
-        );
+        await loadAlertsPage();
       });
 
       after(async () => {

x-pack/test/functional_with_es_ssl/page_objects/triggers_actions_ui_page.ts
@@ -242,5 +242,8 @@ export function TriggersActionsPageProvider({ getService }: FtrProviderContext) 
     async getAlertsPageAppliedFilters() {
       return await find.allByCssSelector('[data-test-subj="filter-items-group"] > *');
     },
+    async getFilterGroupWrapper() {
+      return await find.byCssSelector('.filter-group__wrapper');
+    },
   };
 }
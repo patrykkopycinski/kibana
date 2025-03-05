patrykkopycinski/kibana

commit cc44bbe64921a1818855763ee82522f212c0ae23

Upgrade ES client to 9.0.0-alpha.3 (#208776)
## Summary

Updating the ES client to 9.0. 

Resolves #116102

## What changes?

**Breaking change**: `body` has been removed.

Most of the changes are about bringing all the content inside the body
as a root attribute to the API params:

```diff
const response = await client.search({
  index: 'test',
-  body: {
    query: {
      match_all: {}
    }
-  }
})
```

For this reason, enabling the "Hide whitespace changes" option when
reviewing is recommended.

Some exceptions to this rule:

* Bulk APIs replace the `body` array with `operations` array (direct
replacement)
* Index Put Settings API replace `body` array with `settings` (direct
replacement)
* Msearch replaces the `body` array with `searches` array (direct
replacement)
* Document Index API replaces `body` with `document` (direct
replacement)
* Create Repository replaces `body` with `repository` (direct
replacement)

Because of a known issue in the client
(https://github.com/elastic/elasticsearch-js/issues/2584), there's still
an escape hatch to send data in the body in case the specific use case
requires it via `// @ts-expect-error elasticsearch@9.0.0
https://github.com/elastic/elasticsearch-js/issues/2584`, but it
shouldn't be abused because we lose types. In this PR we've used it in
those scenarios where we reuse the response of a GET as the body of a
PUT/POST.

### Other changes

* `estypes` can be imported from the root of the library as `import type
{ estypes } from '@elastic/elasticsearch';`
* `estypesWithBody` have been removed
* `requestTimeout`'s 30s default has been removed in the client. This PR
explicitly adds the setting in all client usages.


### Identify risks

- [x] The client places unknown properties as querystring, risking body
params leaking there, and causing 400 errors from ES => Solved by
forcing `body` usage there via `// @ts-expect-error elasticsearch@9.0.0
https://github.com/elastic/elasticsearch-js/issues/2584`. The next

src/platform/plugins/shared/data/common/search/search_source/search_source.ts
@@ -71,10 +71,11 @@ import {
   concat,
   omitBy,
   isNil,
+  omit,
 } from 'lodash';
 import { catchError, finalize, first, last, map, shareReplay, switchMap, tap } from 'rxjs';
 import { defer, EMPTY, from, lastValueFrom, Observable } from 'rxjs';
-import type * as estypes from '@elastic/elasticsearch/lib/api/typesWithBodyKey';
+import type { estypes } from '@elastic/elasticsearch';
 import {
   buildEsQuery,
   Filter,
@@ -795,9 +796,15 @@ export class SearchSource {
     const metaFields = getConfig<string[]>(UI_SETTINGS.META_FIELDS) ?? [];
 
     const searchRequest = this.mergeProps();
-    searchRequest.body = searchRequest.body || {};
-    const { body, index } = searchRequest;
-    const dataView = this.getDataView(index);
+    const bodyParams = omit(searchRequest, [
+      'index',
+      'filters',
+      'highlightAll',
+      'fieldsFromSource',
+      'body',
+    ]);
+    const body = { ...bodyParams, ...searchRequest.body };
+    const dataView = this.getDataView(searchRequest.index);
 
     // get some special field types from the index pattern
     const { docvalueFields, scriptFields, runtimeFields } = dataView?.getComputedFields() ?? {
@@ -809,7 +816,9 @@ export class SearchSource {
 
     // set defaults
     const _source =
-      index && !Object.hasOwn(body, '_source') ? dataView?.getSourceFiltering() : body._source;
+      searchRequest.index && !Object.hasOwn(body, '_source')
+        ? dataView?.getSourceFiltering()
+        : body._source;
 
     // get filter if data view specified, otherwise null filter
     const filter = this.getFieldFilter({ bodySourceExcludes: _source?.excludes, metaFields });
@@ -855,7 +864,7 @@ export class SearchSource {
     // 2. Create a data view using the index pattern `kibana*` and don't use a timestamp field.
     // 3. Uncomment the lines below, navigate to Discover,
     //    and switch to the data view created in step 2.
-    // body.query.bool.must.push({
+    // query.bool.must.push({
     //   error_query: {
     //     indices: [
     //       {
@@ -879,7 +888,7 @@ export class SearchSource {
     }
 
     const builtQuery = this.getBuiltEsQuery({
-      index,
+      index: searchRequest.index,
       query: searchRequest.query,
       filters: searchRequest.filters,
       getConfig,
@@ -887,7 +896,7 @@ export class SearchSource {
     });
 
     const bodyToReturn = {
-      ...searchRequest.body,
+      ...body,
       pit: searchRequest.pit,
       query: builtQuery,
       highlight:
@@ -913,9 +922,9 @@ export class SearchSource {
     };
 
     return omitByIsNil({
-      ...searchRequest,
+      ...omit(searchRequest, ['query', 'filters', 'fieldsFromSource']),
       body: omitByIsNil(bodyToReturn),
-      indexType: this.getIndexType(index),
+      indexType: this.getIndexType(searchRequest.index),
       highlightAll:
         searchRequest.highlightAll && builtQuery ? undefined : searchRequest.highlightAll,
     });
@@ -1092,7 +1101,7 @@ export class SearchSource {
       filter: originalFilters,
       aggs: searchSourceAggs,
       parent,
-      size: omit,
+      size: _size, // omit it
       sort,
       index,
       ...searchSourceFields

src/platform/plugins/shared/data/server/search/strategies/ese_search/ese_search_strategy.ts
@@ -10,12 +10,12 @@
 import type { Observable } from 'rxjs';
 import type { Logger, SharedGlobalConfig } from '@kbn/core/server';
 import { catchError, tap } from 'rxjs';
-import type * as estypes from '@elastic/elasticsearch/lib/api/typesWithBodyKey';
 import { firstValueFrom, from } from 'rxjs';
 import type { ISearchOptions, IEsSearchRequest, IEsSearchResponse } from '@kbn/search-types';
 import { getKbnServerError } from '@kbn/kibana-utils-plugin/server';
+import { omit } from 'lodash';
 import { IAsyncSearchRequestParams } from '../..';
-import { getKbnSearchError } from '../../report_search_error';
+import { getKbnSearchError, KbnSearchError } from '../../report_search_error';
 import type { ISearchStrategy, SearchStrategyDependencies } from '../../types';
 import type { IAsyncSearchOptions } from '../../../../common';
 import { DataViewType, isRunningResponse, pollSearch } from '../../../../common';
@@ -156,23 +156,23 @@ export const enhancedEsSearchStrategyProvider = (
   ): Promise<IEsSearchResponse> {
     const client = useInternalUser ? esClient.asInternalUser : esClient.asCurrentUser;
     const legacyConfig = await firstValueFrom(legacyConfig$);
-    const { body, index, ...params } = request.params!;
-    const method = 'POST';
-    const path = encodeURI(`/${index}/_rollup_search`);
     const querystring = {
       ...getShardTimeout(legacyConfig),
       ...(await getIgnoreThrottled(uiSettingsClient)),
       ...(await getDefaultSearchParams(uiSettingsClient)),
-      ...params,
     };
 
+    // Custom 400 error here because the client tries to run `index.toString()` and we no-longer can rely on ES 400
+    if (!request.params?.index) {
+      throw new KbnSearchError(`"params.index" is required when performing a rollup search`, 400);
+    }
+
     try {
-      const esResponse = await client.transport.request(
+      const esResponse = await client.rollup.rollupSearch(
         {
-          method,
-          path,
-          body,
-          querystring,
+          ...querystring,
+          ...omit(request.params, ['indexType']),
+          index: request.params.index,
         },
         {
           signal: options?.abortSignal,
@@ -180,7 +180,7 @@ export const enhancedEsSearchStrategyProvider = (
         }
       );
 
-      const response = esResponse.body as estypes.SearchResponse<any>;
+      const response = esResponse.body;
       return {
         rawResponse: shimHitsTotal(response, options),
         ...(esResponse.meta?.request?.params

commit 49191b324b8187ec091b7031da12862364ee8950

[Search] Fix `unrecognized parameter: [indexType]` error (#212707)
## Summary

This PR fixes an issue introduced in #208776 where switching from ES|QL
mode to classic mode in Discover caused the search request to fail with
an `unrecognized parameter: [indexType]` error. This was because the
`indexType` was actually being sent as part of the search request
`params` when it shouldn't have been.

We already had some rollup tests that would have caught it, but a
workaround was also added to the rollup search strategy to sidestep the
issue, preventing the tests from failing. That workaround has been
removed and existing ES|QL tests updated to catch the issue specifically
for ES|QL mode.

Fixes #212704.

### Checklist

- [ ] Any text added follows [EUI's writing
guidelines](https://elastic.github.io/eui/#/guidelines/writing), uses
sentence case text and includes [i18n
support](https://github.com/elastic/kibana/blob/main/src/platform/packages/shared/kbn-i18n/README.md)
- [ ]
[Documentation](https://www.elastic.co/guide/en/kibana/master/development-documentation.html)
was added for features that require explanation or tutorials
- [x] [Unit or functional
tests](https://www.elastic.co/guide/en/kibana/master/development-tests.html)
were updated or added to match the most common scenarios
- [ ] If a plugin configuration key changed, check if it needs to be
allowlisted in the cloud and added to the [docker
list](https://github.com/elastic/kibana/blob/main/src/dev/build/tasks/os_packages/docker_generator/resources/base/bin/kibana-docker)
- [ ] This was checked for breaking HTTP API changes, and any breaking
changes have been approved by the breaking-change committee. The
`release_note:breaking` label should be applied in these situations.
- [ ] [Flaky Test
Runner](https://ci-stats.kibana.dev/trigger_flaky_test_runner/1) was
used on any tests changed
- [x] The PR description includes the appropriate Release Notes section,
and the correct `release_note:*` label is applied per the

src/platform/plugins/shared/data/common/search/search_source/search_source.ts
@@ -544,12 +544,12 @@ export class SearchSource {
     options: SearchSourceSearchOptions
   ): Observable<IKibanaSearchResponse<unknown>> {
     const { search, getConfig, onResponse } = this.dependencies;
-
-    const params = getSearchParamsFromRequest(searchRequest, {
+    const { indexType, ...restRequest } = searchRequest;
+    const params = getSearchParamsFromRequest(restRequest, {
       getConfig,
     });
 
-    return search({ params, indexType: searchRequest.indexType }, options).pipe(
+    return search({ params, indexType }, options).pipe(
       switchMap((response) => {
         // For testing timeout messages in UI, uncomment the next line
         // response.rawResponse.timed_out = true;

src/platform/plugins/shared/data/server/search/strategies/ese_search/ese_search_strategy.ts
@@ -13,7 +13,6 @@ import { catchError, tap } from 'rxjs';
 import { firstValueFrom, from } from 'rxjs';
 import type { ISearchOptions, IEsSearchRequest, IEsSearchResponse } from '@kbn/search-types';
 import { getKbnServerError } from '@kbn/kibana-utils-plugin/server';
-import { omit } from 'lodash';
 import { IAsyncSearchRequestParams } from '../..';
 import { getKbnSearchError, KbnSearchError } from '../../report_search_error';
 import type { ISearchStrategy, SearchStrategyDependencies } from '../../types';
@@ -171,7 +170,7 @@ export const enhancedEsSearchStrategyProvider = (
       const esResponse = await client.rollup.rollupSearch(
         {
           ...querystring,
-          ...omit(request.params, ['indexType']),
+          ...request.params,
           index: request.params.index,
         },
         {

x-pack/test_serverless/functional/test_suites/common/discover/esql/_esql_view.ts
@@ -318,7 +318,7 @@ export default function ({ getService, getPageObjects }: FtrProviderContext) {
         });
       });
 
-      it('should show available data views after switching to classic mode', async () => {
+      it('should show available data views and search results after switching to classic mode', async () => {
         await PageObjects.discover.selectTextBaseLang();
         await PageObjects.header.waitUntilLoadingHasFinished();
         await PageObjects.discover.waitUntilSearchingHasFinished();
@@ -329,6 +329,7 @@ export default function ({ getService, getPageObjects }: FtrProviderContext) {
         await PageObjects.unifiedSearch.switchToDataViewMode();
         await PageObjects.header.waitUntilLoadingHasFinished();
         await PageObjects.discover.waitUntilSearchingHasFinished();
+        await PageObjects.discover.assertHitCount('14,004');
         const availableDataViews = await PageObjects.unifiedSearch.getDataViewList(
           'discover-dataView-switch-link'
         );

test/functional/apps/discover/esql/_esql_view.ts
@@ -318,7 +318,7 @@ export default function ({ getService, getPageObjects }: FtrProviderContext) {
         });
       });
 
-      it('should show available data views after switching to classic mode', async () => {
+      it('should show available data views and search results after switching to classic mode', async () => {
         await discover.selectTextBaseLang();
         await header.waitUntilLoadingHasFinished();
         await discover.waitUntilSearchingHasFinished();
@@ -329,6 +329,7 @@ export default function ({ getService, getPageObjects }: FtrProviderContext) {
         await unifiedSearch.switchToDataViewMode();
         await header.waitUntilLoadingHasFinished();
         await discover.waitUntilSearchingHasFinished();
+        await discover.assertHitCount('14,004');
         const availableDataViews = await unifiedSearch.getDataViewList(
           'discover-dataView-switch-link'
         );
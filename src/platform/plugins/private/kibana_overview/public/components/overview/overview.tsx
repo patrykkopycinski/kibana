patrykkopycinski/kibana

commit f6fc491235611ef18abb569a7dedce4f30c1b6a3

Hide main analytics applications on overview page if unavailable (#213198)
## Summary

This PR fixes a bug where unavailable (due to lack of permissions)
"main" analytics applications would show for users on Kibana overview
page.
Closes: #212171

src/platform/plugins/private/kibana_overview/public/components/overview/overview.tsx
@@ -10,7 +10,7 @@
 import './overview.scss';
 
 import { snakeCase } from 'lodash';
-import React, { FC, useState, useEffect } from 'react';
+import React, { FC, useState, useEffect, useMemo } from 'react';
 import useObservable from 'react-use/lib/useObservable';
 import {
   EuiCard,
@@ -165,6 +165,19 @@ export const Overview: FC<Props> = ({ newsFetchResult, solutions, features }) =>
   // Dashboard and discover are displayed in larger cards
   const mainApps = ['dashboards', 'discover'];
   const remainingApps = kibanaApps.map(({ id }) => id).filter((id) => !mainApps.includes(id));
+  const mainAppsUserHasAccessTo = useMemo(() => {
+    const applications = [];
+
+    if (application.capabilities.dashboards_v2?.show) {
+      applications.push('dashboards');
+    }
+
+    if (application.capabilities.discover_v2?.show) {
+      applications.push('discover');
+    }
+
+    return applications;
+  }, [application.capabilities]);
 
   const onDataViewCreated = () => {
     setNewKibanaInstance(false);
@@ -256,13 +269,13 @@ export const Overview: FC<Props> = ({ newsFetchResult, solutions, features }) =>
           </h2>
         </EuiScreenReaderOnly>
 
-        {mainApps.length ? (
+        {mainAppsUserHasAccessTo.length ? (
           <>
             <EuiFlexGroup
               className="kbnOverviewApps__group kbnOverviewApps__group--primary"
               justifyContent="center"
             >
-              {mainApps.map(renderAppCard)}
+              {mainAppsUserHasAccessTo.map(renderAppCard)}
             </EuiFlexGroup>
 
             <EuiSpacer size="l" />

commit 4a85e1b9ae8ab0a6eaa3e3b7cd27bc9e7058ab42

Fix wrong permission key for dashboard in analytics overview (#213295)
## Summary

This PR fixes a bug introduced in #213198 - wrong capability name was
being used.

Related to the fix from https://github.com/elastic/kibana/issues/212171

src/platform/plugins/private/kibana_overview/public/components/overview/overview.tsx
@@ -167,8 +167,7 @@ export const Overview: FC<Props> = ({ newsFetchResult, solutions, features }) =>
   const remainingApps = kibanaApps.map(({ id }) => id).filter((id) => !mainApps.includes(id));
   const mainAppsUserHasAccessTo = useMemo(() => {
     const applications = [];
-
-    if (application.capabilities.dashboards_v2?.show) {
+    if (application.capabilities.dashboard_v2?.show) {
       applications.push('dashboards');
     }
 
patrykkopycinski/kibana

commit 221c199ed159e63b28fa0d8d8934832d83dc0beb

[Search Connectors][Serverless] Add Search Connectors UI to the Stack Management data Section (#213509)
**!!MAJORITY OF THE CHANGED FILES ARE MOVED OR COPIED!!**

### Vision

According to the product vision we will build a new simple UI/UX in the
future https://github.com/elastic/security-team/issues/11790

This PR is a first iteration on enabling Content Connectors Management
UI in Serverless Kibana Stack Management.
Elastic Managed content connectors will be available only for Security
and Observability projects.
### Current PR scope

1. Used initial search_connectors plugin and renamed it to
content_connectors + moved from `x-pack/solutions/search` to
`x-pack/platform/plugins/shared`
2. Copy relevant connectors UI and routes from enterprise_search plugin.
3. Introduce the new Stack Management card/navigation option under the
Data section.
4. Enabled this plugin only in Serverless for Security and Observability
projects.
5. For making PR smaller Pipelines tab was not moved. And according to
Search team vision this functionality should be dropped anyway soon.
6. Extended fleet package logic to include elastic_connectors for
security and o11y serverless projects
7. Added back `search:agentless-connectors-manager` task

In Stack Management navigation:
<img width="2062" alt="Screenshot 2025-04-15 at 3 51 43 PM"
src="https://github.com/user-attachments/assets/5c93ba01-9a6a-4eac-a21d-1370f03b8f35"
/>

Stack Management cards:
<img width="2081" alt="Screenshot 2025-04-10 at 8 41 43 PM"
src="https://github.com/user-attachments/assets/3def1c12-561b-4a84-8241-4dd61cd9313d"
/>


Create Elastic Managed Connector UI (on Agentless):

<img width="1822" alt="Screenshot 2025-04-15 at 3 55 29 PM"
src="https://github.com/user-attachments/assets/6e9fea48-85e7-43df-919d-0e5492d0e704"
/>

Create Self Managed Connector UI:

<img width="2064" alt="Screenshot 2025-04-15 at 3 55 49 PM"
src="https://github.com/user-attachments/assets/d5051898-c8fa-4e41-b9ea-b41d4ed4a0d5"
/>

### Next steps

config/serverless.es.yml
@@ -137,3 +137,6 @@ xpack.searchSynonyms.enabled: true
 
 # Query Rules UI
 xpack.searchQueryRules.enabled: false
+
+## Search Connectors in stack management
+xpack.contentConnectors.enabled: false

x-pack/platform/plugins/shared/content_connectors/public/plugin.ts
@@ -1,0 +1,89 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { CoreSetup, CoreStart, Plugin, PluginInitializerContext } from '@kbn/core/public';
+import { docLinks } from '@kbn/search-connectors/constants/doc_links';
+import { ManagementAppMountParams } from '@kbn/management-plugin/public';
+import { getConnectorFullTypes, getConnectorTypes } from '../common/lib/connector_types';
+import {
+  SearchConnectorsPluginSetup,
+  SearchConnectorsPluginSetupDependencies,
+  SearchConnectorsPluginStart,
+  SearchConnectorsPluginStartDependencies,
+} from './types';
+import { PLUGIN_ID } from '../common/constants';
+import { PLUGIN_NAME } from './translations';
+
+export class SearchConnectorsPlugin
+  implements
+    Plugin<
+      SearchConnectorsPluginSetup,
+      SearchConnectorsPluginStart,
+      SearchConnectorsPluginSetupDependencies,
+      SearchConnectorsPluginStartDependencies
+    >
+{
+  private readonly isServerless: boolean;
+  private readonly kibanaVersion: string;
+
+  constructor(initializerContext: PluginInitializerContext) {
+    this.isServerless = initializerContext.env.packageInfo.buildFlavor === 'serverless';
+    this.kibanaVersion = initializerContext.env.packageInfo.version;
+  }
+
+  public setup(
+    core: CoreSetup<SearchConnectorsPluginStartDependencies, SearchConnectorsPluginStart>,
+    dependencies: SearchConnectorsPluginSetupDependencies
+  ): SearchConnectorsPluginSetup {
+    const { management } = dependencies;
+    const connectorTypes = getConnectorTypes(core.http.staticAssets);
+    const kibanaVersion = this.kibanaVersion;
+
+    if (this.isServerless === true) {
+      management.sections.section.data.registerApp({
+        id: PLUGIN_ID,
+        title: PLUGIN_NAME,
+        order: 8,
+        keywords: ['content connectors', 'search'],
+        async mount(params: ManagementAppMountParams) {
+          const [{ renderApp }, [coreStart, pluginsStartDeps, pluginStart]] = await Promise.all([
+            import('./app'),
+            core.getStartServices(),
+          ]);
+
+          const connectorsDefinitions = getConnectorFullTypes(core.http.staticAssets);
+          return renderApp(
+            coreStart,
+            pluginsStartDeps,
+            pluginStart,
+            params,
+            connectorsDefinitions,
+            kibanaVersion
+          );
+        },
+      });
+    }
+    return {
+      getConnectorTypes: () => connectorTypes,
+    };
+  }
+
+  public start(
+    core: CoreStart,
+    services: SearchConnectorsPluginStartDependencies
+  ): SearchConnectorsPluginStart {
+    const { http } = core;
+    docLinks.setDocLinks(core.docLinks.links);
+    const connectorTypes = getConnectorFullTypes(http.staticAssets);
+
+    return {
+      getConnectorTypes: () => connectorTypes,
+    };
+  }
+
+  public stop() {}
+}

x-pack/solutions/search/plugins/enterprise_search/public/plugin.ts
@@ -10,6 +10,7 @@ import { BehaviorSubject, firstValueFrom, type Subscription } from 'rxjs';
 import { ChartsPluginStart } from '@kbn/charts-plugin/public';
 import { CloudSetup, CloudStart } from '@kbn/cloud-plugin/public';
 import { ConsolePluginStart } from '@kbn/console-plugin/public';
+import { SearchConnectorsPluginStart } from '@kbn/content-connectors-plugin/public';
 import {
   AppMountParameters,
   CoreStart,
@@ -33,7 +34,6 @@ import { LicensingPluginStart } from '@kbn/licensing-plugin/public';
 import { MlPluginStart } from '@kbn/ml-plugin/public';
 import type { NavigationPublicPluginStart } from '@kbn/navigation-plugin/public';
 import { ELASTICSEARCH_URL_PLACEHOLDER } from '@kbn/search-api-panels/constants';
-import { SearchConnectorsPluginStart } from '@kbn/search-connectors-plugin/public';
 import type { SearchNavigationPluginStart } from '@kbn/search-navigation/public';
 import { SearchPlaygroundPluginStart } from '@kbn/search-playground/public';
 import { SecurityPluginSetup, SecurityPluginStart } from '@kbn/security-plugin/public';

x-pack/solutions/search/plugins/enterprise_search/server/types.ts
@@ -6,6 +6,7 @@
  */
 
 import type { CloudSetup, CloudStart } from '@kbn/cloud-plugin/server';
+import type { SearchConnectorsPluginSetup } from '@kbn/content-connectors-plugin/server';
 import type {
   Logger,
   SavedObjectsServiceStart,
@@ -22,7 +23,6 @@ import type { GuidedOnboardingPluginSetup } from '@kbn/guided-onboarding-plugin/
 import type { LicensingPluginStart } from '@kbn/licensing-plugin/public';
 import type { LogsSharedPluginSetup } from '@kbn/logs-shared-plugin/server';
 import type { MlPluginSetup } from '@kbn/ml-plugin/server';
-import type { SearchConnectorsPluginSetup } from '@kbn/search-connectors-plugin/server';
 import type { SecurityPluginSetup } from '@kbn/security-plugin/server';
 import type { SpacesPluginStart } from '@kbn/spaces-plugin/server';
 import type { UsageCollectionSetup } from '@kbn/usage-collection-plugin/server';

x-pack/solutions/search/plugins/serverless_search/public/application/hooks/use_kibana.tsx
@@ -11,7 +11,7 @@ import type { AppMountParameters, CoreStart } from '@kbn/core/public';
 import type { SharePluginStart } from '@kbn/share-plugin/public';
 import { useKibana as useKibanaBase } from '@kbn/kibana-react-plugin/public';
 import { AuthenticatedUser } from '@kbn/security-plugin/common';
-import { SearchConnectorsPluginStart } from '@kbn/search-connectors-plugin/public';
+import { SearchConnectorsPluginStart } from '@kbn/content-connectors-plugin/public';
 import { SecurityPluginStart } from '@kbn/security-plugin-types-public';
 
 export interface ServerlessSearchContext {

x-pack/solutions/search/plugins/enterprise_search/public/applications/index.test.tsx
@@ -5,7 +5,7 @@
  * 2.0.
  */
 
-import { searchConnectorsMock } from '@kbn/search-connectors-plugin/public/plugin.mock';
+import { searchConnectorsMock } from '@kbn/content-connectors-plugin/public/plugin.mock';
 
 import React from 'react';
 

x-pack/solutions/search/plugins/serverless_search/public/test/test_utils.tsx
@@ -16,7 +16,7 @@ import { coreMock } from '@kbn/core/public/mocks';
 import { cloudMock } from '@kbn/cloud-plugin/public/mocks';
 import { sharePluginMock } from '@kbn/share-plugin/public/mocks';
 import { userProfileMock } from '@kbn/security-plugin/common/model/user_profile.mock';
-import { searchConnectorsMock } from '@kbn/search-connectors-plugin/public/plugin.mock';
+import { searchConnectorsMock } from '@kbn/content-connectors-plugin/public/plugin.mock';
 
 export const core = coreMock.createStart();
 export const services = {

commit 270de141dbfef7fd88bfd72fa845a9453c8bdaf0

[Search] Fix: serverless search connector types (#218842)
## Summary

Fixes bug introduced by: https://github.com/elastic/kibana/pull/213509
where serverless search & enterprise search could not load list of
available connector types from the connectors plugin. This was caused by
2 things in serverless, 1) the entire plugin was disabled and 2) the
plugin id was renamed.

Updated the `contentConnectors` plugin to have a `ui.enabled` config
value to override just disabling the management UI from being registered
for Serverless Search, while still allowing the plugin to be enabled.
And updated the usages of the `searchConnectors` plugin in
`enteprise_search` & `serverless_search` to account for it being renamed
to `contentConnectors`

Of note this bug would have been caught by FTRs, but they have been
skipped for being
[flakey](https://github.com/elastic/kibana/issues/203462) for some time,
and have proven [hard
fix](https://github.com/elastic/kibana/pull/205971) given the current UX
😔

### Checklist

- [x] [Unit or functional
tests](https://www.elastic.co/guide/en/kibana/master/development-tests.html)
were updated or added to match the most common scenarios
- [ ] [Flaky Test
Runner](https://ci-stats.kibana.dev/trigger_flaky_test_runner/1) was
used on any tests changed

config/serverless.es.yml
@@ -139,4 +139,4 @@ xpack.searchSynonyms.enabled: true
 xpack.searchQueryRules.enabled: false
 
 ## Search Connectors in stack management
-xpack.contentConnectors.enabled: false
+xpack.contentConnectors.ui.enabled: false

x-pack/platform/plugins/shared/content_connectors/common/types/config.ts
@@ -5,10 +5,6 @@
  * 2.0.
  */
 
-import { useKibanaServices } from '../use_kibana';
-
-export const useConnectorTypes = () => {
-  const { contentConnectors } = useKibanaServices();
-
-  return contentConnectors?.getConnectorTypes() || [];
-};
+export interface ClientConfigType {
+  ui: { enabled: boolean };
+}

x-pack/platform/plugins/shared/content_connectors/public/plugin.ts
@@ -8,6 +8,7 @@
 import { CoreSetup, CoreStart, Plugin, PluginInitializerContext } from '@kbn/core/public';
 import { docLinks } from '@kbn/search-connectors/constants/doc_links';
 import { ManagementAppMountParams } from '@kbn/management-plugin/public';
+import { type ClientConfigType } from '../common/types/config';
 import { getConnectorFullTypes, getConnectorTypes } from '../common/lib/connector_types';
 import {
   SearchConnectorsPluginSetup,
@@ -29,10 +30,12 @@ export class SearchConnectorsPlugin
 {
   private readonly isServerless: boolean;
   private readonly kibanaVersion: string;
+  private readonly config: ClientConfigType;
 
   constructor(initializerContext: PluginInitializerContext) {
     this.isServerless = initializerContext.env.packageInfo.buildFlavor === 'serverless';
     this.kibanaVersion = initializerContext.env.packageInfo.version;
+    this.config = initializerContext.config.get<ClientConfigType>();
   }
 
   public setup(
@@ -43,7 +46,7 @@ export class SearchConnectorsPlugin
     const connectorTypes = getConnectorTypes(core.http.staticAssets);
     const kibanaVersion = this.kibanaVersion;
 
-    if (this.isServerless === true) {
+    if (this.config.ui.enabled && this.isServerless === true) {
       management.sections.section.data.registerApp({
         id: PLUGIN_ID,
         title: PLUGIN_NAME,

x-pack/platform/plugins/shared/content_connectors/server/config.ts
@@ -12,11 +12,17 @@ export * from './types';
 
 const configSchema = schema.object({
   enabled: schema.boolean({ defaultValue: true }),
+  ui: schema.object({
+    enabled: schema.boolean({ defaultValue: true }),
+  }),
 });
 
 type ConfigType = TypeOf<typeof configSchema>;
 
 export const config: PluginConfigDescriptor<ConfigType> = {
+  exposeToBrowser: {
+    ui: true,
+  },
   schema: configSchema,
 };
 

x-pack/solutions/search/plugins/enterprise_search/kibana.jsonc
@@ -34,7 +34,7 @@
       "usageCollection",
       "guidedOnboarding",
       "console",
-      "searchConnectors",
+      "contentConnectors",
       "searchNavigation",
       "searchPlayground",
       "embeddable",

x-pack/solutions/search/plugins/enterprise_search/public/applications/index.tsx
@@ -82,7 +82,7 @@ export const renderApp = (
   const store = getContext().store;
   const indexMappingComponent = indexManagementPlugin?.getIndexMappingComponent({ history });
 
-  const connectorTypes = plugins.searchConnectors?.getConnectorTypes() || [];
+  const connectorTypes = plugins.contentConnectors?.getConnectorTypes() || [];
 
   const unmountKibanaLogic = mountKibanaLogic({
     application,

x-pack/solutions/search/plugins/enterprise_search/public/plugin.ts
@@ -82,6 +82,7 @@ export interface PluginsStart {
   charts?: ChartsPluginStart;
   cloud?: CloudSetup & CloudStart;
   console?: ConsolePluginStart;
+  contentConnectors?: SearchConnectorsPluginStart;
   data?: DataPublicPluginStart;
   fleet?: FleetStart;
   guidedOnboarding?: GuidedOnboardingPluginStart;
@@ -90,7 +91,6 @@ export interface PluginsStart {
   licensing?: LicensingPluginStart;
   ml?: MlPluginStart;
   navigation: NavigationPublicPluginStart;
-  searchConnectors?: SearchConnectorsPluginStart;
   searchNavigation?: SearchNavigationPluginStart;
   searchPlayground?: SearchPlaygroundPluginStart;
   security?: SecurityPluginStart;

x-pack/solutions/search/plugins/enterprise_search/server/plugin.ts
@@ -100,7 +100,7 @@ export class EnterpriseSearchPlugin implements Plugin<void, void, PluginsSetup, 
       licensing,
       guidedOnboarding,
       cloud,
-      searchConnectors,
+      contentConnectors,
     }: PluginsSetup
   ) {
     this.globalConfigService.setup(elasticsearch.legacy.config$, cloud);
@@ -317,7 +317,7 @@ export class EnterpriseSearchPlugin implements Plugin<void, void, PluginsSetup, 
 
     if (globalSearch) {
       globalSearch.registerResultProvider(
-        getSearchResultProvider(config, searchConnectors?.getConnectorTypes() || [])
+        getSearchResultProvider(config, contentConnectors?.getConnectorTypes() || [])
       );
       globalSearch.registerResultProvider(getIndicesSearchResultProvider(http.staticAssets));
       globalSearch.registerResultProvider(getConnectorsSearchResultProvider(http.staticAssets));

x-pack/solutions/search/plugins/enterprise_search/server/types.ts
@@ -33,6 +33,7 @@ import type { ConfigType } from '.';
 
 export interface PluginsSetup {
   cloud?: CloudSetup;
+  contentConnectors?: SearchConnectorsPluginSetup;
   customIntegrations?: CustomIntegrationsPluginSetup;
   features: FeaturesPluginSetup;
   globalSearch: GlobalSearchPluginSetup;
@@ -40,7 +41,6 @@ export interface PluginsSetup {
   licensing: LicensingPluginStart;
   logsShared: LogsSharedPluginSetup;
   ml?: MlPluginSetup;
-  searchConnectors?: SearchConnectorsPluginSetup;
   security: SecurityPluginSetup;
   usageCollection?: UsageCollectionSetup;
 }

x-pack/solutions/search/plugins/serverless_search/kibana.jsonc
@@ -33,7 +33,7 @@
     ],
     "optionalPlugins": [
       "indexManagement",
-      "searchConnectors",
+      "contentConnectors",
       "searchInferenceEndpoints",
       "searchPlayground",
       "usageCollection"

x-pack/solutions/search/plugins/serverless_search/public/application/hooks/use_kibana.tsx
@@ -18,7 +18,7 @@ export interface ServerlessSearchContext {
   cloud: CloudStart;
   console: ConsolePluginStart;
   history: AppMountParameters['history'];
-  searchConnectors?: SearchConnectorsPluginStart;
+  contentConnectors?: SearchConnectorsPluginStart;
   security: SecurityPluginStart;
   share: SharePluginStart;
   user?: AuthenticatedUser;

src/platform/test/plugin_functional/test_suites/core_plugins/rendering.ts
@@ -256,6 +256,7 @@ export default function ({ getService }: PluginFunctionalProviderContext) {
         'xpack.cloud.serverless.project_type (string?)',
         'xpack.cloud.serverless.orchestrator_target (string?)',
         'xpack.cloud.onboarding.default_solution (string?)',
+        'xpack.contentConnectors.ui.enabled (boolean?)',
         'xpack.discoverEnhanced.actions.exploreDataInChart.enabled (boolean?)',
         'xpack.discoverEnhanced.actions.exploreDataInContextMenu.enabled (boolean?)',
         'xpack.fleet.agents.enabled (boolean?)',

x-pack/solutions/search/plugins/enterprise_search/public/applications/index.test.tsx
@@ -39,6 +39,7 @@ describe('renderApp', () => {
     params: coreMock.createAppMountParameters(),
     plugins: {
       charts: chartPluginMock.createStartContract(),
+      contentConnectors: searchConnectorsMock.createStart(),
       data: dataPluginMock.createStartContract(),
       guidedOnboarding: guidedOnboardingMock.createStart(),
       indexManagement: {
@@ -47,7 +48,6 @@ describe('renderApp', () => {
       lens: lensPluginMock.createStartContract(),
       licensing: licensingMock.createStart(),
       navigation: navigationPluginMock.createStartContract(),
-      searchConnectors: searchConnectorsMock.createStart(),
       security: securityMock.createStart(),
       share: sharePluginMock.createStartContract(),
       ml: mlPluginMock.createStartContract(),

x-pack/solutions/search/plugins/serverless_search/public/test/test_utils.tsx
@@ -21,7 +21,7 @@ import { searchConnectorsMock } from '@kbn/content-connectors-plugin/public/plug
 export const core = coreMock.createStart();
 export const services = {
   cloud: cloudMock.createStart(),
-  searchConnectors: searchConnectorsMock.createStart(),
+  contentConnectors: searchConnectorsMock.createStart(),
   share: sharePluginMock.createStartContract(),
   userProfile: userProfileMock.createWithSecurity(),
 };
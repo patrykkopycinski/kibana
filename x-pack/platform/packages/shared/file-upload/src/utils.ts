051f139318e25cbe3a63464633f1e8c32193595e
James Gowdy <jgowdy@elastic.co>
2025-06-06 14:52:32 +0100

    [ML] Adding file upload tools to package (#221034)
    Moves the core file upload classes to a package called
    `@kbn/file-upload`
    Moves a lot of shared and duplicated constants to the package
    `@kbn/file-upload-common`
    
    Adds a new context called `FileUploadContext` and related hook
    `useFileUploadContext` to manage all aspects of the file upload process.
    Updates the file upload lite flyout used by the Search solution to use
    this new context.
    
    ---------
    
    Co-authored-by: kibanamachine <42973632+kibanamachine@users.noreply.github.com>

--- x-pack/platform/packages/shared/file-upload/src/utils.ts
+++ x-pack/platform/packages/shared/file-upload/src/utils.ts
@@ -7,6 +7,7 @@
 
 import { isEqual } from 'lodash';
 import type { AnalysisResult, InputOverrides } from '@kbn/file-upload-plugin/common';
+import type { FormattedOverrides } from '@kbn/file-upload-plugin/common/types';
 import { MB, FILE_FORMATS, NO_TIME_FORMAT } from '@kbn/file-upload-common';
 
 export const DEFAULT_LINES_TO_SAMPLE = 1000;
@@ -56,7 +57,7 @@ export function readFile(file: File): Promise<{ fileContents: string; data: Arra
 }
 
 export function createUrlOverrides(overrides: InputOverrides, originalSettings: InputOverrides) {
-  const formattedOverrides: InputOverrides = {};
+  const formattedOverrides: FormattedOverrides = {};
   for (const o in overrideDefaults) {
     if (Object.hasOwn(overrideDefaults, o)) {
       let value = overrides[o];
@@ -83,10 +84,13 @@ export function createUrlOverrides(overrides: InputOverrides, originalSettings: 
     ) {
       formattedOverrides.format = originalSettings.format;
     }
+  }
 
-    if (Array.isArray(formattedOverrides.column_names)) {
-      formattedOverrides.column_names = formattedOverrides.column_names.join();
-    }
+  if (
+    formattedOverrides.format === FILE_FORMATS.DELIMITED &&
+    Array.isArray(formattedOverrides.column_names)
+  ) {
+    formattedOverrides.column_names = formattedOverrides.column_names.join();
   }
 
   if (
@@ -145,4 +149,15 @@ export function processResults({ results, overrides }: AnalysisResult) {
     grokPattern: results.grok_pattern,
     linesToSample,
   };
+}
+
+export type ServerSettings = ReturnType<typeof processResults> | null;
+
+export function isSupportedFormat(format: string) {
+  return (
+    format === FILE_FORMATS.NDJSON ||
+    format === FILE_FORMATS.DELIMITED ||
+    format === FILE_FORMATS.SEMI_STRUCTURED_TEXT ||
+    format === FILE_FORMATS.TIKA
+  );
 }

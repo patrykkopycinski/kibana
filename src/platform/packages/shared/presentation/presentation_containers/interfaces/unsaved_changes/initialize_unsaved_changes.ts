patrykkopycinski/kibana

commit 3e882d8cd9b784dc73961dbdfa79285761eab7ba

[Embeddables] Serialized State Only (#215947)
Closes https://github.com/elastic/kibana/issues/205531
Closes #219877.
Closes https://github.com/elastic/kibana/issues/213153
Closes https://github.com/elastic/kibana/issues/150920
Closes https://github.com/elastic/kibana/issues/203130
 
### Overview
The embeddable framework has two types of state: `SerializedState` and
`RuntimeState`.

`SerializedState` is the form of the state when saved into a Dashboard
saved object. I.e. the References are extracted, and state saved
externally (by reference) is removed. In contrast `RuntimeState` is an
exact snapshot of the state used by the embeddable to render.

<b>Exposing SerializedState and RuntimeState was a mistake</b> that
caused numerous regressions and architectural complexities.

This PR simplifies the embeddable framework by only exposing
`SerializedState`. `RuntimeState` stays localized to the embeddable
implementation and is never leaked to the embeddable framework.

### Whats changed
* `ReactEmbeddableFactory<SerializedState, RuntimeState, Api>` =>
`EmbeddableFactory<SerializedState, Api>`
* `deserializeState` removed from embeddable factory. Instead,
`SerializedState` is passed directly into `buildEmbeddable`.
* `buildEmbeddable` parameter `buildApi` replaced with `finalizeApi`.
`buildApi({ api, comparators })` => `finalizeApi(api)`.
* The embeddable framework previously used its knowledge of
`RuntimeState` to setup and monitor unsaved changes. Now, unsaved
changes setup is pushed down to the embeddable implementation since the
embeddable framework no longer has knowledge of embeddable RuntimeState.

### Reviewer instructions
<b>Please prioritize reviews.</b> This is a large effort from our team
and is blocking many other initiatives. Getting this merged is a top
priority.

This is a large change that would best be reviewed by manually testing
the changes
* adding/editing your embeddable types
* Ensuring dashboard shows unsaved changes as expected

src/platform/packages/shared/presentation/presentation_containers/interfaces/unsaved_changes/initialize_unsaved_changes.ts
@@ -8,111 +8,56 @@
  */
 
 import {
-  BehaviorSubject,
-  combineLatest,
-  combineLatestWith,
-  debounceTime,
-  map,
-  Subscription,
-} from 'rxjs';
-import {
-  getInitialValuesFromComparators,
   PublishesUnsavedChanges,
-  PublishingSubject,
-  runComparators,
+  SerializedPanelState,
   StateComparators,
-  HasSnapshottableState,
+  areComparatorsEqual,
 } from '@kbn/presentation-publishing';
-import { apiHasSaveNotification } from '../has_save_notification';
+import { MaybePromise } from '@kbn/utility-types';
+import { Observable, combineLatestWith, debounceTime, map, of } from 'rxjs';
+import { apiHasLastSavedChildState } from '../last_saved_child_state';
 
-export const COMPARATOR_SUBJECTS_DEBOUNCE = 100;
+const UNSAVED_CHANGES_DEBOUNCE = 100;
 
-export const initializeUnsavedChanges = <RuntimeState extends {} = {}>(
-  initialLastSavedState: RuntimeState,
-  parentApi: unknown,
-  comparators: StateComparators<RuntimeState>
-) => {
-  const subscriptions: Subscription[] = [];
-  const lastSavedState$ = new BehaviorSubject<RuntimeState | undefined>(initialLastSavedState);
-
-  const snapshotRuntimeState = () => {
-    const comparatorKeys = Object.keys(comparators) as Array<keyof RuntimeState>;
-    const snapshot = {} as RuntimeState;
-    comparatorKeys.forEach((key) => {
-      const comparatorSubject = comparators[key][0]; // 0th element of tuple is the subject
-      snapshot[key] = comparatorSubject.value as RuntimeState[typeof key];
-    });
-    return snapshot;
-  };
-
-  if (apiHasSaveNotification(parentApi)) {
-    subscriptions.push(
-      // any time the parent saves, the current state becomes the last saved state...
-      parentApi.saveNotification$.subscribe(() => {
-        lastSavedState$.next(snapshotRuntimeState());
-      })
-    );
+export const initializeUnsavedChanges = <StateType extends object = object>({
+  uuid,
+  onReset,
+  parentApi,
+  getComparators,
+  defaultState,
+  serializeState,
+  anyStateChange$,
+}: {
+  uuid: string;
+  parentApi: unknown;
+  anyStateChange$: Observable<void>;
+  serializeState: () => SerializedPanelState<StateType>;
+  getComparators: () => StateComparators<StateType>;
+  defaultState?: Partial<StateType>;
+  onReset: (lastSavedPanelState?: SerializedPanelState<StateType>) => MaybePromise<void>;
+}): PublishesUnsavedChanges => {
+  if (!apiHasLastSavedChildState<StateType>(parentApi)) {
+    return {
+      hasUnsavedChanges$: of(false),
+      resetUnsavedChanges: () => Promise.resolve(),
+    };
   }
 
-  const comparatorSubjects: Array<PublishingSubject<unknown>> = [];
-  const comparatorKeys: Array<keyof RuntimeState> = []; // index maps comparator subject to comparator key
-  for (const key of Object.keys(comparators) as Array<keyof RuntimeState>) {
-    const comparatorSubject = comparators[key][0]; // 0th element of tuple is the subject
-    comparatorSubjects.push(comparatorSubject as PublishingSubject<unknown>);
-    comparatorKeys.push(key);
-  }
-
-  const unsavedChanges$ = new BehaviorSubject<Partial<RuntimeState> | undefined>(
-    runComparators(
-      comparators,
-      comparatorKeys,
-      lastSavedState$.getValue() as RuntimeState,
-      getInitialValuesFromComparators(comparators, comparatorKeys)
-    )
+  const hasUnsavedChanges$ = anyStateChange$.pipe(
+    combineLatestWith(
+      parentApi.lastSavedStateForChild$(uuid).pipe(map((panelState) => panelState?.rawState))
+    ),
+    debounceTime(UNSAVED_CHANGES_DEBOUNCE),
+    map(([, lastSavedState]) => {
+      const currentState = serializeState().rawState;
+      return !areComparatorsEqual(getComparators(), lastSavedState, currentState, defaultState);
+    })
   );
 
-  subscriptions.push(
-    combineLatest(comparatorSubjects)
-      .pipe(
-        debounceTime(COMPARATOR_SUBJECTS_DEBOUNCE),
-        map((latestStates) =>
-          comparatorKeys.reduce((acc, key, index) => {
-            acc[key] = latestStates[index] as RuntimeState[typeof key];
-            return acc;
-          }, {} as Partial<RuntimeState>)
-        ),
-        combineLatestWith(lastSavedState$)
-      )
-      .subscribe(([latestState, lastSavedState]) => {
-        unsavedChanges$.next(
-          runComparators(comparators, comparatorKeys, lastSavedState, latestState)
-        );
-      })
-  );
-
-  return {
-    api: {
-      unsavedChanges$,
-      resetUnsavedChanges: () => {
-        const lastSaved = lastSavedState$.getValue();
-
-        // Do not reset to undefined or empty last saved state
-        // Temporary fix for https://github.com/elastic/kibana/issues/201627
-        // TODO remove when architecture fix resolves issue.
-        if (comparatorKeys.length && (!lastSaved || Object.keys(lastSaved).length === 0)) {
-          return false;
-        }
-
-        for (const key of comparatorKeys) {
-          const setter = comparators[key][1]; // setter function is the 1st element of the tuple
-          setter(lastSaved?.[key] as RuntimeState[typeof key]);
-        }
-        return true;
-      },
-      snapshotRuntimeState,
-    } as PublishesUnsavedChanges<RuntimeState> & HasSnapshottableState<RuntimeState>,
-    cleanup: () => {
-      subscriptions.forEach((subscription) => subscription.unsubscribe());
-    },
+  const resetUnsavedChanges = async () => {
+    const lastSavedState = parentApi.getLastSavedStateForChild(uuid);
+    await onReset(lastSavedState);
   };
+
+  return { hasUnsavedChanges$, resetUnsavedChanges };
 };

src/platform/plugins/shared/dashboard/public/dashboard_api/get_dashboard_api.ts
@@ -8,10 +8,7 @@
  */
 
 import type { Reference } from '@kbn/content-management-utils';
-import { ControlGroupApi, ControlGroupSerializedState } from '@kbn/controls-plugin/public';
 import { EmbeddablePackageState } from '@kbn/embeddable-plugin/public';
-import { StateComparators } from '@kbn/presentation-publishing';
-import { omit } from 'lodash';
 import { BehaviorSubject, debounceTime, merge } from 'rxjs';
 import { v4 } from 'uuid';
 import {
@@ -19,15 +16,14 @@ import {
   getReferencesForPanelId,
 } from '../../common/dashboard_container/persistable_state/dashboard_container_references';
 import { DASHBOARD_APP_ID } from '../../common/constants';
-import { PANELS_CONTROL_GROUP_KEY } from '../services/dashboard_backup_service';
 import { getDashboardContentManagementService } from '../services/dashboard_content_management_service';
 import { LoadDashboardReturn } from '../services/dashboard_content_management_service/types';
 import { initializeDataLoadingManager } from './data_loading_manager';
 import { initializeDataViewsManager } from './data_views_manager';
 import { DEFAULT_DASHBOARD_STATE } from './default_dashboard_state';
 import { getSerializedState } from './get_serialized_state';
-import { openSaveModal } from './save_modal/open_save_modal';
 import { initializePanelsManager } from './panels_manager';
+import { openSaveModal } from './save_modal/open_save_modal';
 import { initializeSearchSessionManager } from './search_sessions/search_session_manager';
 import { initializeSettingsManager } from './settings_manager';
 import { initializeTrackContentfulRender } from './track_contentful_render';
@@ -38,107 +34,100 @@ import {
   DashboardApi,
   DashboardCreationOptions,
   DashboardInternalApi,
-  UnsavedPanelState,
 } from './types';
 import type { DashboardState } from '../../common/types';
 import { initializeUnifiedSearchManager } from './unified_search_manager';
 import { initializeUnsavedChangesManager } from './unsaved_changes_manager';
 import { initializeViewModeManager } from './view_mode_manager';
+import {
+  CONTROL_GROUP_EMBEDDABLE_ID,
+  initializeControlGroupManager,
+} from './control_group_manager';
 
 export function getDashboardApi({
   creationOptions,
   incomingEmbeddable,
   initialState,
-  initialPanelsRuntimeState,
   savedObjectResult,
   savedObjectId,
 }: {
   creationOptions?: DashboardCreationOptions;
   incomingEmbeddable?: EmbeddablePackageState | undefined;
   initialState: DashboardState;
-  initialPanelsRuntimeState?: UnsavedPanelState;
   savedObjectResult?: LoadDashboardReturn;
   savedObjectId?: string;
 }) {
-  const controlGroupApi$ = new BehaviorSubject<ControlGroupApi | undefined>(undefined);
   const fullScreenMode$ = new BehaviorSubject(creationOptions?.fullScreenMode ?? false);
   const isManaged = savedObjectResult?.managed ?? false;
   const savedObjectId$ = new BehaviorSubject<string | undefined>(savedObjectId);
 
   const viewModeManager = initializeViewModeManager(incomingEmbeddable, savedObjectResult);
-  const trackPanel = initializeTrackPanel(
-    async (id: string) => await panelsManager.api.untilEmbeddableLoaded(id)
-  );
+  const trackPanel = initializeTrackPanel(async (id: string) => {
+    await panelsManager.api.getChildApi(id);
+  });
 
   const references$ = new BehaviorSubject<Reference[] | undefined>(initialState.references);
-  const getPanelReferences = (id: string) => {
+  const getReferences = (id: string) => {
+    if (id === CONTROL_GROUP_EMBEDDABLE_ID) {
+      return getReferencesForControls(references$.value ?? []);
+    }
+
     const panelReferences = getReferencesForPanelId(id, references$.value ?? []);
     // references from old installations may not be prefixed with panel id
     // fall back to passing all references in these cases to preserve backwards compatability
     return panelReferences.length > 0 ? panelReferences : references$.value ?? [];
   };
-  const pushPanelReferences = (refs: Reference[]) => {
-    references$.next([...(references$.value ?? []), ...refs]);
-  };
-  const referencesComparator: StateComparators<Pick<DashboardState, 'references'>> = {
-    references: [references$, (nextRefs) => references$.next(nextRefs)],
-  };
 
   const panelsManager = initializePanelsManager(
     incomingEmbeddable,
     initialState.panels,
-    initialPanelsRuntimeState ?? {},
     trackPanel,
-    getPanelReferences,
-    pushPanelReferences,
-    savedObjectId
+    getReferences
+  );
+  const controlGroupManager = initializeControlGroupManager(
+    initialState.controlGroupInput,
+    getReferences
   );
   const dataLoadingManager = initializeDataLoadingManager(panelsManager.api.children$);
   const dataViewsManager = initializeDataViewsManager(
-    controlGroupApi$,
+    controlGroupManager.api.controlGroupApi$,
     panelsManager.api.children$
   );
   const settingsManager = initializeSettingsManager(initialState);
   const unifiedSearchManager = initializeUnifiedSearchManager(
     initialState,
-    controlGroupApi$,
+    controlGroupManager.api.controlGroupApi$,
     settingsManager.api.timeRestore$,
     dataLoadingManager.internalApi.waitForPanelsToLoad$,
     () => unsavedChangesManager.internalApi.getLastSavedState(),
     creationOptions
   );
   const unsavedChangesManager = initializeUnsavedChangesManager({
+    viewModeManager,
     creationOptions,
-    controlGroupApi$,
-    lastSavedState: omit(savedObjectResult?.dashboardInput, 'controlGroupInput') ?? {
-      ...DEFAULT_DASHBOARD_STATE,
-    },
+    controlGroupManager,
+    lastSavedState: savedObjectResult?.dashboardInput ?? DEFAULT_DASHBOARD_STATE,
     panelsManager,
     savedObjectId$,
     settingsManager,
-    viewModeManager,
     unifiedSearchManager,
-    referencesComparator,
+    getReferences,
   });
+
   function getState() {
-    const { panels, references: panelReferences } = panelsManager.internalApi.getState();
+    const { panels, references: panelReferences } = panelsManager.internalApi.serializePanels();
     const { state: unifiedSearchState, references: searchSourceReferences } =
       unifiedSearchManager.internalApi.getState();
     const dashboardState: DashboardState = {
-      ...settingsManager.internalApi.getState(),
+      ...settingsManager.api.getSettings(),
       ...unifiedSearchState,
       panels,
       viewMode: viewModeManager.api.viewMode$.value,
     };
 
-    const controlGroupApi = controlGroupApi$.value;
-    let controlGroupReferences: Reference[] | undefined;
-    if (controlGroupApi) {
-      const { rawState: controlGroupSerializedState, references: extractedReferences } =
-        controlGroupApi.serializeState();
-      controlGroupReferences = extractedReferences;
-      dashboardState.controlGroupInput = controlGroupSerializedState;
-    }
+    const { controlGroupInput, controlGroupReferences } =
+      controlGroupManager.internalApi.serializeControlGroup();
+    dashboardState.controlGroupInput = controlGroupInput;
 
     return {
       dashboardState,
@@ -161,7 +150,7 @@ export function getDashboardApi({
     ...unsavedChangesManager.api,
     ...trackOverlayApi,
     ...initializeTrackContentfulRender(),
-    controlGroupApi$,
+    ...controlGroupManager.api,
     executionContext: {
       type: 'dashboard',
       description: settingsManager.api.title$.value,
@@ -191,7 +180,10 @@ export function getDashboardApi({
       });
 
       if (saveResult) {
-        unsavedChangesManager.internalApi.onSave(saveResult.savedState);
+        unsavedChangesManager.internalApi.onSave(
+          saveResult.savedState,
+          saveResult.references ?? []
+        );
         const settings = settingsManager.api.getSettings();
         settingsManager.api.setSettings({
           ...settings,
@@ -221,22 +213,34 @@ export function getDashboardApi({
         lastSavedId: savedObjectId$.value,
       });
 
-      unsavedChangesManager.internalApi.onSave(dashboardState);
+      unsavedChangesManager.internalApi.onSave(dashboardState, searchSourceReferences);
       references$.next(saveResult.references);
 
       return;
     },
     savedObjectId$,
     setFullScreenMode: (fullScreenMode: boolean) => fullScreenMode$.next(fullScreenMode),
+    getSerializedStateForChild: (childId: string) => {
+      return childId === CONTROL_GROUP_EMBEDDABLE_ID
+        ? controlGroupManager.internalApi.getStateForControlGroup()
+        : panelsManager.internalApi.getSerializedStateForPanel(childId);
+    },
     setSavedObjectId: (id: string | undefined) => savedObjectId$.next(id),
     type: DASHBOARD_API_TYPE as 'dashboard',
     uuid: v4(),
   } as Omit<DashboardApi, 'searchSessionId$'>;
 
+  const internalApi: DashboardInternalApi = {
+    ...panelsManager.internalApi,
+    ...unifiedSearchManager.internalApi,
+    setControlGroupApi: controlGroupManager.internalApi.setControlGroupApi,
+  };
+
   const searchSessionManager = initializeSearchSessionManager(
     creationOptions?.searchSessionSettings,
     incomingEmbeddable,
-    dashboardApi
+    dashboardApi,
+    internalApi
   );
 
   return {
@@ -244,35 +248,7 @@ export function getDashboardApi({
       ...dashboardApi,
       ...searchSessionManager.api,
     },
-    internalApi: {
-      ...panelsManager.internalApi,
-      ...unifiedSearchManager.internalApi,
-      getSerializedStateForControlGroup: () => {
-        return {
-          rawState: savedObjectResult?.dashboardInput?.controlGroupInput
-            ? savedObjectResult.dashboardInput.controlGroupInput
-            : ({
-                autoApplySelections: true,
-                chainingSystem: 'HIERARCHICAL',
-                controls: [],
-                ignoreParentSettings: {
-                  ignoreFilters: false,
-                  ignoreQuery: false,
-                  ignoreTimerange: false,
-                  ignoreValidations: false,
-                },
-                labelPosition: 'oneLine',
-                showApplySelections: false,
-              } as ControlGroupSerializedState),
-          references: getReferencesForControls(references$.value ?? []),
-        };
-      },
-      getRuntimeStateForControlGroup: () => {
-        return panelsManager!.api.getRuntimeStateForChild(PANELS_CONTROL_GROUP_KEY);
-      },
-      setControlGroupApi: (controlGroupApi: ControlGroupApi) =>
-        controlGroupApi$.next(controlGroupApi),
-    } as DashboardInternalApi,
+    internalApi,
     cleanup: () => {
       dataLoadingManager.cleanup();
       dataViewsManager.cleanup();

commit 1d21f7d2a49ae523140ac361e17f97780c7c0049

[TSVB] Fix missing unsaved changes on linked visualizations (#228685)
## Summary

This PR tries to apply a patch to a logic that is not very clear about
what a serialized state is and how this is used to evaluate if a
dashboard has unsaved changes or not.

The current implementation leverage the type `SerializedPanelState` that
has a `rawState` property and a `references` array.

https://github.com/elastic/kibana/blob/2eb9972ea52589fc2aab16f17bbc99cfedf76783/src/platform/packages/shared/presentation/presentation_publishing/interfaces/has_serializable_state.ts#L12-L19


This type is the result of the call to the passed method called
`serializeState`, an as described in the type, the references are
extracted. To my understanding, extracted refers to the fact that the
`rawState` should only have a link/name to an external reference, and
should not contain any trace of the original referenced object id.

If this is the right way to understand that type definition, the
associated method and the operation of `reference extraction` then I'm
inclined to say that the current implementation of
`initializeUnsavedChanges` doesn't take in consideration the
`references` array in the equality check, because the only property
passed and used with the comparators is the `rawState` as seen here:

https://github.com/elastic/kibana/blob/712af3e143e5ff421b32f0158ea6b6cf58f9a4f8/src/platform/packages/shared/presentation/presentation_containers/interfaces/unsaved_changes/initialize_unsaved_changes.ts#L48-L59

This can create unwanted situation where a change to the used references
can be undetected by the "unsaved changes" method. One example is coming
from the linked [issue](https://github.com/elastic/kibana/issues/228203)
where TSVB was extracting the references as requested, replacing, on the
serializedState, the `index_pattern: {id: "123abc"}` with a
`index_pattern_ref_name: "metric_0_index_pattern"` and moving the actual
index pattern references in the array outside the `rawState`. This
generates the issue where switching an index pattern doesn't result in
an unsaved change detected at the dashboard level because both the
current rawState and the latest saved rawState are in fact the same (but
not he references array).

An AggBased visualization, that uses the same embeddable as TSVB,
suffers from the same problem. If you the switch the data view the
change is not picked up.

TSVB and AggBased issues are both regression coming from
https://github.com/elastic/kibana/pull/215947 (I tested before and after

src/platform/packages/shared/presentation/presentation_containers/interfaces/unsaved_changes/initialize_unsaved_changes.ts
@@ -16,9 +16,9 @@ import {
 } from '@kbn/presentation-publishing';
 import { MaybePromise } from '@kbn/utility-types';
 import { Observable, combineLatestWith, debounceTime, map, of } from 'rxjs';
+import { isEqual, sortBy } from 'lodash';
 import { apiHasLastSavedChildState } from '../last_saved_child_state';
 import { PresentationContainer } from '../presentation_container';
-
 const UNSAVED_CHANGES_DEBOUNCE = 100;
 
 export const initializeUnsavedChanges = <StateType extends object = object>({
@@ -29,6 +29,7 @@ export const initializeUnsavedChanges = <StateType extends object = object>({
   defaultState,
   serializeState,
   anyStateChange$,
+  checkRefEquality,
 }: {
   uuid: string;
   parentApi: unknown;
@@ -37,6 +38,7 @@ export const initializeUnsavedChanges = <StateType extends object = object>({
   getComparators: () => StateComparators<StateType>;
   defaultState?: Partial<StateType>;
   onReset: (lastSavedPanelState?: SerializedPanelState<StateType>) => MaybePromise<void>;
+  checkRefEquality?: boolean;
 }): PublishesUnsavedChanges => {
   if (!apiHasLastSavedChildState<StateType>(parentApi)) {
     return {
@@ -46,16 +48,25 @@ export const initializeUnsavedChanges = <StateType extends object = object>({
   }
 
   const hasUnsavedChanges$ = anyStateChange$.pipe(
-    combineLatestWith(
-      parentApi.lastSavedStateForChild$(uuid).pipe(map((panelState) => panelState?.rawState))
-    ),
+    combineLatestWith(parentApi.lastSavedStateForChild$(uuid)),
     debounceTime(UNSAVED_CHANGES_DEBOUNCE),
     map(([, lastSavedState]) => {
-      const currentState = serializeState().rawState;
+      const currentState = serializeState();
+
+      // check ref equality
+      if (checkRefEquality) {
+        const lastSavedRefs = sortBy(lastSavedState?.references ?? [], 'id');
+        const currentRefs = sortBy(currentState?.references ?? [], 'id');
+        const equalRefs = isEqual(lastSavedRefs, currentRefs);
+
+        if (!equalRefs) return true;
+      }
+
+      // check state equality
       return !areComparatorsEqual(
         getComparators(),
-        lastSavedState,
-        currentState,
+        lastSavedState?.rawState,
+        currentState.rawState,
         defaultState,
         (key: string) => {
           const childApi = (parentApi as Partial<PresentationContainer>).children$?.getValue()[

src/platform/plugins/shared/dashboard/public/dashboard_api/get_dashboard_api.ts
@@ -174,6 +174,7 @@ export function getDashboardApi({
       }
 
       if (saveResult) {
+        references$.next(saveResult.references);
         unsavedChangesManager.internalApi.onSave(saveResult.savedState);
         const settings = settingsManager.api.getSettings();
         settingsManager.api.setSettings({
@@ -185,8 +186,6 @@ export function getDashboardApi({
           title: saveResult.savedState.title,
         });
         savedObjectId$.next(saveResult.id);
-
-        references$.next(saveResult.references);
       }
 
       return saveResult;
@@ -203,8 +202,8 @@ export function getDashboardApi({
       });
 
       if (saveResult?.error) return;
-      unsavedChangesManager.internalApi.onSave(dashboardState);
       references$.next(saveResult.references);
+      unsavedChangesManager.internalApi.onSave(dashboardState);
 
       return;
     },

src/platform/plugins/shared/visualizations/public/embeddable/visualize_embeddable.tsx
@@ -177,6 +177,7 @@ export const getVisualizeEmbeddableFactory: (deps: {
       serializeState: () => {
         return serializeVisualizeEmbeddable(savedObjectId$.getValue(), linkedToLibrary);
       },
+      checkRefEquality: true,
       anyStateChange$: merge(
         ...(dynamicActionsManager ? [dynamicActionsManager.anyStateChange$] : []),
         savedObjectId$,
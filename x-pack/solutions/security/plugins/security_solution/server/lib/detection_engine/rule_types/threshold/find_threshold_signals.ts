patrykkopycinski/kibana

commit dee4dfbe5995614b82792b692775c150dc79635e

[Security Solution][Detection Engine] Split search request building from search (#216887)
## Summary

This PR better separates the request building logic in the detection
engine from query building logic, removes outdated error checking logic,
updates the `singleSearchAfter` `search` call to no longer use the
legacy `meta: true` param, and improves search response type inference.

x-pack/solutions/security/plugins/security_solution/server/lib/detection_engine/rule_types/threshold/find_threshold_signals.ts
@@ -20,16 +20,13 @@ import type {
   TimestampOverride,
 } from '../../../../../common/api/detection_engine/model/rule_schema';
 import { singleSearchAfter } from '../utils/single_search_after';
+import { buildEventsSearchQuery } from '../utils/build_events_query';
 import {
   buildThresholdMultiBucketAggregation,
   buildThresholdSingleBucketAggregation,
 } from './build_threshold_aggregation';
-import type {
-  ThresholdMultiBucketAggregationResult,
-  ThresholdBucket,
-  ThresholdSingleBucketAggregationResult,
-} from './types';
-import { shouldFilterByCardinality, searchResultHasAggs } from './utils';
+import type { ThresholdCompositeBucket } from './types';
+import { shouldFilterByCardinality } from './utils';
 import type { IRuleExecutionLogForExecutors } from '../../rule_monitoring';
 import { getMaxSignalsWarning, stringifyAfterKey } from '../utils/utils';
 import type { RulePreviewLoggedRequest } from '../../../../../common/api/detection_engine/rule_preview/rule_preview.gen';
@@ -58,7 +55,6 @@ interface SearchAfterResults {
   searchErrors: string[];
 }
 
-// eslint-disable-next-line complexity
 export const findThresholdSignals = async ({
   from,
   to,
@@ -74,14 +70,14 @@ export const findThresholdSignals = async ({
   aggregatableTimestampField,
   isLoggedRequestsEnabled,
 }: FindThresholdSignalsParams): Promise<{
-  buckets: ThresholdBucket[];
+  buckets: ThresholdCompositeBucket[];
   searchDurations: string[];
   searchErrors: string[];
   warnings: string[];
   loggedRequests?: RulePreviewLoggedRequest[];
 }> => {
   // Leaf aggregations used below
-  const buckets: ThresholdBucket[] = [];
+  const buckets: ThresholdCompositeBucket[] = [];
   const searchAfterResults: SearchAfterResults = {
     searchDurations: [],
     searchErrors: [],
@@ -94,29 +90,33 @@ export const findThresholdSignals = async ({
   if (hasThresholdFields(threshold)) {
     let sortKeys: Record<string, string | number | null> | undefined;
     do {
-      const {
-        searchResult,
-        searchDuration,
-        searchErrors,
-        loggedRequests: thresholdLoggedRequests,
-      } = await singleSearchAfter({
+      const searchRequest = buildEventsSearchQuery({
         aggregations: buildThresholdMultiBucketAggregation({
           threshold,
           aggregatableTimestampField,
           sortKeys,
         }),
         index: inputIndexPattern,
-        searchAfterSortIds: undefined,
         from,
         to,
-        services,
-        ruleExecutionLogger,
-        filter,
-        pageSize: 0,
-        sortOrder: 'desc',
         runtimeMappings,
+        filter,
+        size: 0,
+        sortOrder: 'desc',
+        searchAfterSortIds: undefined,
         primaryTimestamp,
         secondaryTimestamp,
+      });
+
+      const {
+        searchResult,
+        searchDuration,
+        searchErrors,
+        loggedRequests: thresholdLoggedRequests,
+      } = await singleSearchAfter({
+        searchRequest,
+        services,
+        ruleExecutionLogger,
         loggedRequestsConfig: isLoggedRequestsEnabled
           ? {
               type: 'findThresholdBuckets',
@@ -134,40 +134,40 @@ export const findThresholdSignals = async ({
         sortKeys = undefined; // this will eject us out of the loop
         // if a search failure occurs on a secondary iteration,
         // we will return early.
-      } else if (searchResultHasAggs<ThresholdMultiBucketAggregationResult>(searchResult)) {
-        const thresholdTerms = searchResult.aggregations?.thresholdTerms;
-        sortKeys = thresholdTerms?.after_key;
-        buckets.push(
-          ...((searchResult.aggregations?.thresholdTerms.buckets as ThresholdBucket[]) ?? [])
-        );
+      } else if (searchResult.aggregations != null) {
+        const thresholdTerms = searchResult.aggregations.thresholdTerms;
+        sortKeys = thresholdTerms.after_key;
+        buckets.push(...thresholdTerms.buckets);
       } else {
         throw new Error('Aggregations were missing on threshold rule search result');
       }
     } while (sortKeys && buckets.length <= maxSignals);
   } else {
+    const searchRequest = buildEventsSearchQuery({
+      aggregations: buildThresholdSingleBucketAggregation({
+        threshold,
+        aggregatableTimestampField,
+      }),
+      index: inputIndexPattern,
+      from,
+      to,
+      runtimeMappings,
+      filter,
+      size: 0,
+      sortOrder: 'desc',
+      searchAfterSortIds: undefined,
+      primaryTimestamp,
+      secondaryTimestamp,
+    });
     const {
       searchResult,
       searchDuration,
       searchErrors,
       loggedRequests: thresholdLoggedRequests,
     } = await singleSearchAfter({
-      aggregations: buildThresholdSingleBucketAggregation({
-        threshold,
-        aggregatableTimestampField,
-      }),
-      searchAfterSortIds: undefined,
-      index: inputIndexPattern,
-      from,
-      to,
+      searchRequest,
       services,
       ruleExecutionLogger,
-      filter,
-      pageSize: 0,
-      sortOrder: 'desc',
-      trackTotalHits: true,
-      runtimeMappings,
-      primaryTimestamp,
-      secondaryTimestamp,
       loggedRequestsConfig: isLoggedRequestsEnabled
         ? {
             type: 'findThresholdBuckets',
@@ -180,12 +180,9 @@ export const findThresholdSignals = async ({
     searchAfterResults.searchErrors.push(...searchErrors);
     loggedRequests.push(...(thresholdLoggedRequests ?? []));
 
-    if (
-      !searchResultHasAggs<ThresholdSingleBucketAggregationResult>(searchResult) &&
-      isEmpty(searchErrors)
-    ) {
-      throw new Error('Aggregations were missing on threshold rule search result');
-    } else if (searchResultHasAggs<ThresholdSingleBucketAggregationResult>(searchResult)) {
+    if (isEmpty(searchErrors)) {
+      searchAfterResults.searchErrors.push(...searchErrors);
+    } else if (searchResult.aggregations != null) {
       const docCount = searchResult.hits.total.value;
       if (
         docCount >= threshold.value &&
@@ -196,13 +193,13 @@ export const findThresholdSignals = async ({
         buckets.push({
           doc_count: docCount,
           key: {},
-          max_timestamp: searchResult.aggregations?.max_timestamp ?? { value: null },
-          min_timestamp: searchResult.aggregations?.min_timestamp ?? { value: null },
-          ...(includeCardinalityFilter
-            ? { cardinality_count: searchResult.aggregations?.cardinality_count }
-            : {}),
+          max_timestamp: searchResult.aggregations.max_timestamp,
+          min_timestamp: searchResult.aggregations.min_timestamp,
+          cardinality_count: searchResult.aggregations.cardinality_count,
         });
       }
+    } else {
+      throw new Error('Aggregations were missing on threshold rule search result');
     }
   }
 

x-pack/solutions/security/plugins/security_solution/server/lib/detection_engine/rule_types/__mocks__/es_results.ts
@@ -436,7 +436,7 @@ export const sampleDocRiskScore = (riskScore?: unknown): SignalSourceHit => ({
   sort: [],
 });
 
-export const sampleEmptyDocSearchResults = (): SignalSearchResponse => ({
+export const sampleEmptyDocSearchResults = () => ({
   took: 10,
   timed_out: false,
   _shards: {
@@ -446,7 +446,10 @@ export const sampleEmptyDocSearchResults = (): SignalSearchResponse => ({
     skipped: 0,
   },
   hits: {
-    total: 0,
+    total: {
+      value: 0,
+      relation: 'eq' as const,
+    },
     max_score: 100,
     hits: [],
   },

x-pack/solutions/security/plugins/security_solution/server/lib/detection_engine/rule_types/indicator_match/threat_mapping/get_event_count.ts
@@ -22,7 +22,7 @@ export const getEventList = async ({
   eventListConfig,
   indexFields,
   sortOrder = 'desc',
-}: EventsOptions): Promise<estypes.SearchResponse<EventDoc>> => {
+}: EventsOptions): Promise<estypes.SearchResponse<EventDoc, unknown>> => {
   const {
     inputIndex,
     ruleExecutionLogger,
@@ -53,14 +53,13 @@ export const getEventList = async ({
     fields: indexFields,
   });
 
-  const { searchResult } = await singleSearchAfter({
+  const searchRequest = buildEventsSearchQuery({
+    aggregations: undefined,
     searchAfterSortIds: searchAfter,
     index: inputIndex,
     from: tuple.from.toISOString(),
     to: tuple.to.toISOString(),
-    services,
-    ruleExecutionLogger,
-    pageSize: calculatedPerPage,
+    size: calculatedPerPage,
     filter: queryFilter,
     primaryTimestamp,
     secondaryTimestamp,
@@ -68,6 +67,12 @@ export const getEventList = async ({
     trackTotalHits: false,
     runtimeMappings,
     overrideBody: eventListConfig,
+  });
+
+  const { searchResult } = await singleSearchAfter({
+    searchRequest,
+    services,
+    ruleExecutionLogger,
   });
 
   ruleExecutionLogger.debug(`Retrieved events items of size: ${searchResult.hits.hits.length}`);
@@ -98,6 +103,7 @@ export const getEventCount = async ({
     fields: indexFields,
   });
   const eventSearchQueryBodyQuery = buildEventsSearchQuery({
+    aggregations: undefined,
     index,
     from: tuple.from.toISOString(),
     to: tuple.to.toISOString(),

x-pack/solutions/security/plugins/security_solution/server/lib/detection_engine/rule_types/indicator_match/threat_mapping/types.ts
@@ -238,7 +238,7 @@ export interface SignalMatch {
 
 export type GetDocumentListInterface = (params: {
   searchAfter: estypes.SortResults | undefined;
-}) => Promise<estypes.SearchResponse<EventDoc | ThreatListDoc>>;
+}) => Promise<estypes.SearchResponse<EventDoc | ThreatListDoc, unknown>>;
 
 export type CreateSignalInterface = (
   params: EventItem[] | ThreatListItem[]

x-pack/solutions/security/plugins/security_solution/server/lib/detection_engine/rule_types/new_terms/build_new_terms_aggregation.ts
@@ -11,16 +11,6 @@ import type { SignalSource } from '../types';
 import type { GenericBulkCreateResponse } from '../factories/bulk_create_factory';
 import type { NewTermsFieldsLatest } from '../../../../../common/api/detection_engine/model/alerts';
 
-export type RecentTermsAggResult = ESSearchResponse<
-  SignalSource,
-  { aggregations: ReturnType<typeof buildRecentTermsAgg> }
->;
-
-export type NewTermsAggResult = ESSearchResponse<
-  SignalSource,
-  { aggregations: ReturnType<typeof buildNewTermsAgg> }
->;
-
 export type CompositeDocFetchAggResult = ESSearchResponse<
   SignalSource,
   { aggregations: ReturnType<typeof buildCompositeDocFetchAgg> }

x-pack/solutions/security/plugins/security_solution/server/lib/detection_engine/rule_types/new_terms/create_new_terms_alert_type.ts
@@ -14,16 +14,12 @@ import { SERVER_APP_ID } from '../../../../../common/constants';
 import { NewTermsRuleParams } from '../../rule_schema';
 import type { SecurityAlertType } from '../types';
 import { singleSearchAfter } from '../utils/single_search_after';
+import { buildEventsSearchQuery } from '../utils/build_events_query';
 import { getFilter } from '../utils/get_filter';
 import { wrapNewTermsAlerts } from './wrap_new_terms_alerts';
 import { bulkCreateSuppressedNewTermsAlertsInMemory } from './bulk_create_suppressed_alerts_in_memory';
 import type { EventsAndTerms } from './types';
-import type {
-  RecentTermsAggResult,
-  DocFetchAggResult,
-  NewTermsAggResult,
-  CreateAlertsHook,
-} from './build_new_terms_aggregation';
+import type { CreateAlertsHook } from './build_new_terms_aggregation';
 import type { NewTermsFieldsLatest } from '../../../../../common/api/detection_engine/model/alerts';
 import {
   buildRecentTermsAgg,
@@ -149,7 +145,7 @@ export const createNewTermsAlertType = (): SecurityAlertType<
         alertSuppression: params.alertSuppression,
         licensing,
       });
-      let afterKey;
+      let afterKey: Record<string, string | number | null> | undefined;
 
       const result = createSearchAfterReturnType();
 
@@ -170,12 +166,7 @@ export const createNewTermsAlertType = (): SecurityAlertType<
         // PHASE 1: Fetch a page of terms using a composite aggregation. This will collect a page from
         // all of the terms seen over the last rule interval. In the next phase we'll determine which
         // ones are new.
-        const {
-          searchResult,
-          searchDuration,
-          searchErrors,
-          loggedRequests: firstPhaseLoggedRequests = [],
-        } = await singleSearchAfter({
+        const searchRequest = buildEventsSearchQuery({
           aggregations: buildRecentTermsAgg({
             fields: params.newTermsFields,
             after: afterKey,
@@ -185,13 +176,22 @@ export const createNewTermsAlertType = (): SecurityAlertType<
           // The time range for the initial composite aggregation is the rule interval, `from` and `to`
           from: tuple.from.toISOString(),
           to: tuple.to.toISOString(),
-          services,
-          ruleExecutionLogger,
           filter: esFilter,
-          pageSize: 0,
+          size: 0,
           primaryTimestamp,
           secondaryTimestamp,
           runtimeMappings,
+        });
+
+        const {
+          searchResult,
+          searchDuration,
+          searchErrors,
+          loggedRequests: firstPhaseLoggedRequests = [],
+        } = await singleSearchAfter({
+          searchRequest,
+          services,
+          ruleExecutionLogger,
           loggedRequestsConfig: isLoggedRequestsEnabled
             ? {
                 type: 'findAllTerms',
@@ -203,8 +203,7 @@ export const createNewTermsAlertType = (): SecurityAlertType<
             : undefined,
         });
         loggedRequests.push(...firstPhaseLoggedRequests);
-        const searchResultWithAggs = searchResult as RecentTermsAggResult;
-        if (!searchResultWithAggs.aggregations) {
+        if (!searchResult.aggregations) {
           throw new Error('Aggregations were missing on recent terms search result');
         }
         logger.debug(`Time spent on composite agg: ${searchDuration}`);
@@ -214,10 +213,10 @@ export const createNewTermsAlertType = (): SecurityAlertType<
 
         // If the aggregation returns no after_key it signals that we've paged through all results
         // and the current page is empty so we can immediately break.
-        if (searchResultWithAggs.aggregations.new_terms.after_key == null) {
+        if (searchResult.aggregations.new_terms.after_key == null) {
           break;
         }
-        const bucketsForField = searchResultWithAggs.aggregations.new_terms.buckets;
+        const bucketsForField = searchResult.aggregations.new_terms.buckets;
 
         const createAlertsHook: CreateAlertsHook = async (aggResult) => {
           const eventsAndTerms: EventsAndTerms[] = (
@@ -311,12 +310,7 @@ export const createNewTermsAlertType = (): SecurityAlertType<
           // The aggregation filters out buckets for terms that exist prior to `tuple.from`, so the buckets in the
           // response correspond to each new term.
           const includeValues = transformBucketsToValues(params.newTermsFields, bucketsForField);
-          const {
-            searchResult: pageSearchResult,
-            searchDuration: pageSearchDuration,
-            searchErrors: pageSearchErrors,
-            loggedRequests: pageSearchLoggedRequests = [],
-          } = await singleSearchAfter({
+          const pageSearchRequest = buildEventsSearchQuery({
             aggregations: buildNewTermsAgg({
               newValueWindowStart: tuple.from,
               timestampField: aggregatableTimestampField,
@@ -330,12 +324,20 @@ export const createNewTermsAlertType = (): SecurityAlertType<
             // in addition to the rule interval
             from: parsedHistoryWindowSize.toISOString(),
             to: tuple.to.toISOString(),
-            services,
-            ruleExecutionLogger,
             filter: esFilter,
-            pageSize: 0,
+            size: 0,
             primaryTimestamp,
             secondaryTimestamp,
+          });
+          const {
+            searchResult: pageSearchResult,
+            searchDuration: pageSearchDuration,
+            searchErrors: pageSearchErrors,
+            loggedRequests: pageSearchLoggedRequests = [],
+          } = await singleSearchAfter({
+            searchRequest: pageSearchRequest,
+            services,
+            ruleExecutionLogger,
             loggedRequestsConfig: isLoggedRequestsEnabled
               ? {
                   type: 'findNewTerms',
@@ -350,8 +352,7 @@ export const createNewTermsAlertType = (): SecurityAlertType<
 
           logger.debug(`Time spent on phase 2 terms agg: ${pageSearchDuration}`);
 
-          const pageSearchResultWithAggs = pageSearchResult as NewTermsAggResult;
-          if (!pageSearchResultWithAggs.aggregations) {
+          if (!pageSearchResult.aggregations) {
             throw new Error('Aggregations were missing on new terms search result');
           }
 
@@ -359,17 +360,12 @@ export const createNewTermsAlertType = (): SecurityAlertType<
           // the rule interval for that term. This is the first document to contain the new term, and will
           // become the basis of the resulting alert.
           // One document could become multiple alerts if the document contains an array with multiple new terms.
-          if (pageSearchResultWithAggs.aggregations.new_terms.buckets.length > 0) {
-            const actualNewTerms = pageSearchResultWithAggs.aggregations.new_terms.buckets.map(
+          if (pageSearchResult.aggregations.new_terms.buckets.length > 0) {
+            const actualNewTerms = pageSearchResult.aggregations.new_terms.buckets.map(
               (bucket) => bucket.key
             );
 
-            const {
-              searchResult: docFetchSearchResult,
-              searchDuration: docFetchSearchDuration,
-              searchErrors: docFetchSearchErrors,
-              loggedRequests: docFetchLoggedRequests = [],
-            } = await singleSearchAfter({
+            const docFetchSearchRequest = buildEventsSearchQuery({
               aggregations: buildDocFetchAgg({
                 timestampField: aggregatableTimestampField,
                 field: params.newTermsFields[0],
@@ -381,12 +377,20 @@ export const createNewTermsAlertType = (): SecurityAlertType<
               // For phase 3, we go back to aggregating only over the rule interval - excluding the history window
               from: tuple.from.toISOString(),
               to: tuple.to.toISOString(),
-              services,
-              ruleExecutionLogger,
               filter: esFilter,
-              pageSize: 0,
+              size: 0,
               primaryTimestamp,
               secondaryTimestamp,
+            });
+            const {
+              searchResult: docFetchSearchResult,
+              searchDuration: docFetchSearchDuration,
+              searchErrors: docFetchSearchErrors,
+              loggedRequests: docFetchLoggedRequests = [],
+            } = await singleSearchAfter({
+              searchRequest: docFetchSearchRequest,
+              services,
+              ruleExecutionLogger,
               loggedRequestsConfig: isLoggedRequestsEnabled
                 ? {
                     type: 'findDocuments',
@@ -401,13 +405,11 @@ export const createNewTermsAlertType = (): SecurityAlertType<
             result.errors.push(...docFetchSearchErrors);
             loggedRequests.push(...docFetchLoggedRequests);
 
-            const docFetchResultWithAggs = docFetchSearchResult as DocFetchAggResult;
-
-            if (!docFetchResultWithAggs.aggregations) {
+            if (!docFetchSearchResult.aggregations) {
               throw new Error('Aggregations were missing on document fetch search result');
             }
 
-            const bulkCreateResult = await createAlertsHook(docFetchResultWithAggs);
+            const bulkCreateResult = await createAlertsHook(docFetchSearchResult);
 
             if (bulkCreateResult.alertsWereTruncated) {
               result.warningMessages.push(
@@ -420,7 +422,7 @@ export const createNewTermsAlertType = (): SecurityAlertType<
           }
         }
 
-        afterKey = searchResultWithAggs.aggregations.new_terms.after_key;
+        afterKey = searchResult.aggregations.new_terms.after_key;
       }
 
       scheduleNotificationResponseActionsService({

x-pack/solutions/security/plugins/security_solution/server/lib/detection_engine/rule_types/new_terms/multi_terms_composite.ts
@@ -28,6 +28,7 @@ import {
   stringifyAfterKey,
 } from '../utils/utils';
 import type { GenericBulkCreateResponse } from '../utils/bulk_create_with_suppression';
+import { buildEventsSearchQuery } from '../utils/build_events_query';
 
 import type {
   SecurityRuleServices,
@@ -135,12 +136,7 @@ const multiTermsCompositeNonRetryable = async ({
     // PHASE 2: Take the page of results from Phase 1 and determine if each term exists in the history window.
     // The aggregation filters out buckets for terms that exist prior to `tuple.from`, so the buckets in the
     // response correspond to each new term.
-    const {
-      searchResult: pageSearchResult,
-      searchDuration: pageSearchDuration,
-      searchErrors: pageSearchErrors,
-      loggedRequests: pageSearchLoggedRequests = [],
-    } = await singleSearchAfter({
+    const searchRequest = buildEventsSearchQuery({
       aggregations: buildCompositeNewTermsAgg({
         newValueWindowStart: tuple.from,
         timestampField: aggregatableTimestampField,
@@ -155,12 +151,20 @@ const multiTermsCompositeNonRetryable = async ({
       // in addition to the rule interval
       from: parsedHistoryWindowSize.toISOString(),
       to: tuple.to.toISOString(),
-      services,
-      ruleExecutionLogger,
       filter: esFilterForBatch,
-      pageSize: 0,
+      size: 0,
       primaryTimestamp,
       secondaryTimestamp,
+    });
+    const {
+      searchResult: pageSearchResult,
+      searchDuration: pageSearchDuration,
+      searchErrors: pageSearchErrors,
+      loggedRequests: pageSearchLoggedRequests = [],
+    } = await singleSearchAfter({
+      searchRequest,
+      services,
+      ruleExecutionLogger,
       loggedRequestsConfig: isLoggedRequestsEnabled
         ? {
             type: 'findNewTerms',
@@ -187,12 +191,7 @@ const multiTermsCompositeNonRetryable = async ({
     // become the basis of the resulting alert.
     // One document could become multiple alerts if the document contains an array with multiple new terms.
     if (pageSearchResultWithAggs.aggregations.new_terms.buckets.length > 0) {
-      const {
-        searchResult: docFetchSearchResult,
-        searchDuration: docFetchSearchDuration,
-        searchErrors: docFetchSearchErrors,
-        loggedRequests: docFetchLoggedRequests = [],
-      } = await singleSearchAfter({
+      const searchRequestPhase3 = buildEventsSearchQuery({
         aggregations: buildCompositeDocFetchAgg({
           newValueWindowStart: tuple.from,
           timestampField: aggregatableTimestampField,
@@ -205,12 +204,20 @@ const multiTermsCompositeNonRetryable = async ({
         index: inputIndex,
         from: parsedHistoryWindowSize.toISOString(),
         to: tuple.to.toISOString(),
-        services,
-        ruleExecutionLogger,
         filter: esFilterForBatch,
-        pageSize: 0,
+        size: 0,
         primaryTimestamp,
         secondaryTimestamp,
+      });
+      const {
+        searchResult: docFetchSearchResult,
+        searchDuration: docFetchSearchDuration,
+        searchErrors: docFetchSearchErrors,
+        loggedRequests: docFetchLoggedRequests = [],
+      } = await singleSearchAfter({
+        searchRequest: searchRequestPhase3,
+        services,
+        ruleExecutionLogger,
         loggedRequestsConfig: isLoggedRequestsEnabled
           ? {
               type: 'findDocuments',

x-pack/solutions/security/plugins/security_solution/server/lib/detection_engine/rule_types/query/alert_suppression/group_and_bulk_create.ts
@@ -10,7 +10,7 @@ import type moment from 'moment';
 import type { estypes } from '@elastic/elasticsearch';
 
 import { withSecuritySpan } from '../../../../../utils/with_security_span';
-import { buildTimeRangeFilter } from '../../utils/build_events_query';
+import { buildEventsSearchQuery, buildTimeRangeFilter } from '../../utils/build_events_query';
 import type {
   SecurityRuleServices,
   SecuritySharedParams,
@@ -186,29 +186,31 @@ export const groupAndBulkCreate = async ({
         missingBucket: suppressOnMissingFields,
       });
 
-      const eventsSearchParams = {
+      const searchRequest = buildEventsSearchQuery({
         aggregations: groupingAggregation,
         searchAfterSortIds: undefined,
         index: sharedParams.inputIndex,
         from: tuple.from.toISOString(),
         to: tuple.to.toISOString(),
-        services,
-        ruleExecutionLogger: sharedParams.ruleExecutionLogger,
         filter,
-        pageSize: 0,
+        size: 0,
         primaryTimestamp: sharedParams.primaryTimestamp,
         secondaryTimestamp: sharedParams.secondaryTimestamp,
         runtimeMappings: sharedParams.runtimeMappings,
         additionalFilters: bucketHistoryFilter,
-        loggedRequestsConfig: isLoggedRequestsEnabled
-          ? {
-              type: 'findDocuments',
-              description: i18n.FIND_EVENTS_DESCRIPTION,
-            }
-          : undefined,
-      };
+      });
       const { searchResult, searchDuration, searchErrors, loggedRequests } =
-        await singleSearchAfter(eventsSearchParams);
+        await singleSearchAfter({
+          searchRequest,
+          services,
+          ruleExecutionLogger: sharedParams.ruleExecutionLogger,
+          loggedRequestsConfig: isLoggedRequestsEnabled
+            ? {
+                type: 'findDocuments',
+                description: i18n.FIND_EVENTS_DESCRIPTION,
+              }
+            : undefined,
+        });
 
       if (isLoggedRequestsEnabled) {
         toReturn.loggedRequests = loggedRequests;

commit d48d59e9043f68ae1283394b36b9ddc2a2fd2388

[Security Solution][Detection Engine] Fix threshold rule logic with no group by fields defined (#241022)
Fixes a regression in threshold rule logic introduced by
https://github.com/elastic/kibana/pull/216887 - threshold rules with no
"group by" fields defined would no longer generate alerts.

x-pack/solutions/security/plugins/security_solution/server/lib/detection_engine/rule_types/threshold/find_threshold_signals.ts
@@ -180,9 +180,7 @@ export const findThresholdSignals = async ({
     searchAfterResults.searchErrors.push(...searchErrors);
     loggedRequests.push(...(thresholdLoggedRequests ?? []));
 
-    if (isEmpty(searchErrors)) {
-      searchAfterResults.searchErrors.push(...searchErrors);
-    } else if (searchResult.aggregations != null) {
+    if (searchResult.aggregations != null) {
       const docCount = searchResult.hits.total.value;
       if (
         docCount >= threshold.value &&

x-pack/solutions/security/test/security_solution_api_integration/test_suites/detections_response/detection_engine/rule_execution_logic/threshold/trial_license_complete_tier/threshold.ts
@@ -138,6 +138,19 @@ export default ({ getService }: FtrProviderContext) => {
       expect(logs[0].warnings).not.toContain(getMaxAlertsWarning());
     });
 
+    it('generates alerts from Threshold rules when threshold is met and no field is defined', async () => {
+      const rule: ThresholdRuleCreateProps = {
+        ...getThresholdRuleForAlertTesting(['auditbeat-*']),
+        threshold: {
+          field: [],
+          value: 100,
+        },
+      };
+      const { previewId } = await previewRule({ supertest, rule });
+      const previewAlerts = await getPreviewAlerts({ es, previewId });
+      expect(previewAlerts.length).toEqual(1);
+    });
+
     it('generates 2 alerts from Threshold rules when threshold is met', async () => {
       const rule: ThresholdRuleCreateProps = {
         ...getThresholdRuleForAlertTesting(['auditbeat-*']),
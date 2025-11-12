patrykkopycinski/kibana

commit abd18b04b95d3edd220e68e207ab858387d3fc19

[Infra][Hosts] Refresh data view state on submit (#238872)
fixes [#236271](https://github.com/elastic/kibana/issues/236271)

## Summary

This PR updates the Hosts UI to refresh the data view on every submit.


https://github.com/user-attachments/assets/033134a5-7ac1-4fdb-9dc4-d3abb644d6d5

>[!NOTE]
>It may require submitting more than once to get the updated data view's
fields list because the data view fetching is async and the Lens charts
might take some time to get the updated information


### How to test

- Start a local Kibana and ES instances
- Create a dummy doc 
```
POST /metrics-test/_doc
{
    "host": {
        "name": "test"
    },
     "event": {
            "dataset": "system",
            "module": "system"
          },
   "data_stream": {
        "dataset": "system",
        "namespace": "default",
        "type": "metrics"
    }
}

```
- Navigate to Infrastructure > Hosts 
- The charts should show a warning indicator, since the metric fields
don't exist
- Ingest metric data 
  - Clone https://github.com/simianhacker/simian-forge/tree/main
  - Run  ./forge --dataset hosts --count 25 --interval 30s 
- On the Hosts UI, click on the refresh button next to the search bar

---------

Co-authored-by: Elastic Machine <elasticmachine@users.noreply.github.com>

x-pack/solutions/observability/plugins/infra/public/pages/metrics/hosts/components/tabs/metrics/chart.tsx
@@ -5,11 +5,10 @@
  * 2.0.
  */
 import React from 'react';
-import type { LensConfig, LensDataviewDataset } from '@kbn/lens-embeddable-utils/config_builder';
+import type { LensConfig } from '@kbn/lens-embeddable-utils/config_builder';
+import type { DataView } from '@kbn/data-views-plugin/public';
 import useAsync from 'react-use/lib/useAsync';
 import { useReloadRequestTimeContext } from '../../../../../../hooks/use_reload_request_time';
-import { resolveDataView } from '../../../../../../utils/data_view';
-import { useKibanaContextForPlugin } from '../../../../../../hooks/use_kibana';
 import { HOST_NAME_FIELD } from '../../../../../../../common/constants';
 import { METRIC_CHART_HEIGHT } from '../../../../../../common/visualizations/constants';
 import { LensChart } from '../../../../../../components/lens';
@@ -21,16 +20,14 @@ import { useAfterLoadedState } from '../../../hooks/use_after_loaded_state';
 
 export type ChartProps = LensConfig & {
   id: string;
+  dataView?: DataView;
 };
 
-export const Chart = ({ id, ...chartProps }: ChartProps) => {
+export const Chart = ({ id, dataView, ...chartProps }: ChartProps) => {
   const { searchCriteria } = useUnifiedSearchContext();
   const { loading } = useHostsViewContext();
   const { reloadRequestTime } = useReloadRequestTimeContext();
   const { currentPage } = useHostsTableContext();
-  const {
-    services: { dataViews },
-  } = useKibanaContextForPlugin();
 
   const shouldUseSearchCriteria = currentPage.length === 0;
 
@@ -44,10 +41,9 @@ export const Chart = ({ id, ...chartProps }: ChartProps) => {
   });
 
   const { value: filters = [] } = useAsync(async () => {
-    const resolvedDataView = await resolveDataView({
-      dataViewId: (chartProps.dataset as LensDataviewDataset)?.index,
-      dataViewsService: dataViews,
-    });
+    if (!dataView?.id) {
+      return [];
+    }
 
     return shouldUseSearchCriteria
       ? [...searchCriteria.filters, ...(searchCriteria.panelFilters ?? [])]
@@ -55,13 +51,12 @@ export const Chart = ({ id, ...chartProps }: ChartProps) => {
           buildCombinedAssetFilter({
             field: HOST_NAME_FIELD,
             values: currentPage.map((p) => p.name),
-            dataView: resolvedDataView.dataViewReference,
+            dataView,
           }),
         ];
   }, [
     currentPage,
-    dataViews,
-    chartProps.dataset,
+    dataView,
     searchCriteria.filters,
     searchCriteria.panelFilters,
     shouldUseSearchCriteria,

x-pack/solutions/observability/plugins/infra/public/pages/metrics/hosts/components/tabs/metrics/metrics_grid.tsx
@@ -18,7 +18,7 @@ export const MetricsGrid = () => {
   const { metricsView } = useMetricsDataViewContext();
   const { searchCriteria } = useUnifiedSearchContext();
   const charts = useMetricsCharts({
-    dataViewId: metricsView?.dataViewReference.id,
+    indexPattern: metricsView?.dataViewReference.getIndexPattern(),
     schema: searchCriteria.preferredSchema,
   });
 

x-pack/solutions/observability/plugins/infra/public/components/asset_details/charts/chart.tsx
@@ -9,8 +9,6 @@ import type { LensConfig } from '@kbn/lens-embeddable-utils/config_builder';
 import type { TimeRange } from '@kbn/es-query';
 import useAsync from 'react-use/lib/useAsync';
 import type { DataView } from '@kbn/data-views-plugin/public';
-import { resolveDataView } from '../../../utils/data_view';
-import { useKibanaContextForPlugin } from '../../../hooks/use_kibana';
 import { METRIC_CHART_HEIGHT } from '../../../common/visualizations/constants';
 import { buildCombinedAssetFilter } from '../../../utils/filters/build';
 import type { LensChartProps } from '../../lens';
@@ -39,28 +37,20 @@ export const Chart = ({
 }: ChartProps) => {
   const { setDateRange } = useDatePickerContext();
   const { reloadRequestTime } = useReloadRequestTimeContext();
-  const {
-    services: { dataViews },
-  } = useKibanaContextForPlugin();
 
   const { value: filters = [] } = useAsync(async () => {
-    if (!dataView?.id) {
+    if (!dataView) {
       return [];
     }
-
-    const resolvedDataView = await resolveDataView({
-      dataViewId: dataView.id,
-      dataViewsService: dataViews,
-    });
 
     return [
       buildCombinedAssetFilter({
         field: queryField,
         values: [entityId],
-        dataView: resolvedDataView.dataViewReference,
+        dataView,
       }),
     ];
-  }, [dataView?.id, dataViews, queryField, entityId]);
+  }, [dataView, queryField, entityId]);
 
   const handleBrushEnd = useCallback(
     ({ range, preventDefault }: BrushEndArgs) => {

x-pack/solutions/observability/plugins/infra/public/components/asset_details/charts/docker_charts.tsx
@@ -103,6 +103,7 @@ export const DockerCharts = React.forwardRef<HTMLDivElement, Props>(
               lensAttributes={chart}
               entityId={entityId}
               dateRange={dateRange}
+              dataView={dataView}
               queryField={findInventoryFields('container').id}
             />
           ))}

x-pack/solutions/observability/plugins/infra/public/components/asset_details/charts/host_charts.tsx
@@ -31,7 +31,7 @@ export const HostCharts = React.forwardRef<HTMLDivElement, Props>(
   ({ entityId, dataView, dateRange, metric, onShowAll, overview = false, schema }, ref) => {
     const { charts } = useHostCharts({
       metric,
-      dataViewId: dataView?.getIndexPattern(),
+      indexPattern: dataView?.getIndexPattern(),
       overview,
       schema,
     });

x-pack/solutions/observability/plugins/infra/public/components/asset_details/charts/kubernetes_charts.tsx
@@ -32,7 +32,7 @@ interface Props extends MetricsChartsFields {
 export const KubernetesNodeCharts = React.forwardRef<HTMLDivElement, Props>(
   ({ entityId, dataView, dateRange, onShowAll, overview }, ref) => {
     const { charts } = useKubernetesCharts({
-      dataViewId: dataView?.id,
+      indexPattern: dataView?.getIndexPattern(),
       overview,
     });
 
@@ -80,6 +80,7 @@ export const KubernetesNodeCharts = React.forwardRef<HTMLDivElement, Props>(
               entityId={entityId}
               dateRange={dateRange}
               lensAttributes={chart}
+              dataView={dataView}
               queryField={findInventoryFields('host').id}
             />
           ))}

x-pack/solutions/observability/plugins/infra/public/components/asset_details/components/kpis/host_kpi_charts.tsx
@@ -36,7 +36,7 @@ export const HostKpiCharts = ({
   schema,
 }: HostKpiChartsProps) => {
   const charts = useHostKpiCharts({
-    dataViewId: dataView?.id,
+    indexPattern: dataView?.getIndexPattern(),
     getSubtitle,
     schema,
   });

x-pack/solutions/observability/plugins/infra/public/components/asset_details/hooks/use_host_metrics_charts.test.ts
@@ -9,7 +9,7 @@ import { waitFor, renderHook } from '@testing-library/react';
 import type { HostMetricTypes } from '../charts/types';
 import { useHostKpiCharts, useHostCharts, useKubernetesCharts } from './use_host_metrics_charts';
 
-const dataViewId = 'metricsDataViewId';
+const indexPattern = 'metrics-*';
 const getHostChartsExpectedOrder = (metric: HostMetricTypes, overview: boolean): string[] => {
   switch (metric) {
     case 'cpu':
@@ -40,7 +40,7 @@ describe('useHostCharts', () => {
         async (metric) => {
           const expectedOrder = getHostChartsExpectedOrder(metric, false);
 
-          const { result } = renderHook(() => useHostCharts({ dataViewId, metric }));
+          const { result } = renderHook(() => useHostCharts({ indexPattern, metric }));
           await waitFor(() => new Promise((resolve) => resolve(null)));
 
           const { charts } = result.current;
@@ -59,7 +59,7 @@ describe('useHostCharts', () => {
           const expectedOrder = getHostChartsExpectedOrder(metric, true);
 
           const { result } = renderHook(() =>
-            useHostCharts({ dataViewId, metric, overview: true })
+            useHostCharts({ indexPattern, metric, overview: true })
           );
           await waitFor(() => new Promise((resolve) => resolve(null)));
 
@@ -78,7 +78,7 @@ describe('useHostCharts', () => {
 
 describe('useKubernetesCharts', () => {
   it('should return an array of charts with correct order - overview', async () => {
-    const { result } = renderHook(() => useKubernetesCharts({ dataViewId, overview: true }));
+    const { result } = renderHook(() => useKubernetesCharts({ indexPattern, overview: true }));
     await waitFor(() => new Promise((resolve) => resolve(null)));
 
     const expectedOrder = ['nodeCpuCapacity', 'nodeMemoryCapacity'];
@@ -93,7 +93,7 @@ describe('useKubernetesCharts', () => {
   });
 
   it('should return an array of charts with correct order', async () => {
-    const { result } = renderHook(() => useKubernetesCharts({ dataViewId }));
+    const { result } = renderHook(() => useKubernetesCharts({ indexPattern }));
     await waitFor(() => new Promise((resolve) => resolve(null)));
 
     const expectedOrder = [
@@ -115,7 +115,7 @@ describe('useKubernetesCharts', () => {
 
 describe('useHostKpiCharts', () => {
   it('should return an array of charts with correct order', async () => {
-    const { result } = renderHook(() => useHostKpiCharts({ dataViewId }));
+    const { result } = renderHook(() => useHostKpiCharts({ indexPattern }));
     await waitFor(() => new Promise((resolve) => resolve(null)));
 
     const expectedOrder = ['cpuUsage', 'normalizedLoad1m', 'memoryUsage', 'diskUsage'];
@@ -136,7 +136,7 @@ describe('useHostKpiCharts', () => {
       getSubtitle: () => 'Custom Subtitle',
     };
 
-    const { result } = renderHook(() => useHostKpiCharts({ dataViewId, ...options }));
+    const { result } = renderHook(() => useHostKpiCharts({ indexPattern, ...options }));
     await waitFor(() => new Promise((resolve) => resolve(null)));
 
     expect(result.current).toHaveLength(4);

commit 37aca83397df1850f89bba4f411d2beabad14e0b

[Infra][Hosts] Charts not filtering by host.name fix (#242673)
fixes https://github.com/elastic/kibana/issues/242665

## Summary

This PR fixes a regression caused by
https://github.com/elastic/kibana/pull/238872, which made the chart
component not build the filter by host name.



![host_fix](https://github.com/user-attachments/assets/c733559b-2c1b-4482-9e2f-ac3f2b0ec300)


### How to test

- Start Kibana pointing to an oblt cluster 
- Navigate to Hosts
- Filter by anything that will return at least 1 host. All the charts in
the grid should only show data for the hosts in the table

x-pack/solutions/observability/plugins/infra/public/pages/metrics/hosts/components/tabs/metrics/chart.tsx
@@ -20,7 +20,7 @@ import { useAfterLoadedState } from '../../../hooks/use_after_loaded_state';
 
 export type ChartProps = LensConfig & {
   id: string;
-  dataView?: DataView;
+  dataView: DataView | undefined;
 };
 
 export const Chart = ({ id, dataView, ...chartProps }: ChartProps) => {

x-pack/solutions/observability/plugins/infra/public/pages/metrics/hosts/components/tabs/metrics/metrics_grid.tsx
@@ -43,7 +43,7 @@ export const MetricsGrid = () => {
       <EuiFlexGrid columns={2} gutterSize="s" data-test-subj="hostsView-metricChart">
         {charts.map((chartProp, index) => (
           <EuiFlexItem key={index} grow={false}>
-            <Chart {...chartProp} />
+            <Chart {...chartProp} dataView={metricsView?.dataViewReference} />
           </EuiFlexItem>
         ))}
       </EuiFlexGrid>
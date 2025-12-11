266adbd27327492d881c0860e956033890c071ca
Angela Chuang <6295984+angorayc@users.noreply.github.com>
2025-12-11 16:39:13 +0000

    [Automatic Migrations] UI - Add support for uploading Qradar XML (#244020)
    - Fixes https://github.com/elastic/security-team/issues/14589
    
    ## Summary
    
    <img width="2556" height="1234" alt="Screenshot 2025-12-03 at 23 48 06"
    src="https://github.com/user-attachments/assets/bb0d026d-012f-46e5-aa5a-d0f44085c877"
    />
    
    
    
    
    
    
    ### Checklist
    
    Check the PR satisfies following conditions. 
    
    Reviewers should verify this PR satisfies this list as well.
    
    - [x] Any text added follows [EUI's writing
    guidelines](https://elastic.github.io/eui/#/guidelines/writing), uses
    sentence case text and includes [i18n
    support](https://github.com/elastic/kibana/blob/main/src/platform/packages/shared/kbn-i18n/README.md)
    - [ ]
    [Documentation](https://www.elastic.co/guide/en/kibana/master/development-documentation.html)
    was added for features that require explanation or tutorials
    - [x] [Unit or functional
    tests](https://www.elastic.co/guide/en/kibana/master/development-tests.html)
    were updated or added to match the most common scenarios
    
    ---------
    
    Co-authored-by: Jatin Kathuria <jtn.kathuria@gmail.com>

--- x-pack/solutions/security/plugins/security_solution/public/siem_migrations/rules/components/data_input_flyout/steps/rules/sub_steps/rules_file_upload/rules_xml_file_upload.tsx
+++ x-pack/solutions/security/plugins/security_solution/public/siem_migrations/rules/components/data_input_flyout/steps/rules/sub_steps/rules_file_upload/rules_xml_file_upload.tsx
@@ -6,30 +6,19 @@
  */
 
 import React, { useCallback, useMemo, useRef, useState } from 'react';
-import { isPlainObject } from 'lodash';
 import { EuiFilePicker, EuiFlexGroup, EuiFlexItem, EuiFormRow, EuiText } from '@elastic/eui';
 import type {
   EuiFilePickerClass,
   EuiFilePickerProps,
 } from '@elastic/eui/src/components/form/file_picker/file_picker';
 import { UploadFileButton } from '../../../../../../../common/components';
-import { FILE_UPLOAD_ERROR } from '../../../../../../../common/translations/file_upload_error';
-import {
-  type SplunkRow,
-  useParseFileInput,
-  parseContent,
-} from '../../../../../../../common/hooks/use_parse_file_input';
 import type { CreateMigration } from '../../../../../../service/hooks/use_create_migration';
-import type { CreateRuleMigrationRulesRequestBody } from '../../../../../../../../../common/siem_migrations/model/api/rules/rule_migration.gen';
-import type { OriginalRule } from '../../../../../../../../../common/siem_migrations/model/rule_migration.gen';
 import * as i18n from './translations';
-import { RULES_DATA_INPUT_CHECK_RESOURCES_SPLUNK_DESCRIPTION } from '../check_resources/translations';
-import type { SPLUNK_RULES_COLUMNS } from '../../../../constants';
+import { RULES_DATA_INPUT_CHECK_RESOURCES_QRADAR_DESCRIPTION } from '../check_resources/translations';
+import { useParseFileInput } from '../../../../../../../common/hooks/use_parse_file_input';
 import { MigrationSource } from '../../../../../../../common/types';
 
-type SplunkRulesResult = Partial<Record<(typeof SPLUNK_RULES_COLUMNS)[number], string>>;
-
-export interface RulesFileUploadProps {
+export interface RulesXMLFileUploadProps {
   createMigration: CreateMigration;
   isLoading: boolean;
   isCreated: boolean;
@@ -38,39 +27,38 @@ export interface RulesFileUploadProps {
   apiError: string | undefined;
 }
 
-export const RulesFileUpload = React.memo<RulesFileUploadProps>(
+export const RulesXMLFileUpload = React.memo<RulesXMLFileUploadProps>(
   ({ createMigration, migrationName, apiError, isLoading, isCreated, onRulesFileChanged }) => {
-    const [rulesToUpload, setRulesToUpload] = useState<CreateRuleMigrationRulesRequestBody>([]);
+    const [rulesToUpload, setRulesToUpload] = useState<string>();
     const filePickerRef = useRef<EuiFilePickerClass>(null);
 
     const createRules = useCallback(() => {
-      if (migrationName) {
+      if (migrationName && rulesToUpload) {
         filePickerRef.current?.removeFiles();
         createMigration({
           migrationName,
-          rules: rulesToUpload,
-          migrationSource: MigrationSource.SPLUNK,
+          rules: { xml: rulesToUpload },
+          migrationSource: MigrationSource.QRADAR,
         });
       }
     }, [createMigration, migrationName, rulesToUpload]);
 
-    const onFileParsed = useCallback((content: string) => {
-      const rules = parseContent(content).map(formatRuleRow);
-      setRulesToUpload(rules);
+    const onXMLFileParsed = useCallback((content: string) => {
+      setRulesToUpload(content);
     }, []);
 
-    const { parseFile, isParsing, error: fileError } = useParseFileInput(onFileParsed);
+    const { parseFile, isParsing, error: fileError } = useParseFileInput(onXMLFileParsed);
 
     const onFileChange = useCallback(
       (files: FileList | null) => {
-        setRulesToUpload([]);
+        setRulesToUpload(undefined);
         onRulesFileChanged(files);
         parseFile(files);
       },
       [parseFile, onRulesFileChanged]
     );
 
-    const error = useMemo(() => {
+    const validationError = useMemo(() => {
       if (apiError) {
         return apiError;
       }
@@ -79,26 +67,26 @@ export const RulesFileUpload = React.memo<RulesFileUploadProps>(
 
     const showLoader = isParsing || isLoading;
     const isDisabled = !migrationName || showLoader || isCreated;
-    const isButtonDisabled = isDisabled || rulesToUpload.length === 0;
+    const isButtonDisabled = isDisabled || rulesToUpload == null;
 
     return (
       <EuiFlexGroup direction="column" gutterSize="s">
         <EuiFlexItem>
-          <EuiText size="s">{RULES_DATA_INPUT_CHECK_RESOURCES_SPLUNK_DESCRIPTION}</EuiText>
+          <EuiText size="s">{RULES_DATA_INPUT_CHECK_RESOURCES_QRADAR_DESCRIPTION}</EuiText>
         </EuiFlexItem>
         <EuiFlexItem>
-          <EuiFormRow isInvalid={error != null} fullWidth error={error}>
+          <EuiFormRow isInvalid={validationError != null} fullWidth error={validationError}>
             <EuiFilePicker
-              isInvalid={error != null}
+              isInvalid={validationError != null}
               id="rulesFilePicker"
               ref={filePickerRef as React.Ref<Omit<EuiFilePickerProps, 'stylesMemoizer'>>}
               fullWidth
               initialPromptText={
                 <EuiText size="s" textAlign="center">
-                  {i18n.RULES_DATA_INPUT_FILE_UPLOAD_PROMPT_SPLUNK}
+                  {i18n.RULES_DATA_INPUT_FILE_UPLOAD_PROMPT_QRADAR}
                 </EuiText>
               }
-              accept={'application/json, application/x-ndjson'}
+              accept={'.xml'}
               onChange={onFileChange}
               display="large"
               aria-label="Upload rules file"
@@ -124,29 +112,4 @@ export const RulesFileUpload = React.memo<RulesFileUploadProps>(
     );
   }
 );
-RulesFileUpload.displayName = 'RulesFileUpload';
-
-export const formatRuleRow = (row: SplunkRow<SplunkRulesResult>): OriginalRule => {
-  if (!isPlainObject(row.result)) {
-    throw new Error(FILE_UPLOAD_ERROR.NOT_OBJECT);
-  }
-  const originalRule: Partial<OriginalRule> = {
-    id: row.result.id,
-    vendor: 'splunk',
-    title: row.result.title,
-    query: row.result.search,
-    query_language: 'spl',
-    description: row.result['action.escu.eli5']?.trim() || row.result.description,
-    severity: row.result['alert.severity'] as OriginalRule['severity'],
-  };
-
-  if (row.result['action.correlationsearch.annotations']) {
-    try {
-      originalRule.annotations = JSON.parse(row.result['action.correlationsearch.annotations']);
-    } catch (error) {
-      delete originalRule.annotations;
-    }
-  }
-  // rule document format validation delegated to API
-  return originalRule as OriginalRule;
-};
+RulesXMLFileUpload.displayName = 'RulesXMLFileUpload';

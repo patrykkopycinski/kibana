d7be41e9513d9d90ba9205a0c9c9f924a8b61493
Tom Myers <106530686+tommyers-elastic@users.noreply.github.com>
2025-12-16 09:51:24 +0000

    Expose alert rule templates in 'create rule' UI (#245373)
    ## Summary
    
    - Add find alert rule template API and rule client method.
    - Move 'useTemplate' hook to a shared location so it can be accessed
    from the stack management and observability UIs
    - Add a 'Template' section to the create rule modal which uses the new
    find API to list the installed templates.
    
    
    ![output](https://github.com/user-attachments/assets/702c74af-31f1-4a0f-89ea-36637e2f9d1e)
    
    Closes https://github.com/elastic/kibana-team/issues/2417
    Closes https://github.com/elastic/kibana-team/issues/2104
    
    ### Checklist
    
    Check the PR satisfies following conditions. 
    
    Reviewers should verify this PR satisfies this list as well.
    
    - [x] Any text added follows [EUI's writing
    guidelines](https://elastic.github.io/eui/#/guidelines/writing), uses
    sentence case text and includes [i18n
    support](https://github.com/elastic/kibana/blob/main/src/platform/packages/shared/kbn-i18n/README.md)
    - [ ]
    [Documentation](https://www.elastic.co/guide/en/kibana/master/development-documentation.html)
    was added for features that require explanation or tutorials
    - [x] [Unit or functional
    tests](https://www.elastic.co/guide/en/kibana/master/development-tests.html)
    were updated or added to match the most common scenarios
    - [ ] If a plugin configuration key changed, check if it needs to be
    allowlisted in the cloud and added to the [docker
    list](https://github.com/elastic/kibana/blob/main/src/dev/build/tasks/os_packages/docker_generator/resources/base/bin/kibana-docker)
    - [x] This was checked for breaking HTTP API changes, and any breaking
    changes have been approved by the breaking-change committee. The
    `release_note:breaking` label should be applied in these situations.
    - [ ] [Flaky Test
    Runner](https://ci-stats.kibana.dev/trigger_flaky_test_runner/1) was
    used on any tests changed
    - [x] The PR description includes the appropriate Release Notes section,
    and the correct `release_note:*` label is applied per the
    [guidelines](https://www.elastic.co/guide/en/kibana/master/contributing.html#kibana-release-notes-process)
    - [x] Review the [backport
    guidelines](https://docs.google.com/document/d/1VyN5k91e5OVumlc0Gb9RPa3h1ewuPE705nRtioPiTvY/edit?usp=sharing)
    and apply applicable `backport:*` labels.
    
    ## Release note
    
    Add a 'Find Alert Rule Templates' API and use it to power showing
    installed templates in the create rule modal.
    
    ---------
    
    Co-authored-by: kibanamachine <42973632+kibanamachine@users.noreply.github.com>

--- src/platform/packages/shared/response-ops/rule_form/src/common/apis/create_rule_from_template/index.ts
+++ src/platform/packages/shared/response-ops/rule_form/src/common/apis/create_rule_from_template/index.ts
@@ -7,8 +7,4 @@
  * License v3.0 only", or the "Server Side Public License, v 1".
  */
 
-export * from './create_rule';
-export * from './fetch_ui_config';
-export * from './resolve_rule';
-export * from './update_rule';
-export * from './create_rule_from_template';
+export * from './load_rule_template';

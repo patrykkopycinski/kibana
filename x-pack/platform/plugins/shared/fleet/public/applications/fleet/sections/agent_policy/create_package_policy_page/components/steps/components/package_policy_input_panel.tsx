patrykkopycinski/kibana

commit b799664a2b0d98f7f26f1327eed75e6b2411982a

[Fleet] Remove stream toggle when an input only has a single stream  (#219287)
## Summary
Closes #192588 

- Removed toggle when an input only has one stream as the top-level
toggle controls it anyway. The toggles will only be nested when there is
more than one stream per input.

Before: 
<img width="1512" alt="Screenshot 2025-04-25 at 10 53 51 AM"
src="https://github.com/user-attachments/assets/2edb8108-e1e8-41f0-ae9f-666aa815b10e"
/>


After: 
<img width="1505" alt="image"
src="https://github.com/user-attachments/assets/a90df067-8d13-42e9-be16-2a0723dfd227"
/>


### Checklist

Check the PR satisfies following conditions. 

Reviewers should verify this PR satisfies this list as well.

- [ ] Any text added follows [EUI's writing
guidelines](https://elastic.github.io/eui/#/guidelines/writing), uses
sentence case text and includes [i18n
support](https://github.com/elastic/kibana/blob/main/src/platform/packages/shared/kbn-i18n/README.md)
- [ ]
[Documentation](https://www.elastic.co/guide/en/kibana/master/development-documentation.html)
was added for features that require explanation or tutorials
- [ ] [Unit or functional
tests](https://www.elastic.co/guide/en/kibana/master/development-tests.html)
were updated or added to match the most common scenarios
- [ ] If a plugin configuration key changed, check if it needs to be
allowlisted in the cloud and added to the [docker
list](https://github.com/elastic/kibana/blob/main/src/dev/build/tasks/os_packages/docker_generator/resources/base/bin/kibana-docker)
- [ ] This was checked for breaking HTTP API changes, and any breaking
changes have been approved by the breaking-change committee. The
`release_note:breaking` label should be applied in these situations.
- [ ] [Flaky Test
Runner](https://ci-stats.kibana.dev/trigger_flaky_test_runner/1) was
used on any tests changed
- [ ] The PR description includes the appropriate Release Notes section,
and the correct `release_note:*` label is applied per the

x-pack/platform/plugins/shared/fleet/public/applications/fleet/sections/agent_policy/create_package_policy_page/components/steps/components/package_policy_input_panel.tsx
@@ -6,6 +6,7 @@
  */
 
 import React, { useState, Fragment, memo, useMemo, useCallback } from 'react';
+import ReactMarkdown from 'react-markdown';
 import styled from 'styled-components';
 import { FormattedMessage } from '@kbn/i18n-react';
 import {
@@ -94,7 +95,7 @@ export const PackagePolicyInputPanel: React.FunctionComponent<{
   }) => {
     const defaultDataStreamId = useDataStreamId();
     const { isAgentlessEnabled } = useAgentless();
-
+    const showTopLevelDescription = packagePolicyInput.streams.length === 1;
     // Showing streams toggle state
     const [isShowingStreams, setIsShowingStreams] = useState<boolean>(() =>
       shouldShowStreamsByDefault(
@@ -196,7 +197,17 @@ export const PackagePolicyInputPanel: React.FunctionComponent<{
                 }
               }}
             />
+            <EuiSpacer size="s" />
+            {/* show the description under the top level toggle if theres only one stream */}
+            {showTopLevelDescription && (
+              <EuiText size="s" color="subdued">
+                <ReactMarkdown>
+                  {String(inputStreams[0].packageInputStream.description)}
+                </ReactMarkdown>
+              </EuiText>
+            )}
           </EuiFlexItem>
+
           <EuiFlexItem grow={false}>
             <EuiFlexGroup gutterSize="s" alignItems="center">
               {hasErrors ? (
@@ -259,6 +270,7 @@ export const PackagePolicyInputPanel: React.FunctionComponent<{
                   data-test-subj="PackagePolicy.InputStreamConfig"
                   packageInfo={packageInfo}
                   packageInputStream={packageInputStream}
+                  totalStreams={inputStreams.length}
                   packagePolicyInputStream={packagePolicyInputStream!}
                   updatePackagePolicyInputStream={(
                     updatedStream: Partial<PackagePolicyInputStream>

x-pack/platform/plugins/shared/fleet/public/applications/fleet/sections/agent_policy/create_package_policy_page/components/steps/components/package_policy_input_panel.test.tsx
@@ -304,6 +304,107 @@ describe('PackagePolicyInputPanel', () => {
         ],
       },
     },
+    // Duplicated items to make sure they show up in the UI as we now only show multiple switches/descriptions when theres more than one input
+    {
+      input: 'logfile',
+      title: 'Sample logs hidden in agentless (duplicated)',
+      template_path: 'stream.yml.hbs',
+      vars: [
+        {
+          name: 'paths',
+          type: 'text',
+          title: 'Paths',
+          multi: false,
+          default: ['/var/log/*.log'],
+          required: false,
+          show_user: true,
+          hide_in_deployment_modes: ['agentless'],
+        },
+      ],
+      description: 'Collect sample logs - hidden in agentless',
+      data_stream: {
+        title: 'Default data stream',
+        release: 'ga',
+        type: 'logs',
+        package: 'agentless_test_package',
+        dataset: 'agentless_test_package.default_data_stream',
+        path: 'default_data_stream',
+        elasticsearch: {
+          'ingest_pipeline.name': 'default',
+        },
+        ingest_pipeline: 'default',
+        streams: [
+          {
+            input: 'logfile',
+            title: 'Sample logs hidden in agentless',
+            template_path: 'stream.yml.hbs',
+            vars: [
+              {
+                name: 'paths',
+                type: 'text',
+                title: 'Paths',
+                multi: false,
+                default: ['/var/log/*.log'],
+                required: false,
+                show_user: true,
+                hide_in_deployment_modes: ['agentless'],
+              },
+            ],
+            description: 'Collect sample logs - hidden in agentless',
+          },
+        ],
+      },
+    },
+    {
+      input: 'logfile',
+      title: 'Sample logs on Agentless (duplicated)',
+      template_path: 'stream.yml.hbs',
+      vars: [
+        {
+          name: 'paths',
+          type: 'text',
+          title: 'Paths',
+          multi: true,
+          default: ['/var/log/*.log'],
+          required: false,
+          show_user: true,
+          hide_in_deployment_modes: ['default'],
+        },
+      ],
+      description: 'Collect sample logs on Agentless only',
+      data_stream: {
+        title: 'Logs Stream on Agentless',
+        release: 'ga',
+        type: 'logs',
+        package: 'agentless_test_package',
+        dataset: 'agentless_test_package.logs_stream',
+        path: 'logs_stream',
+        elasticsearch: {
+          'ingest_pipeline.name': 'default',
+        },
+        ingest_pipeline: 'default',
+        streams: [
+          {
+            input: 'logfile',
+            title: 'Sample logs on Agentless',
+            template_path: 'stream.yml.hbs',
+            vars: [
+              {
+                name: 'paths',
+                type: 'text',
+                title: 'Paths',
+                multi: true,
+                default: ['/var/log/*.log'],
+                required: false,
+                show_user: true,
+                hide_in_deployment_modes: ['default'],
+              },
+            ],
+            description: 'Collect sample logs on Agentless only',
+          },
+        ],
+      },
+    },
   ];
   const mockUpdatePackagePolicyInput = jest.fn().mockImplementation((val: any) => {
     return undefined;
@@ -424,6 +525,18 @@ describe('PackagePolicyInputPanel', () => {
         ).toBeInTheDocument();
       });
     });
+    it('should not render multiple toggles when theres only one stream', async () => {
+      //  Only send one input and then it should not be rendered, only the top level switch should render
+      render(mockPackageInfo, mockPackageInputStreams.slice(0, 1));
+      await waitFor(async () => {
+        expect(
+          await renderResult.findByTestId('PackagePolicy.InputStreamConfig.Switch')
+        ).toBeInTheDocument();
+        expect(
+          await renderResult.queryByText('Sample logs hidden in agentless')
+        ).not.toBeInTheDocument();
+      });
+    });
 
     it('should render inputs when hide_in_deployment_modes is not present', async () => {
       const packageInputStreams: RegistryStreamWithDataStream[] = [
@@ -443,6 +556,55 @@ describe('PackagePolicyInputPanel', () => {
             },
           ],
           description: 'Collect sample logs',
+          data_stream: {
+            title: 'Default data stream',
+            release: 'ga',
+            type: 'logs',
+            package: 'agentless_test_package',
+            dataset: 'agentless_test_package.default_data_stream',
+            path: 'default_data_stream',
+            elasticsearch: {
+              'ingest_pipeline.name': 'default',
+            },
+            ingest_pipeline: 'default',
+            streams: [
+              {
+                input: 'logfile',
+                title: 'Sample logs',
+                template_path: 'stream.yml.hbs',
+                vars: [
+                  {
+                    name: 'paths',
+                    type: 'text',
+                    title: 'Paths',
+                    multi: false,
+                    default: ['/var/log/*.log'],
+                    required: false,
+                    show_user: true,
+                  },
+                ],
+                description: 'Collect sample log - hidden in agentless',
+              },
+            ],
+          },
+        },
+        //  Duplicated items to make sure they show up in the UI as we now only show multiple switches/descriptions when theres more than one input
+        {
+          input: 'logfile',
+          title: 'Sample logs (duplicated)',
+          template_path: 'stream.yml.hbs',
+          vars: [
+            {
+              name: 'paths',
+              type: 'text',
+              title: 'Paths',
+              multi: false,
+              default: ['/var/log/*.log'],
+              required: false,
+              show_user: true,
+            },
+          ],
+          description: 'Collect sample logs (duplicated)',
           data_stream: {
             title: 'Default data stream',
             release: 'ga',
@@ -520,7 +682,7 @@ describe('PackagePolicyInputPanel', () => {
         ).toBeInTheDocument();
       });
       await waitFor(async () => {
-        expect(await renderResult.findByText('Sample logs')).toBeInTheDocument();
+        expect(await renderResult.queryByText('Sample logs')).toBeInTheDocument();
       });
       await waitFor(async () => {
         expect(await renderResult.findByText('Collect sample logs')).toBeInTheDocument();

x-pack/platform/plugins/shared/fleet/public/applications/fleet/sections/agent_policy/create_package_policy_page/components/steps/components/package_policy_input_stream.tsx
@@ -65,6 +65,7 @@ interface Props {
   inputStreamValidationResults: PackagePolicyConfigValidationResults;
   forceShowErrors?: boolean;
   isEditPage?: boolean;
+  totalStreams?: number;
 }
 
 export const PackagePolicyInputStreamConfig = memo<Props>(
@@ -76,6 +77,7 @@ export const PackagePolicyInputStreamConfig = memo<Props>(
     inputStreamValidationResults,
     forceShowErrors,
     isEditPage,
+    totalStreams,
   }) => {
     const config = useConfig();
     const isExperimentalDataStreamSettingsEnabled =
@@ -92,7 +94,7 @@ export const PackagePolicyInputStreamConfig = memo<Props>(
       !!packagePolicyInputStream.id &&
       packagePolicyInputStream.id === defaultDataStreamId;
     const isPackagePolicyEdit = !!packagePolicyId;
-
+    const shouldShowStreamsToggles = totalStreams ? totalStreams > 1 : true;
     const customDatasetVar = packagePolicyInputStream.vars?.[DATASET_VAR_NAME];
     const customDatasetVarValue = customDatasetVar?.value?.dataset || customDatasetVar?.value;
 
@@ -182,7 +184,7 @@ export const PackagePolicyInputStreamConfig = memo<Props>(
                   alignItems="flexStart"
                   justifyContent="spaceBetween"
                 >
-                  {packageInfo.type !== 'input' && (
+                  {packageInfo.type !== 'input' && shouldShowStreamsToggles && (
                     <EuiFlexItem grow={false}>
                       <EuiSwitch
                         data-test-subj="streamOptions.switch"
@@ -209,7 +211,9 @@ export const PackagePolicyInputStreamConfig = memo<Props>(
                     </EuiFlexItem>
                   ) : null}
                 </EuiFlexGroup>
-                {packageInfo.type !== 'input' && packageInputStream.description ? (
+                {packageInfo.type !== 'input' &&
+                packageInputStream.description &&
+                shouldShowStreamsToggles ? (
                   <>
                     <EuiSpacer size="s" />
                     <EuiText size="s" color="subdued">

commit 3f4d14cb13f8222786db9fa1668248a96354396b

[Fleet] hide description if there are no visible input streams (#253226)
## Summary

Fix bug introduced in https://github.com/elastic/kibana/pull/219287 and
reported with FIM integration, if the data stream has no vars, the
description was rendered as `undefined`.

Before:
<img width="1486" height="238" alt="image"
src="https://github.com/user-attachments/assets/3a296fa8-3ca8-4de4-bfc6-5529680f0aa0"
/>

After:
<img width="928" height="864" alt="image"
src="https://github.com/user-attachments/assets/8dba9583-17f5-4170-8771-a808b26f62a9"
/>



### Checklist

Check the PR satisfies following conditions. 

Reviewers should verify this PR satisfies this list as well.

- [ ] Any text added follows [EUI's writing
guidelines](https://elastic.github.io/eui/#/guidelines/writing), uses
sentence case text and includes [i18n
support](https://github.com/elastic/kibana/blob/main/src/platform/packages/shared/kbn-i18n/README.md)
- [ ]
[Documentation](https://www.elastic.co/guide/en/kibana/master/development-documentation.html)
was added for features that require explanation or tutorials
- [ ] [Unit or functional
tests](https://www.elastic.co/guide/en/kibana/master/development-tests.html)
were updated or added to match the most common scenarios
- [ ] If a plugin configuration key changed, check if it needs to be
allowlisted in the cloud and added to the [docker
list](https://github.com/elastic/kibana/blob/main/src/dev/build/tasks/os_packages/docker_generator/resources/base/bin/kibana-docker)
- [ ] This was checked for breaking HTTP API changes, and any breaking
changes have been approved by the breaking-change committee. The
`release_note:breaking` label should be applied in these situations.
- [ ] [Flaky Test
Runner](https://ci-stats.kibana.dev/trigger_flaky_test_runner/1) was
used on any tests changed
- [ ] The PR description includes the appropriate Release Notes section,
and the correct `release_note:*` label is applied per the

x-pack/platform/plugins/shared/fleet/public/applications/fleet/sections/agent_policy/create_package_policy_page/components/steps/components/package_policy_input_panel.tsx
@@ -97,7 +97,6 @@ export const PackagePolicyInputPanel: React.FunctionComponent<{
   }) => {
     const defaultDataStreamId = useDataStreamId();
     const { isAgentlessEnabled } = useAgentless();
-    const showTopLevelDescription = packagePolicyInput.streams.length === 1;
     // Showing streams toggle state
     const [isShowingStreams, setIsShowingStreams] = useState<boolean>(() =>
       shouldShowStreamsByDefault(
@@ -159,6 +158,7 @@ export const PackagePolicyInputPanel: React.FunctionComponent<{
           .filter((stream) => Boolean(stream.packagePolicyInputStream)),
       [packageInputStreamShouldBeVisible, packageInputStreams, packagePolicyInput.streams]
     );
+    const showTopLevelDescription = inputStreams.length === 1;
 
     return (
       <>
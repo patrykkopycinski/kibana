patrykkopycinski/kibana

commit be65ea41c5e77b9e2bb429e3ef5c99a83d64a4b6

[Discover] Skip fetch for non-document view modes (#245663)

src/platform/plugins/shared/discover/public/embeddable/initialize_fetch.ts
@@ -45,6 +45,7 @@ import type { PublishesSavedSearch, SearchEmbeddableStateManager } from './types
 import { getTimeRangeFromFetchContext, updateSearchSource } from './utils/update_search_source';
 import { createDataSource } from '../../common/data_sources';
 import type { ScopedProfilesManager } from '../context_awareness';
+import { isFieldStatsMode } from './utils/is_field_stats_mode';
 
 type SavedSearchPartialFetchApi = PublishesSavedSearch &
   PublishesSavedObjectId &
@@ -179,8 +180,13 @@ export function initializeFetch({
       }),
       switchMap(async ([fetchContext, savedSearch, dataViews, esqlVariables]) => {
         const dataView = dataViews?.length ? dataViews[0] : undefined;
+
         setBlockingError(undefined);
-        if (!dataView || !savedSearch.searchSource) {
+        if (
+          !dataView ||
+          !savedSearch.searchSource ||
+          isFieldStatsMode(savedSearch, dataView, discoverServices.uiSettings)
+        ) {
           return;
         }
 

src/platform/plugins/shared/discover/public/embeddable/get_search_embeddable_factory.test.tsx
@@ -144,12 +144,17 @@ describe('saved search embeddable', () => {
       });
     });
 
-    it('should render field stats table in AGGREGATED_LEVEL view mode', async () => {
-      const { search, resolveSearch } = createSearchFnMock(0);
+    it('should render field stats table in AGGREGATED_LEVEL view mode and not fetch documents', async () => {
+      const { search } = createSearchFnMock(0);
       runtimeState = getInitialRuntimeState({
         searchMock: search,
         partialState: { viewMode: VIEW_MODE.AGGREGATED_LEVEL },
       });
+
+      discoverServiceMock.uiSettings.get = jest.fn().mockImplementation((key: string) => {
+        if (key === SHOW_FIELD_STATISTICS) return true;
+      });
+
       const { Component, api } = await factory.buildEmbeddable({
         initialState: { rawState: { savedObjectId: 'id' } }, // runtimeState passed via mocked deserializeState
         finalizeApi: finalizeApiMock,
@@ -158,15 +163,10 @@ describe('saved search embeddable', () => {
       });
       await waitOneTick(); // wait for build to complete
 
-      discoverServiceMock.uiSettings.get = jest.fn().mockImplementationOnce((key: string) => {
-        if (key === SHOW_FIELD_STATISTICS) return true;
-      });
       const discoverComponent = render(<Component />);
 
-      // wait for data fetching
-      expect(api.dataLoading$.getValue()).toBe(true);
-      resolveSearch();
-      await waitOneTick();
+      // Field statistics mode should not trigger document fetching
+      expect(search).not.toHaveBeenCalled();
       expect(api.dataLoading$.getValue()).toBe(false);
 
       expect(discoverComponent.queryByTestId('dscFieldStatsEmbeddedContent')).toBeInTheDocument();
@@ -178,7 +178,7 @@ describe('saved search embeddable', () => {
       const { search, resolveSearch } = createSearchFnMock(1);
       runtimeState = getInitialRuntimeState({
         searchMock: search,
-        partialState: { viewMode: VIEW_MODE.AGGREGATED_LEVEL },
+        partialState: { viewMode: VIEW_MODE.DOCUMENT_LEVEL },
       });
       const { api } = await factory.buildEmbeddable({
         initialState: { rawState: { savedObjectId: 'id' } }, // runtimeState passed via mocked deserializeState

commit d39cea9ce250d350213d9aa55417545eeb308ec2

[ML] Fix field stats saved search not updating when Dashboard changes filter (#257241)
## Summary

This PR fixes
https://github.com/issues/assigned?issue=elastic%7Ckibana%7C254441. This
is a regression introduced in
https://github.com/elastic/kibana/commit/be65ea41c5e77b9e2bb429e3ef5c99a83d64a4b6.
This PR updates so that update the search source when in field stats
mode, just not fetch the rest of data.


https://github.com/user-attachments/assets/b4f8b536-aa99-4f41-b21b-746a0ced345e



### Checklist

Check the PR satisfies following conditions. 

Reviewers should verify this PR satisfies this list as well.

- [ ] Any text added follows [EUI's writing
guidelines](https://elastic.github.io/eui/#/guidelines/writing), uses
sentence case text and includes [i18n
support](https://github.com/elastic/kibana/blob/main/src/platform/packages/shared/kbn-i18n/README.md)
- [ ]
[Documentation](https://www.elastic.co/guide/en/kibana/master/development-documentation.html)
was added for features that require explanation or tutorials
- [ ] [Unit or functional
tests](https://www.elastic.co/guide/en/kibana/master/development-tests.html)
were updated or added to match the most common scenarios
- [ ] If a plugin configuration key changed, check if it needs to be
allowlisted in the cloud and added to the [docker
list](https://github.com/elastic/kibana/blob/main/src/dev/build/tasks/os_packages/docker_generator/resources/base/bin/kibana-docker)
- [ ] This was checked for breaking HTTP API changes, and any breaking
changes have been approved by the breaking-change committee. The
`release_note:breaking` label should be applied in these situations.
- [ ] [Flaky Test
Runner](https://ci-stats.kibana.dev/trigger_flaky_test_runner/1) was
used on any tests changed
- [ ] The PR description includes the appropriate Release Notes section,
and the correct `release_note:*` label is applied per the
[guidelines](https://www.elastic.co/guide/en/kibana/master/contributing.html#kibana-release-notes-process)

src/platform/plugins/shared/discover/public/embeddable/initialize_fetch.ts
@@ -162,11 +162,7 @@ export function initializeFetch({
         const dataView = dataViews?.length ? dataViews[0] : undefined;
 
         setBlockingError(undefined);
-        if (
-          !dataView ||
-          !savedSearch.searchSource ||
-          isFieldStatsMode(savedSearch, dataView, discoverServices.uiSettings)
-        ) {
+        if (!dataView || !savedSearch.searchSource) {
           return;
         }
 
@@ -181,6 +177,11 @@ export function initializeFetch({
             sortDir: discoverServices.uiSettings.get(SORT_DEFAULT_ORDER_SETTING),
           }
         );
+        // Still update search source for field stats mode, but not necessarily fetch data
+        if (isFieldStatsMode(savedSearch, dataView, discoverServices.uiSettings)) {
+          api.fetchContext$.next(fetchContext);
+          return;
+        }
 
         const searchSessionId = fetchContext.searchSessionId;
         const searchSourceQuery = savedSearch.searchSource.getField('query');

x-pack/platform/test/functional/apps/ml/data_visualizer/group1/index_data_visualizer_filters.ts
@@ -9,7 +9,6 @@ import type { FtrProviderContext } from '../../../../ftr_provider_context';
 import {
   farequoteKQLFiltersSearchTestData,
   farequoteLuceneFiltersSearchTestData,
-  farequoteDataViewTestData,
 } from '../index_test_data';
 import type { TestData } from '../types';
 
@@ -139,10 +138,6 @@ export default function ({ getService, getPageObjects }: FtrProviderContext) {
     after(async function () {
       await ml.testResources.deleteSavedSearches();
       await ml.testResources.deleteDataViewByTitle('ft_farequote');
-    });
-
-    describe(`with ${farequoteDataViewTestData.suiteTitle}`, function () {
-      runTests(farequoteDataViewTestData);
     });
 
     describe(`with ${farequoteLuceneFiltersSearchTestData.suiteTitle}`, function () {

x-pack/platform/test/functional/apps/ml/data_visualizer/group1/index_data_visualizer_grid_in_discover.ts
@@ -1,116 +1,0 @@
-/*
- * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
- * or more contributor license agreements. Licensed under the Elastic License
- * 2.0; you may not use this file except in compliance with the Elastic License
- * 2.0.
- */
-
-import type { FtrProviderContext } from '../../../../ftr_provider_context';
-import type { TestData, MetricFieldVisConfig } from '../types';
-
-const SHOW_FIELD_STATISTICS = 'discover:showFieldStatistics';
-import {
-  farequoteDataViewTestData,
-  farequoteKQLSearchTestData,
-  farequoteLuceneFiltersSearchTestData,
-  farequoteKQLFiltersSearchTestData,
-  farequoteLuceneSearchTestData,
-  sampleLogTestData,
-} from '../index_test_data';
-
-export default function ({ getService, getPageObjects }: FtrProviderContext) {
-  const esArchiver = getService('esArchiver');
-  const PageObjects = getPageObjects(['common', 'discover', 'timePicker', 'settings']);
-  const ml = getService('ml');
-  const retry = getService('retry');
-  const dataViews = getService('dataViews');
-
-  const startTime = 'Jan 1, 2016 @ 00:00:00.000';
-  const endTime = 'Nov 1, 2020 @ 00:00:00.000';
-
-  function runTests(testData: TestData) {
-    describe(`with ${testData.suiteTitle}`, function () {
-      it(`displays the 'Field statistics' table content correctly`, async function () {
-        await PageObjects.common.navigateToApp('discover');
-        if (testData.isSavedSearch) {
-          await retry.tryForTime(2 * 1000, async () => {
-            await PageObjects.discover.loadSavedSearch(testData.sourceIndexOrSavedSearch);
-          });
-        } else {
-          await dataViews.switchToAndValidate(testData.sourceIndexOrSavedSearch);
-        }
-        await PageObjects.timePicker.setAbsoluteRange(startTime, endTime);
-
-        await PageObjects.discover.assertHitCount(testData.expected.totalDocCountFormatted);
-        await PageObjects.discover.assertViewModeToggleExists();
-        await PageObjects.discover.clickViewModeFieldStatsButton();
-        await ml.testExecution.logTestStep(
-          'displays details for metric fields and non-metric fields correctly'
-        );
-        for (const fieldRow of testData.expected.metricFields as Array<
-          Required<MetricFieldVisConfig>
-        >) {
-          await ml.dataVisualizerTable.assertNumberFieldContents(
-            fieldRow.fieldName,
-            fieldRow.docCountFormatted,
-            fieldRow.topValuesCount,
-            fieldRow.viewableInLens
-          );
-        }
-
-        for (const fieldRow of testData.expected.nonMetricFields!) {
-          await ml.dataVisualizerTable.assertNonMetricFieldContents(
-            fieldRow.type,
-            fieldRow.fieldName!,
-            fieldRow.docCountFormatted,
-            fieldRow.exampleCount,
-            fieldRow.viewableInLens,
-            false,
-            fieldRow.exampleContent
-          );
-        }
-      });
-    });
-  }
-
-  describe('field statistics in Discover', function () {
-    before(async function () {
-      await esArchiver.loadIfNeeded('x-pack/platform/test/fixtures/es_archives/ml/farequote');
-      await esArchiver.loadIfNeeded(
-        'x-pack/platform/test/fixtures/es_archives/ml/module_sample_logs'
-      );
-      await ml.testResources.createDataViewIfNeeded('ft_farequote', '@timestamp');
-      await ml.testResources.createDataViewIfNeeded('ft_module_sample_logs', '@timestamp');
-      await ml.testResources.createSavedSearchFarequoteKueryIfNeeded();
-      await ml.testResources.createSavedSearchFarequoteLuceneIfNeeded();
-      await ml.testResources.createSavedSearchFarequoteFilterAndLuceneIfNeeded();
-      await ml.testResources.createSavedSearchFarequoteFilterAndKueryIfNeeded();
-
-      await ml.securityUI.loginAsMlPowerUser();
-    });
-
-    after(async function () {
-      await ml.testResources.clearAdvancedSettingProperty(SHOW_FIELD_STATISTICS);
-      await ml.testResources.deleteSavedSearches();
-      await ml.testResources.deleteDataViewByTitle('ft_farequote');
-      await ml.testResources.deleteDataViewByTitle('ft_module_sample_logs');
-    });
-
-    describe('when enabled', function () {
-      before(async function () {
-        await ml.testResources.setAdvancedSettingProperty(SHOW_FIELD_STATISTICS, true);
-      });
-
-      after(async function () {
-        await ml.testResources.clearAdvancedSettingProperty(SHOW_FIELD_STATISTICS);
-      });
-
-      runTests(farequoteDataViewTestData);
-      runTests(farequoteKQLSearchTestData);
-      runTests(farequoteLuceneSearchTestData);
-      runTests(farequoteKQLFiltersSearchTestData);
-      runTests(farequoteLuceneFiltersSearchTestData);
-      runTests(sampleLogTestData);
-    });
-  });
-}

x-pack/platform/test/functional/apps/ml/data_visualizer/group3/index_data_visualizer_grid_in_dashboard.ts
@@ -24,7 +24,12 @@ export default function ({ getService, getPageObjects }: FtrProviderContext) {
   const dataViews = getService('dataViews');
   const retry = getService('retry');
   const dashboardAddPanel = getService('dashboardAddPanel');
+  const filterBar = getService('filterBar');
 
+  const PINNED_FILTER = {
+    key: 'type.keyword',
+    value: 'farequote',
+  };
   function runTests(testData: TestData) {
     const savedSearchTitle = `Field stats for ${testData.suiteTitle} ${Date.now()}`;
     const dashboardTitle = `Dashboard for ${testData.suiteTitle} ${Date.now()}`;
@@ -88,6 +93,19 @@ export default function ({ getService, getPageObjects }: FtrProviderContext) {
             fieldRow.exampleContent
           );
         }
+        await filterBar.addFilter({
+          field: PINNED_FILTER.key,
+          operation: 'is not',
+          value: PINNED_FILTER.value,
+        });
+        await ml.dataVisualizerTable.assertNonMetricFieldContents(
+          'text',
+          '@version',
+          '0 (0%)',
+          0,
+          false
+        );
+        await filterBar.removeFilter(PINNED_FILTER.key);
 
         await PageObjects.dashboard.saveDashboard(dashboardTitle);
       });

x-pack/platform/test/functional/apps/ml/data_visualizer/group3/index_data_visualizer_grid_in_discover.ts
@@ -13,8 +13,6 @@ import {
   farequoteDataViewTestData,
   farequoteKQLSearchTestData,
   farequoteLuceneFiltersSearchTestData,
-  farequoteKQLFiltersSearchTestData,
-  farequoteLuceneSearchTestData,
   sampleLogTestData,
 } from '../index_test_data';
 
@@ -107,8 +105,6 @@ export default function ({ getService, getPageObjects }: FtrProviderContext) {
 
       runTests(farequoteDataViewTestData);
       runTests(farequoteKQLSearchTestData);
-      runTests(farequoteLuceneSearchTestData);
-      runTests(farequoteKQLFiltersSearchTestData);
       runTests(farequoteLuceneFiltersSearchTestData);
       runTests(sampleLogTestData);
     });

x-pack/platform/test/functional/apps/ml/data_visualizer/index_test_data.ts
@@ -45,16 +45,6 @@ export const farequoteDataViewTestData: TestData = {
         viewableInLens: true,
       },
       {
-        fieldName: '@version',
-        type: ML_JOB_FIELD_TYPES.TEXT,
-        existsInDocs: true,
-        aggregatable: false,
-        loading: false,
-        exampleCount: 1,
-        docCountFormatted: '1,000 (100%)',
-        viewableInLens: false,
-      },
-      {
         fieldName: '@version.keyword',
         type: ML_JOB_FIELD_TYPES.KEYWORD,
         existsInDocs: true,
@@ -71,26 +61,6 @@ export const farequoteDataViewTestData: TestData = {
         aggregatable: true,
         loading: false,
         exampleCount: 11,
-        docCountFormatted: '5,000 (100%)',
-        viewableInLens: true,
-      },
-      {
-        fieldName: 'type',
-        type: ML_JOB_FIELD_TYPES.TEXT,
-        existsInDocs: true,
-        aggregatable: false,
-        loading: false,
-        exampleCount: 1,
-        docCountFormatted: '1,000 (100%)',
-        viewableInLens: false,
-      },
-      {
-        fieldName: 'type.keyword',
-        type: ML_JOB_FIELD_TYPES.KEYWORD,
-        existsInDocs: true,
-        aggregatable: true,
-        loading: false,
-        exampleCount: 1,
         docCountFormatted: '5,000 (100%)',
         viewableInLens: true,
       },
@@ -153,36 +123,6 @@ export const farequoteKQLSearchTestData: TestData = {
       },
       {
         fieldName: '@version.keyword',
-        type: ML_JOB_FIELD_TYPES.KEYWORD,
-        existsInDocs: true,
-        aggregatable: true,
-        loading: false,
-        exampleCount: 1,
-        docCountFormatted: '5,000 (100%)',
-        viewableInLens: true,
-      },
-      {
-        fieldName: 'airline',
-        type: ML_JOB_FIELD_TYPES.KEYWORD,
-        existsInDocs: true,
-        aggregatable: true,
-        loading: false,
-        exampleCount: 5,
-        docCountFormatted: '5,000 (100%)',
-        viewableInLens: true,
-      },
-      {
-        fieldName: 'type',
-        type: ML_JOB_FIELD_TYPES.TEXT,
-        existsInDocs: true,
-        aggregatable: false,
-        loading: false,
-        exampleCount: 1,
-        docCountFormatted: '1,000 (100%)',
-        viewableInLens: false,
-      },
-      {
-        fieldName: 'type.keyword',
         type: ML_JOB_FIELD_TYPES.KEYWORD,
         existsInDocs: true,
         aggregatable: true,
@@ -270,26 +210,6 @@ export const farequoteKQLFiltersSearchTestData: TestData = {
         docCountFormatted: '5,000 (100%)',
         viewableInLens: true,
       },
-      {
-        fieldName: 'type',
-        type: ML_JOB_FIELD_TYPES.TEXT,
-        existsInDocs: true,
-        aggregatable: false,
-        loading: false,
-        exampleCount: 1,
-        docCountFormatted: '1,000 (100%)',
-        viewableInLens: false,
-      },
-      {
-        fieldName: 'type.keyword',
-        type: ML_JOB_FIELD_TYPES.KEYWORD,
-        existsInDocs: true,
-        aggregatable: true,
-        loading: false,
-        exampleCount: 1,
-        docCountFormatted: '5,000 (100%)',
-        viewableInLens: true,
-      },
     ],
     emptyFields: ['sourcetype'],
     visibleMetricFieldsCount: 1,
@@ -348,42 +268,12 @@ export const farequoteLuceneSearchTestData: TestData = {
         viewableInLens: false,
       },
       {
-        fieldName: '@version.keyword',
-        type: ML_JOB_FIELD_TYPES.KEYWORD,
-        existsInDocs: true,
-        aggregatable: true,
-        loading: false,
-        exampleCount: 1,
-        docCountFormatted: '5,000 (100%)',
-        viewableInLens: true,
-      },
-      {
         fieldName: 'airline',
         type: ML_JOB_FIELD_TYPES.KEYWORD,
         existsInDocs: true,
         aggregatable: true,
         loading: false,
         exampleCount: 5,
-        docCountFormatted: '5,000 (100%)',
-        viewableInLens: true,
-      },
-      {
-        fieldName: 'type',
-        type: ML_JOB_FIELD_TYPES.TEXT,
-        existsInDocs: true,
-        aggregatable: false,
-        loading: false,
-        exampleCount: 1,
-        docCountFormatted: '1,000 (100%)',
-        viewableInLens: false,
-      },
-      {
-        fieldName: 'type.keyword',
-        type: ML_JOB_FIELD_TYPES.KEYWORD,
-        existsInDocs: true,
-        aggregatable: true,
-        loading: false,
-        exampleCount: 1,
         docCountFormatted: '5,000 (100%)',
         viewableInLens: true,
       },
@@ -436,26 +326,6 @@ export const farequoteLuceneFiltersSearchTestData: TestData = {
         viewableInLens: true,
       },
       {
-        fieldName: '@version',
-        type: ML_JOB_FIELD_TYPES.TEXT,
-        existsInDocs: true,
-        aggregatable: false,
-        loading: false,
-        exampleCount: 1,
-        docCountFormatted: '1,000 (100%)',
-        viewableInLens: false,
-      },
-      {
-        fieldName: '@version.keyword',
-        type: ML_JOB_FIELD_TYPES.KEYWORD,
-        existsInDocs: true,
-        aggregatable: true,
-        loading: false,
-        exampleCount: 1,
-        docCountFormatted: '5,000 (100%)',
-        viewableInLens: true,
-      },
-      {
         fieldName: 'airline',
         type: ML_JOB_FIELD_TYPES.KEYWORD,
         existsInDocs: true,
@@ -475,16 +345,6 @@ export const farequoteLuceneFiltersSearchTestData: TestData = {
         exampleCount: 1,
         docCountFormatted: '1,000 (100%)',
         viewableInLens: false,
-      },
-      {
-        fieldName: 'type.keyword',
-        type: ML_JOB_FIELD_TYPES.KEYWORD,
-        existsInDocs: true,
-        aggregatable: true,
-        loading: false,
-        exampleCount: 1,
-        docCountFormatted: '5,000 (100%)',
-        viewableInLens: true,
       },
     ],
     emptyFields: ['sourcetype'],

x-pack/platform/test/functional/apps/ml/data_visualizer/index_test_data_random_sampler.ts
@@ -45,26 +45,6 @@ export const farequoteDataViewTestData: TestData = {
         viewableInLens: true,
       },
       {
-        fieldName: '@version',
-        type: ML_JOB_FIELD_TYPES.TEXT,
-        existsInDocs: true,
-        aggregatable: false,
-        loading: false,
-        exampleCount: 1,
-        docCountFormatted: '1,000 (100%)',
-        viewableInLens: false,
-      },
-      {
-        fieldName: '@version.keyword',
-        type: ML_JOB_FIELD_TYPES.KEYWORD,
-        existsInDocs: true,
-        aggregatable: true,
-        loading: false,
-        exampleCount: 1,
-        docCountFormatted: '86,274 (100%)',
-        viewableInLens: true,
-      },
-      {
         fieldName: 'airline',
         type: ML_JOB_FIELD_TYPES.KEYWORD,
         existsInDocs: true,
@@ -83,16 +63,6 @@ export const farequoteDataViewTestData: TestData = {
         exampleCount: 1,
         docCountFormatted: '1,000 (100%)',
         viewableInLens: false,
-      },
-      {
-        fieldName: 'type.keyword',
-        type: ML_JOB_FIELD_TYPES.KEYWORD,
-        existsInDocs: true,
-        aggregatable: true,
-        loading: false,
-        exampleCount: 1,
-        docCountFormatted: '86,274 (100%)',
-        viewableInLens: true,
       },
     ],
     emptyFields: ['sourcetype'],
@@ -142,26 +112,6 @@ export const farequoteKQLSearchTestData: TestData = {
         viewableInLens: true,
       },
       {
-        fieldName: '@version',
-        type: ML_JOB_FIELD_TYPES.TEXT,
-        existsInDocs: true,
-        aggregatable: false,
-        loading: false,
-        exampleCount: 1,
-        docCountFormatted: '1,000 (100%)',
-        viewableInLens: false,
-      },
-      {
-        fieldName: '@version.keyword',
-        type: ML_JOB_FIELD_TYPES.KEYWORD,
-        existsInDocs: true,
-        aggregatable: true,
-        loading: false,
-        exampleCount: 1,
-        docCountFormatted: '34,415 (100%)',
-        viewableInLens: true,
-      },
-      {
         fieldName: 'airline',
         type: ML_JOB_FIELD_TYPES.KEYWORD,
         existsInDocs: true,
@@ -180,16 +130,6 @@ export const farequoteKQLSearchTestData: TestData = {
         exampleCount: 1,
         docCountFormatted: '1,000 (100%)',
         viewableInLens: false,
-      },
-      {
-        fieldName: 'type.keyword',
-        type: ML_JOB_FIELD_TYPES.KEYWORD,
-        existsInDocs: true,
-        aggregatable: true,
-        loading: false,
-        exampleCount: 1,
-        docCountFormatted: '34,415 (100%)',
-        viewableInLens: true,
       },
     ],
     emptyFields: ['sourcetype'],
@@ -250,16 +190,6 @@ export const farequoteKQLFiltersSearchTestData: TestData = {
         viewableInLens: false,
       },
       {
-        fieldName: '@version.keyword',
-        type: ML_JOB_FIELD_TYPES.KEYWORD,
-        existsInDocs: true,
-        aggregatable: true,
-        loading: false,
-        exampleCount: 1,
-        docCountFormatted: '5,674 (100%)',
-        viewableInLens: true,
-      },
-      {
         fieldName: 'airline',
         type: ML_JOB_FIELD_TYPES.KEYWORD,
         existsInDocs: true,
@@ -267,26 +197,6 @@ export const farequoteKQLFiltersSearchTestData: TestData = {
         loading: false,
         exampleCount: 1,
         exampleContent: ['ASA'],
-        docCountFormatted: '5,674 (100%)',
-        viewableInLens: true,
-      },
-      {
-        fieldName: 'type',
-        type: ML_JOB_FIELD_TYPES.TEXT,
-        existsInDocs: true,
-        aggregatable: false,
-        loading: false,
-        exampleCount: 1,
-        docCountFormatted: '1,000 (100%)',
-        viewableInLens: false,
-      },
-      {
-        fieldName: 'type.keyword',
-        type: ML_JOB_FIELD_TYPES.KEYWORD,
-        existsInDocs: true,
-        aggregatable: true,
-        loading: false,
-        exampleCount: 1,
         docCountFormatted: '5,674 (100%)',
         viewableInLens: true,
       },
@@ -338,26 +248,6 @@ export const farequoteLuceneSearchTestData: TestData = {
         viewableInLens: true,
       },
       {
-        fieldName: '@version',
-        type: ML_JOB_FIELD_TYPES.TEXT,
-        existsInDocs: true,
-        aggregatable: false,
-        loading: false,
-        exampleCount: 1,
-        docCountFormatted: '1,000 (100%)',
-        viewableInLens: false,
-      },
-      {
-        fieldName: '@version.keyword',
-        type: ML_JOB_FIELD_TYPES.KEYWORD,
-        existsInDocs: true,
-        aggregatable: true,
-        loading: false,
-        exampleCount: 1,
-        docCountFormatted: '34,416 (100%)',
-        viewableInLens: true,
-      },
-      {
         fieldName: 'airline',
         type: ML_JOB_FIELD_TYPES.KEYWORD,
         existsInDocs: true,
@@ -376,16 +266,6 @@ export const farequoteLuceneSearchTestData: TestData = {
         exampleCount: 1,
         docCountFormatted: '1,000 (100%)',
         viewableInLens: false,
-      },
-      {
-        fieldName: 'type.keyword',
-        type: ML_JOB_FIELD_TYPES.KEYWORD,
-        existsInDocs: true,
-        aggregatable: true,
-        loading: false,
-        exampleCount: 1,
-        docCountFormatted: '34,416 (100%)',
-        viewableInLens: true,
       },
     ],
     emptyFields: ['sourcetype'],
@@ -436,26 +316,6 @@ export const farequoteLuceneFiltersSearchTestData: TestData = {
         viewableInLens: true,
       },
       {
-        fieldName: '@version',
-        type: ML_JOB_FIELD_TYPES.TEXT,
-        existsInDocs: true,
-        aggregatable: false,
-        loading: false,
-        exampleCount: 1,
-        docCountFormatted: '1,000 (100%)',
-        viewableInLens: false,
-      },
-      {
-        fieldName: '@version.keyword',
-        type: ML_JOB_FIELD_TYPES.KEYWORD,
-        existsInDocs: true,
-        aggregatable: true,
-        loading: false,
-        exampleCount: 1,
-        docCountFormatted: '5,673 (100%)',
-        viewableInLens: true,
-      },
-      {
         fieldName: 'airline',
         type: ML_JOB_FIELD_TYPES.KEYWORD,
         existsInDocs: true,
@@ -475,16 +335,6 @@ export const farequoteLuceneFiltersSearchTestData: TestData = {
         exampleCount: 1,
         docCountFormatted: '1,000 (100%)',
         viewableInLens: false,
-      },
-      {
-        fieldName: 'type.keyword',
-        type: ML_JOB_FIELD_TYPES.KEYWORD,
-        existsInDocs: true,
-        aggregatable: true,
-        loading: false,
-        exampleCount: 1,
-        docCountFormatted: '5,673 (100%)',
-        viewableInLens: true,
       },
     ],
     emptyFields: ['sourcetype'],

x-pack/platform/test/functional/services/ml/data_visualizer_table.ts
@@ -480,10 +480,11 @@ export function MachineLearningDataVisualizerTableProvider(
       await this.assertRowExists(fieldName);
       await this.assertFieldDocCount(fieldName, docCountFormatted);
 
-      await this.ensureDetailsOpen(fieldName);
-
-      await this.assertExamplesList(fieldName, expectedExamplesCount);
-      await this.ensureDetailsClosed(fieldName);
+      if (expectedExamplesCount > 0) {
+        await this.ensureDetailsOpen(fieldName);
+        await this.assertExamplesList(fieldName, expectedExamplesCount);
+        await this.ensureDetailsClosed(fieldName);
+      }
     }
 
     public async assertGeoPointFieldContents(

x-pack/platform/test/functional_basic/apps/ml/data_visualizer/group3/index.ts
@@ -36,13 +36,6 @@ export default function ({ getService, loadTestFile }: FtrProviderContext) {
       await ml.testResources.resetKibanaTimeZone();
     });
 
-    // The data visualizer should work the same as with a trial license, except the missing create actions
-    // That's why the 'basic' version of 'index_data_visualizer_actions_panel' is loaded here
-    loadTestFile(
-      require.resolve(
-        '../../../../../functional/apps/ml/data_visualizer/group1/index_data_visualizer_grid_in_discover'
-      )
-    );
     loadTestFile(
       require.resolve(
         '../../../../../functional/apps/ml/data_visualizer/group3/index_data_visualizer_grid_in_discover_basic'
patrykkopycinski/kibana

commit 1e92576375ab784fd704ca46087e19702f2a7a7c

[Search Connectors][Serverless] Add Search Connectors UI to the Stack Management data Section (#213509)
**!!MAJORITY OF THE CHANGED FILES ARE MOVED OR COPIED!!**

### Vision

According to the product vision we will build a new simple UI/UX in the
future https://github.com/elastic/security-team/issues/11790

This PR is a first iteration on enabling Content Connectors Management
UI in Serverless Kibana Stack Management.
Elastic Managed content connectors will be available only for Security
and Observability projects.
### Current PR scope

1. Used initial search_connectors plugin and renamed it to
content_connectors + moved from `x-pack/solutions/search` to
`x-pack/platform/plugins/shared`
2. Copy relevant connectors UI and routes from enterprise_search plugin.
3. Introduce the new Stack Management card/navigation option under the
Data section.
4. Enabled this plugin only in Serverless for Security and Observability
projects.
5. For making PR smaller Pipelines tab was not moved. And according to
Search team vision this functionality should be dropped anyway soon.
6. Extended fleet package logic to include elastic_connectors for
security and o11y serverless projects
7. Added back `search:agentless-connectors-manager` task

In Stack Management navigation:
<img width="2062" alt="Screenshot 2025-04-15 at 3 51 43 PM"
src="https://github.com/user-attachments/assets/5c93ba01-9a6a-4eac-a21d-1370f03b8f35"
/>

Stack Management cards:
<img width="2081" alt="Screenshot 2025-04-10 at 8 41 43 PM"
src="https://github.com/user-attachments/assets/3def1c12-561b-4a84-8241-4dd61cd9313d"
/>


Create Elastic Managed Connector UI (on Agentless):

<img width="1822" alt="Screenshot 2025-04-15 at 3 55 29 PM"
src="https://github.com/user-attachments/assets/6e9fea48-85e7-43df-919d-0e5492d0e704"
/>

Create Self Managed Connector UI:

<img width="2064" alt="Screenshot 2025-04-15 at 3 55 49 PM"
src="https://github.com/user-attachments/assets/d5051898-c8fa-4e41-b9ea-b41d4ed4a0d5"
/>

### Next steps

x-pack/platform/plugins/shared/content_connectors/public/plugin.ts
@@ -1,0 +1,89 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { CoreSetup, CoreStart, Plugin, PluginInitializerContext } from '@kbn/core/public';
+import { docLinks } from '@kbn/search-connectors/constants/doc_links';
+import { ManagementAppMountParams } from '@kbn/management-plugin/public';
+import { getConnectorFullTypes, getConnectorTypes } from '../common/lib/connector_types';
+import {
+  SearchConnectorsPluginSetup,
+  SearchConnectorsPluginSetupDependencies,
+  SearchConnectorsPluginStart,
+  SearchConnectorsPluginStartDependencies,
+} from './types';
+import { PLUGIN_ID } from '../common/constants';
+import { PLUGIN_NAME } from './translations';
+
+export class SearchConnectorsPlugin
+  implements
+    Plugin<
+      SearchConnectorsPluginSetup,
+      SearchConnectorsPluginStart,
+      SearchConnectorsPluginSetupDependencies,
+      SearchConnectorsPluginStartDependencies
+    >
+{
+  private readonly isServerless: boolean;
+  private readonly kibanaVersion: string;
+
+  constructor(initializerContext: PluginInitializerContext) {
+    this.isServerless = initializerContext.env.packageInfo.buildFlavor === 'serverless';
+    this.kibanaVersion = initializerContext.env.packageInfo.version;
+  }
+
+  public setup(
+    core: CoreSetup<SearchConnectorsPluginStartDependencies, SearchConnectorsPluginStart>,
+    dependencies: SearchConnectorsPluginSetupDependencies
+  ): SearchConnectorsPluginSetup {
+    const { management } = dependencies;
+    const connectorTypes = getConnectorTypes(core.http.staticAssets);
+    const kibanaVersion = this.kibanaVersion;
+
+    if (this.isServerless === true) {
+      management.sections.section.data.registerApp({
+        id: PLUGIN_ID,
+        title: PLUGIN_NAME,
+        order: 8,
+        keywords: ['content connectors', 'search'],
+        async mount(params: ManagementAppMountParams) {
+          const [{ renderApp }, [coreStart, pluginsStartDeps, pluginStart]] = await Promise.all([
+            import('./app'),
+            core.getStartServices(),
+          ]);
+
+          const connectorsDefinitions = getConnectorFullTypes(core.http.staticAssets);
+          return renderApp(
+            coreStart,
+            pluginsStartDeps,
+            pluginStart,
+            params,
+            connectorsDefinitions,
+            kibanaVersion
+          );
+        },
+      });
+    }
+    return {
+      getConnectorTypes: () => connectorTypes,
+    };
+  }
+
+  public start(
+    core: CoreStart,
+    services: SearchConnectorsPluginStartDependencies
+  ): SearchConnectorsPluginStart {
+    const { http } = core;
+    docLinks.setDocLinks(core.docLinks.links);
+    const connectorTypes = getConnectorFullTypes(http.staticAssets);
+
+    return {
+      getConnectorTypes: () => connectorTypes,
+    };
+  }
+
+  public stop() {}
+}

x-pack/platform/plugins/shared/content_connectors/server/plugin.ts
@@ -1,0 +1,144 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import type {
+  PluginInitializerContext,
+  Plugin,
+  CoreSetup,
+  CoreStart,
+  LoggerFactory,
+} from '@kbn/core/server';
+import type { ConnectorServerSideDefinition } from '@kbn/search-connectors';
+import { isAgentlessEnabled } from '@kbn/fleet-plugin/server/services/utils/agentless';
+import { getConnectorTypes } from '../common/lib/connector_types';
+import type {
+  SearchConnectorsPluginSetup,
+  SearchConnectorsPluginStart,
+  SearchConnectorsPluginSetupDependencies,
+  SearchConnectorsPluginStartDependencies,
+} from './types';
+import { registerConnectorRoutes } from './routes/connectors';
+import { registerStatsRoutes } from './routes/stats';
+import { registerMappingRoute } from './routes/mapping';
+import { registerSearchRoute } from './routes/search';
+import { PLUGIN_ID } from '../common/constants';
+import { registerApiKeysRoutes } from './routes/api_keys';
+import { SearchConnectorsConfig } from './config';
+import { AgentlessConnectorDeploymentsSyncService } from './task';
+
+export class SearchConnectorsPlugin
+  implements
+    Plugin<
+      SearchConnectorsPluginSetup,
+      SearchConnectorsPluginStart,
+      SearchConnectorsPluginSetupDependencies,
+      SearchConnectorsPluginStartDependencies
+    >
+{
+  private connectors: ConnectorServerSideDefinition[];
+  private readonly logger: LoggerFactory;
+  private readonly config: SearchConnectorsConfig;
+  private agentlessConnectorDeploymentsSyncService: AgentlessConnectorDeploymentsSyncService;
+  private isServerless: boolean;
+
+  constructor(initializerContext: PluginInitializerContext) {
+    this.connectors = [];
+    this.logger = initializerContext.logger;
+    this.config = initializerContext.config.get();
+    this.agentlessConnectorDeploymentsSyncService = new AgentlessConnectorDeploymentsSyncService(
+      this.logger.get()
+    );
+    this.isServerless = false;
+  }
+
+  public setup(
+    coreSetup: CoreSetup<SearchConnectorsPluginStartDependencies, SearchConnectorsPluginStart>,
+    plugins: SearchConnectorsPluginSetupDependencies
+  ) {
+    const http = coreSetup.http;
+
+    plugins.features.registerElasticsearchFeature({
+      id: PLUGIN_ID,
+      management: {
+        data: [PLUGIN_ID],
+      },
+      privileges: [
+        {
+          requiredClusterPrivileges: ['monitor'],
+          ui: [],
+        },
+      ],
+    });
+
+    this.connectors = getConnectorTypes(http.staticAssets);
+    this.isServerless = plugins.cloud && plugins.cloud.isServerlessEnabled;
+    const coreStartServices = coreSetup.getStartServices();
+
+    // There seems to be no way to check for agentless here
+    // So we register a task, but do not execute it in `start` method
+    this.logger.get().debug('Registering agentless connectors infra sync task');
+
+    coreStartServices
+      .then(([coreStart, searchConnectorsPluginStartDependencies]) => {
+        this.agentlessConnectorDeploymentsSyncService.registerInfraSyncTask(
+          plugins,
+          coreStart,
+          searchConnectorsPluginStartDependencies
+        );
+      })
+      .catch((err) => {
+        this.logger.get().error(`Error registering agentless connectors infra sync task`, err);
+      });
+    const router = http.createRouter();
+
+    // Enterprise Search Routes
+    if (this.connectors.length > 0) {
+      /**
+       * Register routes
+       */
+      registerConnectorRoutes({
+        ...plugins,
+        router,
+        getStartServices: coreSetup.getStartServices,
+        log: this.logger.get(),
+      });
+      registerStatsRoutes({ ...plugins, router, log: this.logger.get() });
+      registerMappingRoute({ ...plugins, router, log: this.logger.get() });
+      registerSearchRoute({ ...plugins, router, log: this.logger.get() });
+      registerApiKeysRoutes({ ...plugins, router });
+    }
+    return {
+      getConnectorTypes: () => this.connectors,
+    };
+  }
+
+  public start(core: CoreStart, plugins: SearchConnectorsPluginStartDependencies) {
+    if (this.isServerless && isAgentlessEnabled()) {
+      this.logger
+        .get()
+        .info(
+          'Agentless is supported, scheduling initial agentless connectors infrastructure watcher task'
+        );
+      this.agentlessConnectorDeploymentsSyncService
+        .scheduleInfraSyncTask(this.config, plugins.taskManager)
+        .catch((err) => {
+          this.logger.get().error(`Error scheduling agentless connectors infra sync task`, err);
+        });
+    } else {
+      this.logger
+        .get()
+        .info(
+          'Agentless is not supported, skipping scheduling initial agentless connectors infrastructure watcher task'
+        );
+    }
+    return {
+      getConnectors: () => this.connectors,
+    };
+  }
+
+  public stop() {}
+}

x-pack/solutions/search/plugins/enterprise_search/public/plugin.ts
@@ -10,6 +10,7 @@ import { BehaviorSubject, firstValueFrom, type Subscription } from 'rxjs';
 import { ChartsPluginStart } from '@kbn/charts-plugin/public';
 import { CloudSetup, CloudStart } from '@kbn/cloud-plugin/public';
 import { ConsolePluginStart } from '@kbn/console-plugin/public';
+import { SearchConnectorsPluginStart } from '@kbn/content-connectors-plugin/public';
 import {
   AppMountParameters,
   CoreStart,
@@ -33,7 +34,6 @@ import { LicensingPluginStart } from '@kbn/licensing-plugin/public';
 import { MlPluginStart } from '@kbn/ml-plugin/public';
 import type { NavigationPublicPluginStart } from '@kbn/navigation-plugin/public';
 import { ELASTICSEARCH_URL_PLACEHOLDER } from '@kbn/search-api-panels/constants';
-import { SearchConnectorsPluginStart } from '@kbn/search-connectors-plugin/public';
 import type { SearchNavigationPluginStart } from '@kbn/search-navigation/public';
 import { SearchPlaygroundPluginStart } from '@kbn/search-playground/public';
 import { SecurityPluginSetup, SecurityPluginStart } from '@kbn/security-plugin/public';

x-pack/solutions/search/plugins/search_connectors/public/plugin.ts
@@ -1,50 +1,0 @@
-/*
- * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
- * or more contributor license agreements. Licensed under the Elastic License
- * 2.0; you may not use this file except in compliance with the Elastic License
- * 2.0.
- */
-
-import { CoreSetup, CoreStart, Plugin } from '@kbn/core/public';
-import { docLinks } from '@kbn/search-connectors';
-import { getConnectorFullTypes, getConnectorTypes } from '../common/lib/connector_types';
-import {
-  SearchConnectorsPluginSetup,
-  SearchConnectorsPluginSetupDependencies,
-  SearchConnectorsPluginStart,
-  SearchConnectorsPluginStartDependencies,
-} from './types';
-
-export class SearchConnectorsPlugin
-  implements
-    Plugin<
-      SearchConnectorsPluginSetup,
-      SearchConnectorsPluginStart,
-      SearchConnectorsPluginSetupDependencies,
-      SearchConnectorsPluginStartDependencies
-    >
-{
-  public setup(
-    core: CoreSetup<SearchConnectorsPluginStartDependencies, SearchConnectorsPluginStart>,
-    setupDeps: SearchConnectorsPluginSetupDependencies
-  ): SearchConnectorsPluginSetup {
-    const connectorTypes = getConnectorTypes(core.http.staticAssets);
-    return {
-      getConnectorTypes: () => connectorTypes,
-    };
-  }
-
-  public start(
-    core: CoreStart,
-    services: SearchConnectorsPluginStartDependencies
-  ): SearchConnectorsPluginStart {
-    const { http } = core;
-    docLinks.setDocLinks(core.docLinks.links);
-    const connectorTypes = getConnectorFullTypes(http.staticAssets);
-    return {
-      getConnectorTypes: () => connectorTypes,
-    };
-  }
-
-  public stop() {}
-}

commit 69db96adfff1b42182ad89c5da80e721574a7163

[Search][Content Connectors] Fix management app shown to users without capability (#271709)
## Closes https://github.com/elastic/search-team/issues/11985

## Summary

The Content Connectors entry under Stack Management was registered
inside a
`core.getStartServices().then(...)` callback in `setup()`, so
registration
happened after `management.start()` had already run its per-app
capability
sweep (`management_sections_service.ts` start()). The app stayed
`enabled: true` regardless of the user's
`capabilities.management.data.content_connectors` value — e.g. a role
with
only "Observability > APM and User Experience: Read" still saw the
"Content Connectors" entry in the Stack Management sidebar (it then
403'd
on navigation, which was the original support ticket).

Fix mirrors the pattern in

`x-pack/platform/plugins/private/cross_cluster_replication/public/plugin.ts`:

- Register the management app synchronously in `setup()` and keep the
  returned `ManagementApp`, so the management plugin's start-phase
capability sweep correctly disables it for users without the capability.
- In `start()`, additionally disable the app when
`xpack.contentConnectors.ui.enabled` is `false` or no `securitySolution`
  / `observability` solution is registered (existing gating, preserved).
- The mount callback now resolves `getStartServices()` / `renderApp`
  lazily, only when the user navigates.

Adds `public/plugin.test.ts` covering synchronous setup-time
registration
and the start-time disable matrix.

### How to test

1. Start ES + Kibana from this branch.
2. Create role `apm_reader` with only Observability → APM and User
   Experience: Read. Assign to a new user.
3. Log in as that user → Stack Management. **"Content Connectors" should
   no longer appear in the sidebar.**
4. Log in as `elastic` (or any role granting the `content_connectors`
Elasticsearch feature capability) → "Content Connectors" still appears.

Before:
<img width="1893" height="857" alt="image"
src="https://github.com/user-attachments/assets/7a937839-6178-4993-86aa-cc6ec08226dc"
/>
After:
<img width="1891" height="784" alt="image"
src="https://github.com/user-attachments/assets/bd0bd7eb-6cdd-4a1c-86cc-c24a2ed8eb3b"
/>


### Checklist

Check the PR satisfies following conditions. 

Reviewers should verify this PR satisfies this list as well.

- [x] Any text added follows [EUI's writing
guidelines](https://elastic.github.io/eui/#/guidelines/writing), uses
sentence case text and includes [i18n
support](https://github.com/elastic/kibana/blob/main/src/platform/packages/shared/kbn-i18n/README.md)
— no user-facing text added; existing strings unchanged.
- [x]
[Documentation](https://www.elastic.co/guide/en/kibana/master/development-documentation.html)
was added for features that require explanation or tutorials — bug fix
to existing behavior, no new feature to document.
- [x] [Unit or functional
tests](https://www.elastic.co/guide/en/kibana/master/development-tests.html)
were updated or added to match the most common scenarios — adds
`x-pack/platform/plugins/shared/content_connectors/public/plugin.test.ts`
covering (a) synchronous registration in `setup()` without touching
`getStartServices`, and (b) the start-time disable matrix across
`config.ui.enabled`, no solutions, security solution, observability
solution, and unrelated solution.
- [x] If a plugin configuration key changed, check if it needs to be
allowlisted in the cloud and added to the [docker
list](https://github.com/elastic/kibana/blob/main/src/dev/build/tasks/os_packages/docker_generator/resources/base/bin/kibana-docker)
— no configuration key changed.
- [x] This was checked for breaking HTTP API changes, and any breaking
changes have been approved by the breaking-change committee. The
`release_note:breaking` label should be applied in these situations. —
public-only plugin change, no HTTP API surface affected.
- [ ] [Flaky Test
Runner](https://ci-stats.kibana.dev/trigger_flaky_test_runner/1) was
used on any tests changed — new test is deterministic and lifecycle-only
(no async timing/UI rendering), so flaky test runner was not triggered.
Happy to run it if a reviewer requests.
- [x] The PR description includes the appropriate Release Notes section,
and the correct `release_note:*` label is applied per the
[guidelines](https://www.elastic.co/guide/en/kibana/master/contributing.html#kibana-release-notes-process)
— `release_note:fix` applied.
- [x] Review the [backport
guidelines](https://docs.google.com/document/d/1VyN5k91e5OVumlc0Gb9RPa3h1ewuPE705nRtioPiTvY/edit?usp=sharing)
and apply applicable `backport:*` labels. — bug present since the plugin
file was introduced in #213509 (April 2025); `backport:prev-minor`

x-pack/platform/plugins/shared/content_connectors/public/plugin.ts
@@ -7,7 +7,7 @@
 
 import type { CoreSetup, CoreStart, Plugin, PluginInitializerContext } from '@kbn/core/public';
 import { docLinks } from '@kbn/search-connectors/constants/doc_links';
-import type { ManagementAppMountParams } from '@kbn/management-plugin/public';
+import type { ManagementApp, ManagementAppMountParams } from '@kbn/management-plugin/public';
 import type { FeatureCatalogueSolution } from '@kbn/home-plugin/public';
 import { type ClientConfigType } from '../common/types/config';
 import { getConnectorFullTypes, getConnectorTypes } from '../common/lib/connector_types';
@@ -31,6 +31,7 @@ export class SearchConnectorsPlugin
 {
   private readonly kibanaVersion: string;
   private readonly config: ClientConfigType;
+  private contentConnectorsApp?: ManagementApp;
 
   constructor(initializerContext: PluginInitializerContext) {
     this.kibanaVersion = initializerContext.env.packageInfo.version;
@@ -45,34 +46,29 @@ export class SearchConnectorsPlugin
     const connectorTypes = getConnectorTypes(core.http.staticAssets);
     const kibanaVersion = this.kibanaVersion;
 
-    core.getStartServices().then(([coreStart, pluginsStartDeps, pluginStart]) => {
-      const hasAnyContentConnectorsSolutions = pluginsStartDeps.home.featureCatalogue
-        .getSolutions()
-        .some(({ id }: FeatureCatalogueSolution) =>
-          ['securitySolution', 'observability'].includes(id)
+    // Register the management app synchronously in setup so the management plugin's
+    // start-phase capability sweep can disable it for users without the
+    // `management.data.content_connectors` capability. Solution/config gating is
+    // applied later in start() via this.contentConnectorsApp.disable().
+    this.contentConnectorsApp = management.sections.section.data.registerApp({
+      id: PLUGIN_ID,
+      title: PLUGIN_NAME,
+      order: 8,
+      keywords: ['content connectors', 'search'],
+      async mount(params: ManagementAppMountParams) {
+        const [coreStart, pluginsStartDeps, pluginStart] = await core.getStartServices();
+        const { renderApp } = await import('./app');
+
+        const connectorsDefinitions = getConnectorFullTypes(core.http.staticAssets);
+        return renderApp(
+          coreStart,
+          pluginsStartDeps,
+          pluginStart,
+          params,
+          connectorsDefinitions,
+          kibanaVersion
         );
-
-      if (this.config.ui.enabled && hasAnyContentConnectorsSolutions) {
-        management.sections.section.data.registerApp({
-          id: PLUGIN_ID,
-          title: PLUGIN_NAME,
-          order: 8,
-          keywords: ['content connectors', 'search'],
-          async mount(params: ManagementAppMountParams) {
-            const { renderApp } = await import('./app');
-
-            const connectorsDefinitions = getConnectorFullTypes(core.http.staticAssets);
-            return renderApp(
-              coreStart,
-              pluginsStartDeps,
-              pluginStart,
-              params,
-              connectorsDefinitions,
-              kibanaVersion
-            );
-          },
-        });
-      }
+      },
     });
 
     return {
@@ -87,6 +83,16 @@ export class SearchConnectorsPlugin
     const { http } = core;
     docLinks.setDocLinks(core.docLinks.links);
     const connectorTypes = getConnectorFullTypes(http.staticAssets);
+
+    const hasAnyContentConnectorsSolutions = services.home.featureCatalogue
+      .getSolutions()
+      .some(({ id }: FeatureCatalogueSolution) =>
+        ['securitySolution', 'observability'].includes(id)
+      );
+
+    if (!this.config.ui.enabled || !hasAnyContentConnectorsSolutions) {
+      this.contentConnectorsApp?.disable();
+    }
 
     return {
       getConnectorTypes: () => connectorTypes,

src/platform/plugins/shared/management/test/scout/ui/parallel_tests/nav_access.spec.ts
@@ -29,7 +29,7 @@ test.describe('Stack Management — nav link access', { tag: tags.stateful.class
       const sections = await pageObjects.management.readSidebarSections();
       expect(sections).toHaveLength(7);
       const dataSection = sections.find((s) => s.sectionId === 'data');
-      expect(dataSection?.sectionLinks).toStrictEqual(['data_quality', 'content_connectors']);
+      expect(dataSection?.sectionLinks).toStrictEqual(['data_quality']);
       const insightsSection = sections.find((s) => s.sectionId === 'insightsAndAlerting');
       expect(insightsSection?.sectionLinks).toStrictEqual([
         'triggersActionsAlerts',

x-pack/platform/plugins/shared/content_connectors/public/plugin.test.ts
@@ -1,0 +1,158 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import type { CoreSetup } from '@kbn/core/public';
+import { coreMock } from '@kbn/core/public/mocks';
+import { managementPluginMock } from '@kbn/management-plugin/public/mocks';
+import type { FeatureCatalogueSolution } from '@kbn/home-plugin/public';
+import type { ManagementApp } from '@kbn/management-plugin/public';
+
+import { SearchConnectorsPlugin } from './plugin';
+import type { ClientConfigType } from '../common/types/config';
+import { PLUGIN_ID } from '../common/constants';
+import type {
+  SearchConnectorsPluginStart,
+  SearchConnectorsPluginSetupDependencies,
+  SearchConnectorsPluginStartDependencies,
+} from './types';
+
+const securitySolution: FeatureCatalogueSolution = {
+  id: 'securitySolution',
+  title: 'Security',
+  description: '',
+  icon: 'logoSecurity',
+  path: '/app/security',
+  order: 300,
+};
+
+const observabilitySolution: FeatureCatalogueSolution = {
+  id: 'observability',
+  title: 'Observability',
+  description: '',
+  icon: 'logoObservability',
+  path: '/app/observability',
+  order: 200,
+};
+
+const otherSolution: FeatureCatalogueSolution = {
+  id: 'kibana',
+  title: 'Analytics',
+  description: '',
+  icon: 'logoKibana',
+  path: '/app/home',
+  order: 400,
+};
+
+const createPlugin = (config: ClientConfigType) => {
+  const initializerContext = coreMock.createPluginInitializerContext(config);
+  return new SearchConnectorsPlugin(initializerContext);
+};
+
+const createSetupDependencies = () => {
+  const managementSetup = managementPluginMock.createSetupContract();
+  const registeredApp = {
+    enabled: true,
+    disable: jest.fn(),
+    enable: jest.fn(),
+  } as unknown as ManagementApp;
+  (managementSetup.sections.section.data.registerApp as jest.Mock).mockReturnValue(registeredApp);
+  return { managementSetup, registeredApp };
+};
+
+const createStartDependencies = (
+  solutions: FeatureCatalogueSolution[]
+): SearchConnectorsPluginStartDependencies => {
+  const home = {
+    featureCatalogue: {
+      getSolutions: jest.fn(() => solutions),
+    },
+  };
+  return { home } as unknown as SearchConnectorsPluginStartDependencies;
+};
+
+type PluginCoreSetup = CoreSetup<
+  SearchConnectorsPluginStartDependencies,
+  SearchConnectorsPluginStart
+>;
+
+const setupPlugin = ({ uiEnabled }: { uiEnabled: boolean }) => {
+  const plugin = createPlugin({ ui: { enabled: uiEnabled } });
+  const coreSetup = coreMock.createSetup() as unknown as PluginCoreSetup;
+  const { managementSetup, registeredApp } = createSetupDependencies();
+  plugin.setup(coreSetup, {
+    management: managementSetup,
+  } as unknown as SearchConnectorsPluginSetupDependencies);
+  return { plugin, coreSetup, managementSetup, registeredApp };
+};
+
+describe('SearchConnectorsPlugin (public)', () => {
+  describe('setup()', () => {
+    it('registers the Content Connectors management app synchronously, before getStartServices resolves', () => {
+      const plugin = createPlugin({ ui: { enabled: true } });
+      const coreSetup = coreMock.createSetup() as unknown as PluginCoreSetup;
+      const { managementSetup } = createSetupDependencies();
+      const getStartServices = jest.spyOn(coreSetup, 'getStartServices');
+
+      plugin.setup(coreSetup, {
+        management: managementSetup,
+      } as unknown as SearchConnectorsPluginSetupDependencies);
+
+      expect(managementSetup.sections.section.data.registerApp).toHaveBeenCalledTimes(1);
+      expect(managementSetup.sections.section.data.registerApp).toHaveBeenCalledWith(
+        expect.objectContaining({
+          id: PLUGIN_ID,
+          order: 8,
+          keywords: ['content connectors', 'search'],
+          mount: expect.any(Function),
+        })
+      );
+      expect(getStartServices).not.toHaveBeenCalled();
+    });
+  });
+
+  describe('start()', () => {
+    it('keeps the management app enabled when config is enabled and a supported solution is registered', () => {
+      const { plugin, registeredApp } = setupPlugin({ uiEnabled: true });
+
+      plugin.start(coreMock.createStart(), createStartDependencies([securitySolution]));
+
+      expect(registeredApp.disable).not.toHaveBeenCalled();
+    });
+
+    it('keeps the management app enabled when observability solution is registered', () => {
+      const { plugin, registeredApp } = setupPlugin({ uiEnabled: true });
+
+      plugin.start(coreMock.createStart(), createStartDependencies([observabilitySolution]));
+
+      expect(registeredApp.disable).not.toHaveBeenCalled();
+    });
+
+    it('disables the management app when no security or observability solution is registered', () => {
+      const { plugin, registeredApp } = setupPlugin({ uiEnabled: true });
+
+      plugin.start(coreMock.createStart(), createStartDependencies([otherSolution]));
+
+      expect(registeredApp.disable).toHaveBeenCalledTimes(1);
+    });
+
+    it('disables the management app when no solutions are registered at all', () => {
+      const { plugin, registeredApp } = setupPlugin({ uiEnabled: true });
+
+      plugin.start(coreMock.createStart(), createStartDependencies([]));
+
+      expect(registeredApp.disable).toHaveBeenCalledTimes(1);
+    });
+
+    it('disables the management app when config.ui.enabled is false even if a supported solution is registered', () => {
+      const { plugin, registeredApp } = setupPlugin({ uiEnabled: false });
+
+      plugin.start(coreMock.createStart(), createStartDependencies([securitySolution]));
+
+      expect(registeredApp.disable).toHaveBeenCalledTimes(1);
+    });
+  });
+});